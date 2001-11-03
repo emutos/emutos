64f9658afe7adda8739e5ec7b97f41e0970b8baa
Laurent Vogel <lvl@users.sourceforge.net>
2001-11-03 17:25:32 +0000

    clock, almost forgotten

---
+++ bios/clock.h
@@ -0,0 +1,36 @@
+/*
+ * clock.h - BIOS time and date routines
+ *
+ * Copyright (c) 2001 by Authors:
+ *
+ *  LVL   Laurent Vogel
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
+ */
+
+#ifndef _CLOCK_H
+#define _CLOCK_H
+
+#include "portab.h"
+
+/* BDOS interface */
+
+#define GET_TIME 0
+#define SET_TIME 1
+#define GET_DATE 2
+#define SET_DATE 3
+
+extern void date_time(WORD flag, WORD *dt);
+
+/* internal init */
+
+extern void clock_init(void);
+
+/* xbios functions */
+
+extern void settime(LONG time);
+extern LONG gettime(void);
+
+#endif /* _CLOCK_H */
+
