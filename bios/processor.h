5b257153dba86ab5687ded5be16217de1bb11775
Martin Döring <mdoering@users.sourceforge.net>
2001-11-05 19:02:26 +0000

    processor.h

---
+++ bios/processor.h
@@ -0,0 +1,21 @@
+/*
+ * processor.h - declarations for processor type check
+ *
+ * Copyright (c) 2001 EmuTOS development team.
+ *
+ * Authors:
+ *  MAD     Martin Doering
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
+ */
+
+#ifndef _PROCESSOR_H
+#define _PROCESSOR_H
+ 
+#include "portab.h"
+
+extern VOID frame_set(VOID);
+
+#endif /* PROCESSOR_H */
+  
