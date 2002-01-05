834bf2a71f2fb89975fa459d35950c0fb290344a
Laurent Vogel <lvl@users.sourceforge.net>
2002-01-05 10:40:48 +0000

    get_sr() macro, fixed stop macro

--- include/asm.h
+++ include/asm.h
@@ -18,7 +18,12 @@
  *
  * WORD set_sr(WORD new); 
  *   sets sr to the new value, and return the old sr value 
- * 
+ * WORD get_sr(void);
+ *   returns the current value of sr. the CCR bits are not meaningful.
+ * void stop2300(void);
+ * void stop2500(void);
+ *   the STOP immediate instruction
+ *
  * For clarity, please add such two lines above when adding 
  * new macros below.
  */
@@ -38,23 +43,45 @@ __extension__                               \
   __asm__ volatile                          \
   ("move.w sr,d0;                           \
     move.w %1,sr "                          \
-  : "=r"(retvalue)   /* outputs */	    \
-  : "d"(_a)          /* inputs  */	    \
+  : "=r"(retvalue)   /* outputs */          \
+  : "d"(_a)          /* inputs  */          \
   : "d0"             /* clobbered regs */   \
   );                                        \
   retvalue;                                 \
 })
 
 /*
- * stop
- *   this is the m68k STOP #<imm> instruction
+ * WORD get_sr(void); 
+ *   returns the current value of sr. 
  */
 
-#define stop								\
-__extension__								\
-({__asm__ volatile							\
-  ("stop #0x2000 ");						\
+#define get_sr()                            \
+__extension__                               \
+({register short retvalue __asm__("d0");    \
+  __asm__ volatile                          \
+  ("move.w sr,d0 "                          \
+  : "=r"(retvalue)   /* outputs */          \
+  :                  /* inputs  */          \
+  : "d0"             /* clobbered regs */   \
+  );                                        \
+  retvalue;                                 \
 })
 
+/*
+ * void stop2x00(void)
+ *   the m68k STOP immediate instruction
+ */
+
+#define stop2300()                              \
+__extension__                                   \
+({__asm__ volatile                              \
+  ("stop #0x2300 ");                            \
+})
+
+#define stop2500()                              \
+__extension__                                   \
+({__asm__ volatile                              \
+  ("stop #0x2500 ");                            \
+})
 
 #endif /* _ASM_H */
