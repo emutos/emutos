4cf8d9bc15ea3223d855a25f9f80940e934b10cf
Laurent Vogel <lvl@users.sourceforge.net>
2002-01-05 10:40:08 +0000

    fixed set_sr(0x0300) to 0x2300

--- bios/screen.c
+++ bios/screen.c
@@ -25,10 +25,10 @@ static void setphys(LONG addr);
 /* Define pallette */
 
 static WORD dflt_palette[] = {
-    RGB_WHITE,   RGB_RED,       RGB_GREEN,   RGB_YELLOW,
-    RGB_BLUE,    RGB_MAGENTA,   RGB_CYAN,    RGB_LTGRAY,
-    RGB_GRAY,    RGB_LTRED,     RGB_LTGREEN, RGB_LTYELLOW,
-    RGB_LTBLUE,  RGB_LTMAGENTA, RGB_LTCYAN,  RGB_BLACK
+    RGB_WHITE, RGB_RED, RGB_GREEN, RGB_YELLOW,
+    RGB_BLUE, RGB_MAGENTA, RGB_CYAN, RGB_LTGRAY,
+    RGB_GRAY, RGB_LTRED, RGB_LTGREEN, RGB_LTYELLOW,
+    RGB_LTBLUE, RGB_LTMAGENTA, RGB_LTCYAN, RGB_BLACK
 };
 
 
@@ -43,48 +43,52 @@ void screen_init(void)
 {
     volatile BYTE *rez_reg = (BYTE *) 0xffff8260;
     volatile WORD *col_regs = (WORD *) 0xffff8240;
-    volatile struct {BYTE gpip;} *mfp = (void *)0xfffffa01;
+    volatile struct {
+        BYTE gpip;
+    } *mfp = (void *) 0xfffffa01;
     WORD rez;
     WORD i;
-    
-    *(BYTE *)0xffff820a = 2;  /* sync-mode to 50 hz pal, internal sync */
-    
-    for(i = 0 ; i < 16 ; i++) {
+
+    *(BYTE *) 0xffff820a = 2;   /* sync-mode to 50 hz pal, internal sync */
+
+    for (i = 0; i < 16; i++) {
         col_regs[i] = dflt_palette[i];
     }
-    
+
     rez = *rez_reg;
 
     rez &= 3;
-    if(rez == 3) {
+    if (rez == 3) {
         rez = 2;
     }
-    if((mfp->gpip & 0x80) != 0) {
+    if ((mfp->gpip & 0x80) != 0) {
         /* color monitor */
-        if(rez == 2) rez = 0;
+        if (rez == 2)
+            rez = 0;
     } else {
-        if(rez < 2) rez = 2;
+        if (rez < 2)
+            rez = 2;
     }
-    
+
     *rez_reg = rez;
     sshiftmod = rez;
-    
-    if(rez == 1) {
+
+    if (rez == 1) {
         col_regs[3] = col_regs[15];
     }
 
-    v_bas_ad = (BYTE *)(phystop - 0x8000L);
-    setphys((LONG)v_bas_ad);
+    v_bas_ad = (BYTE *) (phystop - 0x8000L);
+    setphys((LONG) v_bas_ad);
 }
 
 /* misc routines */
 
 static void setphys(LONG addr)
 {
-    *(UBYTE *)0xffff8201 = ((ULONG)addr) >> 16;
-    *(UBYTE *)0xffff8203 = ((ULONG)addr) >> 8;
-    if(has_ste_shifter) {
-        *(UBYTE *)0xffff820d = ((ULONG)addr);
+    *(UBYTE *) 0xffff8201 = ((ULONG) addr) >> 16;
+    *(UBYTE *) 0xffff8203 = ((ULONG) addr) >> 8;
+    if (has_ste_shifter) {
+        *(UBYTE *) 0xffff820d = ((ULONG) addr);
     }
 }
 
@@ -94,82 +98,82 @@ LONG physbase(void)
 {
     LONG addr;
 
-    addr = *(UBYTE *)0xffff8201;
+    addr = *(UBYTE *) 0xffff8201;
     addr <<= 8;
-    addr += *(UBYTE *)0xffff8203;
+    addr += *(UBYTE *) 0xffff8203;
     addr <<= 8;
-    if(has_ste_shifter) {
-        addr += *(UBYTE *)0xffff820D;  
+    if (has_ste_shifter) {
+        addr += *(UBYTE *) 0xffff820D;
     }
 
-    return(addr);
+    return (addr);
 }
 
 LONG logbase(void)
 {
-    return((ULONG)v_bas_ad);
+    return ((ULONG) v_bas_ad);
 }
 
 WORD getrez(void)
 {
-    return( *(UBYTE *)0xffff8260 );
+    return (*(UBYTE *) 0xffff8260);
 }
 
 
 void setscreen(LONG logLoc, LONG physLoc, WORD rez)
 {
-    if(logLoc >= 0) {
-      v_bas_ad = (char *)logLoc;
+    if (logLoc >= 0) {
+        v_bas_ad = (char *) logLoc;
     }
-    if(physLoc >= 0) {
-      screenpt = (char *)physLoc;
-      /* will be set up at next VBL */
+    if (physLoc >= 0) {
+        screenpt = (char *) physLoc;
+        /* will be set up at next VBL */
     }
-    if(rez >= 0) {
-      /* rez ignored for now */
-    }    
+    if (rez >= 0) {
+        /* rez ignored for now */
+    }
 }
 
 void setpalette(LONG palettePtr)
 {
     /* next VBL will do this */
-    colorptr = (WORD *)palettePtr;
+    colorptr = (WORD *) palettePtr;
 }
 
 WORD setcolor(WORD colorNum, WORD color)
 {
-  WORD rez = getrez();
-  WORD max;
-  WORD mask;
-  WORD *palette = (WORD *)0xffff8240;
-  switch(rez) {
-  case 0:
-    max = 15;
-    break;
-  case 1:
-    max = 3;
-    break;
-  case 2:
-    max = 1;
-    break;
-  default:
-    max = 0;
-  }
-  if(has_ste_shifter) {
-    mask = 0xfff;
-  } else {
-    mask = 0x777;
-  }
-  if(colorNum >= 0 && colorNum <= max) {
-    if(color == -1) {     
-      return palette[colorNum] & mask;
-    } else {
-      palette[colorNum] = color;
-      return color & mask;
+    WORD rez = getrez();
+    WORD max;
+    WORD mask;
+    WORD *palette = (WORD *) 0xffff8240;
+    switch (rez) {
+    case 0:
+        max = 15;
+        break;
+    case 1:
+        max = 3;
+        break;
+    case 2:
+        max = 1;
+        break;
+    default:
+        max = 0;
     }
-  } else {
-    return 0;
-  }
+    if (has_ste_shifter) {
+        mask = 0xfff;
+    } else {
+        mask = 0x777;
+    }
+    if (colorNum >= 0 && colorNum <= max) {
+        if (color == -1) {
+            return palette[colorNum] & mask;
+        } else {
+            palette[colorNum] = color;
+            return color & mask;
+        }
+    } else {
+        return 0;
+    }
 }
 
 
@@ -177,12 +181,11 @@ void vsync(void)
 {
     WORD old_sr;
     LONG a;
-    volatile LONG frclock; 
+    volatile LONG frclock;
 
-    old_sr = set_sr(0x0300);   /* allow VBL interrupt */
+    old_sr = set_sr(0x2300);    /* allow VBL interrupt */
     a = frclock;
-    while(frclock == a) 
-      ;
+    while (frclock == a)        /* TODO, we could use stop2300() later */
+        ;
     set_sr(old_sr);
 }
-
