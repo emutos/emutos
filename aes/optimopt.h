0ba3e27013811c22685864a49259bd21efed5743
Laurent Vogel <lvl@users.sourceforge.net>
2002-04-27 08:42:54 +0000

    removed redundant functions and rewrote other

--- aes/optimopt.h
+++ aes/optimopt.h
@@ -1,3 +1,12 @@
+/*
+ * optimopt.h - misc functions
+ *
+ * Copyright (c) 2002 EmuTOS DEvelopment Team
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
+ */
+
 
 #ifndef OPTIMOPT_H
 #define OPTIMOPT_H
@@ -5,7 +14,5 @@
 void r_get(GRECT *pxywh, WORD *px, WORD *py, WORD *pw, WORD *ph);
 void r_set(GRECT *pxywh, WORD x, WORD y, WORD w, WORD h);
 BYTE *scasb(BYTE *p, BYTE b);
-void bfill(WORD num, BYTE bval, void *addr);
-WORD strchk(BYTE *s,  BYTE *t);
 
 #endif
