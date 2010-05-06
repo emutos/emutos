79b35f717c60931e574c3697b393ddd51c1ea829
Thomas Huth <thothy@users.sourceforge.net>
2010-05-06 23:47:57 +0000

    Basic support for 32 drives instead of only 16.

--- bdos/fsglob.c
+++ bdos/fsglob.c
@@ -2,9 +2,7 @@
  * fsglob.c - global variables for the file system                
  *
  * Copyright (c) 2001 Lineo, Inc.
- *
- * Authors:
- *  xxx <xxx@xxx>
+ *               2002 - 2010 The EmuTOS development team
  *
  * This file is distributed under the GPL, version 2 or at your
  * option any later version.  See doc/license.txt for details.
@@ -37,7 +35,7 @@
  *  drvtbl -
  */
 
-DMD *drvtbl[16];
+DMD *drvtbl[32];
 
 
 
--- bdos/fsmain.c
+++ bdos/fsmain.c
@@ -2,13 +2,11 @@
  * fsmain.c - GEMDOS file system notes and misc routines          
  *
  * Copyright (c) 2001 Lineo, Inc.
- *               2002-2008 The EmuTOS development team
+ *               2002 - 2010 The EmuTOS development team
  *
  * This file is distributed under the GPL, version 2 or at your
  * option any later version.  See doc/license.txt for details.
  */
-
-
 
 #include "portab.h"
 #include "asm.h"
@@ -430,7 +428,7 @@ void    xsetdta(char *addr)     /* set transfer address to addr */
 
 long    xsetdrv(int drv)
 {
-    int drvmap ;
+    long drvmap ;
 
     drvmap = Drvmap();
 
