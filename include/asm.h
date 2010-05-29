83d3bff3988a4716bc5d2fb6940cf7ea66a12318
Vincent Rivière <vriviere@users.sourceforge.net>
2010-05-29 06:08:52 +0000

    Added ColdFire CPU support.

--- include/asm.h
+++ include/asm.h
@@ -45,29 +45,71 @@ extern long trap1_pexec(short mode, const char * path,
  *   swap endianess of val, 16 bits only.
  */
 
+#ifdef __mcoldfire__
+#define swpw(a)                           \
+  __extension__                           \
+  ({long _tmp;                            \
+    __asm__ __volatile__                  \
+    ("move.w  %0,%1\n\t"                  \
+     "lsl.l   #8,%0\n\t"                  \
+     "lsr.l   #8,%1\n\t"                  \
+     "move.b  %1,%0"                      \
+    : "=d"(a), "=d"(_tmp) /* outputs */   \
+    : "0"(a)     /* inputs  */            \
+    : "cc"       /* clobbered */          \
+    );                                    \
+  })
+#else
 #define swpw(a)                           \
   __asm__ __volatile__                    \
-  ("ror   #8,%0\n\t"                      \
+  ("ror   #8,%0"                          \
   : "=d"(a)          /* outputs */        \
   : "0"(a)           /* inputs  */        \
   : "cc"             /* clobbered */      \
-  )                                       \
+  )
+#endif
 
 
 /*
- * WORD swpw(WORD val); 
- *   swap endianess of val, 16 bits only.
+ * WORD swpl(LONG val); 
+ *   swap endianess of val, 32 bits only.
  */
 
+#ifdef __mcoldfire__
+#define swpl(a)                           \
+  __extension__                           \
+  ({long _tmp;                            \
+    __asm__ __volatile__                  \
+    ("move.b  (%1),%0\n\t"                \
+     "move.b  3(%1),(%1)\n\t"             \
+     "move.b  %0,3(%1)\n\t"               \
+     "move.b  1(%1),%0\n\t"               \
+     "move.b  2(%1),1(%1)\n\t"            \
+     "move.b  %0,2(%1)"                   \
+    : "=d"(_tmp)      /* outputs */       \
+    : "a"(&a)        /* inputs  */        \
+    : "cc", "memory" /* clobbered */      \
+    );                                    \
+  })
+#else
 #define swpl(a)                           \
   __asm__ __volatile__                    \
   ("ror   #8,%0\n\t"                      \
    "swap  %0\n\t"                         \
-   "ror   #8,%0\n\t"                      \
+   "ror   #8,%0"                          \
   : "=d"(a)          /* outputs */        \
   : "0"(a)           /* inputs  */        \
   : "cc"             /* clobbered */      \
-  )                                       \
+  )
+#endif
+
+
+/*
+ * Warning: The following macros use "memory" in the clobber list,
+ * even if the memory is not modified. On ColdFire, this is necessary
+ * to avoid these instructions to be reordered by the compiler.
+ * This may be a GCC bug.
+ */
 
 
 /*
@@ -83,7 +125,7 @@ __extension__                             \
    "move.w %1,sr"                         \
   : "=&dm"(_r)       /* outputs */        \
   : "ndm"(_a)        /* inputs  */        \
-  : "cc"             /* clobbered */      \
+  : "cc", "memory"   /* clobbered */      \
   );                                      \
   _r;                                     \
 })
@@ -101,6 +143,7 @@ __extension__                             \
   ("move.w sr,%0"                         \
   : "=dm"(_r)        /* outputs */        \
   :                  /* inputs  */        \
+  : "cc", "memory"   /* clobbered */      \
   );                                      \
   _r;                                     \
 })
@@ -114,12 +157,22 @@ __extension__                             \
 
 #define stop2300()                        \
 __extension__                             \
-({__asm__ volatile ("stop #0x2300");      \
+({__asm__ volatile                        \
+  ("stop #0x2300"                         \
+  :                  /* outputs */        \
+  :                  /* inputs  */        \
+  : "cc", "memory"   /* clobbered */      \
+  );                                      \
 })
 
 #define stop2500()                        \
 __extension__                             \
-({__asm__ volatile ("stop #0x2500");      \
+({__asm__ volatile                        \
+  ("stop #0x2500"                         \
+  :                  /* outputs */        \
+  :                  /* inputs  */        \
+  : "cc", "memory"   /* clobbered */      \
+  );                                      \
 })
 
 
@@ -129,16 +182,35 @@ __extension__                             \
  *   Saves all registers to the stack, calls the function
  *   that addr points to, and restores the registers afterwards.
  */
+#ifdef __mcoldfire__
+#define regsafe_call(addr)                         \
+__extension__                                      \
+({__asm__ volatile ("lea     -60(sp),sp\n\t"       \
+                    "movem.l d0-d7/a0-a6,(sp)");   \
+  ((void (*)(void))addr)();                        \
+  __asm__ volatile ("movem.l (sp),d0-d7/a0-a6\n\t" \
+                    "lea     60(sp),sp");          \
+})
+#else
 #define regsafe_call(addr)                         \
 __extension__                                      \
 ({__asm__ volatile ("movem.l d0-d7/a0-a6,-(sp)");  \
   ((void (*)(void))addr)();                        \
   __asm__ volatile ("movem.l (sp)+,d0-d7/a0-a6");  \
 })
+#endif
 
 /*
  * No operation opcode (just waits for some few cycles)
  */
-#define nop() asm volatile (" nop ")
+#define nop()                             \
+__extension__                             \
+({__asm__ volatile                        \
+  ("nop"                                  \
+  :                  /* outputs */        \
+  :                  /* inputs  */        \
+  : "memory"         /* clobbered */      \
+  );                                      \
+})
 
 #endif /* ASM_H */
