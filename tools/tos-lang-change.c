4737daee9d56b458fc8e25a68b9d23c878108f0a
Vincent Rivière <vriviere@users.sourceforge.net>
2010-09-21 20:37:53 +0000

    Changed the copyright notice.

--- tools/tos-lang-change.c
+++ tools/tos-lang-change.c
@@ -4,7 +4,10 @@
  * Compile with:
  *      gcc -o tos-lang-change -O -Wall tos-lang-change.c
  * 
- * (w) 2005 by Eero Tamminen
+ * Copyright 2005 by Eero Tamminen
+ * 
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
  */
 
 #include <stdio.h>
