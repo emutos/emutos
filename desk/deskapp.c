92ebf33ef6147b2b4f32566f2fa409578edee3f9
Vincent Rivière <vriviere@users.sourceforge.net>
2011-12-22 18:36:41 +0000

    Use "View as icons" by default for a more Atari feeling.

--- desk/deskapp.c
+++ desk/deskapp.c
@@ -66,7 +66,7 @@ static BYTE     gl_buffer[SIZE_BUFF];
 /* We use this DESKTOP.INF here when we can't load that file from disk: */
 
 static const char *desk_inf_data1 =
-    "#E 9A 01\r\n"
+    "#E 1A 01\r\n"
     "#W 00 00 02 06 26 0C 00 @\r\n"
     "#W 00 00 02 08 26 0C 00 @\r\n"
     "#W 00 00 02 0A 26 0C 00 @\r\n"
