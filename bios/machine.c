49256c28c221dc1d3a82320d2ffb987db85343a5
Vincent Rivière <vriviere@users.sourceforge.net>
2012-02-19 23:16:48 +0000

    In the _SND cookie, remove SND_16BIT and SND_MATRIX until the Falcon XBIOS sound functions are implemented.

--- bios/machine.c
+++ bios/machine.c
@@ -251,7 +251,8 @@ static void setvalue_snd(void)
   }
 
   if (has_falcon_dmasound) {
-    cookie_snd |= SND_16BIT | SND_MATRIX;
+    /* FIXME: Enable the following when Falcon XBIOS functions are implemented */
+    /*cookie_snd |= SND_16BIT | SND_MATRIX;*/
   }
 #endif
 
