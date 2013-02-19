0d4b8cdc080500419ce8d81f038976dbddcbe9bf
Vincent Rivière <vriviere@users.sourceforge.net>
2013-02-19 12:20:15 +0000

    Added Authors.

--- release.mk
+++ release.mk
@@ -3,6 +3,9 @@
 #
 # Copyright (c) 2011-2013 The EmuTOS development team.
 #
+# Authors:
+#  VRI      Vincent Rivière
+#
 # This file is distributed under the GPL, version 2 or at your
 # option any later version.  See doc/license.txt for details.
 #
