83559e74da8c8f200fccb2400361a3555ffcf3fa
Vincent Rivière <vriviere@users.sourceforge.net>
2013-03-01 21:39:42 +0000

    Updated copyright year.

--- tools/mkrom.c
+++ tools/mkrom.c
@@ -1,7 +1,7 @@
 /*
  * mkrom.c - Create an EmuTOS ROM image
  *
- * Copyright (c) 2013 The EmuTOS development team
+ * Copyright (c) 2012-2013 The EmuTOS development team
  *
  * Authors:
  *  VRI   Vincent Rivière
