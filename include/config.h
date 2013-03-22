002adaaa0b2aa5d8a264364fdb50f0f0f6bc84ac
Vincent Rivière <vriviere@users.sourceforge.net>
2013-03-22 19:42:41 +0000

    Enable CONF_WITH_FASTRAM when CONF_FASTRAM_SIZE is  defined.

--- include/config.h
+++ include/config.h
@@ -200,7 +200,7 @@
 # ifndef CONF_WITH_FRB
 #  define CONF_WITH_FRB 0
 # endif
-# ifndef CONF_WITH_FASTRAM
+# if !(defined(CONF_WITH_FASTRAM) || defined(CONF_FASTRAM_SIZE))
 #  define CONF_WITH_FASTRAM 0
 # endif
 # ifndef CONF_WITH_MFP
@@ -357,18 +357,22 @@
 #endif
 
 /*
- * Set CONF_WITH_FASTRAM to 1 to enable detection and usage of FastRAM (TT-RAM)
- */
-#ifndef CONF_WITH_FASTRAM
-# define CONF_WITH_FASTRAM 1
-#endif
-
-/*
  * Define CONF_FASTRAM_SIZE to the actual size of the FastRAM, in bytes.
  * If set to 0, the amount of FastRAM will be autodetected.
  */
 #ifndef CONF_FASTRAM_SIZE
 # define CONF_FASTRAM_SIZE 0
+#endif
+
+/*
+ * Set CONF_WITH_FASTRAM to 1 to enable detection and usage of FastRAM (TT-RAM)
+ */
+#ifndef CONF_WITH_FASTRAM
+# if CONF_FASTRAM_SIZE != 0
+#  define CONF_WITH_FASTRAM 1
+# else
+#  define CONF_WITH_FASTRAM 0
+# endif
 #endif
 
 /*
@@ -880,6 +884,12 @@
 #if !CONF_WITH_ALT_RAM
 # if CONF_WITH_FASTRAM
 #  error "CONF_WITH_FASTRAM requires CONF_WITH_ALT_RAM."
+# endif
+#endif
+
+#if !CONF_WITH_FASTRAM
+# if CONF_FASTRAM_SIZE != 0
+#  error "CONF_FASTRAM_SIZE != 0 requires CONF_WITH_FASTRAM."
 # endif
 #endif
 
