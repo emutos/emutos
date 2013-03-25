86893a35c070a2d9b37f3d7359e5c68bf069d269
Thomas Huth <thothy@users.sourceforge.net>
2013-03-25 00:27:00 +0000

    Added volume control via soundcmd() for STE/TT, too

--- bios/dmasound.c
+++ bios/dmasound.c
@@ -213,10 +213,12 @@ static LONG sndcmd_falcon(WORD mode, WORD data)
 {
     UWORD val;
 
-    switch (mode) {
+    switch (mode)
+    {
      case 0:                // LTATTEN
        val = DMASOUND->channel_attenuation;
-       if (data != -1) {
+       if (data != -1)
+       {
            data = (data & 0x00f0) << 4;
            val = (val & 0xf0ff) | data;
            DMASOUND->channel_attenuation = val;
@@ -224,7 +226,8 @@ static LONG sndcmd_falcon(WORD mode, WORD data)
        return (val >> 4) & 0x00f0;
      case 1:                // RTATTEN
        val = DMASOUND->channel_attenuation;
-       if (data != -1) {
+       if (data != -1)
+       {
            data = data & 0x00f0;
            val = (val & 0xff0f) | data;
            DMASOUND->channel_attenuation = val;
@@ -232,7 +235,8 @@ static LONG sndcmd_falcon(WORD mode, WORD data)
        return val & 0x00f0;
      case 2:                // LTGAIN
        val = DMASOUND->channel_amplification;
-       if (data != -1) {
+       if (data != -1)
+       {
            data = data & 0xf0;
            val = (val & 0x0f) | data;
            DMASOUND->channel_attenuation = val;
@@ -240,22 +244,59 @@ static LONG sndcmd_falcon(WORD mode, WORD data)
        return val & 0x0f0;
      case 3:                // RTGAIN
        val = DMASOUND->channel_amplification;
-       if (data != -1) {
+       if (data != -1)
+       {
            data = (data & 0xf0) >> 4;
            val = (val & 0xf0) | data;
            DMASOUND->channel_attenuation = val;
        }
        return (val & 0x0f) << 4;
      case 4:                // ADDERIN
-       if (data != -1) {
+       if (data != -1)
+       {
            DMASOUND->codec_16bit_source = data;
        }
        return DMASOUND->codec_16bit_source & 0x3;
      case 5:                // ADCINPUT
-       if (data != -1) {
+       if (data != -1)
+       {
            DMASOUND->codec_adc_source = data;
        }
        return DMASOUND->codec_adc_source & 0x3;
+    }
+
+    return 0;
+}
+
+/**
+ * Configure various sound settings of STE DMA sound
+ */
+static LONG sndcmd_ste(WORD mode, WORD data)
+{
+    static UWORD ltatten, rtatten;
+
+    switch (mode)
+    {
+     case 0:                // LTATTEN
+        if (data != -1)
+        {
+            ltatten = ((UWORD)data & 0xff)/6;
+            if (ltatten > 40)
+                ltatten = 40;
+            write_microwire(LMC1992_COMMAND(LMC1992_FUNCTION_LEFT_FRONT_FADER,
+                                            LMC1992_FADER(-ltatten)));
+        }
+        return ltatten * 6;
+     case 1:                // RTATTEN
+        if (data != -1)
+        {
+            rtatten = ((UWORD)data & 0xff)/6;
+            if (rtatten > 40)
+                rtatten = 40;
+            write_microwire(LMC1992_COMMAND(LMC1992_FUNCTION_RIGHT_FRONT_FADER,
+                                            LMC1992_FADER(-rtatten)));
+        }
+        return rtatten * 6;
     }
 
     return 0;
@@ -281,8 +322,10 @@ LONG soundcmd(WORD mode, WORD data)
     {
         return sndcmd_falcon(mode, data);
     }
-
-    // TODO: Volume settings via STE microwire interface
+    else if (has_microwire)
+    {
+        return sndcmd_ste(mode, data);
+    }
 
     return 0;
 }
