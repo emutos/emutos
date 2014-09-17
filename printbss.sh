1dd4f45eb61c7a09d88409f4c4d98b59f124cbc8
Vincent Rivière <vriviere@users.sourceforge.net>
2014-09-17 19:00:12 +0000

    Rewritten using the "size" tool. Much simpler.

--- printbss.sh
+++ printbss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Determine the BSS usage of each subdirectory
-# Originally written by Vincent Rivière, 2014.
+# Written by Vincent Rivière, 2014.
 # License: Public domain
 
 total=0
@@ -10,15 +10,9 @@ do
   # Find all the .o corresponding to the sources of the directory (may exist or not)
   objects=$(find $dir -name '*.c' -or -name '*.S' |sed -n 's:[^/]*/\([^.]*\).*:obj/\1.o:p')
 
-  # Compute the sum of the sizes of the .bss sections
-  bsssize=$(( $(m68k-atari-mint-objdump -h $objects 2>/dev/null |grep '\.bss' |awk '{print "+ 0x" $3}' |xargs) ))
+  # Get the total size of the .bss and COMMON sections
+  dirsize=$(m68k-atari-mint-size -t --common $objects 2>/dev/null |grep '(TOTALS)' |awk '{print $3}')
 
-  # Compute the sum of the sizes of all the COMMON symbols
-  # This is not 100% accurate because we miss a few bytes of alignments and fillers
-  commonsize=$(( $(m68k-atari-mint-objdump -t $objects 2>/dev/null |grep '\*COM\*' |awk '{print " + 0x" $1}' |xargs) ))
-
-  # The final .bss segment contains all the .bss and COMMON sections
-  dirsize=$(($bsssize + $commonsize))
   ((total += $dirsize))
 
   echo -e "$dir \\t $dirsize"
