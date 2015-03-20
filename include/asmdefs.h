c0c0cefec38e408275909d2b3ae39110b2b5dad6
Roger Burrows <anodynesoftware@users.sourceforge.net>
2015-03-20 01:43:18 +0000

    Code cleanup: convert assembler instructions-defined-as-constants to macros.

--- include/asmdefs.h
+++ include/asmdefs.h
@@ -1,7 +1,7 @@
 /*
  * asmdefs.h - definitions to include in top of all assembler files
  *
- * Copyright (c) 2001-2013 The EmuTOS development team
+ * Copyright (c) 2001-2015 The EmuTOS development team
  *
  * Authors:
  *  LVL   Laurent Vogel
@@ -20,11 +20,21 @@
 #include "config.h"
 
 /* generate constants for opcodes that are 68010 and above */
+#define MOVEW_CCR_D0        .dc.w 0x42c0            /* 68010-68060 */
+
 #define MOVEC_CACR_D0       .dc.l 0x4e7a0002        /* 68020-68060 */
+#define MOVEC_CACR_D1       .dc.l 0x4e7a1002
+
+#define MOVEC_PCR_D0        .dc.l 0x4e7a0808        /* 68060 */
+#define MOVEC_PCR_D1        .dc.l 0x4e7a1808
 
 #define MOVEC_D0_CACR       .dc.l 0x4e7b0002        /* 68020-68060 */
+#define MOVEC_D1_CACR       .dc.l 0x4e7b1002
+#define MOVEC_D2_CACR       .dc.l 0x4e7b2002
+
 #define MOVEC_D0_VBR        .dc.l 0x4e7b0801        /* 68010-68060 */
 #define MOVEC_D0_CAAR       .dc.l 0x4e7b0802        /* 68020-68030 */
+#define MOVEC_D0_PCR        .dc.l 0x4e7b0808        /* 68060 */
 
 #define MOVEC_D0_TC         .dc.l 0x4e7b0003        /* 68040-68060 (except 68ec040) */
 #define MOVEC_D0_ITT0       .dc.l 0x4e7b0004        /* 68040-68060 */
@@ -38,6 +48,10 @@
 #define PMOVE_TO_CRP(addr)  .dc.l 0xf0394c00,addr   /* 68030 (except 68ec030) */
 #define PMOVE_TO_TTR0(addr) .dc.l 0xf0390800,addr   /* 68030 */
 #define PMOVE_TO_TTR1(addr) .dc.l 0xf0390c00,addr   /* 68030 */
+
+#define FNOP                .dc.l 0xf2800000        /* 6888X, 68040-68060 */
+#define FSAVE_MINUS_SP      .dc.w 0xf327            /* 6888X, 68040-68060 */
+#define FRESTORE_SP_PLUS    .dc.w 0xf35f            /* 6888X, 68040-68060 */
 
 #define CINVA_IC            .dc.w 0xf498            /* 68040 */
 
