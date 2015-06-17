55a1be38263a45d6f0ae2ff60b36746954cc15f0
Roger Burrows <anodynesoftware@users.sourceforge.net>
2015-06-17 13:31:08 -0400

    Move Super()/SuperToUser() macros to new header
    A bug in 'bug translate' was triggered by the Super() and
    SuperToUser() macros in kprint.c.  Although that bug should
    now be fixed, these macros have been moved to a separate
    header for safety, and to make them more generally available.

--- bios/kprint.c
+++ bios/kprint.c
@@ -29,62 +29,10 @@
 #include "coldfire.h"
 #include "asm.h"
 #include "vectors.h"
+#include "super.h"      /* for Super() and SuperToUser() */
 #ifdef MACHINE_AMIGA
 #include "amiga.h"
 #endif
-
-#undef Super
-/* Standard Super() binding */
-#define Super(ptr)                          \
-__extension__                               \
-({                                          \
-    register long retvalue __asm__("d0");   \
-    long  _ptr = (long) (ptr);              \
-                                            \
-    __asm__ volatile                        \
-    (                                       \
-        "move.l %1,-(sp)\n\t"               \
-        "move.w #0x20,-(sp)\n\t"             \
-        "trap   #1\n\t"                     \
-        "addq.l #6,sp"                      \
-    : "=r"(retvalue)                    /* outputs */       \
-    : "r"(_ptr)                         /* inputs  */       \
-    : __CLOBBER_RETURN("d0") "d1", "d2", "a0", "a1", "a2"   \
-      AND_MEMORY                            \
-    );                                      \
-    retvalue;                               \
-})
-
-#undef SuperToUser      /* prevent "redefined" warning message */
-/*
- * Safe binding to switch back from supervisor to user mode.
- * On TOS or EmuTOS, if the stack pointer has changed between Super(0)
- * and Super(oldssp), the resulting user stack pointer is wrong.
- * This bug does not occur with FreeMiNT.
- * So the safe way to return from supervisor to user mode is to backup
- * the stack pointer then restore it after the trap.
- * Sometimes, GCC optimizes the stack usage, so this matters.
- *
- * Binding originally by Vincent Rivière, from MiNTlib's osbind.h
- */
-#define SuperToUser(ptr)                    \
-(void)__extension__                         \
-({                                          \
-    register long retvalue __asm__("d0");   \
-    register long sp_backup;                \
-                                            \
-    __asm__ volatile                        \
-    (                                       \
-        "move.l  sp,%1\n\t"                 \
-        "move.l  %2,-(sp)\n\t"              \
-        "move.w  #0x20,-(sp)\n\t"           \
-        "trap    #1\n\t"                    \
-        "move.l  %1,sp"                     \
-    : "=r"(retvalue), "=&r"(sp_backup)  /* outputs */       \
-    : "g"((long)(ptr))                  /* inputs */        \
-    : __CLOBBER_RETURN("d0") "d1", "d2", "a0", "a1", "a2"   \
-    );                                      \
-})
 
 
 /* extern declarations */
