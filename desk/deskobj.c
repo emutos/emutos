cc53f4b5619e1ab76b198bd6486e752f30e8fc1a
Roger Burrows <anodynesoftware@users.sourceforge.net>
2015-07-02 21:30:48 -0400

    Reformat source for improved readability
        - use 4-space indents
        - reformat/update comments

--- desk/deskobj.c
+++ desk/deskobj.c
@@ -37,98 +37,99 @@
 
 static const OBJECT gl_sampob[2] =
 {
-        { NIL, NIL, NIL, G_IBOX, NONE, NORMAL, 0x0L, 0, 0, 0, 0 },
-        { NIL, NIL, NIL, G_BOX,  NONE, NORMAL, 0x00001100L, 0, 0, 0, 0 }
+    { NIL, NIL, NIL, G_IBOX, NONE, NORMAL, 0x0L, 0, 0, 0, 0 },
+    { NIL, NIL, NIL, G_BOX,  NONE, NORMAL, 0x00001100L, 0, 0, 0, 0 }
 };
 
 
 
 /*
-*       Initialize all objects as children of the 0th root which is
-*       the parent of unused objects.
-*/
+ *  Initialize all objects as children of the 0th root which is
+ *  the parent of unused objects
+ */
 void obj_init(void)
 {
-        WORD            ii;
+    WORD ii;
 
-        for (ii = 0; ii < NUM_SOBS; ii++)
-        {
-          G.g_screen[ii].ob_head = NIL;
-          G.g_screen[ii].ob_next = NIL;
-          G.g_screen[ii].ob_tail = NIL;
-        }
-        memcpy(&G.g_screen[ROOT], &gl_sampob[0], sizeof(OBJECT));
-        r_set((GRECT *)&G.g_screen[ROOT].ob_x, 0, 0, gl_width, gl_height);
-        for (ii = 0; ii < (NUM_WNODES+1); ii++)
-        {
-          memcpy(&G.g_screen[DROOT+ii], &gl_sampob[1], sizeof(OBJECT));
-          objc_add(G.g_screen, ROOT, DROOT+ii);
-        } /* for */
-} /* obj_init */
+    for (ii = 0; ii < NUM_SOBS; ii++)
+    {
+        G.g_screen[ii].ob_head = NIL;
+        G.g_screen[ii].ob_next = NIL;
+        G.g_screen[ii].ob_tail = NIL;
+    }
+    memcpy(&G.g_screen[ROOT], &gl_sampob[0], sizeof(OBJECT));
+    r_set((GRECT *)&G.g_screen[ROOT].ob_x, 0, 0, gl_width, gl_height);
+
+    for (ii = 0; ii < (NUM_WNODES+1); ii++)
+    {
+        memcpy(&G.g_screen[DROOT+ii], &gl_sampob[1], sizeof(OBJECT));
+        objc_add(G.g_screen, ROOT, DROOT+ii);
+    }
+}
 
 
 /*
-*       Allocate a window object from the screen tree by looking for
-*       the child of the parent with no size
-*/
+ *  Allocate a window object from the screen tree by looking for
+ *  the child of the parent with no size
+ */
 WORD obj_walloc(WORD x, WORD y, WORD w, WORD h)
 {
-        WORD            ii;
+    WORD ii;
 
-        for (ii = DROOT; ii < (NUM_WNODES+1); ii++)
-        {
-          if ( !(G.g_screen[ii+1].ob_width && G.g_screen[ii+1].ob_height) )
+    for (ii = DROOT; ii < (NUM_WNODES+1); ii++)
+    {
+        if (!(G.g_screen[ii+1].ob_width && G.g_screen[ii+1].ob_height))
             break;
-        }
-        if ( ii < (NUM_WNODES+1) )
-        {
-          r_set((GRECT *)&G.g_screen[ii+1].ob_x, x, y, w, h);
-          return(ii+1);
-        }
-        else
-          return(0);
-} /* obj_walloc */
+    }
+    if (ii < (NUM_WNODES+1))
+    {
+        r_set((GRECT *)&G.g_screen[ii+1].ob_x, x, y, w, h);
+        return ii+1;
+    }
+    else
+        return 0;
+}
 
 
 /*
-*       Free a window object by changing its size to zero and
-*       NILing out all its children.
-*/
+ *  Free a window object by changing its size to zero and
+ *  NILing out all its children
+ */
 void obj_wfree(WORD obj, WORD x, WORD y, WORD w, WORD h)
 {
-        WORD            ii, nxtob;
+    WORD ii, nxtob;
 
-        r_set((GRECT *)&G.g_screen[obj].ob_x, x, y, w, h);
-        for (ii = G.g_screen[obj].ob_head; ii > obj; ii = nxtob)
-        {
-          nxtob = G.g_screen[ii].ob_next;
-          G.g_screen[ii].ob_next = NIL;
-        }
-        G.g_screen[obj].ob_head = G.g_screen[obj].ob_tail = NIL;
-} /* obj_wfree */
+    r_set((GRECT *)&G.g_screen[obj].ob_x, x, y, w, h);
+    for (ii = G.g_screen[obj].ob_head; ii > obj; ii = nxtob)
+    {
+        nxtob = G.g_screen[ii].ob_next;
+        G.g_screen[ii].ob_next = NIL;
+    }
+    G.g_screen[obj].ob_head = G.g_screen[obj].ob_tail = NIL;
+}
 
 
 /*
-*       Routine to find and allocate a particular item object.  The
-*       next free object is found by looking for any object that
-*       is available (i.e., a next pointer of NIL).
-*/
+ *  Routine to find and allocate a particular item object.  The
+ *  next free object is found by looking for any object that
+ *  is available (i.e. a next pointer of NIL).
+ */
 WORD obj_ialloc(WORD wparent, WORD x, WORD y, WORD w, WORD h)
 {
-        WORD            ii;
+    WORD ii;
 
-        for (ii = NUM_WNODES+2; ii < NUM_SOBS; ii++)
-        {
-          if (G.g_screen[ii].ob_next == NIL)
+    for (ii = NUM_WNODES+2; ii < NUM_SOBS; ii++)
+    {
+        if (G.g_screen[ii].ob_next == NIL)
             break;
-        }
-        if (ii < NUM_SOBS)
-        {
-          G.g_screen[ii].ob_head = G.g_screen[ii].ob_tail = NIL;
-          objc_add(G.g_screen, wparent, ii);
-          r_set((GRECT *)&G.g_screen[ii].ob_x, x, y, w, h);
-          return(ii);
-        }
-        else
-          return(0);
-} /* obj_ialloc */
+    }
+    if (ii < NUM_SOBS)
+    {
+        G.g_screen[ii].ob_head = G.g_screen[ii].ob_tail = NIL;
+        objc_add(G.g_screen, wparent, ii);
+        r_set((GRECT *)&G.g_screen[ii].ob_x, x, y, w, h);
+        return ii;
+    }
+    else
+        return 0;
+}
