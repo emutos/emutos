758729a86801ce1139c5bc0316ca743006d25461
Roger Burrows <anodynesoftware@users.sourceforge.net>
2015-12-03 23:32:08 -0500

    Improve comments

--- vdi/vdi_esc.c
+++ vdi/vdi_esc.c
@@ -1,5 +1,5 @@
 /*
- * esclisa.c - GSX escapes for the VDI screen driver
+ * vdi_esc.c - GSX escapes for the VDI screen driver
  *
  * Copyright (c) 2002-2013 The EmuTOS development team
  *
@@ -7,15 +7,11 @@
  * option any later version.  See doc/license.txt for details.
  */
 
-
-
 #include "config.h"
 #include "portab.h"
 #include "vdi_defs.h"
 #include "lineavars.h"
 #include "asm.h"
-
-
 
 /* Local Constants */
 
@@ -27,39 +23,33 @@
 #define wntstr                  0x09    // write null terminated string to std output
 
 
-
 /*
- * escfn0 - Don't know, what it does...
+ * escfn0 - stub to simplify lookup table
  */
-
 static void escfn0(Vwk * vwk)
 {
 }
 
 
-
 /*
- * escfn1 - returns the current number of columns and rows
+ * escfn1: vq_chcells() - returns the current number of rows and columns
  *
  * outputs:
  *   CONTRL[4] = 2 (# of integers returned)
  *   INTOUT[0] = number of rows
  *   INTOUT[1] = number of columns
  */
-
 static void escfn1(Vwk * vwk)
 {
-    CONTRL[4] = 2;              // 2 integers are returned
+    CONTRL[4] = 2;
     INTOUT[0] = v_cel_my + 1;
     INTOUT[1] = v_cel_mx + 1;
 }
 
 
-
 /*
- * escfn2 - exit the alpha mode and enter the graphics mode.
+ * escfn2: v_exit_cur() - exit alpha mode and enter graphics mode
  */
-
 static void escfn2(Vwk * vwk)
 {
     trap1(wntstr, "\033f\033E");   // hide alpha cursor
@@ -67,9 +57,8 @@ static void escfn2(Vwk * vwk)
 }
 
 
-
 /*
- * escfn3 - enter the alpha mode and exit the graphics mode.
+ * escfn3: v_enter_cur() - enter alpha mode and exit graphics mode
  */
 static void escfn3(Vwk * vwk)
 {
@@ -78,86 +67,71 @@ static void escfn3(Vwk * vwk)
 }
 
 
-
 /*
- * escfn4 - moves the alpha cursor up one line.
+ * escfn4: v_curup() - moves the alpha cursor up one line
  */
-
 static void escfn4(Vwk * vwk)
 {
     trap1(wntstr, "\033A");
 }
 
 
-
 /*
- * escfn5 - moves the alpha cursor down one line.
+ * escfn5: v_curdown() - moves the alpha cursor down one line
  */
-
 static void escfn5(Vwk * vwk)
 {
     trap1(wntstr, "\033B");
 }
 
 
-
 /*
- * escfn6 - moves the alpha cursor right one column.
+ * escfn6: v_curright() - moves the alpha cursor right one column
  */
-
 static void escfn6(Vwk * vwk)
 {
     trap1(wntstr, "\033C");
 }
 
 
-
 /*
- * escfn7 - moves the alpha cursor left one column.
+ * escfn7: v_curleft() - moves the alpha cursor left one column
  */
-
 static void escfn7(Vwk * vwk)
 {
     trap1(wntstr, "\033D");
 }
 
 
-
 /*
- * escfn8 - moves the alpha cursor home.
+ * escfn8: v_curhome() - moves the alpha cursor home
  */
-
 static void escfn8(Vwk * vwk)
 {
     trap1(wntstr, "\033H");
 }
 
 
-
 /*
- * escfn9 - clears screen from cursor position to the end of the screen.
+ * escfn9: v_eeos() - clears screen from cursor position to end of screen
  */
-
 static void escfn9(Vwk * vwk)
 {
     trap1(wntstr, "\033J");
 }
 
 
-
 /*
- * escfn10 - clears screen from cursor position to the end of the line.
+ * escfn10: v_eeol() - clears screen from cursor position to end of line
  */
-
 static void escfn10(Vwk * vwk)
 {
-    trap1(wntstr, "\033K");      // cursor down
+    trap1(wntstr, "\033K");
 }
 
 
-
 /*
- * escfn11 - sets the cursor position
+ * escfn11: vs_curaddress() - sets the cursor position
  *
  * The cursor will be displayed at the new location,
  * if it is not currently hidden.
@@ -166,7 +140,6 @@ static void escfn10(Vwk * vwk)
  *   INTIN[0] = cursor row (0 - max_y_cell)
  *   INTIN[1] = cursor column (0 - max_x_cell)
  */
-
 static void escfn11(Vwk * vwk)
 {
     char out[5];
@@ -174,15 +147,14 @@ static void escfn11(Vwk * vwk)
     out[0] = '\033';
     out[1] = 'Y';
     out[2] = 0x20 + INTIN[0];   /* get row number */
-    out[3] = 0x20 + INTIN[1];  /* get col number */
+    out[3] = 0x20 + INTIN[1];   /* get col number */
     out[4] = '\0';
     trap1(wntstr, out);
 }
 
 
-
 /*
- * escfn12 - outputs cursor addressable alpha text.
+ * escfn12: v_curtext() - outputs cursor addressable alpha text
  *
  * The cursor will be displayed at the new location,
  * if it is not currently hidden.
@@ -191,7 +163,6 @@ static void escfn11(Vwk * vwk)
  *   CONTRL[3] = character count
  *   INTIN = character array
  */
-
 static void escfn12(Vwk * vwk)
 {
     int cnt;
@@ -206,36 +177,30 @@ static void escfn12(Vwk * vwk)
 }
 
 
-
 /*
- * escfn13 - switch to reverse video
+ * escfn13: v_rvon(): - switch to reverse video
  */
-
 static void escfn13(Vwk * vwk)
 {
     trap1(wntstr, "\033p");      /* enter reverse video */
 }
 
 
-
 /*
- * escfn14 - switch to normal video
+ * escfn14: v_rvoff() - switch to normal video
  */
-
 static void escfn14(Vwk * vwk)
 {
-    trap1(wntstr, "\033q");      /* enter reverse video */
+    trap1(wntstr, "\033q");      /* enter normal video */
 }
 
 
-
 /*
- * escfn15 - returns the current x and y-coordinates of the alpha cursor.
+ * escfn15: vq_curaddress() - returns current x- and y-coordinates of the alpha cursor
  *
  * This function is currently just a stub because this information
- * is not readilly available.
+ * is not readily available.
  */
-
 static void escfn15(Vwk * vwk)
 {
     CONTRL[4] = 2;              // 2 integers are returned
@@ -244,58 +209,54 @@ static void escfn15(Vwk * vwk)
 }
 
 
-
 /*
- * escfn16 - returns the availability of a mouse or similar
+ * escfn16: vq_tabstatus() - returns the availability of a "tablet device"
+ *
+ * Atari TOS always returns 1, so do we
  *
  * outputs:
  *   CONTRL[4] = 1  (# of parameters returned)
- *   INTOUT[0] = 1  (mouse is available)
+ *   INTOUT[0] = 1  (device is available)
  */
-
 static void escfn16(Vwk * vwk)
 {
-    CONTRL[4] = 1;              // 2 integers are returned
+    CONTRL[4] = 1;              // 1 integer is returned
     INTOUT[0] = 1;              // there is a mouse
 }
 
 
-
 /*
- * escfn17 - obtain hard copy of the contents of the screen.
+ * escfn17: v_hardcopy() - output screen to printer
  *
  * This function is currently just a stub.
  */
-
 static void escfn17(Vwk * vwk)
 {
 }
 
 
-
 /*
- * escfn18 - displays the graphics cursor at its current address.
+ * escfn18: v_dspcur() - display the graphics cursor
  */
-
 static void escfn18(Vwk * vwk)
 {
-    INTIN[0] = 0;       /* show regardless */
+    INTIN[0] = 0;           /* show regardless */
     _v_show_c(vwk);         /* display the graphics cursor */
 }
 
 
-
 /*
- * escfn19 - hides the graphics cursor.
+ * escfn19: v_rmcur() - remove the graphics cursor
  */
-
 static void escfn19(Vwk * vwk)
 {
     _v_hide_c(vwk);         /* hide the graphics cursor */
 }
 
 
-
+/*
+ * function lookup table
+ */
 static void (* const esctbl[])(Vwk *) =
 {
     escfn0,
@@ -321,11 +282,10 @@ static void (* const esctbl[])(Vwk *) =
 };
 
 
-
 /*
- * chk_esc -  This routine decodes the escape functions.
+ * chk_esc - this routine is called to decode the escape subfunctions
  *
- * The following inputs and outputs may be used by a sub-function:
+ * The following inputs and outputs may be used by a subfunction:
  *
  * input:
  *   CONTRL[5] = escape function ID.
@@ -338,7 +298,6 @@ static void (* const esctbl[])(Vwk *) =
  *   INTOUT[]  = array of output parameters.
  *   PTSOUT[]  = array of output vertices.
  */
-
 void chk_esc(Vwk * vwk)
 {
     UWORD escfun = CONTRL[5];
@@ -349,20 +308,25 @@ void chk_esc(Vwk * vwk)
         return;
     }
 #endif
+
     if (escfun > ldri_escape)
         return;
     (*esctbl[escfun])(vwk);
 }
 
 
-
+/*
+ * called by v_opnwk() to enter graphics mode
+ */
 void esc_init(Vwk * vwk)
 {
     escfn2(vwk);
 }
 
 
-
+/*
+ * called by v_clswk() to exit graphics mode
+ */
 void esc_exit(Vwk * vwk)
 {
     escfn3(vwk);
