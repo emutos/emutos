707eca4f1579e5e68688716311851756ba3095fd
Vincent Rivière <vriviere@users.sourceforge.net>
2016-04-04 21:47:15 +0200

    Initialize the IKBD clock on first boot only

--- bios/clock.c
+++ bios/clock.c
@@ -515,7 +515,8 @@ void clock_init(void)
         /* The IKBD clock is lost at power off, and has bogus values at
          * power on.  So initialize it to the default date/time at startup.
          */
-        isetdt(DEFAULT_DATETIME);
+        if (first_boot)
+            isetdt(DEFAULT_DATETIME);
     }
 #endif /* CONF_WITH_IKBD_CLOCK */
 }
