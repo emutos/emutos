emutos/emutos

commit 979c08959b8d432694e98902cc028d978486ff5e

Respect standard TOS usage of DIP switch 8

bios/machine.c
@@ -298,6 +298,19 @@ static void setvalue_snd(void)
         cookie_snd |= SND_16BIT | SND_MATRIX;
     }
 
+    if (HAS_DIP_SWITCHES)
+    {
+        /*
+         * if DIP sw 8 is on (i.e. bit 7 is off), then we turn off the
+         * indicator for 8-bit DMA stereo in the _SND cookie, just like
+         * TOS3/TOS4 do.
+         */
+        if (!(cookie_swi & 0x80))
+        {
+            cookie_snd &= ~SND_8BIT;
+        }
+    }
+
     KDEBUG(("cookie_snd = 0x%08lx\n", cookie_snd));
 }
 

commit 5ee19cee49f34922a9ab3e9cf646a19f00528daa

Fix compile-time error with 192K ROMs
This error was introduced by commit 979c0895.

bios/machine.c
@@ -298,7 +298,8 @@ static void setvalue_snd(void)
         cookie_snd |= SND_16BIT | SND_MATRIX;
     }
 
-    if (HAS_DIP_SWITCHES)
+#if CONF_WITH_DIP_SWITCHES
+    if (has_dip_switches)
     {
         /*
          * if DIP sw 8 is on (i.e. bit 7 is off), then we turn off the
@@ -310,6 +311,7 @@ static void setvalue_snd(void)
             cookie_snd &= ~SND_8BIT;
         }
     }
+#endif
 
     KDEBUG(("cookie_snd = 0x%08lx\n", cookie_snd));
 }