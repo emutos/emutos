dbf96bf372009a9324020e45454e3578b62c5ef1
Roger Burrows <anodynesoftware@users.sourceforge.net>
2016-08-17 23:53:09 -0400

    Clean up source code
    Remove unused #defines, add explanatory comments, use standard
    indentation.

--- desk/deskapp.h
+++ desk/deskapp.h
@@ -20,67 +20,45 @@
 #define _DESKAPP_H
 #include "deskconf.h"
 
-#define AP_APPLOPEN 0
-#define AP_DATAOPEN 1
-#define AP_DATADRAG 2
+/*
+ * bit masks for the a_flags field in the ANODE
+ */
+#define AF_ISCRYS 0x0001        /* is crystal (i.e. GEM) application */
+#define AF_ISGRAF 0x0002        /* is graphic appl. (always set when AF_ISCRYS is set) */
+#define AF_ISDESK 0x0004        /* requires desktop icon */
+#define AF_ISPARM 0x0008        /* requires input parameters (TTP or GTP) */
 
-#define AF_ISCRYS 0x0001                        /* is crystal appl.     */
-#define AF_ISGRAF 0x0002                        /* is graphic appl.     */
-#define AF_ISDESK 0x0004                        /* is placed on desk    */
-#define AF_ISPARM 0x0008                        /* is in need of input  */
-                                                /*   parameters         */
-
+/*
+ * values for the a_type field in the ANODE
+ */
 #define AT_ISFILE 0
 #define AT_ISFOLD 1
 #define AT_ISDISK 2
 #define AT_ISTRSH 3
 
-                                                /* gem icons            */
-#define IG_HARD 0
-#define IG_FLOPPY 1
-#define IG_FOLDER 2
-#define IG_TRASH 3
-#define IG_4RESV 4
-#define IG_5RESV 5
-#define IA_GENERIC_ALT 6
-#define ID_GENERIC_ALT 7
+/*
+ * standard gem icons, always available
+ */
+#define IG_HARD   0             /* hard drive */
+#define IG_FLOPPY 1             /* floppy drive */
+#define IG_FOLDER 2             /* folder */
+#define IG_TRASH  3             /* trash */
+#define IG_4RESV  4
+#define IG_5RESV  5
+#define IA_GENERIC_ALT 6        /* generic application icon */
+#define ID_GENERIC_ALT 7        /* generic document icon */
 #define NUM_GEM_IBLKS (ID_GENERIC_ALT+1)
-                                                /* application icons    */
-#define IA_GENERIC 8
-#define IA_SS 9
-#define IA_WP 10
-#define IA_DB 11
-#define IA_DRAW 12
-#define IA_PAINT 13
-#define IA_PROJECT 14
-#define IA_GRAPH 15
-#define IA_OUTLINE 16
-#define IA_ACCNT 17
-#define IA_MULTI 18
-#define IA_EDUC 19
-#define IA_COMM 20
-#define IA_TOOL 21
-#define IA_GAME 22
-#define IA_OUTPUT 23
-                                                /* document icons       */
-#define ID_GENERIC 40
-#define ID_SS 41
-#define ID_WP 42
-#define ID_DB 43
-#define ID_DRAW 44
-#define ID_PAINT 45
-#define ID_PROJECT 46
-#define ID_GRAPH 47
-#define ID_OUTLINE 48
-#define ID_ACCNT 49
-#define ID_MULTI 50
-#define ID_EDUC 51
-#define ID_COMM 52
-#define ID_TOOL 53
-#define ID_GAME 54
-#define ID_OUTPUT 55
 
-#define NUM_ANODES 64                           /* # of appl. nodes     */
+/*
+ * icon numbers available iff CONF_WITH_DESKTOP_ICONS is defined
+ */
+#define IA_GENERIC 8            /* the first application icon # */
+#define ID_GENERIC 40           /* the first document icon # */
+
+/*
+ * configuration parameters
+ */
+#define NUM_ANODES  64          /* # of application nodes */
 #define SIZE_AFILE  2048        /* size of buffer for EMUDESK.INF file */
 #define SIZE_BUFF   4096        /* size of buffer used to store ANODE text */
 
@@ -100,57 +78,63 @@
 typedef struct _applstr ANODE;
 struct _applstr
 {
-        ANODE           *a_next;
-        WORD            a_flags;
-        WORD            a_type;                 /* icon/appl type       */
-        WORD            a_obid;                 /* object index         */
-        BYTE            *a_pappl;               /* filename.ext of ap.  */
-        BYTE            *a_pdata;               /* wldcards of data file*/
-        WORD            a_aicon;                /* application icon #   */
-        WORD            a_dicon;                /* data icon #          */
-        WORD            a_letter;               /* letter for icon      */
-        WORD            a_xspot;                /* desired spot on desk */
-        WORD            a_yspot;                /* desired spot on desk */
+    ANODE *a_next;
+    WORD a_flags;               /* see above for usage */
+    WORD a_type;                /* icon type (see above) */
+    WORD a_obid;                /* object index */
+    BYTE *a_pappl;              /* filename.ext of appplication */
+    BYTE *a_pdata;              /* mask for data files */
+    WORD a_aicon;               /* application icon # */
+    WORD a_dicon;               /* data icon # */
+    WORD a_letter;              /* letter for icon */
+    WORD a_xspot;               /* desired spot on desk */
+    WORD a_yspot;               /* desired spot on desk */
 };
 
 
-/* save current info for one window */
+/*
+ * save current info for one window
+ */
 typedef struct
 {
-        WORD    x_save;
-        WORD    y_save;
-        WORD    w_save;
-        WORD    h_save;
-        WORD    vsl_save;
-        WORD    obid_save;
-        BYTE    pth_save[LEN_ZPATH];
+    WORD x_save;                /* window position */
+    WORD y_save;
+    WORD w_save;
+    WORD h_save;
+    WORD vsl_save;              /* vertical slider position */
+    WORD obid_save;             /* currently-selected object */
+    BYTE pth_save[LEN_ZPATH];
 } WSAVE;
 
 
 /* values stored in cs_sort (below) range from 0 to CS_NOSORT */
 #define CS_NOSORT   (NSRTITEM-NAMEITEM)
 
-/* save desktop context (preferences and windows) */
+/*
+ * save desktop context (preferences and windows)
+ */
 typedef struct
 {
-        BYTE    cs_sort;        /* Sort mode */
-        BYTE    cs_view;        /* Show files as icons or text */
-        BYTE    cs_confcpy;     /* Confirm copies */
-        BYTE    cs_confdel;     /* Confirm deletes */
-        BYTE    cs_confovwr;    /* Confirm overwrite */
-        BYTE    cs_dblclick;    /* Double click speed */
-        BYTE    cs_mnuclick;    /* Drop down menu click mode */
-        BYTE    cs_timefmt;     /* Time format */
-        BYTE    cs_datefmt;     /* Date format */
-        WSAVE   cs_wnode[NUM_WNODES];
+    BYTE cs_sort;               /* Sort mode */
+    BYTE cs_view;               /* Show files as icons or text */
+    BYTE cs_confcpy;            /* Confirm copies */
+    BYTE cs_confdel;            /* Confirm deletes */
+    BYTE cs_confovwr;           /* Confirm overwrite */
+    BYTE cs_dblclick;           /* Double click speed */
+    BYTE cs_mnuclick;           /* Drop down menu click mode */
+    BYTE cs_timefmt;            /* Time format */
+    BYTE cs_datefmt;            /* Date format */
+    WSAVE cs_wnode[NUM_WNODES]; /* window save info */
 } CSAVE;
 
 
-extern WORD     gl_numics;
-extern WORD     gl_stdrv;
+extern WORD gl_numics;
+extern WORD gl_stdrv;
 
 
-/* Prototypes: */
+/*
+ * Function prototypes
+ */
 ANODE *app_alloc(WORD tohead);
 void app_free(ANODE *pa);
 BYTE *scan_str(BYTE *pcurr, BYTE **ppstr);
