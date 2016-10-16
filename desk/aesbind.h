e44e8533e5b2df9c1daf0b796530021bdb2e4528
Roger Burrows <anodynesoftware@users.sourceforge.net>
2016-10-16 18:47:23 -0400

    Comment-out declarations for commented-out functions

--- desk/aesbind.h
+++ desk/aesbind.h
@@ -16,8 +16,9 @@
 #define END_MCTRL  2
 #define BEG_MCTRL  3
 
-/* AES prototypes: */
-
+/*
+ * Application Manager
+ */
 WORD appl_init(void);
 WORD appl_exit(void);
 /*
@@ -28,6 +29,10 @@ WORD appl_tplay(LONG tbuffer, WORD tlength, WORD tscale);
 WORD appl_trecord(LONG tbuffer, WORD tlength);
 */
 
+
+/*
+ *  Event Manager
+ */
 WORD evnt_button(WORD clicks, UWORD mask, UWORD state,
                  WORD *pmx, WORD *pmy, WORD *pmb, WORD *pks);
 WORD evnt_timer(UWORD locnt, UWORD hicnt);
@@ -37,14 +42,30 @@ WORD evnt_multi(UWORD flags, UWORD bclk, UWORD bmsk, UWORD bst,
                 WORD *mepbuff, UWORD tlc, UWORD thc, UWORD *pmx, UWORD *pmy,
                 UWORD *pmb, UWORD *pks, UWORD *pkr, UWORD *pbr );
 WORD evnt_dclick(WORD rate, WORD setit);
+/*
+UWORD evnt_keybd(void);
+WORD evnt_mouse(WORD flags, WORD x, WORD y, WORD width, WORD height,
+                WORD *pmx, WORD *pmy, WORD *pmb, WORD *pks);
+WORD evnt_mesag(LONG pbuff);
+*/
 
+
+/*
+ *  Menu Manager
+ */
 WORD menu_bar(LONG tree, WORD showit);
 WORD menu_icheck(LONG tree, WORD itemnum, WORD checkit);
 WORD menu_ienable(LONG tree, WORD itemnum, WORD enableit);
 WORD menu_tnormal(LONG tree, WORD titlenum, WORD normalit);
 /* WORD menu_text(LONG tree, WORD inum, LONG ptext); */
+/* WORD menu_register(WORD pid, LONG pstr); */
+/* WORD menu_unregister(WORD mid); */
 WORD menu_click(WORD click, WORD setit);
 
+
+/*
+ *  Object Manager
+ */
 WORD objc_add(OBJECT *tree, WORD parent, WORD child);
 /*WORD objc_delete(OBJECT *tree, WORD delob);*/
 WORD objc_draw(OBJECT *tree, WORD drawob, WORD depth, WORD xc, WORD yc,
@@ -52,36 +73,70 @@ WORD objc_draw(OBJECT *tree, WORD drawob, WORD depth, WORD xc, WORD yc,
 WORD objc_find(OBJECT *tree, WORD startob, WORD depth, WORD mx, WORD my);
 WORD objc_order(OBJECT *tree, WORD mov_obj, WORD newpos);
 WORD objc_offset(OBJECT *tree, WORD obj, WORD *poffx, WORD *poffy);
+/* WORD objc_edit(LONG tree, WORD obj, WORD inchar, WORD *idx, WORD kind); */
 WORD objc_change(OBJECT *tree, WORD drawob, WORD depth, WORD xc, WORD yc,
                  WORD wc, WORD hc, WORD newstate, WORD redraw);
 
+
+/*
+ *  Form Manager
+ */
 WORD form_do(OBJECT *form, WORD start);
 WORD form_dial(WORD dtype, WORD ix, WORD iy, WORD iw, WORD ih,
                WORD x, WORD y, WORD w, WORD h);
 WORD form_alert(WORD defbut, LONG astring);
 WORD form_error(WORD errnum);
 WORD form_center(OBJECT *tree, WORD *pcx, WORD *pcy, WORD *pcw, WORD *pch);
+/* 
+WORD form_keybd(LONG form, WORD obj, WORD nxt_obj, WORD thechar,
+                WORD *pnxt_obj, WORD *pchar);
+WORD form_button(LONG form, WORD obj, WORD clks, WORD *pnxt_obj);
+*/
 
 
+/*
+ *  Graphics Manager
+ */
 WORD graf_rubbox(WORD xorigin, WORD yorigin, WORD wmin, WORD hmin,
                  WORD *pwend, WORD *phend);
+/*
 WORD graf_dragbox(WORD w, WORD h, WORD sx, WORD sy, WORD xc, WORD yc,
                   WORD wc, WORD hc, WORD *pdx, WORD *pdy);
 WORD graf_mbox(WORD w, WORD h, WORD srcx, WORD srcy, WORD dstx, WORD dsty);
+*/
 WORD graf_growbox(WORD orgx, WORD orgy, WORD orgw, WORD orgh,
                   WORD x, WORD y, WORD w, WORD h);
 WORD graf_shrinkbox(WORD orgx, WORD orgy, WORD orgw, WORD orgh,
                     WORD x, WORD y, WORD w, WORD h);
+/*
 WORD graf_watchbox(LONG tree, WORD obj, UWORD instate, UWORD outstate);
 WORD graf_slidebox(LONG tree, WORD parent, WORD obj, WORD isvert);
+*/
 WORD graf_handle(WORD *pwchar, WORD *phchar, WORD *pwbox, WORD *phbox);
 WORD graf_mouse(WORD m_number, void *m_addr);
 void graf_mkstate(WORD *pmx, WORD *pmy, WORD *pmstate, WORD *pkstate);
 
+
+/*
+ *  Scrap Manager
+ */
+/*
+WORD scrp_read(LONG pscrap);
+WORD scrp_write(LONG pscrap);
+*/
+
+
+/*
+ * Form Selector Manager
+ */
 /*
 WORD fsel_input(LONG pipath, LONG pisel, WORD *pbutton);
 */
 
+
+/*
+ *  Window Manager
+ */
 WORD wind_create(UWORD kind, WORD wx, WORD wy, WORD ww, WORD wh);
 WORD wind_open(WORD handle, WORD wx, WORD wy, WORD ww, WORD wh);
 WORD wind_close(WORD handle);
@@ -96,16 +151,32 @@ WORD wind_calc(WORD wctype, UWORD kind, WORD x, WORD y, WORD w, WORD h,
                WORD *px, WORD *py, WORD *pw, WORD *ph);
 
 
+/*
+ *  Resource Manager
+ */
+/*
 WORD rsrc_load(LONG rsname);
 WORD rsrc_free(void);
-WORD rsrc_gaddr(WORD rstype, WORD rsid, LONG *paddr);
+*/
+WORD rsrc_gaddr(WORD rstype, WORD rsid, LONG *paddr);   /* see deskmain.c */
+/*
+WORD rsrc_saddr(WORD rstype, WORD rsid, LONG lngval);
+*/
 WORD rsrc_obfix(LONG tree, WORD obj);
 
-WORD shel_read(LONG pcmd, LONG ptail);
+
+/*
+ *  Shell Manager
+ */
+/* WORD shel_read(LONG pcmd, LONG ptail); */
 WORD shel_write(WORD doex, WORD isgr, WORD iscr, BYTE *pcmd, BYTE *ptail);
 WORD shel_get(void *pbuffer, WORD len);
 WORD shel_put(void *pdata, WORD len);
 WORD shel_find(BYTE *ppath);
+/*
 WORD shel_envrn(LONG ppath, LONG psrch);
+WORD shel_rdef(LONG lpcmd, LONG lpdir);
+WORD shel_wdef(LONG lpcmd, LONG lpdir);
+*/
 
 #endif  /* _AESBIND_H */
