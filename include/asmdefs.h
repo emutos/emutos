0397db901797b9739d2f0912d17f248be38c5b69
Vincent Rivière <vriviere@users.sourceforge.net>
2017-01-12 01:49:06 +0100

    Fix Apollo Core detection for buggy GOLD release
    - Apollo core is mainly like 68040 + additional instructions
    - 68040 instuction cache bit is not properly set when CACR is read
    - The PCR identifier 0x0440 will only be present in GOLD2

--- include/asmdefs.h
+++ include/asmdefs.h
@@ -58,6 +58,8 @@
 #define CINVA_IC            .dc.w 0xf498            /* 68040 */
 #define CINVL_IC_A0         .dc.w 0xf488            /* 68040 */
 
+#define ADDIWL_0_A0         .dc.l 0x06d00000        /* 68080 */
+
 /* ELF toolchain support */
 #ifdef ELF_TOOLCHAIN
 #define DEFINE_FUNCTION_ALIAS(alias, impl) .globl alias; alias:; jmp impl
