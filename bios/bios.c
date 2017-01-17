emutos/emutos

commit def704731fcc8cd17052ecdde340a7527ec51f62

Convert FastRAM detection to C
This implementation is simpler and more maintainable than the previous
one. Furthermore, it avoids calling detect_cpu() twice during boot.
fastram_init() can be called at any time after processor_init()
and is_bus32 initialization.

bios/bios.c
@@ -387,16 +387,13 @@ static void bios_init(void)
     }
 #endif
 
-#if CONF_WITH_ALT_RAM
+    /* Initialize TT-like FastRAM.
+     * This always intializes the FastRAM system variables,
+     * even if there is no FastRAM. */
+    KDEBUG(("fastram_init()\n"));
+    fastram_init();
 
-#if CONF_WITH_FASTRAM
-    /* add TT-RAM that was detected in memory.S */
-    if (ramtop != NULL)
-    {
-        KDEBUG(("xmaddalt(): ramtop=%p\n",ramtop));
-        xmaddalt(FASTRAM_START, ramtop - FASTRAM_START);
-    }
-#endif
+#if CONF_WITH_ALT_RAM
 
 #if CONF_WITH_MONSTER
     /* Add MonSTer alt-RAM detected in machine.c */

bios/memory.h
@@ -27,4 +27,6 @@ void init_tt_fastram(void);
 
 #endif /* CONF_WITH_TT_MMU */
 
+void fastram_init(void);
+
 #endif /* MEMORY_H */

bios/memory2.c
@@ -1,0 +1,102 @@
+/*
+ * memory2.c - Memory functions
+ *
+ * Copyright (C) 2016 The EmuTOS development team
+ *
+ * Authors:
+ *  VRI   Vincent Rivière
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
+ */
+
+/* #define ENABLE_KDEBUG */
+
+#include "config.h"
+#include "portab.h"
+#include "memory.h"
+#include "tosvars.h"
+#include "kprint.h"
+#include "machine.h"
+#include "cookie.h"
+#include "vectors.h"
+
+extern long xmaddalt(UBYTE *start, long size); /* found in bdos/mem.h */
+
+#if CONF_WITH_FASTRAM
+
+/* Detect hardware FastRAM size */
+static ULONG detect_fastram_size(void)
+{
+#if CONF_FASTRAM_SIZE
+    /* Fixed-size FastRAM */
+    return CONF_FASTRAM_SIZE;
+#else
+    UBYTE *addr;
+
+    /* By design, TT-like FastRAM is always in 32-bit address space */
+    if (!IS_BUS32)
+        return 0;
+
+#if CONF_WITH_TT_MMU
+    if (cookie_mch == MCH_TT)
+    {
+        /* On TT, FastRAM requires special refresh rate initialization */
+        init_tt_fastram();
+    }
+#endif /* CONF_WITH_TT_MMU */
+
+    /* Detect FastRAM size. Assume that the size is always a multiple of 1 MB.
+     * Try to read the last byte of each FastRAM megabyte. If it does not cause
+     * a Bus Error, assume that that megabyte is valid FastRAM.
+     * We artificially limit FastRAM detection to positive addresses to avoid
+     * shocking BDOS. */
+    for (addr = FASTRAM_START + 1024UL*1024 - 1;
+        (long)addr > 0; addr += 1024UL*1024)
+    {
+        if (!check_read_byte((long)addr))
+            break;
+    }
+
+    /* Valid FastRAM stops at the beginning of current megabyte */
+    return ((ULONG)addr & 0xfff00000) - (ULONG)FASTRAM_START;
+#endif /* !CONF_FASTRAM_SIZE */
+}
+
+#endif /* CONF_WITH_FASTRAM */
+
+/* TT-like FastRAM: Detect, initialize and add to BDOS */
+void fastram_init(void)
+{
+#if CONF_WITH_FASTRAM
+    if (ramvalid == RAMVALID_MAGIC)
+    {
+        /* Previous FastRAM settings were valid. Just trust them blindly. */
+    }
+    else
+    {
+        /* Previous FastRAM settings were invalid, detect them now. */
+        ULONG fastram_size = detect_fastram_size();
+
+        if (fastram_size)
+            ramtop = FASTRAM_START + fastram_size;
+        else
+            ramtop = NULL;
+
+        /* Always validate ramvalid even if ramtop == 0, like TOS 1.6+ */
+        ramvalid = RAMVALID_MAGIC;
+    }
+#else
+    /* Force no FastRAM */
+    ramtop = NULL;
+    ramvalid = RAMVALID_MAGIC;
+#endif
+
+    KDEBUG(("fastram_init(): ramtop=%p\n", ramtop));
+
+#if CONF_WITH_FASTRAM
+    /* Add eventual FastRAM to BDOS pool */
+    if (ramtop != NULL)
+        xmaddalt(FASTRAM_START, ramtop - FASTRAM_START);
+#endif
+}

Makefile
@@ -188,7 +188,7 @@ bios_src +=  memory.S processor.S vectors.S aciavecs.S bios.c xbios.c acsi.c \
              parport.c screen.c serport.c sound.c videl.c vt52.c xhdi.c \
              pmmu030.c 68040_pmmu.S \
              amiga.c amiga2.S aros.c aros2.S \
-             delay.c delayasm.S sd.c
+             delay.c delayasm.S sd.c memory2.c
 
 ifeq (1,$(COLDFIRE))
   bios_src += coldfire.c coldfire2.S spi.c

bios/memory.S
@@ -32,9 +32,8 @@
         .extern _warm_magic
         .extern _stktop
 
-        // functions for detecting hardware and CPU type
+        // functions for detecting hardware
         .extern _check_read_byte
-        .extern _detect_cpu
         .extern _bzero
 
         // ST-RAM settings
@@ -61,7 +60,7 @@ meminit:
 
 /*
  * The caller (either startup.S or comprimg.S) needs to have initialized sp
- * with a temporary supervisor stack (jsr _check_read_byte/_detect_cpu)
+ * with a temporary supervisor stack (for i.e. jsr _check_read_byte)
  * The stack will be overwritten and lost during clearing of memory
  */
 
@@ -585,104 +584,12 @@ stram_detected:
         move.l  d1, (a0)                // Next boot will not be a first boot
 #endif
 
-// ===========================================================================
-// ==== TT-RAM (FastRAM) detection (PES) =====================================
-// ===========================================================================
-//
-// This alternate RAM begins at 16MB boundary ($1000000)
-//
-// Inputs (#if CONF_WITH_FASTRAM):
-//   d3.l: old _ramvalid (#if CONF_WITH_FASTRAM)
-//   a3.l: old _ramtop (#if CONF_WITH_FASTRAM)
-
-        move.l  #0x1357BD13,d1          // _ramvalid magic
-
-#if !CONF_WITH_FASTRAM
-        // Fake FastRAM detection for no FastRAM
-        sub.l   a3, a3                  // _ramtop = 0
-        // Fall into fastram_detected
-#else // CONF_WITH_FASTRAM
-        cmp.l   d1,d3                   // old _ramvalid valid ?
-        beq     fastram_detected        // yes: skip detection
-
-#if CONF_FASTRAM_SIZE
-        // FastRAM detection for hardcoded FastRAM size
-        move.l  #0x1000000+CONF_FASTRAM_SIZE,a3    // fixed ramtop
-#else
-        /* clear MB counter */
-        moveq   #0, d6
-
-#ifndef __mcoldfire__
-        /* test CPU - FastRAM is possible on 68020+ (32-bit address space) */
-        jsr     _detect_cpu
-        cmp.w   #20,d0                  // 68020 or better?
-        blt     setramtop               // no -> skip the whole FastRAM stuff
-#endif
-
-#if CONF_WITH_TT_MMU
-        bsr     _init_tt_fastram
-#endif
-
-        /* Can we access FastRAM region? */
-        pea     0x1000400
-        jsr     _check_read_byte
-        addq.l  #4,sp
-        tst.w   d0
-        beq     setramtop
-
-#ifndef __mcoldfire__
-        /* Skip FastRAM detection if we only have a 24-bit address bus */
-        bsr     _detect_32bit_address_bus
-        tst.w   d0
-        beq     setramtop               // 24-bit addresses, no FastRAM
-#endif
-
-        /* No wrap around detected, let's scan for FastRAM: */
-        lea     0x1000000, a3           // start from 16MB boundary
-        move.l  #1024*1024, d3          // test by blocks of 1MB size
-loop:
-        lea     -1(a3,d3.l), a0         // last byte of current megabyte
-
-        // Currently, the BDOS memory management functions handle sizes
-        // as signed long integers. So limit detection to the positive range.
-        // We thereby limit FastRAM support to 2032 MB.
-        // This should be enough for most cases, anyway.
-        move.l  a0,d0
-        bmi.s   setramtop
-
-        pea     (a0)
-        jsr     _check_read_byte
-        addq.l  #4,sp
-        tst.w   d0
-        beq.s   setramtop
-        addq.l  #1, d6
-        add.l   d3, a3
-        bra.s   loop
-
-setramtop:
-        /* test MB counter */
-        tst.l   d6
-        bne.s   alter
-
-        /* no FastRAM found */
-        sub.l   a3, a3                  // _ramtop = 0
-alter:
-#endif // !CONF_FASTRAM_SIZE
-
-#endif // CONF_WITH_FASTRAM
-
-/*
- * At this point:
- *   a3.l = new _ramtop
- *   d1.l = _ramvalid magic
- *   d7.b = _first_boot
- */
-fastram_detected:
-        // Write FastRAM settings into system variables (in ST-RAM)
-        // Note that, like TOS 1.06 or TOS 2.06, we always set _ramvalid
-        // as valid even if _ramtop is 0.
+#if CONF_WITH_FASTRAM
+        // After clearing sysvars, restore old FastRAM settings.
+        // They will be used later for FastRAM initialization.
         move.l  a3, _ramtop             // set ramtop to size of found memory
-        move.l  d1, _ramvalid
+        move.l  d3, _ramvalid
+#endif
 
 // ===========================================================================
 // ==== This part is executed on any boot (first boot or reset) ==============

bios/processor.S
@@ -29,7 +29,6 @@
         .globl  _invalidate_data_cache
         .globl  _mcpu
         .globl  _fputype
-        .globl  _detect_cpu
 #if CONF_WITH_APOLLO_CORE
         .globl  _is_apollo_core
 #endif

bios/processor.h
@@ -87,8 +87,6 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
-long detect_cpu(void);
-
 extern void processor_init(void);
 /* invalidate_instruction_cache() is declared in include/biosext.h */
 extern void instruction_cache_kludge(void *start,long size);

commit 669ea39d080496cad1ba2858fb322bc99e60a353

Split TT-RAM detection and Alt-RAM initialization
This fixes a regression introduced in commit def7047.
The _FRB cookie was not properly installed, because
fill_cookie_jar() is called before altram_init().

Now the TT-RAM is detected in machine_detect(), before
fill_cookie_jar(). So the _FRB cookie is properly installed.

This can be tested with:
- TT hardware
- FastRAM
- ACSI disk (needs _FRB cookie)
Then run some program from disk.

bios/bios.c
@@ -362,11 +362,11 @@ static void bios_init(void)
     KDEBUG(("after osinit()\n"));
     boot_status |= DOS_AVAILABLE;   /* track progress */
 
-    /* Initialize all Alt-RAM.
-     * This always intializes the TT-RAM system variables,
-     * even if there is no TT-RAM. */
+#if CONF_WITH_ALT_RAM
+    /* Add Alt-RAM to BDOS pool */
     KDEBUG(("altram_init()\n"));
     altram_init();
+#endif
 
     /* Enable VBL processing */
     vblsem = 1;

bios/machine.c
@@ -33,6 +33,7 @@
 #include "delay.h"
 #include "mfp.h"
 #include "scc.h"
+#include "memory.h"
 #include "coldfire.h"
 #ifdef MACHINE_AMIGA
 #include "amiga.h"
@@ -402,6 +403,11 @@ void machine_detect(void)
 #ifdef MACHINE_AMIGA
     amiga_machine_detect();
 #endif
+
+    /* Detect TT-RAM and set up ramtom/ramvalid */
+    KDEBUG(("ttram_detect()\n"));
+    ttram_detect();
+
     detect_video();
     detect_serial_ports();
 #if CONF_WITH_VME

bios/memory.h
@@ -27,6 +27,12 @@ void set_ttram_refresh_rate(void);
 
 #endif /* CONF_WITH_TT_MMU */
 
+void ttram_detect(void);
+
+#if CONF_WITH_ALT_RAM
+
 void altram_init(void);
+
+#endif /* CONF_WITH_ALT_RAM */
 
 #endif /* MEMORY_H */

bios/memory2.c
@@ -68,8 +68,8 @@ static ULONG detect_ttram_size(void)
 
 #endif /* CONF_WITH_TTRAM */
 
-/* TT-RAM: Detect, initialize and add to BDOS */
-static void ttram_init(void)
+/* Detect TT-RAM and set ramtom/ramvalid */
+void ttram_detect(void)
 {
 #if CONF_WITH_TTRAM
     if (ramvalid == RAMVALID_MAGIC)
@@ -95,23 +95,19 @@ static void ttram_init(void)
     ramvalid = RAMVALID_MAGIC;
 #endif
 
-    KDEBUG(("ttram_init(): ramtop=%p\n", ramtop));
+    KDEBUG(("ttram_detect(): ramtop=%p\n", ramtop));
+}
 
+#if CONF_WITH_ALT_RAM
+
+/* Initialize all Alt-RAM */
+void altram_init(void)
+{
 #if CONF_WITH_TTRAM
     /* Add eventual TT-RAM to BDOS pool */
     if (ramtop != NULL)
         xmaddalt(TTRAM_START, ramtop - TTRAM_START);
 #endif
-}
-
-/* Detect and initialize all Alt-RAM */
-void altram_init(void)
-{
-    /* Always intialize the TT-RAM system variables */
-    KDEBUG(("ttram_init()\n"));
-    ttram_init();
-
-#if CONF_WITH_ALT_RAM
 
 #if CONF_WITH_MONSTER
     /* Add MonSTer alt-RAM detected in machine.c */
@@ -139,6 +135,6 @@ void altram_init(void)
     KDEBUG(("amiga_add_alt_ram()\n"));
     amiga_add_alt_ram();
 #endif
+}
 
 #endif /* CONF_WITH_ALT_RAM */
-}