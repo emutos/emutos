emutos/emutos

commit 669ea39d080496cad1ba2858fb322bc99e60a353

Split TT-RAM detection and Alt-RAM initialization
This fixes a regression introduced in commit def7047.
The _FRB cookie was not properly installed, because
fill_cookie_jar() is called before altram_init().

Now the TT-RAM is detected in machine_detect(), before
fill_cookie_jar(). So the _FRB cookie is properly installed.

This can be tested with:
- TT hardware
- FastRAM
- ACSI disk (needs _FRB cookie)
Then run some program from disk.

bios/memory2.c
@@ -68,8 +68,8 @@ static ULONG detect_ttram_size(void)
 
 #endif /* CONF_WITH_TTRAM */
 
-/* TT-RAM: Detect, initialize and add to BDOS */
-static void ttram_init(void)
+/* Detect TT-RAM and set ramtom/ramvalid */
+void ttram_detect(void)
 {
 #if CONF_WITH_TTRAM
     if (ramvalid == RAMVALID_MAGIC)
@@ -95,23 +95,19 @@ static void ttram_init(void)
     ramvalid = RAMVALID_MAGIC;
 #endif
 
-    KDEBUG(("ttram_init(): ramtop=%p\n", ramtop));
+    KDEBUG(("ttram_detect(): ramtop=%p\n", ramtop));
+}
 
+#if CONF_WITH_ALT_RAM
+
+/* Initialize all Alt-RAM */
+void altram_init(void)
+{
 #if CONF_WITH_TTRAM
     /* Add eventual TT-RAM to BDOS pool */
     if (ramtop != NULL)
         xmaddalt(TTRAM_START, ramtop - TTRAM_START);
 #endif
-}
-
-/* Detect and initialize all Alt-RAM */
-void altram_init(void)
-{
-    /* Always intialize the TT-RAM system variables */
-    KDEBUG(("ttram_init()\n"));
-    ttram_init();
-
-#if CONF_WITH_ALT_RAM
 
 #if CONF_WITH_MONSTER
     /* Add MonSTer alt-RAM detected in machine.c */
@@ -139,6 +135,6 @@ void altram_init(void)
     KDEBUG(("amiga_add_alt_ram()\n"));
     amiga_add_alt_ram();
 #endif
+}
 
 #endif /* CONF_WITH_ALT_RAM */
-}

bios/bios.c
@@ -362,11 +362,11 @@ static void bios_init(void)
     KDEBUG(("after osinit()\n"));
     boot_status |= DOS_AVAILABLE;   /* track progress */
 
-    /* Initialize all Alt-RAM.
-     * This always intializes the TT-RAM system variables,
-     * even if there is no TT-RAM. */
+#if CONF_WITH_ALT_RAM
+    /* Add Alt-RAM to BDOS pool */
     KDEBUG(("altram_init()\n"));
     altram_init();
+#endif
 
     /* Enable VBL processing */
     vblsem = 1;

bios/machine.c
@@ -33,6 +33,7 @@
 #include "delay.h"
 #include "mfp.h"
 #include "scc.h"
+#include "memory.h"
 #include "coldfire.h"
 #ifdef MACHINE_AMIGA
 #include "amiga.h"
@@ -402,6 +403,11 @@ void machine_detect(void)
 #ifdef MACHINE_AMIGA
     amiga_machine_detect();
 #endif
+
+    /* Detect TT-RAM and set up ramtom/ramvalid */
+    KDEBUG(("ttram_detect()\n"));
+    ttram_detect();
+
     detect_video();
     detect_serial_ports();
 #if CONF_WITH_VME

bios/memory.h
@@ -27,6 +27,12 @@ void set_ttram_refresh_rate(void);
 
 #endif /* CONF_WITH_TT_MMU */
 
+void ttram_detect(void);
+
+#if CONF_WITH_ALT_RAM
+
 void altram_init(void);
+
+#endif /* CONF_WITH_ALT_RAM */
 
 #endif /* MEMORY_H */

commit fd534498fbe31b2e3edf5641aaefe6ab41ccfd91

Fix TT-RAM refresh rate initialization
This fixes a regression introduced in commit 669ea39.
Cookies can't be used to detect the machine,
because they are initialized later in fill_cookie_jar().
As a result, TT was never detected.

bios/memory2.c
@@ -18,7 +18,6 @@
 #include "tosvars.h"
 #include "kprint.h"
 #include "machine.h"
-#include "cookie.h"
 #include "vectors.h"
 #ifdef MACHINE_AMIGA
 #include "amiga.h"
@@ -42,7 +41,8 @@ static ULONG detect_ttram_size(void)
         return 0;
 
 #if CONF_WITH_TT_MMU
-    if (cookie_mch == MCH_TT)
+    /* FIXME: This should only be done on TT, but we don't know yet if we are
+     * running on TT or not. Anyway, this is harmless on other hardware. */
     {
         /* On TT, TT-RAM requires special refresh rate initialization */
         set_ttram_refresh_rate();