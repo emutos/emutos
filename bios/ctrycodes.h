5b4dd02f1f4accfecd00db0e9cf74cfa859e8878
Thorsten Otto <admin@tho-otto.de>
2017-03-03 18:05:30 +0100

    Update some links to point to their new location on github

--- bios/ctrycodes.h
+++ bios/ctrycodes.h
@@ -40,8 +40,8 @@
  * Before defining new ones, be sure to register them in tos.hyp:
  * http://toshyp.atari.org/en/003007.html#Cookie_2C_20_AKP
  * Note that those codes are also used in FreeMiNT/XaAES and must match:
- * http://sparemint.atariforge.net/cgi-bin/cvsweb/freemint/sys/keyboard.c?rev=1.114&content-type=text/x-cvsweb-markup
- * http://sparemint.atariforge.net/cgi-bin/cvsweb/freemint/xaaes/src.km/init.c?rev=1.125&content-type=text/x-cvsweb-markup
+ * https://github.com/freemint/freemint/blob/master/sys/keyboard.c
+ * https://github.com/freemint/freemint/blob/master/xaaes/src.km/init.c
  */
 #define COUNTRY_RU 19   /* Russia */
 #define COUNTRY_GR 31   /* Greece */
