emutos/emutos

commit 17a77aecef767990bd2d88f829f1e9f30e201a4b

Fix bug in desktop floppy formatter
The boot sector was written by Rwabs(); this failed when writing to
a floppy that had not been accessed previously.  It is now written
using Flopwr().  Thanks to Thorsten Otto for pointing this out.

desk/desksupp.c
@@ -1078,7 +1078,7 @@ static WORD write_boot(BYTE *buf, WORD disktype, WORD drive)
     Protobt((LONG)buf, RANDOM_SERIAL, disktype, 0);
     *buf = 0xe9;        /* DOS compatibility */
 
-    return Rwabs(WRITESEC, (LONG)buf, 1, 0, drive, 0);
+    return Flopwr((LONG)buf, 0L, drive, 1, 0, 0, 1);
 }
 
 /*

commit b9516e2fb4baa4ee0ace8ac318378eadb51e7045

Ensure floppy formatter triggers mediachange
Formatting a floppy should force mediachange but did not.  This
could cause several problems; for example, if a diskette was
formatted as HD, then reformatted as DD without removing the
diskette, the free space reported was the free space after HD
formatting.  This bug was introduced by commit 17a77ae.
Thanks to Thorsten Otto for reporting this.

desk/desksupp.c
@@ -1078,7 +1078,7 @@ static WORD write_boot(BYTE *buf, WORD disktype, WORD drive)
     Protobt((LONG)buf, RANDOM_SERIAL, disktype, 0);
     *buf = 0xe9;        /* DOS compatibility */
 
-    return Flopwr((LONG)buf, 0L, drive, 1, 0, 0, 1);
+    return Rwabs(WRITESEC, (LONG)buf, 1, 0, drive, 0);
 }
 
 /*