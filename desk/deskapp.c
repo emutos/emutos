emutos/emutos

commit b88d64334747b7aeb7fb6a9e70f76b437114f885

Allow 'Install icon' to install window icons
This allows window icons (the icons displayed within a window in
icon mode) to be customised.  The interface support is modelled
after TOS2/TOS3.  Note: at this time, only folder icons are
preserved by 'Save desktop'.

desk/deskins.c
@@ -30,6 +30,7 @@
 #include "obdefs.h"
 #include "gsxdefs.h"
 #include "gemdos.h"
+#include "dos.h"
 
 #include "deskapp.h"
 #include "deskfpd.h"
@@ -632,24 +633,296 @@ static WORD install_desktop_icon(ANODE *pa)
 }
 
 
+#if CONF_WITH_WINDOW_ICONS
+
+#define NUM_EXTS    5
+static const BYTE *exec_ext[NUM_EXTS] = { "TOS", "TTP", "PRG", "APP", "GTP" };
+
 /*
- * install icon on desktop or (in future) window
+ * test if file is executable, based on extension
+ */
+static BOOL is_executable(BYTE *filename)
+{
+    WORD i, n;
+
+    n = strlen(filename);
+    if (n < 5)          /* must be at least A.XYZ */
+        return FALSE;
+
+    filename += n - 3;  /* point to start of extension */
+
+    for (i = 0; i < NUM_EXTS; i++)
+        if (strcmp(filename,exec_ext[i]) == 0)
+            return TRUE;
+
+    return FALSE;
+}
+
+
+/*
+ * set icon numbers into ANODE
+ */
+static void set_icon(ANODE *pa, WORD icon)
+{
+    pa->a_aicon = NIL;
+    pa->a_dicon = icon;
+
+    if (is_executable(pa->a_pdata))
+        pa->a_aicon = icon;     /* this marks it as an executable file */
+}
+
+
+/*
+ * allocate ANODE for window icon
+ */
+static ANODE *allocate_window_anode(WORD type)
+{
+    ANODE *pa;
+
+    pa = app_alloc(TRUE);
+    if (!pa)
+        return NULL;
+
+    pa->a_flags = 0;
+    pa->a_funkey = 0;
+    pa->a_letter = '\0';
+    pa->a_type = type;
+    pa->a_obid = 0;
+    pa->a_pappl = "";
+    pa->a_pdata = "";
+    pa->a_pargs = "";
+    pa->a_aicon = NIL;
+    pa->a_dicon = (type==AT_ISFOLD) ? IG_FOLDER : ID_GENERIC_ALT;
+    pa->a_xspot = 0;
+    pa->a_yspot = 0;
+
+    return pa;
+}
+
+
+/*
+ * install window icon
+ *
+ * returns:
+ *  >0  icon has been changed
+ *  0   icon not changed (user said skip)
+ *  <0  icon not changed (user said cancel)
+ */
+
+//FIXME: add I/N support to app_save(), app_start()
+
+static WORD install_window_icon(FNODE *pf)
+{
+    BOOL identical;
+    WORD edit_start, exitobj, dummy, type;
+    WORD new_icon, curr_icon;
+    WORD change = 0;
+    OBJECT *tree;
+    ANODE *pa;
+    BYTE curr_name[LEN_ZFNAME], new_name[LEN_ZFNAME], temp[LEN_ZFNAME];
+
+    /*
+     * deselect all objects
+     */
+    tree = (OBJECT *)G.a_trees[ADINSWIN];
+    deselect_all(tree);
+
+    /*
+     * initialise pointer to ANODE
+     */
+    pa = pf ? pf->f_pa : NULL;
+#if 0
+    if (pf)
+    {
+        pa = pf->f_pa;
+        generic = is_generic(pa);
+    }
+    if (generic)
+        pa = NULL;
+    if (!pa)
+    {
+        pa = app_alloc(TRUE);
+        if (!pa)
+            return -1;              /* don't try any more */
+        allocated = TRUE;
+        pa->a_flags = 0;
+        pa->a_funkey = 0;
+        pa->a_letter = '\0';
+        if (pf)
+            pa->a_type = (pf->f_attr&F_SUBDIR) ? AT_ISFOLD : AT_ISFILE;
+        else pa->a_type = AT_ISFILE;
+        pa->a_obid = 0;
+        pa->a_pappl = "";
+        pa->a_pdata = "";
+        pa->a_pargs = "";
+        pa->a_aicon = NIL;
+        pa->a_dicon = (pa->a_type==AT_ISFOLD) ? IG_FOLDER : ID_GENERIC_ALT;
+        pa->a_xspot = 0;
+        pa->a_yspot = 0;
+    }
+#endif
+
+    /*
+     * fill in dialog
+     */
+    if (pf)
+    {
+        /*
+         * handle existing FNODE:
+         *  the name is fixed & not editable
+         *  the file type (file or folder) is fixed & not changeable
+         */
+        strcpy(curr_name,pf->f_name);
+        tree[IW_NAME].ob_flags &= ~EDITABLE;
+        edit_start = 0;
+        if (pa->a_type == AT_ISFILE)
+            tree[IW_FILE].ob_state |= SELECTED;
+        else
+            tree[IW_FOLD].ob_state |= SELECTED;
+        tree[IW_FILE].ob_state |= DISABLED;
+        tree[IW_FOLD].ob_state |= DISABLED;
+    }
+    else
+    {
+        curr_name[0] = '\0';
+        tree[IW_NAME].ob_flags |= EDITABLE;
+        edit_start = IW_NAME;
+        tree[IW_FILE].ob_state |= SELECTED;
+        tree[IW_FILE].ob_state &= ~DISABLED;
+        tree[IW_FOLD].ob_state &= ~DISABLED;
+    }
+    fmt_str(curr_name, temp);
+    inf_sset((LONG)tree, IW_NAME, temp);
+
+    curr_icon = pa ? pa->a_dicon : 0;
+    if (curr_icon < 0)
+        curr_icon = 0;
+    else if (curr_icon > NUM_IBLKS-1)
+        curr_icon = NUM_IBLKS - 1;
+    new_icon = curr_icon;
+
+    insert_icon(tree, IW_ICON, curr_icon);
+
+    show_hide(FMD_START, (LONG)tree);
+    while(1)
+    {
+        exitobj = form_do((LONG)tree, edit_start) & 0x7fff;
+
+        switch(exitobj)
+        {
+        case IW_UP:             /* handle button up */
+            new_icon = (curr_icon > 0) ? curr_icon-1 : 0;
+            break;
+        case IW_DOWN:           /* handle button down */
+            new_icon = (curr_icon < NUM_IBLKS-1) ? curr_icon+1 : NUM_IBLKS-1;
+            break;
+        case IW_INST:           /* (re)install an icon */
+        case IW_REMV:           /* remove an icon */
+            inf_sget((LONG)tree, IW_NAME, temp);
+            unfmt_str(temp, new_name);
+            if (new_name[0] == '\0')    /* treat as skip */
+                break;
+
+            type = (tree[IW_FILE].ob_state & SELECTED) ? AT_ISFILE : AT_ISFOLD;
+            if (!pf)
+                pa = app_afind_by_name(type, "", new_name, &dummy);
+            else pa = pf->f_pa;
+
+            /* set flag if the names are exactly the same */
+            identical = !strcmp(pa->a_pdata,new_name);
+
+            if (exitobj == IW_INST)
+            {
+                if (!identical)
+                {
+                    pa = allocate_window_anode(type);
+                    if (!pa)
+                    {
+                        change = -1;    /* cancel further installs */
+                        break;
+                    }
+                    scan_str(new_name, &pa->a_pdata);
+                    KDEBUG(("Installed window icon for %s\n",pa->a_pdata));
+                }
+                set_icon(pa, curr_icon);
+            }
+            else        /* IW_REMV */
+            {
+                if (!identical)
+                {
+                    fun_alert(1,STNOMTCH);  /* no matching file type */
+                    break;
+                }
+                KDEBUG(("Removed window icon for %s\n",pa->a_pdata));
+                app_free(pa);
+            }
+            change = 1;
+            break;
+        case IW_CNCL:           /* cancel further installs */
+            change = -1;
+            /* drop thru */
+        case IW_SKIP:           /* skip this application */
+            break;
+        }
+        if ((exitobj != IW_UP) && (exitobj != IW_DOWN))
+            break;
+        tree[exitobj].ob_state &= ~SELECTED;
+
+        if (new_icon != curr_icon)
+        {
+            curr_icon = new_icon;
+            insert_icon(tree, IW_ICON, curr_icon);
+        }
+        draw_fld((LONG)tree, IW_IBOX);
+    }
+    show_hide(FMD_FINISH, (LONG)tree);
+
+    return change;
+}
+#endif
+
+
+/*
+ * install icon (desktop or window)
  *
  * returns:
  *  >0      need to rebuild desktop
  *  0       no need to rebuild display
- *  <0      need to rebuild windows (future update)
+ *  <0      need to rebuild windows
  */
 WORD ins_icon(WORD sobj)
 {
     WORD rebuild = 0;
     WORD rc;
     ANODE *pa;
+#if CONF_WITH_WINDOW_ICONS
+    WNODE *pw;
+    FNODE *pf;
+#endif
 
     if (!sobj)              /* no icon selected */
     {
+#if CONF_WITH_WINDOW_ICONS
+        WORD icon_type;
+
+        icon_type = fun_alert(1, STICNTYP);
+        switch(icon_type)
+        {
+        case 1:         /* desktop */
+            if (install_desktop_icon(NULL) > 0)
+                return 1;
+            break;
+        case 2:         /* window */
+            if (install_window_icon(NULL) > 0)
+                return -1;
+            break;
+        default:        /* cancel */
+            break;
+        }
+#else
         if (install_desktop_icon(NULL) > 0)
             return 1;
+#endif
         return 0;
     }
 
@@ -672,6 +945,28 @@ WORD ins_icon(WORD sobj)
         }
         return rebuild;
     }
+
+#if CONF_WITH_WINDOW_ICONS
+    /*
+     * handle one or more window icons
+     */
+    pw = win_find(G.g_cwin);    /* get WNODE for current window */
+    if (pw)
+    {
+        for (pf = pw->w_path->p_flist; pf; pf = pf->f_next)
+        {
+            if (pf->f_obid == NIL)
+                continue;
+            if (!(G.g_screen[pf->f_obid].ob_state & SELECTED))
+                continue;
+            rc = install_window_icon(pf);
+            if (rc > 0)
+                rebuild--;
+            if (rc < 0)
+                break;
+        }
+    }
+#endif
 
     return rebuild;
 }

desk/deskmain.c
@@ -505,6 +505,13 @@ static WORD do_optnmenu(WORD item)
             app_blddesk();
             do_wredraw(0, G.g_xdesk, G.g_ydesk, G.g_wdesk, G.g_hdesk);
         }
+#if CONF_WITH_WINDOW_ICONS
+        else if (rebld < 0)
+        {
+            win_bdall();
+            win_shwall();
+        }
+#endif
         break;
     case RICNITEM:
         if (curr)

commit 050d09237918682c97708d2933d6832c20891d09

Fix bug with handling of installed window icons
If an icon was associated with a program file via "Install icon",
that file was subsequently treated as a non-executable file, and so
could not be launched by the desktop.  This problem has existed since
"Install icon" was introduced by commit b88d643 (in release 0.9.7).
Problem reported by Dima Sobolev.

desk/deskapp.c
@@ -1151,11 +1151,14 @@ ANODE *app_afind_by_id(WORD obid)
 /*
  *  Find ANODE by name & type
  *
+ *  Note: ANODEs with a flag bit matching the 'ignore' parameter are
+ *  ignored during the search. 
+ *
  *  Returns match type in *pisapp:
  *      TRUE if name matches application name
  *      FALSE if name matches data name
  */
-ANODE *app_afind_by_name(WORD atype, BYTE *pspec, BYTE *pname, WORD *pisapp)
+ANODE *app_afind_by_name(WORD atype, WORD ignore, BYTE *pspec, BYTE *pname, WORD *pisapp)
 {
     ANODE *pa;
     WORD match;
@@ -1166,7 +1169,9 @@ ANODE *app_afind_by_name(WORD atype, BYTE *pspec, BYTE *pname, WORD *pisapp)
 
     for (pa = G.g_ahead; pa; pa = pa->a_next)
     {
-        if ((pa->a_type == atype) && !(pa->a_flags & AF_ISDESK))
+        if (pa->a_flags & ignore)
+            continue;
+        if (pa->a_type == atype)
         {
             if (wildcmp(pa->a_pdata, pname))
             {

desk/deskapp.h
@@ -140,7 +140,7 @@ void app_start(void);
 void app_save(WORD todisk);
 void app_blddesk(void);
 ANODE *app_afind_by_id(WORD obid);
-ANODE *app_afind_by_name(WORD atype, BYTE *pspec, BYTE *pname, WORD *pisapp);
+ANODE *app_afind_by_name(WORD atype, WORD ignore, BYTE *pspec, BYTE *pname, WORD *pisapp);
 void nomem_alert(void);
 
 #endif  /* _DESKAPP_H */

desk/deskins.c
@@ -838,7 +838,7 @@ static WORD install_window_icon(FNODE *pf)
 
             type = (tree[IW_FILE].ob_state & SELECTED) ? AT_ISFILE : AT_ISFOLD;
             if (!pf)
-                pa = app_afind_by_name(type, "", new_name, &dummy);
+                pa = app_afind_by_name(type, AF_ISDESK, "", new_name, &dummy);
             else pa = pf->f_pa;
 
             if (!pa)                /* can't happen ... */

desk/desksupp.c
@@ -666,7 +666,7 @@ WORD do_aopen(ANODE *pa, WORD isapp, WORD curr, BYTE *pathname, BYTE *pname)
             remove_locate_shortcut(curr);
             return done;
         }
-        tmp = app_afind_by_name(AT_ISFILE, pathname, pname, &isapp);
+        tmp = app_afind_by_name(AT_ISFILE, AF_ISDESK|AF_WINDOW, pathname, pname, &isapp);
         if (tmp)
             pa = tmp;
     }
@@ -1399,7 +1399,7 @@ ANODE *i_find(WORD wh, WORD item, FNODE **ppf, WORD *pisapp)
             pf = fpd_ofind(pw->w_path->p_flist, item);
             if (pf)
                 pa = app_afind_by_name((pf->f_attr&F_SUBDIR)?AT_ISFOLD:AT_ISFILE,
-                                        pw->w_path->p_spec, pf->f_name, &isapp);
+                            AF_ISDESK|AF_WINDOW, pw->w_path->p_spec, pf->f_name, &isapp);
         }
     }
 

desk/deskwin.c
@@ -287,7 +287,7 @@ static void win_ocalc(WNODE *pwin, WORD wfit, WORD hfit, FNODE **ppstart)
 static void win_icalc(FNODE *pfnode, WNODE *pwin)
 {
     pfnode->f_pa = app_afind_by_name((pfnode->f_attr&F_SUBDIR) ? AT_ISFOLD : AT_ISFILE,
-                                    pwin->w_path->p_spec, pfnode->f_name, &pfnode->f_isap);
+                        AF_ISDESK, pwin->w_path->p_spec, pfnode->f_name, &pfnode->f_isap);
 }
 
 