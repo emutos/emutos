emutos/emutos

commit 050d09237918682c97708d2933d6832c20891d09

Fix bug with handling of installed window icons
If an icon was associated with a program file via "Install icon",
that file was subsequently treated as a non-executable file, and so
could not be launched by the desktop.  This problem has existed since
"Install icon" was introduced by commit b88d643 (in release 0.9.7).
Problem reported by Dima Sobolev.

desk/deskins.c
@@ -838,7 +838,7 @@ static WORD install_window_icon(FNODE *pf)
 
             type = (tree[IW_FILE].ob_state & SELECTED) ? AT_ISFILE : AT_ISFOLD;
             if (!pf)
-                pa = app_afind_by_name(type, "", new_name, &dummy);
+                pa = app_afind_by_name(type, AF_ISDESK, "", new_name, &dummy);
             else pa = pf->f_pa;
 
             if (!pa)                /* can't happen ... */

desk/deskapp.c
@@ -1151,11 +1151,14 @@ ANODE *app_afind_by_id(WORD obid)
 /*
  *  Find ANODE by name & type
  *
+ *  Note: ANODEs with a flag bit matching the 'ignore' parameter are
+ *  ignored during the search. 
+ *
  *  Returns match type in *pisapp:
  *      TRUE if name matches application name
  *      FALSE if name matches data name
  */
-ANODE *app_afind_by_name(WORD atype, BYTE *pspec, BYTE *pname, WORD *pisapp)
+ANODE *app_afind_by_name(WORD atype, WORD ignore, BYTE *pspec, BYTE *pname, WORD *pisapp)
 {
     ANODE *pa;
     WORD match;
@@ -1166,7 +1169,9 @@ ANODE *app_afind_by_name(WORD atype, BYTE *pspec, BYTE *pname, WORD *pisapp)
 
     for (pa = G.g_ahead; pa; pa = pa->a_next)
     {
-        if ((pa->a_type == atype) && !(pa->a_flags & AF_ISDESK))
+        if (pa->a_flags & ignore)
+            continue;
+        if (pa->a_type == atype)
         {
             if (wildcmp(pa->a_pdata, pname))
             {

desk/deskapp.h
@@ -140,7 +140,7 @@ void app_start(void);
 void app_save(WORD todisk);
 void app_blddesk(void);
 ANODE *app_afind_by_id(WORD obid);
-ANODE *app_afind_by_name(WORD atype, BYTE *pspec, BYTE *pname, WORD *pisapp);
+ANODE *app_afind_by_name(WORD atype, WORD ignore, BYTE *pspec, BYTE *pname, WORD *pisapp);
 void nomem_alert(void);
 
 #endif  /* _DESKAPP_H */

desk/desksupp.c
@@ -666,7 +666,7 @@ WORD do_aopen(ANODE *pa, WORD isapp, WORD curr, BYTE *pathname, BYTE *pname)
             remove_locate_shortcut(curr);
             return done;
         }
-        tmp = app_afind_by_name(AT_ISFILE, pathname, pname, &isapp);
+        tmp = app_afind_by_name(AT_ISFILE, AF_ISDESK|AF_WINDOW, pathname, pname, &isapp);
         if (tmp)
             pa = tmp;
     }
@@ -1399,7 +1399,7 @@ ANODE *i_find(WORD wh, WORD item, FNODE **ppf, WORD *pisapp)
             pf = fpd_ofind(pw->w_path->p_flist, item);
             if (pf)
                 pa = app_afind_by_name((pf->f_attr&F_SUBDIR)?AT_ISFOLD:AT_ISFILE,
-                                        pw->w_path->p_spec, pf->f_name, &isapp);
+                            AF_ISDESK|AF_WINDOW, pw->w_path->p_spec, pf->f_name, &isapp);
         }
     }
 

desk/deskwin.c
@@ -287,7 +287,7 @@ static void win_ocalc(WNODE *pwin, WORD wfit, WORD hfit, FNODE **ppstart)
 static void win_icalc(FNODE *pfnode, WNODE *pwin)
 {
     pfnode->f_pa = app_afind_by_name((pfnode->f_attr&F_SUBDIR) ? AT_ISFOLD : AT_ISFILE,
-                                    pwin->w_path->p_spec, pfnode->f_name, &pfnode->f_isap);
+                        AF_ISDESK, pwin->w_path->p_spec, pfnode->f_name, &pfnode->f_isap);
 }
 
 

commit 80f6faf304fe9e293b202da9b85cd75d65e331ab

Install correct icon for desktop shortcut
When a desktop shortcut was created, the icon used was the default
icon for the file, rather than the current installed icon.  This bug
was introduced by commit 050d092.

desk/deskins.c
@@ -1021,7 +1021,7 @@ WORD rmv_icon(WORD sobj)
 void ins_shortcut(WORD wh, WORD mx, WORD my)
 {
     BYTE pathname[MAXPATHLEN], *p, *q;
-    WORD sobj, x, y;
+    WORD sobj, x, y, dummy;
     ANODE *pa, *newpa;
     FNODE *pf;
     WNODE *pw;
@@ -1071,6 +1071,16 @@ void ins_shortcut(WORD wh, WORD mx, WORD my)
         newpa->a_xspot = x;
         newpa->a_yspot = y;
         ins_posdisk(x, y, &x, &y);  /* update position for next icon */
+        /*
+         * override the default icon with an installed icon (if it exists)
+         */
+        pa = app_afind_by_name((pf->f_attr&F_SUBDIR)?AT_ISFOLD:AT_ISFILE,
+                        AF_ISDESK, pw->w_path->p_spec, pf->f_name, &dummy);
+        if (pa)                     /* paranoia */
+        {
+            newpa->a_aicon = pa->a_aicon;
+            newpa->a_dicon = pa->a_dicon;
+        }
     }
 
     app_blddesk();