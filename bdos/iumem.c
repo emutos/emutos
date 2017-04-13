ab1b331f3b1a1c9c91106145f69dc5ae7c5ae36a
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-04-13 22:48:21 +0200

    Remove bogus Authors: xxx <xxx@xxx>

--- bdos/iumem.c
+++ bdos/iumem.c
@@ -4,9 +4,6 @@
  * Copyright (C) 2001 Lineo, Inc.
  *               2013-2017 The EmuTOS development team
  *
- * Authors:
- *  xxx <xxx@xxx>
- *
  * This file is distributed under the GPL, version 2 or at your
  * option any later version.  See doc/license.txt for details.
  */
