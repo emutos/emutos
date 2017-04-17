emutos/emutos

commit e579ffbd609f18691cad9fcc22800a100ae6bbb1

Rewrite arrow/function key handling in desktop
This saves ROM space and makes the code more readable.

desk/deskmain.c
@@ -124,6 +124,24 @@ static const BYTE     ILL_NOTOP[] = {NFOLITEM,CLOSITEM,CLSWITEM,0};
 static const BYTE     ILL_DESKTOP[] = {NFOLITEM,CLOSITEM,CLSWITEM,ICONITEM,
                                 NAMEITEM,DATEITEM,SIZEITEM,TYPEITEM,0};
 
+/*
+ * table to map the keyboard arrow character to the corresponding
+ * value to put in msg[3] of the WM_ARROWED msg
+ *
+ * the table consists of pairs of values: { scancode, value }
+ */
+static const WORD arrow_table[] =
+{
+    SHIFT_ARROW_UP, WA_UPPAGE,
+    SHIFT_ARROW_DOWN, WA_DNPAGE,
+    ARROW_UP, WA_UPLINE,
+    ARROW_DOWN, WA_DNLINE,
+    ARROW_LEFT, WA_LFLINE,
+    ARROW_RIGHT, WA_RTLINE,
+    0
+};
+
+
 #if CONF_WITH_EASTER_EGG
 /* easter egg */
 static const WORD  freq[]=
@@ -659,9 +677,31 @@ static void kbd_arrow(WORD type)
 
 
 /*
+ * check for arrow key & handle appropriately
+ *
+ * return TRUE iff matching key found & processed
+ */
+static BOOL check_arrow_key(WORD thechar)
+{
+    const WORD *p;
+
+    for (p = arrow_table; *p; p += 2)
+    {
+        if (*p == thechar)
+        {
+            kbd_arrow(*(p+1));
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+
+/*
  * search ANODEs for matching function key and launch corresponding application
  */
-static WORD process_funkey(WORD funkey)
+static BOOL process_funkey(WORD funkey)
 {
     ANODE *pa;
     BYTE pathname[MAXPATHLEN];
@@ -684,13 +724,36 @@ static WORD process_funkey(WORD funkey)
 }
 
 
+/*
+ * check for function key & handle appropriately
+ *
+ * return TRUE iff matching key found & processed
+ */
+static BOOL check_function_key(WORD thechar)
+{
+    WORD funkey;
+
+    if ((thechar >= FUNKEY_01) && (thechar <= FUNKEY_10))
+        funkey = ((thechar-FUNKEY_01) >> 8) + 1;
+    else if ((thechar >= FUNKEY_11) && (thechar <= FUNKEY_20))
+        funkey = ((thechar-FUNKEY_11) >> 8) + 11;
+    else return FALSE;
+
+    return process_funkey(funkey);
+}
+
+
 static WORD hndl_kbd(WORD thechar)
 {
     WNODE *pw;
-    WORD done, funkey;
+    WORD done = FALSE;
     WORD title = -1, item;
 
-    done = FALSE;
+    if (check_arrow_key(thechar))
+        return done;
+
+    if (check_function_key(thechar))
+        return done;
 
     switch(thechar)
     {
@@ -766,34 +829,6 @@ static WORD hndl_kbd(WORD thechar)
         }
         break;
 #endif
-    case SHIFT_ARROW_UP:
-        kbd_arrow(WA_UPPAGE);
-        break;
-    case SHIFT_ARROW_DOWN:
-        kbd_arrow(WA_DNPAGE);
-        break;
-    case ARROW_UP:
-        kbd_arrow(WA_UPLINE);
-        break;
-    case ARROW_DOWN:
-        kbd_arrow(WA_DNLINE);
-        break;
-    case ARROW_LEFT:
-        kbd_arrow(WA_LFLINE);
-        break;
-    case ARROW_RIGHT:
-        kbd_arrow(WA_RTLINE);
-        break;
-    default:
-        if ((thechar & 0xff) != 0)      /* can't be a function key */
-            break;
-        funkey = 0;
-        if ((thechar >= FUNKEY_01) && (thechar <= FUNKEY_10))
-            funkey = ((thechar-FUNKEY_01) >> 8) + 1;
-        else if ((thechar >= FUNKEY_11) && (thechar <= FUNKEY_20))
-            funkey = ((thechar-FUNKEY_11) >> 8) + 11;
-        if (funkey)
-            done = process_funkey(funkey);
     }
 
     /*

commit bb4c60862a9307914e26f58a67ffeb3de9467bcb

Fix bug w/ installed application & function key
An installed graphics (APP/PRG/GTP) application was not launched by
the function key assigned to the application.  This problem did not
occur for installed TOS/TTP applications.
This bug was introduced by commit e579ffb.

desk/deskmain.c
@@ -709,7 +709,7 @@ static BOOL check_arrow_key(WORD thechar)
 /*
  * search ANODEs for matching function key and launch corresponding application
  */
-static BOOL process_funkey(WORD funkey)
+static WORD process_funkey(WORD funkey)
 {
     ANODE *pa;
     BYTE pathname[MAXPATHLEN];
@@ -724,20 +724,22 @@ static BOOL process_funkey(WORD funkey)
         pfname = filename_start(pa->a_pappl);
         /* copy pathname including trailing backslash */
         strlcpy(pathname,pa->a_pappl,pfname-pa->a_pappl+1);
-        do_aopen(pa,1,-1,pathname,pfname);
-        return TRUE;
+        return do_aopen(pa,1,-1,pathname,pfname);
     }
 
-    return FALSE;
+    return -1;
 }
 
 
 /*
  * check for function key & handle appropriately
  *
- * return TRUE iff matching key found & processed
+ * returns:
+ *  -1  key not a function key, or matching function key not found
+ *  0   non-graphics program launched
+ *  1   graphics program launched
  */
-static BOOL check_function_key(WORD thechar)
+static WORD check_function_key(WORD thechar)
 {
     WORD funkey;
 
@@ -745,7 +747,7 @@ static BOOL check_function_key(WORD thechar)
         funkey = ((thechar-FUNKEY_01) >> 8) + 1;
     else if ((thechar >= FUNKEY_11) && (thechar <= FUNKEY_20))
         funkey = ((thechar-FUNKEY_11) >> 8) + 11;
-    else return FALSE;
+    else return -1;
 
     return process_funkey(funkey);
 }
@@ -854,7 +856,7 @@ static WORD lookup_ascii_shortcut(WORD ascii, WORD *itemptr)
 static WORD hndl_kbd(WORD thechar)
 {
     WNODE *pw;
-    WORD done = FALSE, ascii;
+    WORD done, ascii;
     WORD title = -1, item;
 
     ascii = thechar & 0x00ff;
@@ -863,17 +865,18 @@ static WORD hndl_kbd(WORD thechar)
         pw = win_ontop();
         if (pw)
             do_refresh(pw);
-        return done;
+        return FALSE;
     }
 
     if (check_arrow_key(thechar))
-        return done;
+        return FALSE;
 
-    if (check_function_key(thechar))
+    done = check_function_key(thechar);
+    if (done >= 0)
         return done;
 
     if (check_alt_letter_key(thechar))
-        return done;
+        return FALSE;
 
     /* else normal ASCII character (ctl-Z etc) */
     title = lookup_ascii_shortcut(ascii,&item);
@@ -881,6 +884,7 @@ static WORD hndl_kbd(WORD thechar)
     /*
      * actually handle shortcuts
      */
+    done = FALSE;
     if (title >= 0)
     {
         menu_tnormal(G.a_trees[ADMENU], title, 0);