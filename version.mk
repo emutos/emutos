fa9ebc1e6e12c185b92723fedc0cb910b8cb00b0
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-04-21 22:43:39 +0200

    version.mk: Add copyright header

--- version.mk
+++ version.mk
@@ -1,4 +1,16 @@
 #
+# version.mk - Makefile fragment for version numbers
+#
+# Copyright (C) 2017 The EmuTOS development team.
+#
+# Authors:
+#  VRI      Vincent Rivière
+#
+# This file is distributed under the GPL, version 2 or at your
+# option any later version.  See doc/license.txt for details.
+#
+
+#
 # EmuTOS and EmuCON2 version
 #
 
