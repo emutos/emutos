014f1504cb5a9117d0d9ba5f4ab599fbe036c297
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-05-20 16:19:17 +0200

    Deploy the result of Travis CI builds to SourceForge
    https://sourceforge.net/projects/emutos/files/travis/

---
+++ .travis/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error when substituting.
+
+# This scripts deploys the built binaries to SourceForge:
+# https://sourceforge.net/projects/emutos/files/travis/
+
+# To enable passwordless SSH:
+# - Our private/public key pair must have been installed (see install_ssh_id.sh)
+# - Our public key must have been authorized on SourceForge, in the account of
+#   a user which has write access to the project's files:
+#   https://sourceforge.net/auth/shell_services
+
+if [ -z ${VERSION+x} ]
+then
+  echo "error: VERSION is undefined" >&2
+  exit 1
+fi
+
+LOCAL_DIRNAME=release-archives
+REMOTE_DIRNAME=$VERSION
+SSH_USER=vriviere,emutos
+SSH_HOST=frs.sourceforge.net
+SSH_PATH=/home/pfs/p/emutos/travis
+
+echo "Deploying $LOCAL_DIRNAME to $SSH_HOST:$SSH_PATH/$REMOTE_DIRNAME"
+set -x
+scp -o StrictHostKeyChecking=no -r release-archives $SSH_USER@$SSH_HOST:$SSH_PATH/$REMOTE_DIRNAME
