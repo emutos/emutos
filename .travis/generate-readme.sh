35af32e10334007bc6771e22ed49895421b0a6c0
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-05-21 09:45:47 +0200

    Use Markdown syntax in README to allow hyperlinks
    https://sourceforge.net/p/forge/documentation/markdown_syntax/
    
    NB: Preformatted text blocks using ~~~ or ``` does not seem to be
    actually supported by SourceForge.

--- .travis/generate-readme.sh
+++ .travis/generate-readme.sh
@@ -2,11 +2,13 @@
 # -e: Exit immediately if a command exits with a non-zero status.
 # -u: Treat unset variables as an error when substituting.
 
-echo "Built by Travis CI:"
-echo "https://travis-ci.org/emutos/emutos/builds/$TRAVIS_BUILD_ID"
+echo "These binaries have been produced by"
+echo "[Travis CI Build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/emutos/emutos/builds/$TRAVIS_BUILD_ID)."
 echo
 
 # The variable TRAVIS_COMMIT_RANGE includes 3 dots, which is suitable for
 # "git diff". But as we are going to use "git log", we need 2 dots instead.
 LOG_RANGE=$(echo $TRAVIS_COMMIT_RANGE | sed -n 's/\.\.\./../p')
-git log --name-status $LOG_RANGE --
+
+# Generate preformatted text with indentation of 4 spaces.
+git log --name-status $LOG_RANGE -- | sed 's/^/    /'
