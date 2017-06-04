emutos/emutos

commit 5839fee8f923927ce0284982440905ea36535a62

Change type of palette registers to UWORD

bios/screen.c
@@ -43,7 +43,7 @@ static void setphys(const UBYTE *addr);
 
 /* Define palette */
 
-static const WORD dflt_palette[] = {
+static const UWORD dflt_palette[] = {
     RGB_WHITE, RGB_RED, RGB_GREEN, RGB_YELLOW,
     RGB_BLUE, RGB_MAGENTA, RGB_CYAN, RGB_LTGRAY,
     RGB_GRAY, RGB_LTRED, RGB_LTGREEN, RGB_LTYELLOW,
@@ -53,9 +53,9 @@ static const WORD dflt_palette[] = {
 /*
  * Initialise ST(e) palette registers
  */
-static void initialise_ste_palette(WORD mask)
+static void initialise_ste_palette(UWORD mask)
 {
-    volatile WORD *col_regs = (WORD *) ST_PALETTE_REGS;
+    volatile UWORD *col_regs = (UWORD *) ST_PALETTE_REGS;
     int i;
 
     for (i = 0; i < 16; i++)
@@ -67,7 +67,7 @@ static void initialise_ste_palette(WORD mask)
  */
 static void fixup_ste_palette(WORD rez)
 {
-    volatile WORD *col_regs = (WORD *) ST_PALETTE_REGS;
+    volatile UWORD *col_regs = (UWORD *) ST_PALETTE_REGS;
 
     if (rez == ST_MEDIUM)
         col_regs[3] = col_regs[15];
@@ -120,7 +120,7 @@ static WORD shifter_get_monitor_type(void)
 
 #if CONF_WITH_TT_SHIFTER
 
-static const WORD tt_dflt_palette[] = {
+static const UWORD tt_dflt_palette[] = {
  TTRGB_WHITE, TTRGB_RED, TTRGB_GREEN, TTRGB_YELLOW,
  TTRGB_BLUE, TTRGB_MAGENTA, TTRGB_CYAN, TTRGB_LTGRAY,
  TTRGB_GRAY, TTRGB_LTRED, TTRGB_LTGREEN, TTRGB_LTYELLOW,
@@ -215,10 +215,10 @@ WORD esetbank(WORD bank)
 /*
  * Read/modify TT palette colour entry
  */
-WORD esetcolor(WORD index,WORD color)
+WORD esetcolor(WORD index,UWORD color)
 {
-    volatile WORD *ttcol_regs = (WORD *) TT_PALETTE_REGS;
-    WORD oldcolor;
+    volatile UWORD *ttcol_regs = (UWORD *) TT_PALETTE_REGS;
+    UWORD oldcolor;
 
     if (!has_tt_shifter)
         return 0x53;    /* unimplemented xbios call: return function # */
@@ -240,7 +240,7 @@ WORD esetcolor(WORD index,WORD color)
  * which is the de facto TOS standard for unimplemented xbios functions.
  * Therefore internally we make it return a WORD.
  */
-WORD esetpalette(WORD index,WORD count,WORD *rgb)
+WORD esetpalette(WORD index,WORD count,UWORD *rgb)
 {
     volatile WORD *ttcolour;
 
@@ -265,7 +265,7 @@ WORD esetpalette(WORD index,WORD count,WORD *rgb)
  *
  * See the comments for esetpalette() above
  */
-WORD egetpalette(WORD index,WORD count,WORD *rgb)
+WORD egetpalette(WORD index,WORD count,UWORD *rgb)
 {
     volatile WORD *ttcolour;
 
@@ -331,7 +331,7 @@ WORD esetsmear(WORD mode)
  */
 static void initialise_tt_palette(WORD rez)
 {
-    volatile WORD *ttcol_regs = (WORD *) TT_PALETTE_REGS;
+    volatile UWORD *ttcol_regs = (UWORD *) TT_PALETTE_REGS;
     int i;
 
     for (i = 0; i < 256; i++)
@@ -386,7 +386,7 @@ WORD check_moderez(WORD moderez)
 void initialise_palette_registers(WORD rez,WORD mode)
 {
 #if CONF_WITH_ATARI_VIDEO
-WORD mask;
+UWORD mask;
 
     if (HAS_VIDEL || HAS_TT_SHIFTER || HAS_STE_SHIFTER)
         mask = 0x0fff;
@@ -967,7 +967,7 @@ void setpalette(LONG palettePtr)
 {
 #ifdef ENABLE_KDEBUG
     int i, max;
-    WORD *p = (WORD *)palettePtr;
+    UWORD *p = (UWORD *)palettePtr;
     max = getrez() == 0 ? 15 : getrez() == 1 ? 3 : 1;
     KDEBUG(("Setpalette("));
     for(i = 0 ; i <= max ; i++) {
@@ -977,7 +977,7 @@ void setpalette(LONG palettePtr)
     KDEBUG((")\n"));
 #endif
     /* next VBL will do this */
-    colorptr = (WORD *) palettePtr;
+    colorptr = (UWORD *) palettePtr;
 }
 
 /*

bios/screen.h
@@ -48,9 +48,9 @@
 WORD esetshift(WORD mode);
 WORD egetshift(void);
 WORD esetbank(WORD bank);
-WORD esetcolor(WORD index,WORD color);
-WORD esetpalette(WORD index,WORD count,WORD *rgb);
-WORD egetpalette(WORD index,WORD count,WORD *rgb);
+WORD esetcolor(WORD index,UWORD color);
+WORD esetpalette(WORD index,WORD count,UWORD *rgb);
+WORD egetpalette(WORD index,WORD count,UWORD *rgb);
 WORD esetgray(WORD mode);
 WORD esetsmear(WORD mode);
 

bios/tosvars.h
@@ -36,7 +36,7 @@ extern WORD cmdload;
 
 extern UBYTE *v_bas_ad;
 
-extern WORD *colorptr;
+extern UWORD *colorptr;
 extern UBYTE *screenpt;
 extern BYTE sshiftmod;
 extern BYTE defshiftmod;

bios/videl.c
@@ -98,7 +98,7 @@ static const LONG videl_dflt_palette[] = {
 };
 
 GLOBAL LONG falcon_shadow_palette[256];   /* real Falcon does this, used by vectors.S */
-static WORD ste_shadow_palette[16];
+static UWORD ste_shadow_palette[16];
 
 #define MON_ALL     -1  /* code used in VMODE_ENTRY for match on mode only */
 
@@ -613,7 +613,7 @@ LONG vgetsize(WORD mode)
  * convert from Falcon palette format to STe palette format
  */
 #define falc2ste(a) ((((a)>>1)&0x08)|(((a)>>5)&0x07))
-static void convert2ste(WORD *ste,LONG *falcon)
+static void convert2ste(UWORD *ste,LONG *falcon)
 {
     union {
         LONG l;
@@ -704,7 +704,7 @@ WORD vsetrgb(WORD index,WORD count,LONG *rgb)
         return 0; /* OK */
     }
 
-    colorptr = (limit==256) ? (WORD *)0x01L : (WORD *)((LONG)falcon_shadow_palette|0x01L);
+    colorptr = (limit==256) ? (UWORD *)0x01L : (UWORD *)((LONG)falcon_shadow_palette|0x01L);
 
     return 0; /* OK */
 }
@@ -719,7 +719,8 @@ WORD vgetrgb(WORD index,WORD count,LONG *rgb)
         LONG l;
         UBYTE b[4];
     } u;
-    WORD limit, mode, value;
+    WORD limit, mode;
+    UWORD value;
 
     if (!has_videl)
         return 0x5e;    /* unimplemented xbios call: return function # */
@@ -738,7 +739,7 @@ WORD vgetrgb(WORD index,WORD count,LONG *rgb)
      * values derived from the STe palette
      */
     if (use_ste_palette(mode)) {
-        WORD *ste_shadow = ste_shadow_palette + index;
+        UWORD *ste_shadow = ste_shadow_palette + index;
         u.l = 0;
         while(count--) {
             value = *ste_shadow++;
@@ -834,7 +835,7 @@ void videl_get_current_mode_info(UWORD *planes, UWORD *hz_rez, UWORD *vt_rez)
  */
 void initialise_falcon_palette(WORD mode)
 {
-    volatile WORD *col_regs = (WORD *) ST_PALETTE_REGS;
+    volatile UWORD *col_regs = (UWORD *) ST_PALETTE_REGS;
     volatile LONG *fcol_regs = (LONG *) FALCON_PALETTE_REGS;
     int i, limit;
 

bios/xbios.c
@@ -883,12 +883,12 @@ static WORD xbios_53(WORD index, WORD color)
     kprintf("XBIOS: EsetColor\n");
     return esetcolor(index, color);
 }
-static void xbios_54(WORD index,WORD count,WORD *rgb)
+static void xbios_54(WORD index,WORD count,UWORD *rgb)
 {
     kprintf("XBIOS: EsetPalette\n");
     esetpalette(index, count, rgb);
 }
-static void xbios_55(WORD index, WORD count, WORD *rgb)
+static void xbios_55(WORD index, WORD count, UWORD *rgb)
 {
     kprintf("XBIOS: EgetPalette\n");
     egetpalette(index, count, rgb);

commit 57b2ac8f2f7007d78ac9ac4d93454cdd104dc18f

Fix EsetColor() when color < 0
This is a regression introduced in commit 5839fee8.
Reported by Christian Zietz.

bios/screen.c
@@ -225,7 +225,7 @@ WORD esetcolor(WORD index,UWORD color)
 
     index &= 0xff;                  /* force valid index number */
     oldcolor = ttcol_regs[index] & TT_PALETTE_BITMASK;
-    if (color >= 0)
+    if ((WORD)color >= 0)
         ttcol_regs[index] = color & TT_PALETTE_BITMASK;
 
     return oldcolor;