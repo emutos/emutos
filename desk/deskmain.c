emutos/emutos

commit 798e4037a5b2a02bbdc0735fed51d7ffbd33f30f

Allow configuration of window/desktop backgrounds
This allows the background fill pattern & colour to be set for the
desktop and windows, via a desktop dialog.  The equivalent feature
is available in Atari TOS2/3/4.  This feature is disabled by default
for the 192K ROMs, since it is quite expensive in ROM space.
This update was inspired by Thorsten Otto.

desk/deskmain.c
@@ -85,6 +85,7 @@ typedef struct {
  */
 #define VIEW_HAS_CHANGED    0x0001
 #define SORT_HAS_CHANGED    0x0002
+#define BACKGROUND_HAS_CHANGED  0x0004
 
 
 /*
@@ -230,7 +231,7 @@ static void desk_all(WORD flags)
     desk_wait(TRUE);
     if (flags & SORT_HAS_CHANGED)
         win_srtall();
-    if (flags)          /* either sort or view has changed */
+    if (flags & (VIEW_HAS_CHANGED|SORT_HAS_CHANGED))
         win_bdall();
     win_shwall();
     desk_wait(FALSE);
@@ -327,6 +328,9 @@ static void men_update(void)
     menu_ienable(tree, CLIITEM, 0);
 #endif
 
+#if !CONF_WITH_BACKGROUNDS
+    menu_ienable(tree, BACKGRND, 0);
+#endif
 }
 
 
@@ -466,6 +470,12 @@ static WORD do_viewmenu(WORD item)
     case NSRTITEM:
         newsort = S_NSRT;
         break;
+#if CONF_WITH_BACKGROUNDS
+    case BACKGRND:
+        if (inf_backgrounds())
+            rc |= BACKGROUND_HAS_CHANGED;
+        break;
+#endif
     }
 
     if (newview != G.g_iview)
@@ -483,7 +493,7 @@ static WORD do_viewmenu(WORD item)
         rc |= SORT_HAS_CHANGED;
     }
 
-    if (rc)
+    if (rc & (VIEW_HAS_CHANGED|SORT_HAS_CHANGED))
         win_view(newview, newsort);
 
     return rc;
@@ -641,8 +651,8 @@ static WORD hndl_menu(WORD title, WORD item)
     case VIEWMENU:
         done = FALSE;
         rc = do_viewmenu(item);
-        if (rc)             /* if sort and/or view has changed,  */
-            desk_all(rc);   /* rebuild all windows appropriately */
+        if (rc)             /* if sort, view, or background has changed, */
+            desk_all(rc);   /* rebuild/show all windows as appropriate   */
         break;
     case OPTNMENU:
         done = do_optnmenu(item);

desk/deskapp.c
@@ -71,6 +71,12 @@
 
 
 /*
+ *  standard ob_spec value for desktop/window border & text colours
+ */
+#define BORDER_TEXT_COLOURS ((BLACK << 12) | (BLACK << 8))
+
+
+/*
  *  the following bit masks apply to EMUDESK.INF
  */
                             /* 'E' byte 1 */
@@ -96,6 +102,14 @@
                             /* 'E' byte 5 */
 #define INF_E5_NOSORT   0x80    /* 1 => do not sort folder contents (overrides INF_E1_SORTMASK) */
 
+                            /* 'Q' bytes 1-6 (default desktop/window pattern/colour values) */
+#define INF_Q1_DEFAULT  (IP_4PATT << 4) | BLACK     /* desktop, 1 plane */
+#define INF_Q2_DEFAULT  (IP_4PATT << 4) | WHITE     /* window, 1 plane */
+#define INF_Q3_DEFAULT  (IP_4PATT << 4) | GREEN     /* desktop, 2 planes */
+#define INF_Q4_DEFAULT  (IP_4PATT << 4) | WHITE     /* window, 2 planes */
+#define INF_Q5_DEFAULT  (IP_4PATT << 4) | GREEN     /* desktop, >2 planes */
+#define INF_Q6_DEFAULT  (IP_4PATT << 4) | WHITE     /* window, >2 planes */
+
                             /* application type entries (F/G/P/Y): first byte of 3-byte string */
 #define INF_AT_APPDIR   0x01    /* 1 => set current dir to app's dir (else to top window dir) */
 #define INF_AT_ISFULL   0x02    /* 1 => pass full path in args (else filename only) */
@@ -110,7 +124,7 @@ static BYTE     *gl_buffer;
 
 /* When we can't get EMUDESK.INF via shel_get() or by reading from
  * the disk, we create one dynamically from three sources:
- *  desk_inf_data1 below, for the #R, #E and #W lines
+ *  desk_inf_data1 below, for the #R, #E, #Q and #W lines
  *  the drivemask, for #M lines
  *  desk_inf_data2 below, for most of the remaining lines
  * The #T line is added at the end.
@@ -125,6 +139,9 @@ static BYTE     *gl_buffer;
 static const char desk_inf_data1[] =
     "#R 01\r\n"                         /* INF_REV_LEVEL */
     "#E 1A 61\r\n"                      /* INF_E1_DEFAULT and INF_E2_DEFAULT */
+#if CONF_WITH_BACKGROUNDS
+    "#Q 41 40 43 40 43 40\r\n"          /* INF_Q1_DEFAULT -> INF_Q6_DEFAULT */
+#endif
     "#W 00 00 02 06 26 0C 00 @\r\n"
     "#W 00 00 02 08 26 0C 00 @\r\n"
     "#W 00 00 02 0A 26 0C 00 @\r\n"
@@ -635,6 +652,19 @@ void app_start(void)
     ycnt = G.g_hdesk / (G.g_hicon+MIN_HINT);/* icon count */
     G.g_ich = G.g_hdesk / ycnt;             /* height */
 
+#if CONF_WITH_BACKGROUNDS
+    /*
+     * set up the default background pattern/colours, in case
+     * we have an old saved EMUDESK.INF (without a 'Q' line)
+     */
+    G.g_patcol[0].desktop = INF_Q1_DEFAULT;
+    G.g_patcol[0].window = INF_Q2_DEFAULT;
+    G.g_patcol[1].desktop = INF_Q3_DEFAULT;
+    G.g_patcol[1].window = INF_Q4_DEFAULT;
+    G.g_patcol[2].desktop = INF_Q5_DEFAULT;
+    G.g_patcol[2].window = INF_Q6_DEFAULT;
+#endif
+
     shel_get(gl_afile, SIZE_AFILE);
     if (!(bootflags & BOOTFLAG_SKIP_AUTO_ACC)
         && gl_afile[0] != '#')              /* invalid signature    */
@@ -801,6 +831,16 @@ void app_start(void)
             if (envr & INF_E5_NOSORT)
                 G.g_cnxsave.cs_sort = CS_NOSORT;
             break;
+#if CONF_WITH_BACKGROUNDS
+        case 'Q':                       /* desktop/window pattern/colour */
+            pcurr++;
+            for (i = 0; i < 3; i++)
+            {
+                pcurr = scan_2(pcurr, &G.g_patcol[i].desktop);
+                pcurr = scan_2(pcurr, &G.g_patcol[i].window);
+            }
+            break;
+#endif
         }
     }
 
@@ -982,6 +1022,14 @@ void app_save(WORD todisk)
     pcurr += sprintf(pcurr,"#E %02X %02X %02X %02X %02X\r\n",
                     env1,env2,HIBYTE(mode),LOBYTE(mode),env5);
 
+#if CONF_WITH_BACKGROUNDS
+    /* save desktop/window colour/patterns */
+    pcurr += sprintf(pcurr,"#Q %02X %02X %02X %02X %02X %02X\r\n",
+                    G.g_patcol[0].desktop,G.g_patcol[0].window,
+                    G.g_patcol[1].desktop,G.g_patcol[1].window,
+                    G.g_patcol[2].desktop,G.g_patcol[2].window);
+#endif
+
     /* save windows */
     for (i = 0; i < NUM_WNODES; i++)
     {
@@ -1078,6 +1126,37 @@ void app_save(WORD todisk)
 
 
 /*
+ *  Set desktop (& window) background pattern/colour
+ */
+static void set_background(void)
+{
+#if CONF_WITH_BACKGROUNDS
+    int i, n;
+    OBJECT *tree;
+
+    switch(gl_nplanes)
+    {
+    case 1:
+        n = 0;
+        break;
+    case 2:
+        n = 1;
+        break;
+    default:
+        n = 2;
+        break;
+    }
+
+    for (i = DROOT+1, tree = G.g_screen+i; i < WOBS_START; i++, tree++)
+        tree->ob_spec = G.g_patcol[n].window | BORDER_TEXT_COLOURS;
+    G.g_screen[DROOT].ob_spec = G.g_patcol[n].desktop | BORDER_TEXT_COLOURS;
+#else
+    G.g_screen[DROOT].ob_spec = AP_PRIVATE;
+#endif
+}
+
+
+/*
  *  Build and redraw the desktop list of objects
  */
 void app_blddesk(void)
@@ -1090,7 +1169,9 @@ void app_blddesk(void)
 
     /* free all this window's kids and set size  */
     obj_wfree(DROOT, 0, 0, gl_width, gl_height);
-    G.g_screen[DROOT].ob_spec = AP_PRIVATE;
+
+    /* set background pattern/colours */
+    set_background();
 
     for(pa = G.g_ahead; pa; pa = pa->a_next)
     {

desk/deskbind.h
@@ -83,6 +83,21 @@ typedef union
 } SCREENINFO;
 
 
+#if CONF_WITH_BACKGROUNDS
+/*
+ * the following structure is used in GLOBES to store the current
+ * background pattern/colour values for the desktop & windows, for
+ * a resolution with a given number of planes.  note that only the
+ * low-order byte is used.
+ */
+typedef struct
+{
+    WORD desktop;       /* for desktop background */
+    WORD window;        /* for window backgrounds */
+} PATCOL;
+#endif
+
+
 /*
  * The desktop global data area
  */
@@ -181,6 +196,15 @@ typedef struct
 /*GLOBAL*/ ICONBLK      *g_iblist;              /* ptr to array of icon blocks */
 
 /*GLOBAL*/ CSAVE        g_cnxsave;
+
+#if CONF_WITH_BACKGROUNDS
+/* Default pattern/colour for desktop/windows:
+ * [0] applies to 1-plane (ST high, TT high)
+ * [1] applies to 2-plane (ST medium)
+ * [2] applies to everything else
+ */
+        PATCOL          g_patcol[3];
+#endif
 
 /* Number of first free item object within g_screen[]; free objects
  * are chained via ob_next.

desk/deskinf.c
@@ -47,6 +47,12 @@
 
 
 /*
+ * Border flags for 'SAMPLE' object in 'Set background' dialog:
+ * outside border, thickness=1, border colour black, text black.
+ */
+#define SAMPLE_BORDER_FLAGS 0x00ff1100L
+
+/*
  * Routine to format DOS style time
  *
  *  15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
@@ -300,7 +306,7 @@ static WORD dr_fnode(UWORD last_state, UWORD curr_state, WORD x, WORD y,
     else
     {
         len = format_fnode(fnode, temp);    /* convert to text */
-        gsx_attr(TRUE, MD_REPLACE, BLACK);
+        gsx_attr(TRUE, MD_TRANS, BLACK);
         expand_string(intin, temp);
         gsx_tblt(IBM, x, y, len);
         gsx_attr(FALSE, MD_XOR, BLACK);
@@ -744,6 +750,109 @@ WORD inf_pref(void)
 
     return FALSE;
 }
+
+
+#if CONF_WITH_BACKGROUNDS
+/*
+ *      Handle background pattern/colour configuration dialog
+ */
+BOOL inf_backgrounds(void)
+{
+    OBJECT *tree, *obj;
+    WORD ret;
+    LONG curdesk, curwin;
+    WORD i, index, unused;
+
+    /* set pattern/colour index and first unused colour */
+    switch(gl_nplanes)
+    {
+    case 1:
+        index = 0;
+        unused = 2;
+        break;
+    case 2:
+        index = 1;
+        unused = 4;
+        break;
+    default:
+        index = 2;
+        unused = 16;
+        break;
+    }
+
+    tree = G.a_trees[ADBKGND];
+
+    /* hide colours that are not available in the current resolution */
+    for (i = unused, obj = tree+COLOR0+unused; i < 16; i++, obj++)
+        obj->ob_flags |= HIDETREE;
+
+    /* set the initially-displayed background pattern */
+    curdesk = G.g_screen[DROOT].ob_spec;
+    curwin = G.g_screen[DROOT+1].ob_spec;
+    tree[SAMPLE].ob_spec = ((tree[DESKPREF].ob_state & SELECTED) ? curdesk : curwin)
+                            | SAMPLE_BORDER_FLAGS;
+
+    /* handle the dialog */
+    show_hide(FMD_START, tree);
+
+    while(1)
+    {
+        draw_fld(tree, SAMPLE);
+        ret = form_do(tree, ROOT);
+
+        if ((ret == SCOK) || (ret == SCCANCEL))
+        {
+            tree[ret].ob_state &= ~SELECTED;
+            break;
+        }
+
+        if (ret == DESKPREF)
+            tree[SAMPLE].ob_spec = curdesk;
+        else if (ret == WINPREF)
+            tree[SAMPLE].ob_spec = curwin;
+        else if ((ret >= PATTERN0) && (ret <= PATTERN7))
+        {
+            tree[SAMPLE].ob_spec &= ~FILLPAT_MASK;
+            tree[SAMPLE].ob_spec |= tree[ret].ob_spec & FILLPAT_MASK;
+        }
+        else if ((ret >= COLOR0) && (ret <= COLOR15))
+        {
+            tree[SAMPLE].ob_spec &= ~FILLCOL_MASK;
+            tree[SAMPLE].ob_spec |= tree[ret].ob_spec & FILLCOL_MASK;
+        }
+
+        if (tree[DESKPREF].ob_state & SELECTED)
+            curdesk = tree[SAMPLE].ob_spec;
+        else
+            curwin = tree[SAMPLE].ob_spec;
+    }
+
+    show_hide(FMD_FINISH, tree);
+
+    /* handle dialog exit */
+    if (ret == SCOK)
+    {
+        /* check for desktop background change */
+        if (G.g_screen[DROOT].ob_spec != curdesk)
+        {
+            G.g_patcol[index].desktop = curdesk & 0xff;
+            G.g_screen[DROOT].ob_spec = curdesk;
+            do_wredraw(0, G.g_xdesk, G.g_ydesk, G.g_wdesk, G.g_hdesk);
+        }
+
+        /* check for window background change */
+        if (G.g_screen[DROOT+1].ob_spec != curwin)
+        {
+            G.g_patcol[index].window = curwin & 0xff;
+            for (i = DROOT+1, tree = G.g_screen+i; i < WOBS_START; i++, tree++)
+                tree->ob_spec = curwin;
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+#endif
 
 
 /*

desk/deskinf.h
@@ -18,4 +18,8 @@ void inf_numset(OBJECT *tree, WORD obj, ULONG value);
 WORD inf_pref(void);
 WORD opn_appl(BYTE *papname, BYTE *ptail);
 
+#if CONF_WITH_BACKGROUNDS
+BOOL inf_backgrounds(void);
+#endif
+
 #endif  /* _DESKINF_H */

include/config.h
@@ -236,6 +236,9 @@
 # ifndef CONF_WITH_DESKTOP_SHORTCUTS
 #  define CONF_WITH_DESKTOP_SHORTCUTS 0
 # endif
+# ifndef CONF_WITH_BACKGROUNDS
+#  define CONF_WITH_BACKGROUNDS 0
+# endif
 # ifndef CONF_WITH_PCGEM
 #  define CONF_WITH_PCGEM 0
 # endif
@@ -1006,6 +1009,14 @@
  */
 #ifndef CONF_WITH_DESKTOP_SHORTCUTS
 # define CONF_WITH_DESKTOP_SHORTCUTS 1
+#endif
+
+/*
+ * Set CONF_WITH_BACKGROUNDS to 1 to allow the background pattern/colour
+ * of the desktop & windows to be configured
+ */
+#ifndef CONF_WITH_BACKGROUNDS
+# define CONF_WITH_BACKGROUNDS 1
 #endif
 
 /*

commit e78e312b715bba3630a8f856601d3bcc5a6b103c

Improve enabling/disabling of basic menu items
The men_update() function initially enables all menu items, then
disables the separator lines.  Previously, both the last item to
be enabled & the separator lines had to be manually identified.
Both are now handled automagically.  This fixes a problem introduced
by commit 798e403 (a newly-introduced separator line was enabled).

desk/deskmain.c
@@ -122,7 +122,6 @@ static WORD     ig_close;
  *  there is one array of items to enable:
  *      ILL_OPENWIN[]   enabled if there is an open window
  *  and many arrays of items to disable:
- *      ILL_ITEM[]      always disabled
  *      ILL_NOWIN[]     disabled if there are no open windows
  *      ILL NOSEL[]     disabled if there are no icons selected
  *      ILL_MULTSEL[]   disabled if two or more icons are selected
@@ -134,7 +133,6 @@ static WORD     ig_close;
  *      ILL_FOLD[]      disabled if a folder is selected
  *      ILL_TRASH[]     disabled if the trash can is selected
  */
-static const BYTE     ILL_ITEM[] = {L1ITEM, L2ITEM, L3ITEM, L4ITEM, L5ITEM, 0};
 static const BYTE     ILL_FILE[] = {IDSKITEM,RICNITEM,0};
 static const BYTE     ILL_DOCU[] = {IDSKITEM,IAPPITEM,RICNITEM,0};
 static const BYTE     ILL_FOLD[] = {IDSKITEM,IAPPITEM,RICNITEM,0};
@@ -258,15 +256,25 @@ static void men_update(void)
     const BYTE *pvalue;
     ANODE *appl;
     OBJECT *tree = G.a_trees[ADMENU];
+    OBJECT *obj;
 
     pvalue = 0;
 
-    /* enable all items */
-    for (item = OPENITEM; item <= PREFITEM; item++)
-        menu_ienable(tree, item, 1);
-
-    /* disable some items */
-    men_list(tree, ILL_ITEM, FALSE);
+    /*
+     * disable separator strings, enable remaining menu items
+     */
+    for (obj = tree+OPENITEM; ; obj++)
+    {
+        if (obj->ob_type == G_STRING)
+        {
+            if (*(BYTE *)obj->ob_spec == '-')   /* must be a separator */
+                obj->ob_state |= DISABLED;
+            else
+                obj->ob_state &= ~DISABLED;
+        }
+        if (obj->ob_flags & LASTOB)
+            break;
+    }
 
     nsel = 0;
     for (item = 0; (item=win_isel(G.g_screen, G.g_croot, item)) != 0; nsel++)