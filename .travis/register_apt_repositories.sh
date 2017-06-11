44c1c4f3071120fd2f01e45cdc1fa874a68764fa
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-06-11 12:27:53 +0200

    Update location of cross-tools repository
    New location:
    https://launchpad.net/~vriviere/+archive/ubuntu/ppa
    
    Instructions:
    http://vincent.riviere.free.fr/soft/m68k-atari-mint/ubuntu.php

--- .travis/register_apt_repositories.sh
+++ .travis/register_apt_repositories.sh
@@ -8,19 +8,12 @@ then
   exit 1
 fi
 
-# In order to avoid security warnings when installing packages from third-party
-# APT repositories, we need to trust their public key. Instead of downloading
-# the public keys every time, we download them once and we store them here.
-# So if the remote website gets hijacked and provides packages signed with a
-# different key, we will not inadvertently install bad packages.
-
 # Display expanded script commands
 set -x
 
 # Vincent Rivière's m68k-atari-mint cross-tools
 # http://vincent.riviere.free.fr/soft/m68k-atari-mint/ubuntu.php
-apt-key add .travis/vr-apt-key.txt
-echo "deb http://vincent.riviere.free.fr/debian/ $(lsb_release -cs) contrib" >>/etc/apt/sources.list
+sudo add-apt-repository -y ppa:vriviere/ppa
 
 # Update the packages list
 apt-get update -qq
