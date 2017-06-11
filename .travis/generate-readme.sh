emutos/emutos

commit 943665c8148dba0b977c9da539652daa44cbe912

Use FIXED_TRAVIS_COMMIT_RANGE
TRAVIS_COMMIT_RANGE is not reliable. It caused a build error when
generating README.txt after git push -f.
Even if we forbid forced pushes in official sources, users of forked
repositories may use that.

.travis/generate-readme.sh
@@ -2,17 +2,15 @@
 # -e: Exit immediately if a command exits with a non-zero status.
 # -u: Treat unset variables as an error when substituting.
 
+# See .travis/fix_travis_commit_range.sh for details about the commit range.
+
 echo "These binaries have been produced by"
 echo "[Travis CI Build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/emutos/emutos/builds/$TRAVIS_BUILD_ID)"
-echo "for commits [$TRAVIS_COMMIT_RANGE](https://github.com/emutos/emutos/compare/$TRAVIS_COMMIT_RANGE)."
+echo "for commits [$FIXED_TRAVIS_COMMIT_RANGE](https://github.com/emutos/emutos/compare/$FIXED_TRAVIS_COMMIT_RANGE)."
 echo
 
-# The variable TRAVIS_COMMIT_RANGE includes 3 dots, which is suitable for
-# "git diff". But as we are going to use "git log", we need 2 dots instead.
-LOG_RANGE=$(echo $TRAVIS_COMMIT_RANGE | sed -n 's/\.\.\./../p')
-
 # Generate log as preformatted text with hyperlinks
-git log --name-status $LOG_RANGE -- | sed \
+git log --name-status $FIXED_TRAVIS_COMMIT_RANGE -- | sed \
   -e 's|^commit \([0-9a-z]*\).*|``commit`` \[\1\](https://github.com/emutos/emutos/commit/\1)  |' \
   -e 's/^$/`` ``  /' \
   -e 's/^[^`].*/``&``  /'

.travis.yml
@@ -27,6 +27,9 @@ before_install:
   - lftp --version
 
 script:
+  # Set FIXED_TRAVIS_COMMIT_RANGE
+  - . .travis/fix_travis_commit_range.sh
+  # Build binaries
   - export VERSION=$(date -u +%Y%m%d-%H%M%S)-$(git rev-parse --short $TRAVIS_COMMIT)
   - make clean
   - make release VERSION=$VERSION

.travis/fix_travis_commit_range.sh
@@ -1,0 +1,35 @@
+# This script fragment must be sourced by the main script file
+# in order to define the FIXED_TRAVIS_COMMIT_RANGE variable
+# . .travis/fix_travis_commit_range.sh
+
+# TRAVIS_COMMIT_RANGE uses triple-dot syntax to specify which commits have
+# triggered the build. This is wrong. In case of forced push, the previous
+# commit is not an ancestor of the last commit, so it is not present in the
+# cloned repository. And also, the range contains unrelated commits.
+# https://docs.travis-ci.com/user/environment-variables/
+# https://git-scm.com/book/tr/v2/Git-Tools-Revision-Selection
+# https://github.com/travis-ci/travis-ci/issues/4596
+#
+# The solution is to use double-dot syntax instead.
+# In case of forced push, we can't know the previous commit. So we just use
+# the parent commit as previous one.
+
+echo TRAVIS_COMMIT_RANGE=$TRAVIS_COMMIT_RANGE
+
+PREVIOUS_COMMIT=$(echo $TRAVIS_COMMIT_RANGE | sed 's/\.\.\..*//')
+CURRENT_COMMIT=$TRAVIS_COMMIT
+
+if ! git merge-base --is-ancestor $PREVIOUS_COMMIT $CURRENT_COMMIT 2>/dev/null
+then
+  # Forced push: use parent commit as previous commit
+  PREVIOUS_COMMIT=$CURRENT_COMMIT~
+fi
+
+# Normalize commits
+PREVIOUS_COMMIT=$(git rev-parse --short $PREVIOUS_COMMIT)
+CURRENT_COMMIT=$(git rev-parse --short $CURRENT_COMMIT)
+
+export FIXED_TRAVIS_COMMIT_RANGE=$PREVIOUS_COMMIT..$CURRENT_COMMIT
+echo FIXED_TRAVIS_COMMIT_RANGE=$FIXED_TRAVIS_COMMIT_RANGE
+
+unset PREVIOUS_COMMIT CURRENT_COMMIT

commit 6a567be89d1889a06371f2c3d10ebd1f3fd83075

Fix "for commits" link in README.txt
It has been broken in commit 943665c8148dba0b977c9da539652daa44cbe912.

.travis/generate-readme.sh
@@ -4,9 +4,12 @@
 
 # See .travis/fix_travis_commit_range.sh for details about the commit range.
 
+# The argument for GitHub /compare/ requires 3 dots.
+COMPARE_ARG=$(echo $FIXED_TRAVIS_COMMIT_RANGE | sed 's/\.\./.../')
+
 echo "These binaries have been produced by"
 echo "[Travis CI Build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/emutos/emutos/builds/$TRAVIS_BUILD_ID)"
-echo "for commits [$FIXED_TRAVIS_COMMIT_RANGE](https://github.com/emutos/emutos/compare/$FIXED_TRAVIS_COMMIT_RANGE)."
+echo "for commits [$FIXED_TRAVIS_COMMIT_RANGE](https://github.com/emutos/emutos/compare/$COMPARE_ARG)."
 echo
 
 # Generate log as preformatted text with hyperlinks