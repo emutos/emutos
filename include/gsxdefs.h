15724c45bbbe783226df46b16a477be056662d21
Roger Burrows <anodynesoftware@users.sourceforge.net>
2017-07-09 20:39:56 -0400

    Clarify definition of MFORM structure contents

--- include/gsxdefs.h
+++ include/gsxdefs.h
@@ -81,8 +81,8 @@ typedef struct mform
         WORD    mf_xhot;
         WORD    mf_yhot;
         WORD    mf_nplanes;
-        WORD    mf_fg;
-        WORD    mf_bg;
+        WORD    mf_bg;          /* mask colour index */
+        WORD    mf_fg;          /* data colour index */
         UWORD   mf_mask[16];
         UWORD   mf_data[16];
 } MFORM;
