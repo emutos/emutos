emutos/emutos

commit a75c80fb3157e948d6119d9fccbc1a123911fc12

Deselect desktop icons when window icon is selected
Previously, desktop icons remain selected when icons in a window were
selected; this was confusing and not TOS-compatible.  This was likely
a remnant of the old desktop code (the crippled 2-window desktop)
before the Desktop 1.2 changes that recreated the Atari TOS desktop.

desk/desksupp.c
@@ -66,18 +66,21 @@ void desk_clear(WORD wh)
 {
     WNODE *pw;
     GRECT c;
+    WORD root = -1;
 
     /* get current size */
     wind_get(wh, WF_WXYWH, &c.g_x, &c.g_y, &c.g_w, &c.g_h);
 
-    /* find its tree of items */
-    pw = win_find(wh);
-    if (pw)
+    if (wh)         /* not the desktop */
     {
-        /* clear all selections */
-        act_allchg(wh, G.g_screen, pw->w_root, 0, &gl_rfull, &c,
-                    SELECTED, FALSE, TRUE);
+        pw = win_find(wh);  /* find its tree of items */
+        if (pw)
+            root = pw->w_root;
     }
+    else root = DROOT;
+
+    /* clear all selections */
+    act_allchg(wh, G.g_screen, root, 0, &gl_rfull, &c, SELECTED, FALSE, TRUE);
 }
 
 

commit 82766f1e6563788e5ec9df446ff5a4fb5e14f4c0

Fix bug in desk_clear()
If desk_clear() was passed an invalid window handle, act_allchg()
was called with 'root' set to -1.  This caused act_chkobj() to be
called with the same invalid value, which could cause a crash in
rc_intersect().  This bug was introduced by commit a75c80f.

desk/desksupp.c
@@ -110,10 +110,7 @@ void desk_clear(WORD wh)
 {
     WNODE *pw;
     GRECT c;
-    WORD root = -1;
-
-    /* get current size */
-    wind_get_grect(wh, WF_WXYWH, &c);
+    WORD root = DROOT;
 
     if (wh)         /* not the desktop */
     {
@@ -121,7 +118,17 @@ void desk_clear(WORD wh)
         if (pw)
             root = pw->w_root;
     }
-    else root = DROOT;
+
+    /*
+     * if 'root' is still DROOT, then either the 'window' is the desktop
+     * (wh==0), or something is wrong with the window setup.  to handle
+     * the latter case, we force the handle to 0 anyway for safety.
+     */
+    if (root == DROOT)
+        wh = 0;
+
+    /* get current size */
+    wind_get_grect(wh, WF_WXYWH, &c);
 
     /* clear all selections */
     act_allchg(wh, G.g_screen, root, 0, &gl_rfull, &c, SELECTED, FALSE, TRUE);