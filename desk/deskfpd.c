emutos/emutos

commit 5973f1e03edbb50c39149414856a189370809c2f

Do not open desktop directory if error occurs
If the desktop encounters an error when opening a directory, we no
longer allow the window to open.  Among other things, this prevents
opening an empty window when double-clicking on the icon for an
empty floppy drive.

desk/deskfpd.c
@@ -25,6 +25,7 @@
 #include "gemdos.h"
 #include "optimize.h"
 #include "optimopt.h"
+#include "gemerror.h"
 
 #include "deskbind.h"
 #include "deskglob.h"
@@ -225,6 +226,10 @@ FNODE *pn_sort(PNODE *pn)
 
 /*
  *  Build the filenode list for the specified pathnode
+ *
+ *  returns 0   0 or more files found without error
+ *          <0  error (other than ENMFIL) returned by dos_sfirst()/dos_snext()
+ *              (e.g. when attempting to open a floppy drive with no disk)
  */
 WORD pn_active(PNODE *pn)
 {
@@ -284,11 +289,12 @@ WORD pn_active(PNODE *pn)
     {
         dos_free(pn->p_fbase);
         pn->p_fbase = NULL;
-        return 0;
     }
-    dos_shrink(pn->p_fbase,count*sizeof(FNODE));
+    else
+    {
+        dos_shrink(pn->p_fbase,count*sizeof(FNODE));
+        pn->p_flist = pn_sort(pn);
+    }
 
-    pn->p_flist = pn_sort(pn);
-
-    return 0;   /* TODO: return error if error occurred? */
+    return (ret==ENMFIL) ? 0 : ret;
 }

desk/desksupp.c
@@ -390,9 +390,12 @@ WORD do_diropen(WNODE *pw, WORD new_win, WORD curr_icon,
 
     /* activate path by search and sort of directory */
     ret = pn_active(&pw->w_pnode);
-    if (ret != E_NOFILES)
+    if (ret < 0)    /* error reading directory */
     {
-        /* some error condition */
+        KDEBUG(("Error reading directory %s\n",pathname));
+        pn_close(&pw->w_pnode);
+        graf_mouse(ARROW, NULL);
+        return FALSE;
     }
 
     /* set new name and info lines for window */

commit ed7ffe5b960ca6b3d94818ec6480c8a995412077

Fix bug: desktop didn't open window for empty drives
This bug was likely introduced by commit 5973f1e.

desk/deskfpd.c
@@ -228,7 +228,7 @@ FNODE *pn_sort(PNODE *pn)
  *  Build the filenode list for the specified pathnode
  *
  *  returns 0   0 or more files found without error
- *          <0  error (other than ENMFIL) returned by dos_sfirst()/dos_snext()
+ *          <0  error (other than EFILNF/ENMFIL) returned by dos_sfirst()/dos_snext()
  *              (e.g. when attempting to open a floppy drive with no disk)
  */
 WORD pn_active(PNODE *pn)
@@ -296,5 +296,5 @@ WORD pn_active(PNODE *pn)
         pn->p_flist = pn_sort(pn);
     }
 
-    return (ret==ENMFIL) ? 0 : ret;
+    return ((ret==ENMFIL) || (ret==EFILNF)) ? 0 : ret;
 }