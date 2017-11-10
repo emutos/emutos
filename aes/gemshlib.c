emutos/emutos

commit 9e0a3362660d32cf3829f95a33c939dfde4b6c31

Do not display 'EMUCON' when launching desktop
This makes EmuTOS more like Atari TOS (which does not display a name
when the desktop is launched).  It also saves a few bytes and makes
the desktop launch imperceptibly faster.

aes/gemshlib.c
@@ -551,7 +551,6 @@ static WORD sh_ldapp(SHELL *psh)
     if (psh->sh_isdef && strcmp(D.s_cmd, DEF_DESKTOP) == 0)
     {
         /* Start the ROM desktop: */
-        sh_show(D.s_cmd);
         p_nameit(rlr, sh_name(D.s_cmd));
         p_setappdir(rlr, D.s_cmd);
         aes_run_rom_program(deskstart);

commit 1bc6e484771b6a1fef0b1c005c097227ac02aded

Fix menu display bug
When a program returns to the desktop, the line between the menu bar
and the rest of the desktop is occasionally not displayed.  To
reproduce: run QED, open a new window, enter text, exit without
saving anything.  This bug was introduced by commit 9e0a336.

aes/gemshlib.c
@@ -577,7 +577,7 @@ static WORD sh_ldapp(SHELL *psh)
     if (psh->sh_isdef && strcmp(D.s_cmd, DEF_DESKTOP) == 0)
     {
         /* Start the ROM desktop: */
-        sh_draw(D.s_cmd, ROOT, TRUE);   /* clear the screen */
+        sh_show("");        /* like TOS, we don't display a name */
         p_nameit(rlr, sh_name(D.s_cmd));
         p_setappdir(rlr, D.s_cmd);
         aes_run_rom_program(deskstart);