emutos/emutos

commit 6209f4883a4b6a334dc5d49160c10953292942a5

Fix bug in desktop window save
The status of a desktop window that was opened via an alt key was
not saved properly: specifically, the field corresponding to the
obid_save field was incorrect. This applied to both the EMUDESK.INF
file created via "Save desktop" and the information saved internally
when the desktop exited in order to run a program. As a result, the
window was not restored when the desktop restarted. This bug was
introduced by commit 44791b.

desk/desksupp.c
@@ -50,6 +50,7 @@
 #include "deskdir.h"
 #include "desksupp.h"
 #include "deskins.h"
+#include "deskobj.h"
 #include "nls.h"
 #include "scancode.h"
 #include "../bios/machine.h"
@@ -220,7 +221,7 @@ void do_xyfix(WORD *px, WORD *py)
 /*
  * open a window, normally corresponding to a disk drive icon on the desktop
  *
- * if curr < 0, there is no corresponding screen object, so we do not
+ * if curr == 0, there is no corresponding screen object, so we do not
  * do the zoom effect & we do not try to reset the object state
  */
 void do_wopen(WORD new_win, WORD wh, WORD curr, WORD x, WORD y, WORD w, WORD h)
@@ -230,12 +231,10 @@ void do_wopen(WORD new_win, WORD wh, WORD curr, WORD x, WORD y, WORD w, WORD h)
 
     do_xyfix(&x, &y);
 
-    if (curr >= 0)
+    if (curr > 0)
     {
         get_xywh(G.g_screen, G.g_croot, &c.g_x, &c.g_y, &c.g_w, &c.g_h);
         get_xywh(G.g_screen, curr, &d.g_x, &d.g_y, &d.g_w, &d.g_h);
-        d.g_x += c.g_x;
-        d.g_y += c.g_y;
         graf_growbox(d.g_x, d.g_y, d.g_w, d.g_h, x, y, w, h);
         act_chg(G.g_cwin, G.g_screen, G.g_croot, curr, &c, SELECTED, FALSE, TRUE, TRUE);
     }
@@ -323,13 +322,10 @@ WORD do_diropen(WNODE *pw, WORD new_win, WORD curr_icon,
     wind_set(pw->w_id, WF_INFO, pw->w_info, 0, 0);
 
     /* do actual wind_open  */
-    if (curr_icon)
-    {
-        do_wopen(new_win, pw->w_id, curr_icon,
-                    pt->g_x, pt->g_y, pt->g_w, pt->g_h);
-        if (new_win)
-            win_top(pw);
-    }
+    do_wopen(new_win, pw->w_id, curr_icon,
+                pt->g_x, pt->g_y, pt->g_w, pt->g_h);
+    if (new_win)
+        win_top(pw);
 
     /*
      * verify contents of window's object list
@@ -702,7 +698,11 @@ WORD do_dopen(WORD curr)
         pib = (ICONBLK *) get_iconblk_ptr(G.g_screen, curr);
         drv = pib->ib_char & 0x00ff;
     }
-    else drv = -curr;
+    else
+    {
+        drv = -curr;
+        curr = obj_get_obid(drv);
+    }
 
     pw = win_alloc(curr);
     if (pw)

desk/desk1.c
@@ -67,10 +67,16 @@ void true_closewnd(WNODE *pw)
     wind_get_grect(pw->w_id,WF_WXYWH, &rc);
     wind_close(pw->w_id);
 
-    icx = G.g_screen[pw->w_obid].ob_x;
-    icy = G.g_screen[pw->w_obid].ob_y;
-    graf_shrinkbox(icx, icy, G.g_wicon, G.g_hicon,
-                    rc.g_x, rc.g_y, rc.g_w, rc.g_h);
+    /*
+     * only do the shrinkbox effect if there is a matching icon
+     */
+    if (pw->w_obid > 0)
+    {
+        icx = G.g_screen[pw->w_obid].ob_x;
+        icy = G.g_screen[pw->w_obid].ob_y;
+        graf_shrinkbox(icx, icy, G.g_wicon, G.g_hicon,
+                        rc.g_x, rc.g_y, rc.g_w, rc.g_h);
+    }
 
     pn_close(pw->w_path);
     win_free(pw);

desk/deskapp.c
@@ -745,9 +745,8 @@ void app_start(void)
                 pcurr = scan_2(pcurr, &pws->h_save);
                 pws->h_save *= gl_hchar;
 /* */
-                pcurr = scan_2(pcurr, &pws->obid_save);
+                pcurr += 4;             /* skip no-longer-used field */
                 ptmp = pws->pth_save;
-                pcurr++;
                 while(*pcurr != '@')
                     *ptmp++ = *pcurr++;
                 *ptmp = '\0';
@@ -976,7 +975,7 @@ void app_save(WORD todisk)
         pcurr += sprintf(pcurr,"#W %02X %02X %02X %02X %02X %02X %02X",
                         0,pws->vsl_save,pws->x_save/gl_wchar,
                         pws->y_save/gl_hchar,pws->w_save/gl_wchar,
-                        pws->h_save/gl_hchar,pws->obid_save);
+                        pws->h_save/gl_hchar,0);
         pcurr += sprintf(pcurr," %s@\r\n",(*ptmp!='@')?ptmp:"");
     }
 

desk/deskapp.h
@@ -104,7 +104,6 @@ typedef struct
     WORD w_save;
     WORD h_save;
     WORD vsl_save;              /* vertical slider position */
-    WORD obid_save;             /* currently-selected object */
     BYTE pth_save[LEN_ZPATH];
 } WSAVE;
 

desk/deskmain.c
@@ -52,6 +52,7 @@
 #include "deskfun.h"
 #include "deskpro.h"
 #include "deskact.h"
+#include "deskobj.h"
 #include "desk1.h"
 #include "deskrez.h"
 #include "kprint.h"
@@ -1029,7 +1030,6 @@ static void cnx_put(void)
 
     for (i = 0, pws = &G.g_cnxsave.cs_wnode[NUM_WNODES-1]; i < unused; i++, pws--)
     {
-        pws->obid_save = 0;
         pws->pth_save[0] = 0;
     }
 
@@ -1044,7 +1044,6 @@ static void cnx_put(void)
         wind_get(pw->w_id,WF_CXYWH,&pws->x_save,&pws->y_save,&pws->w_save,&pws->h_save);
         do_xyfix(&pws->x_save,&pws->y_save);
         pws->vsl_save  = pw->w_cvrow;
-        pws->obid_save = pw->w_obid;
         strcpy(pws->pth_save,pw->w_path->p_spec);
         pws--;
     }
@@ -1099,11 +1098,13 @@ static void cnx_get(void)
 
         if (pws->pth_save[0])
         {
-            if ((pw = win_alloc(pws->obid_save)))
+            WORD obid = obj_get_obid(pws->pth_save[0]);
+
+            if ((pw = win_alloc(obid)))
             {
                 pw->w_cvrow = pws->vsl_save;
                 do_xyfix(&pws->x_save, &pws->y_save);
-                if (!do_diropen(pw, TRUE, pws->obid_save, pws->pth_save, (GRECT *)pws, TRUE))
+                if (!do_diropen(pw, TRUE, obid, pws->pth_save, (GRECT *)pws, TRUE))
                     win_free(pw);
             }
         }

desk/deskobj.c
@@ -238,3 +238,24 @@ WORD obj_ialloc(WORD wparent, WORD x, WORD y, WORD w, WORD h)
 
     return 0;
 }
+
+
+/*
+ *  Return the object number of the icon on the desktop corresponding
+ *  to the specified drive.
+ *
+ *  Returns 0 if no match
+ */
+WORD obj_get_obid(WORD drive)
+{
+    WORD objnum;
+
+    for (objnum = G.g_screen[DROOT].ob_head; objnum >= 0; objnum = G.g_screen[objnum].ob_next)
+    {
+        if (G.g_screen[objnum].ob_type == G_ICON)
+            if (LOBYTE(G.g_screeninfo[objnum].icon.block.ib_char) == drive)
+                return objnum;
+    }
+
+    return 0;
+}

desk/deskobj.h
@@ -14,5 +14,6 @@ void obj_init(void);
 WORD obj_walloc(WORD x, WORD y, WORD w, WORD h);
 void obj_wfree(WORD obj, WORD x, WORD y, WORD w, WORD h);
 WORD obj_ialloc(WORD wparent, WORD x, WORD y, WORD w, WORD h);
+WORD obj_get_obid(WORD drive);
 
 #endif  /* _DESKOBJ_H */

commit fae4158ce8a05c0147f0425a94205dd55acca7d3

Fix zoom effect when launching program in window
Commit 6209f48 caused the 'from' coordinates for the zoom effect to
be wrong.  Commit b83e054 fixed this for opening folders within a
window.  This commit fixes the problem when launching a program from
within a window.

desk/desksupp.c
@@ -259,8 +259,10 @@ void do_wopen(WORD new_win, WORD wh, WORD curr, WORD x, WORD y, WORD w, WORD h)
 
         if (!new_win)   /* no new window: window-relative coordinates */
         {
-            d.g_x += x; /* window to screen coordinates */
-            d.g_y += y;
+            GRECT c;
+            get_xywh(G.g_screen, G.g_croot, &c.g_x, &c.g_y, &c.g_w, &c.g_h);
+            d.g_x += c.g_x; /* convert window to screen coordinates */
+            d.g_y += c.g_y;
         }
 
         graf_growbox(d.g_x, d.g_y, d.g_w, d.g_h, x, y, w, h);