emutos/emutos

commit b0605190a1e159c4f05369226c17f5c2fd871f92

Allow desktop window file mask to be specified
By default, the file mask for a window is *.*, i.e. all files in the
selected folder are displayed.  The new menu item 'Set file mask...'
allows you to specify an alternate filemask to restrict the display,
just like Atari TOS 2/3/4.  Note that folders are always displayed,
irrespective of file mask.
This feature is disabled by default in the 192K ROMs.

desk/deskfpd.c
@@ -23,12 +23,15 @@
 #include "portab.h"
 #include "obdefs.h"
 #include "gemdos.h"
+#include "optimize.h"
 #include "optimopt.h"
 
 #include "deskbind.h"
 #include "deskglob.h"
 #include "deskapp.h"
+#include "deskdir.h"
 #include "deskfpd.h"
+#include "deskins.h"
 #include "deskwin.h"
 #include "dos.h"
 #include "deskrsrc.h"
@@ -297,6 +300,10 @@ WORD pn_active(PNODE *pn)
     FNODE *fn, *prev;
     LONG maxmem, maxcount, size = 0L;
     WORD count, ret;
+#if CONF_WITH_FILEMASK
+    BYTE search[MAXPATHLEN];
+    BYTE *match;
+#endif
 
     fl_free(pn);                    /* free any existing filenodes */
 
@@ -312,8 +319,24 @@ WORD pn_active(PNODE *pn)
 
     dos_sdta(&G.g_wdta);
 
+#if CONF_WITH_FILEMASK
+    /*
+     * we cannot use the pathnode specification as-is, because the filenode
+     * list must include all folders, not just the ones that match p_spec.
+     * so we use a file mask of *.* and do the wildcard matching ourselves.
+     */
+    strcpy(search, pn->p_spec);
+    del_fname(search);                  /* change search filespec to *.* */
+    match = filename_start(pn->p_spec); /* match filespec is unaltered */
+    for (ret = dos_sfirst(search, pn->p_attr), count = 0; (ret == 0) && (count < maxcount); ret = dos_snext())
+    {
+        if (G.g_wdta.d_attrib != F_SUBDIR)  /* skip *files* that don't match */
+            if (!wildcmp(match, G.g_wdta.d_fname))
+                continue;
+#else
     for (ret = dos_sfirst(pn->p_spec,pn->p_attr), count = 0; (ret == 0) && (count < maxcount); ret = dos_snext())
     {
+#endif
         if (G.g_wdta.d_fname[0] == '.') /* skip "." & ".." entries */
             continue;
         memcpy(&fn->f_junk, &G.g_wdta.d_reserved[20], 23);

desk/deskfun.c
@@ -153,6 +153,44 @@ void fun_rebld(BYTE *ptst)
 } /* fun_rebld */
 
 
+#if CONF_WITH_FILEMASK
+/*
+ *  Routine to update the file mask for the current window
+ */
+void fun_mask(WNODE *pw)
+{
+    BYTE *maskptr, filemask[LEN_ZFNAME];
+    OBJECT *tree;
+
+    tree = G.a_trees[ADFMASK];
+
+    /*
+     * get current filemask & insert in dialog
+     */
+    maskptr = filename_start(pw->w_path->p_spec);
+    fmt_str(maskptr, filemask);
+    inf_sset(tree, FMMASK, filemask);
+
+    /*
+     * get user input
+     */
+    show_hide(FMD_START, tree);
+    form_do(tree, 0);
+    show_hide(FMD_FINISH, tree);
+
+    /*
+     * if 'OK', extract filemask from dialog, update pnode/display
+     */
+    if (inf_what(tree, FMOK, FMCANCEL) == 1)
+    {
+        inf_sget(tree, FMMASK, filemask);
+        unfmt_str(filemask, maskptr);
+        do_refresh(pw);
+    }
+}
+#endif
+
+
 /*
  *  Routine that creates a new directory in the specified window/path
  */

desk/deskfun.h
@@ -28,6 +28,10 @@ WORD fun_alert_long(WORD defbut, WORD stnum, LONG merge);
 WORD fun_alert_string(WORD defbut, WORD stnum, BYTE *merge);
 #endif
 
+#if CONF_WITH_FILEMASK
+void fun_mask(WNODE *pw);
+#endif
+
 void fun_close(WNODE *pw, WORD closetype);
 BOOL fun_drag(WORD wh, WORD dest_wh, WORD sobj, WORD dobj, WORD mx, WORD my, WORD keystate);
 void fun_msg(WORD type, WORD w3, WORD w4, WORD w5, WORD w6, WORD w7);

desk/deskmain.c
@@ -143,9 +143,9 @@ static const BYTE     ILL_NOSEL[] = {OPENITEM,DELTITEM,
 static const BYTE     ILL_MULTSEL[] = {OPENITEM, IDSKITEM, SHOWITEM, 0};
 static const BYTE     ILL_TRASH[] = {OPENITEM,DELTITEM,IDSKITEM,
                                 IAPPITEM,0};
-static const BYTE     ILL_NOWIN[] = {NFOLITEM,CLOSITEM,CLSWITEM,0};
-static const BYTE     ILL_OPENWIN[] = {SHOWITEM,NFOLITEM,CLOSITEM,CLSWITEM,ICONITEM,
-                                NAMEITEM,DATEITEM,SIZEITEM,TYPEITEM,0};
+static const BYTE     ILL_NOWIN[] = {NFOLITEM,CLOSITEM,CLSWITEM,MASKITEM,0};
+static const BYTE     ILL_OPENWIN[] = {SHOWITEM,NFOLITEM,CLOSITEM,CLSWITEM,MASKITEM,
+                                ICONITEM,NAMEITEM,DATEITEM,SIZEITEM,TYPEITEM,0};
 
 /*
  * table to map the keyboard arrow character to the corresponding
@@ -339,6 +339,10 @@ static void men_update(void)
 #if !CONF_WITH_BACKGROUNDS
     menu_ienable(tree, BACKGRND, 0);
 #endif
+
+#if !CONF_WITH_FILEMASK
+    menu_ienable(tree, MASKITEM, 0);
+#endif
 }
 
 
@@ -418,6 +422,12 @@ static WORD do_filemenu(WORD item)
         if (pw)
             fun_close(pw, CLOSE_WINDOW);
         break;
+#if CONF_WITH_FILEMASK
+    case MASKITEM:
+        if (pw)
+            fun_mask(pw);
+        break;
+#endif
     case DELTITEM:
         if (curr)
             fun_del(curr);

include/config.h
@@ -239,6 +239,9 @@
 # ifndef CONF_WITH_BACKGROUNDS
 #  define CONF_WITH_BACKGROUNDS 0
 # endif
+# ifndef CONF_WITH_FILEMASK
+#  define CONF_WITH_FILEMASK 0
+# endif
 # ifndef CONF_WITH_PCGEM
 #  define CONF_WITH_PCGEM 0
 # endif
@@ -1017,6 +1020,14 @@
  */
 #ifndef CONF_WITH_BACKGROUNDS
 # define CONF_WITH_BACKGROUNDS 1
+#endif
+
+/*
+ * Set CONF_WITH_FILEMASK to 1 to allow the filemask used for desktop
+ * windows to be configured
+ */
+#ifndef CONF_WITH_FILEMASK
+# define CONF_WITH_FILEMASK 1
 #endif
 
 /*

commit cb7d311ffcb9dbff000a33a9a01ef4b36c562064

Fix bug when deleting files by dragging icon
If CONF_WITH_FILEMASK was #defined and:
. a desktop shortcut for a file/folder was dragged to the trash and
  the user chose to delete the file/folder, or
. a desktop shortcut for a file/folder was dragged to a disk icon
  or an open window (causing a disk move or copy),
then all the folders at the same level as the selected file/folder
were included in the operation, causing unwanted deletes/moves/copies.
This bug was introduced by commit b060519.

desk/deskfpd.c
@@ -227,11 +227,19 @@ FNODE *pn_sort(PNODE *pn)
 /*
  *  Build the filenode list for the specified pathnode
  *
+ *  if 'include_folders' is TRUE, the filenode list will include all
+ *  folders in the pathnode: this is required for correct display of
+ *  directory contents when the filemask is other than *.*
+ *
+ *  if 'include_folders' is FALSE, the filenode list will only include
+ *  files/folders matching the filespec in the pathnode: this is used by
+ *  fun_file2any() when dragging a desktop icon representing a file/folder
+ *
  *  returns 0   0 or more files found without error
  *          <0  error (other than EFILNF/ENMFIL) returned by dos_sfirst()/dos_snext()
  *              (e.g. when attempting to open a floppy drive with no disk)
  */
-WORD pn_active(PNODE *pn)
+WORD pn_active(PNODE *pn, BOOL include_folders)
 {
     FNODE *fn, *prev;
     LONG maxmem, maxcount, size = 0L;
@@ -256,14 +264,13 @@ WORD pn_active(PNODE *pn)
     dos_sdta(&G.g_wdta);
 
 #if CONF_WITH_FILEMASK
-    /*
-     * we cannot use the pathnode specification as-is, because the filenode
-     * list must include all folders, not just the ones that match p_spec.
-     * so we use a file mask of *.* and do the wildcard matching ourselves.
-     */
     strcpy(search, pn->p_spec);
-    del_fname(search);                  /* change search filespec to *.* */
-    match = filename_start(pn->p_spec); /* match filespec is unaltered */
+    /*
+     * do we want to include folders in the match list?
+     */
+    if (include_folders)                /* match all folders? */
+        del_fname(search);              /* yes - change search filespec to *.* */
+    match = filename_start(pn->p_spec); /* the match filespec is always unaltered */
     for (ret = dos_sfirst(search, pn->p_attr), count = 0; (ret == 0) && (count < maxcount); ret = dos_snext())
     {
         if (G.g_wdta.d_attrib != F_SUBDIR)  /* skip *files* that don't match */

desk/deskfpd.h
@@ -82,6 +82,6 @@ FNODE *fpd_ofind(FNODE *pf, WORD obj);
 void pn_close(PNODE *thepath);
 PNODE *pn_open(BYTE *pathname, WNODE *pw);
 FNODE *pn_sort(PNODE *pn);
-WORD pn_active(PNODE *thepath);
+WORD pn_active(PNODE *thepath, BOOL include_folders);
 
 #endif  /* _DESKFPD_H */

desk/deskfun.c
@@ -140,7 +140,7 @@ void fun_rebld(BYTE *ptst)
         /* if opened and same path then rebuild */
         if ( (pwin->w_id) && (strcmp(pwin->w_pnode.p_spec, ptst)==0) )
         {
-            pn_active(&pwin->w_pnode);
+            pn_active(&pwin->w_pnode, TRUE);
             desk_verify(pwin->w_id, TRUE);
             win_sinfo(pwin);
             wind_set(pwin->w_id, WF_INFO, pwin->w_info, 0, 0);
@@ -745,7 +745,7 @@ static WORD fun_file2any(WORD sobj, WNODE *wn_dest, ANODE *an_dest, FNODE *fn_de
 
     if (pn_src)
     {
-        okay = pn_active(pn_src);
+        okay = pn_active(pn_src, FALSE);
 
         if (pn_src->p_flist)
         {
@@ -891,7 +891,7 @@ static WORD delete_disk(ANODE *pa)
         return 0;
 
     graf_mouse(HGLASS, NULL);
-    pn_active(pn);
+    pn_active(pn, TRUE);
     if (pn->p_flist)
     {
         /*

desk/desksupp.c
@@ -389,7 +389,7 @@ WORD do_diropen(WNODE *pw, WORD new_win, WORD curr_icon,
     }
 
     /* activate path by search and sort of directory */
-    ret = pn_active(&pw->w_pnode);
+    ret = pn_active(&pw->w_pnode, TRUE);
     if (ret < 0)    /* error reading directory */
     {
         KDEBUG(("Error reading directory %s\n",pathname));