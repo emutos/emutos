emutos/emutos

commit dbaa64931702ad4dfd410bd85f9f4f82a6a8ba36

Simplify init of EmuDesk view/sort preferences
This also allows us to remove an unneeded global in GLOBES.

desk/deskmain.c
@@ -66,7 +66,7 @@ typedef struct {
 } KEYTAB;
 
 #define abs(x) ( (x) < 0 ? -(x) : (x) )
-#define menu_text(tree,inum,ptext) (((tree)+(inum))->ob_spec = (LONG)ptext)
+#define menu_text(tree,inum,ptext) (((tree)+(inum))->ob_spec = (LONG)(ptext))
 
 
 #define ESC     0x1b
@@ -520,15 +520,13 @@ static WORD do_viewmenu(WORD item)
 
     if (newview != G.g_iview)
     {
-        G.g_iview = newview;
         ptext = (newview == V_TEXT) ? ad_picon : ad_ptext;
         menu_text(G.a_trees[ADMENU], ICONITEM, ptext);
         rc |= VIEW_HAS_CHANGED;
     }
     if (newsort != G.g_isort)
     {
-        menu_icheck(G.a_trees[ADMENU], G.g_csortitem, 0);
-        G.g_csortitem = item;
+        menu_icheck(G.a_trees[ADMENU], G.g_isort, 0);
         menu_icheck(G.a_trees[ADMENU], item, 1);
         rc |= SORT_HAS_CHANGED;
     }
@@ -1081,8 +1079,8 @@ static void cnx_put(void)
     WSAVE *pws;
     WNODE *pw;
 
-    G.g_cnxsave.cs_view = (G.g_iview == V_ICON) ? 0 : 1;
-    G.g_cnxsave.cs_sort = G.g_csortitem - NAMEITEM;
+    G.g_cnxsave.cs_view = G.g_iview;        /* V_ICON/V_TEXT */
+    G.g_cnxsave.cs_sort = G.g_isort;        /* S_NAME etc */
     G.g_cnxsave.cs_confcpy = G.g_ccopypref;
     G.g_cnxsave.cs_confdel = G.g_cdelepref;
     G.g_cnxsave.cs_dblclick = G.g_cdclkpref;
@@ -1135,8 +1133,8 @@ static void cnx_get(void)
     WSAVE *pws;
     WNODE *pw;
 
-    G.g_iview = (G.g_cnxsave.cs_view == 0) ? V_TEXT : V_ICON;
-    do_viewmenu(ICONITEM);
+    if (G.g_cnxsave.cs_view != G.g_iview)
+        do_viewmenu(ICONITEM);      /* toggle display */
     do_viewmenu(NAMEITEM + G.g_cnxsave.cs_sort);
     G.g_ccopypref = G.g_cnxsave.cs_confcpy;
     G.g_cdelepref = G.g_cnxsave.cs_confdel;
@@ -1652,7 +1650,9 @@ WORD deskmain(void)
      */
     app_start();
 
-    /* initialize windows */
+    /*
+     * initialize windows: win_view() initialises g_iview, g_isort
+     */
     if (win_start() < 0)
     {
         KDEBUG(("insufficient memory for desktop windows (need %ld bytes)\n",
@@ -1663,11 +1663,8 @@ WORD deskmain(void)
     desk_verify(0, FALSE);      /* initialise g_croot, g_cwin, g_wlastsel  */
 
     /* establish menu items */
-    G.g_iview = V_ICON;
-    menu_text(G.a_trees[ADMENU], ICONITEM, ad_ptext);
-
-    G.g_csortitem = NAMEITEM;
-    menu_icheck(G.a_trees[ADMENU], G.g_csortitem, 1);
+    menu_text(G.a_trees[ADMENU], ICONITEM, (G.g_iview==V_ICON)?ad_ptext:ad_picon);
+    menu_icheck(G.a_trees[ADMENU], NAMEITEM+G.g_isort, 1);
 
     /* initialize desktop and its objects */
     app_blddesk();

desk/deskapp.c
@@ -789,7 +789,7 @@ void app_start(void)
         case 'E':                       /* Environment */
             pcurr++;
             pcurr = scan_2(pcurr, &envr);
-            G.g_cnxsave.cs_view = ( (envr & INF_E1_VIEWTEXT) != 0);
+            G.g_cnxsave.cs_view = (envr & INF_E1_VIEWTEXT) ? V_TEXT : V_ICON;
             G.g_cnxsave.cs_sort = ( (envr & INF_E1_SORTMASK) >> 5);
             G.g_cnxsave.cs_confdel = ( (envr & INF_E1_CONFDEL) != 0);
             G.g_cnxsave.cs_confcpy = ( (envr & INF_E1_CONFCPY) != 0);
@@ -980,7 +980,7 @@ void app_save(WORD todisk)
             pcurr += sprintf(pcurr,"#Z %02X %s@\r\n",pa->a_flags&AF_ISCRYS,pa->a_pappl);
 
     /* save environment */
-    env1 = (G.g_cnxsave.cs_view) ? INF_E1_VIEWTEXT : 0x00;
+    env1 = (G.g_cnxsave.cs_view == V_TEXT) ? INF_E1_VIEWTEXT : 0x00;
     env1 |= ((G.g_cnxsave.cs_sort) << 5) & INF_E1_SORTMASK;
     env1 |= (G.g_cnxsave.cs_confdel) ? INF_E1_CONFDEL : 0x00;
     env1 |= (G.g_cnxsave.cs_confcpy) ? INF_E1_CONFCPY : 0x00;

desk/deskbind.h
@@ -114,14 +114,14 @@ typedef struct
                                             /* view-related parms:      */
 /*GLOBAL*/ WORD         g_num;                  /* number of points     */
 /*GLOBAL*/ WORD         *g_pxy;                 /* outline pts to drag  */
-/*GLOBAL*/ WORD         g_iview;                /* current view type    */
+/*GLOBAL*/ WORD         g_iview;                /* current view type (V_ICON/V_TEXT) */
 /*GLOBAL*/ WORD         g_iwext;                /* w,h of extent of a   */
 /*GLOBAL*/ WORD         g_ihext;                /*   single item        */
 /*GLOBAL*/ WORD         g_iwint;                /* w,h of interval      */
 /*GLOBAL*/ WORD         g_ihint;                /*   between items      */
 /*GLOBAL*/ WORD         g_iwspc;                /* w,h of space used by */
 /*GLOBAL*/ WORD         g_ihspc;                /*   a single item      */
-/*GLOBAL*/ WORD         g_isort;                /* current sort type    */
+/*GLOBAL*/ WORD         g_isort;                /* current sort type (S_NAME etc) */
 
                                                 /* stack of fcb's to use*/
                                                 /*   for non-recursive  */
@@ -159,7 +159,6 @@ typedef struct
 /*GLOBAL*/ WORD         g_wlastsel;             /* window holding last  */
                                                 /*   selection          */
                                             /* current desktop preference values */
-/*GLOBAL*/ WORD         g_csortitem;            /* curr. sort item chked*/
 /*GLOBAL*/ WORD         g_ccopypref;            /* curr. copy pref.     */
 /*GLOBAL*/ WORD         g_cdelepref;            /* curr. delete pref.   */
 /*GLOBAL*/ WORD         g_covwrpref;            /* curr. overwrite pref.*/

commit a3068cbcc87e3761bea36d2097887f2038d56578

Fix bug in handling EmuDesk sort sequence
The menu item checkmark to indicate the current sort sequence was not
cleared correctly, resulting in multiple checkmarks being displayed.
This bug was introduced by commit dbaa649.

desk/deskfpd.h
@@ -17,6 +17,7 @@
 #ifndef _DESKFPD_H
 #define _DESKFPD_H
 #include "deskconf.h"
+#include "desk_rsc.h"           /* for ICONITEM, NAMEITEM etc */
 
 #define OP_COUNT 0
 #define OP_DELETE 1
@@ -34,11 +35,11 @@
  *
  * Note: folders are always listed before files, except for 'no sort'
  */
-#define S_NAME 0                /* file name (ascending) */
-#define S_DATE 1                /* date (newest first), then name */
-#define S_SIZE 2                /* size (largest first), then name */
-#define S_TYPE 3                /* file extension (ascending), then name */
-#define S_NSRT 4                /* no sort (directory sequence) */
+#define S_NAME (NAMEITEM-NAMEITEM)  /* file name (ascending) */
+#define S_TYPE (TYPEITEM-NAMEITEM)  /* file extension (ascending), then name */
+#define S_SIZE (SIZEITEM-NAMEITEM)  /* size (largest first), then name */
+#define S_DATE (DATEITEM-NAMEITEM)  /* date (newest first), then name */
+#define S_NSRT (NSRTITEM-NAMEITEM)  /* no sort (directory sequence) */
 #define START_SORT  S_NAME      /* default */
 
 #define E_NOERROR 0

desk/deskmain.c
@@ -495,19 +495,11 @@ static WORD do_viewmenu(WORD item)
         newview = (G.g_iview == V_ICON) ? V_TEXT : V_ICON;
         break;
     case NAMEITEM:
-        newsort = S_NAME;
-        break;
-    case DATEITEM:
-        newsort = S_DATE;
-        break;
-    case SIZEITEM:
-        newsort = S_SIZE;
-        break;
     case TYPEITEM:
-        newsort = S_TYPE;
-        break;
+    case SIZEITEM:
+    case DATEITEM:
     case NSRTITEM:
-        newsort = S_NSRT;
+        newsort = item - NAMEITEM;
         break;
 #if CONF_WITH_BACKGROUNDS
     case BACKGRND:
@@ -525,7 +517,7 @@ static WORD do_viewmenu(WORD item)
     }
     if (newsort != G.g_isort)
     {
-        menu_icheck(G.a_trees[ADMENU], G.g_isort, 0);
+        menu_icheck(G.a_trees[ADMENU], NAMEITEM+G.g_isort, 0);
         menu_icheck(G.a_trees[ADMENU], item, 1);
         rc |= SORT_HAS_CHANGED;
     }