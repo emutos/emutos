emutos/emutos

commit 14626b97d5ad93cbbb1767c2f33f667a2d3870ec

Replace magic numbers with #defines

bios/scsi.c
@@ -73,8 +73,12 @@
  * SCSI commands
  */
 #define REQUEST_SENSE   0x03
+#define READ6           0x08
+#define WRITE6          0x0a
 #define INQUIRY         0x12
 #define READ_CAPACITY   0x25
+#define READ10          0x28
+#define WRITE10         0x2a
 
 
 /*
@@ -1455,7 +1459,7 @@ int build_rw_command(UBYTE *cdb, UWORD rw, ULONG sector, UWORD count)
      */
     if ((sector <= 0x1fffffL) && (count <= 255))    /* we can use 6-byte CDBs */
     {
-        cdb[0] = (rw==RW_WRITE) ? 0x0a : 0x08;
+        cdb[0] = (rw==RW_WRITE) ? WRITE6 : READ6;
         cdb[1] = (sector >> 16) & 0x1f;
         cdb[2] = sector >> 8;
         cdb[3] = sector;
@@ -1464,7 +1468,7 @@ int build_rw_command(UBYTE *cdb, UWORD rw, ULONG sector, UWORD count)
         return 6;
     }
 
-    cdb[0] = (rw==RW_WRITE) ? 0x2a : 0x28;
+    cdb[0] = (rw==RW_WRITE) ? WRITE10 : READ10;
     cdb[1] = 0x00;
     cdb[2] = sector >> 24;
     cdb[3] = sector >> 16;

commit c2c083128c5bd2fc16f408927f383d107a0065d2

Fix compile error when building 192K ROMs
This bug was introduced by commit 14626b9.

bios/scsi.c
@@ -73,12 +73,8 @@
  * SCSI commands
  */
 #define REQUEST_SENSE   0x03
-#define READ6           0x08
-#define WRITE6          0x0a
 #define INQUIRY         0x12
 #define READ_CAPACITY   0x25
-#define READ10          0x28
-#define WRITE10         0x2a
 
 
 /*
@@ -1431,6 +1427,14 @@ static LONG scsi_inquiry(WORD dev, UBYTE *buffer)
 #endif
 
 #if (CONF_WITH_ACSI || CONF_WITH_SCSI)
+/*
+ * SCSI commands used by build_rw_command()
+ */
+#define READ6           0x08
+#define WRITE6          0x0a
+#define READ10          0x28
+#define WRITE10         0x2a
+
 /*
  * build ACSI/SCSI read/write command
  * returns length of command built