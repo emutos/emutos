emutos/emutos

commit 49d8373dcce9e67c1869ef9d2cdbd642ff393e56

Rewrite Fsfirst/Fsnext to fix design problem
Previously, the private area of the DTA contained a pointer to a DND.
This needed to be maintained until an ENMFIL condition was reached;
if this did not occur, a DND would remain allocated, which could make
it impossible to delete certain files until a reboot.

bdos/fsdir.c
@@ -461,14 +461,27 @@ long ixsfirst(char *name, WORD att, DTAINFO *addr)
     if ((f = scan(dn,s,att,&pos)) == (FCB*)NULL)
         return EFILNF;
 
-    KDEBUG(("\nixfirst(%s, DND=%p, DTA=%p)",s,dn,addr));
-
     if (addr)
     {
-        memcpy(&addr->dt_name[0], s, 12);
+        OFD *ofd = dn->d_ofd;
+        memcpy(addr->dt_name, s, 12);
+        if (!ofd->o_dnode)              /* i.e. root directory */
+        {
+            addr->dt_offset_drive = pos;
+            addr->dt_cloffset = 0;
+            addr->dt_clnum = 0;
+        }
+        else
+        {
+            addr->dt_offset_drive = 0L;
+            addr->dt_cloffset = ofd->o_curbyt;
+            addr->dt_clnum = ofd->o_curcl;
+        }
+        addr->dt_offset_drive |= dn->d_drv->m_drvnum & DTA_DRIVEMASK;
         addr->dt_attr = att;
-        addr->dt_pos = pos;
-        addr->dt_dnd = dn;
+
+        KDEBUG(("ixsfirst(%s,0x%02x,%p): DTA pvt=%08lx/%04x/%04x\n",
+                name,att,addr,addr->dt_offset_drive,addr->dt_cloffset,addr->dt_clnum));
 
         makbuf(f, addr);
     }
@@ -506,21 +519,119 @@ long xsfirst(char *name, int att)
     dt = (DTAINFO *)(run->p_xdta);          /* M01.01.1209.01 */
 
     /* set an indication of 'uninitialized DTA' */
-    dt->dt_dnd = (DND*)NULL;                /* M01.01.1209.01 */
-
-    KDEBUG(("\nxsfirst(%s, DTA=%p)",name,dt));
+    dt->dt_offset_drive = -1L;
 
     result = ixsfirst(name, att, dt);       /* M01.01.1209.01 */
 
     if ((result < 0) || !contains_wildcard_characters(name))
         return result;
 
-    /*
-     * an Fsnext() is likely, so we must mark the DND as in-use
-     */
-    dt->dt_dnd->d_usecount++;
-
     return E_OK;
+}
+
+
+/*
+ * ixsnest
+ */
+static FCB *ixsnext(DTAINFO *dt)
+{
+    char name[12];
+    char *buf, *bufend;
+    DMD *dmd;
+    BCB *bcb;
+    FCB *fcb;
+    LONG offset, recnum;
+    WORD drive, buftype, rootdirlen, found;
+    CLNO cluster = 0;
+
+    drive = dt->dt_offset_drive & DTA_DRIVEMASK;
+    if (drive >= BLKDEVNUM)
+        return NULL;
+
+    builds(dt->dt_name,name);   /* build FCB-style name */
+    name[11] = dt->dt_attr;
+
+    dmd = drvtbl[drive];
+
+    /*
+     * determine starting point
+     */
+    if ((dt->dt_cloffset == 0) && (dt->dt_clnum == 0))
+    {
+        buftype = BT_ROOT;
+        offset = dt->dt_offset_drive & ~DTA_DRIVEMASK;
+        recnum = offset >> dmd->m_rblog;
+        offset &= dmd->m_rbm;           /* within record */
+        rootdirlen = dmd->m_recoff[BT_DATA] - dmd->m_recoff[BT_ROOT];
+    }
+    else
+    {
+        buftype = BT_DATA;
+        offset = dt->dt_cloffset;       /* within cluster */
+        cluster = dt->dt_clnum;
+        recnum = cl2rec(cluster,dmd) + (offset >> dmd->m_rblog);
+        offset &= dmd->m_rbm;           /* within record */
+    }
+
+    KDEBUG(("ixsnext(%p): drv=%d,buftype=%d,recnum=%ld,offset=%ld\n",
+            dt,drive,buftype,recnum,offset));
+
+    /*
+     * search directory
+     */
+    found = 0;
+    while(1)
+    {
+        if (buftype == BT_ROOT)
+        {
+            if (recnum >= rootdirlen)       /* end of root */
+                break;
+        }
+        else
+        {
+            if (((recnum&dmd->m_clrm) == 0) && (offset == 0))   /* end of cluster */
+            {
+                cluster = getrealcl(cluster,dmd);
+                if (endofchain(cluster))    /* end of directory */
+                    break;
+                recnum = cl2rec(cluster,dmd);
+            }
+        }
+
+        bcb = getbcb(dmd,buftype,recnum);
+        buf = bcb->b_bufr;
+        bufend = buf + dmd->m_recsiz;
+        for (fcb = (FCB *)(buf+offset); fcb < (FCB *)bufend; fcb++) {
+            if (fcb->f_name[0] == 0x00)     /* never used, so must be end */
+                break;
+            if ((found = match(name,fcb->f_name)))
+                break;
+        }
+        if (found)
+            break;
+        recnum++;
+        offset = 0;
+    }
+
+    if (!found)
+        return NULL;
+
+    /*
+     * update the private area
+     */
+    offset = (char *)fcb - buf + sizeof(FCB);   /* to next FCB within buffer */
+    if (buftype == BT_ROOT)
+    {
+        dt->dt_offset_drive = (recnum << dmd->m_rblog) + offset;
+        dt->dt_offset_drive |= dmd->m_drvnum;
+    }
+    else
+    {
+        dt->dt_cloffset = ((recnum&dmd->m_clrm) << dmd->m_rblog) + offset;
+        dt->dt_clnum = cluster;
+    }
+
+    return fcb;
 }
 
 
@@ -535,24 +646,18 @@ long xsnext(void)
 {
     FCB *f;
     DTAINFO *dt;
-    DND *dn;
 
     dt = (DTAINFO *)run->p_xdta;            /* M01.01.1209.01 */
-    dn = dt->dt_dnd;
 
     /* has the DTA been initialized? */
-    if (dn == (DND *)NULL)                  /* M01.01.1209.01 */
-        return ENMFIL;                      /* M01.01.1209.01 */
+    if (dt->dt_offset_drive < 0L)
+        return ENMFIL;
 
-    KDEBUG(("\n xsnext(pos=%ld DTA=%p DND=%p)",dt->dt_pos,dt,dn));
+    f = ixsnext(dt);
 
-    f = scan(dn,dt->dt_name,dt->dt_attr,&dt->dt_pos);
-
-    if (f == (FCB *)NULL)       /* end of directory, no longer in-use */
+    if (f == NULL)                          /* end of directory */
     {
-        dn->d_usecount--;
-        if (dn->d_usecount < 0)             /* shouldn't happen */
-            dn->d_usecount = 0;
+        dt->dt_offset_drive = -1L;
         return ENMFIL;
     }
 

bdos/fs.h
@@ -319,16 +319,25 @@ FTAB
 
 DTAINFO
 {
-    char  dt_name[12];          /*  file name: filename.typ     00-11   */
-    long  dt_pos;               /*  dir position                12-15   */
-    DND   *dt_dnd;              /*  pointer to DND              16-19   */
-    char  dt_attr;              /*  attributes of file          20      */
-                                /*  --  below must not change -- [1]    */
+                            /* EmuTOS private area, subject to change   */
+    char  dt_name[12];          /*  file spec from Fsfirst()            */
+    LONG  dt_offset_drive;      /*  -1 => uninitialised DTA, else:      */
+                                /*   bits 4-0: drive id                 */
+                                /*   bits 31-5: if root, offset to next */
+                                /*    FCB, otherwise 0                  */
+    UWORD dt_cloffset;          /*  if subdir, offset within cluster to */
+                                /*   next FCB, otherwise 0              */
+    CLNO  dt_clnum;             /*  if subdir, current cluster number,  */
+                                /*   otherwise 0                        */
+    char  dt_attr;              /*  attribute from Fsfirst()            */
+                            /* public area, must not change             */
     char  dt_fattr;             /*  attrib from fcb             21      */
     DOSTIME dt_td;              /*  time, date fields from fcb  22-25   */
     long  dt_fileln;            /*  file length field from fcb  26-29   */
     char  dt_fname[14];         /*  file name from fcb          30-43   */
 } ;                             /*    includes null terminator          */
+
+#define DTA_DRIVEMASK   0x0000001fL
 
 /* the following structure is used to track current directories */
 typedef struct {
@@ -408,6 +417,7 @@ void bufl_init(void);
 void flush(BCB *b);
 /* return the ptr to the buffer containing the desired record */
 char *getrec(RECNO recn, OFD *of, int wrtflg);
+BCB *getbcb(DMD *dmd,WORD buftype,RECNO recnum);
 
 /*
  * in fsfat.c

bdos/fsbuf.c
@@ -113,7 +113,7 @@ void flush(BCB *b)
  *
  * buftype is BT_FAT, BT_ROOT, or BT_DATA
  */
-static BCB *getbcb(DMD *dmd,WORD buftype,RECNO recnum)
+BCB *getbcb(DMD *dmd,WORD buftype,RECNO recnum)
 {
     BCB *b;
     BCB *p, *mtbuf, **q, **phdr;

commit 5bfb8c216b62cc9f6c033e896a0b155c4b037f20

Improve Fsnext() performance
Fsnext() should stop its search when an unused directory entry is
encountered.  Instead, it continued until the end of the directory.
This fix significantly reduces the time to open a new floppy on the
desktop.  The bug was introduced by commit 49d8373.

bdos/fsdir.c
@@ -599,7 +599,7 @@ static FCB *ixsnext(DTAINFO *dt)
         bufend = buf + dmd->m_recsiz;
         for (fcb = (FCB *)(buf+offset); fcb < (FCB *)bufend; fcb++) {
             if (fcb->f_name[0] == 0x00)     /* never used, so must be end */
-                break;
+                return NULL;
             if ((found = match(name,fcb->f_name)))
                 break;
         }