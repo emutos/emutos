emutos/emutos

commit d4c07d12a38c96a0abc58d158d8031b447f1b2df

Always set floppy density
On a TT system, it is possible to have a DD drive rather than an
HD drive; this is indicated by one of the DIP switches.  In this
case, the hardware register that selects density ($ff860f) was not
initialised, which could cause problems.  This is now fixed.

bios/floppy.c
@@ -291,7 +291,7 @@ static void flop_init(WORD dev)
 
     f->rate = seekrate;
 #if CONF_WITH_FDC
-    f->actual_rate = f->rate;       /* updated by set_density() if HD drive */
+    /* actual_rate is set by set_density() which is called by floplock() */
     f->drive_type = (cookie_fdc >> 24) ? HD_DRIVE : DD_DRIVE;
     f->cur_density = (f->drive_type == HD_DRIVE) ? DENSITY_HD : DENSITY_DD;
     f->cur_track = -1;
@@ -1120,13 +1120,13 @@ static WORD flopwtrack(UBYTE *userbuf, WORD dev, WORD track, WORD side, WORD tra
 /*==== internal status, flopvbl ===========================================*/
 
 /*
- * set density & update step rate (HD drives only)
+ * set density & update step rate
+ *
+ * note: on pre-HD floppy systems (i.e. before the TT), writing to
+ * 'modectl' is safe (does nothing)
  */
 static void set_density(struct flop_info *f)
 {
-    if (f->drive_type != HD_DRIVE)
-        return;
-
     DMA->modectl = f->cur_density;
     f->actual_rate = (f->cur_density == DENSITY_HD) ? hd_steprate[f->rate] : f->rate;
 }
@@ -1165,7 +1165,7 @@ struct flop_info *f = &finfo[dev];
         }
     }
 
-    /* set density if required */
+    /* set density (if applicable) */
     set_density(f);
 }
 

commit 714eabe78f406b3cc25591fc4aa47e9b2ffe66c1

Fix startup crash on certain ST systems
On some ST systems, accessing the DMA 'modectl' register causes a
bus error (this register is only defined for systems with HD floppy
support, but it can be written without problems on many ST systems).
This bug was introduced by commit d4c07d1.

bios/floppy.c
@@ -1198,13 +1198,12 @@ static WORD flopwtrack(UBYTE *userbuf, WORD dev, WORD track, WORD side, WORD tra
 
 /*
  * set density & update step rate
- *
- * note: on pre-HD floppy systems (i.e. before the TT), writing to
- * 'modectl' is safe (does nothing)
  */
 static void set_density(struct flop_info *f)
 {
-    DMA->modectl = f->cur_density;
+    if (has_modectl)
+        DMA->modectl = f->cur_density;
+
     f->actual_rate = (f->cur_density == DENSITY_HD) ? hd_steprate[f->rate] : f->rate;
 }
 

bios/machine.c
@@ -36,6 +36,7 @@
 #include "scc.h"
 #include "memory.h"
 #include "coldfire.h"
+#include "dma.h"
 #include "nova.h"
 #ifdef MACHINE_AMIGA
 #include "amiga.h"
@@ -59,7 +60,7 @@ long cookie_swi;
 /*
  * test specific hardware features
  */
-
+int has_modectl;
 #if CONF_WITH_STE_SHIFTER
 int has_ste_shifter;
 #endif
@@ -69,6 +70,22 @@ int has_tt_shifter;
 #if CONF_WITH_VIDEL
 int has_videl;
 #endif
+
+/*
+ * Check if the DMA 'modectl' register exists
+ *
+ * A bus error _may_ occur when accessing 'modectl' on some systems that
+ * do not support HD floppies [Christian Zietz's investigations indicate
+ * that the error occurs on systems with the IMP chip set].
+ */
+static void detect_modectl(void)
+{
+    has_modectl = 0;
+    if (check_read_byte((LONG)&DMA->modectl))
+        has_modectl = 1;
+
+    KDEBUG(("has_modectl = %d\n", has_modectl));
+}
 
 /*
  * Tests video capabilities (STEnhanced Shifter, TT Shifter and VIDEL)
@@ -424,6 +441,7 @@ void machine_detect(void)
 #ifdef MACHINE_AMIGA
     amiga_machine_detect();
 #endif
+    detect_modectl();
 
     /* Detect TT-RAM and set up ramtop/ramvalid */
     KDEBUG(("ttram_detect()\n"));

bios/machine.h
@@ -170,6 +170,8 @@ extern int has_dip_switches;
   #define HAS_DIP_SWITCHES 0
 #endif
 
+extern int has_modectl;
+
 /*
  * functions
  */