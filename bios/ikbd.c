emutos/emutos

commit c2f25da5655eb2849053bbb9df6e8a5da3905cd4

Fix bug in emulated mouse handling
When using the keyboard for mouse movement, pressing a new arrow key
caused the mouse emulation to terminate, just as though you released
the Alt key.  This was due to the wrong argument being passed to
handle_mouse_mode().  This bug has existed since the mouse emulation
code was introduced by commit a7bf8d8.

bios/ikbd.c
@@ -664,7 +664,7 @@ void kbd_int(UBYTE scancode)
         default:                    /* non-modifier keys: */
             kb_ticks = 0;               /*  stop key repeat */
         }
-        handle_mouse_mode(kb_last.key); /* exit mouse mode if appropriate */
+        handle_mouse_mode(scancode);    /* exit mouse mode if appropriate */
         return;
     }
 
@@ -709,7 +709,7 @@ void kbd_int(UBYTE scancode)
         /*
          * check if an arrow key was already down and, if so, send the appropriate mouse packet
          */
-        handle_mouse_mode(kb_last.key);
+        handle_mouse_mode(scancode);
         return;
     }
 

commit 47ee881a2db5b89134486b20c52ebfa91b0f13bf

Fix bug in mouse emulation
In mouse emulation mode, releasing an arrow key generated a mouse
packet, causing an unwanted mouse cursor movement.
This bug was introduced by commit c2f25da5.

bios/ikbd.c
@@ -290,7 +290,7 @@ static BOOL handle_mouse_mode(WORD newkey)
      * if we shouldn't be in emulation mode, but we are, send an
      * appropriate mouse packet and exit
      */
-    if (!(shifty&MODE_ALT) || !is_mouse_key(newkey)) {
+    if (!(shifty&MODE_ALT) || !is_mouse_key(newkey & ~KEY_RELEASED)) {
         if (mouse_packet[0]) {  /* emulating, need to clean up */
             init_mouse_packet(mouse_packet);
             call_mousevec(mouse_packet);
@@ -657,8 +657,7 @@ void kbd_int(UBYTE scancode)
     }
 
     if (scancode & KEY_RELEASED) {
-        scancode = scancode_only;
-        switch (scancode) {
+        switch (scancode_only) {
         case KEY_RSHIFT:
             shifty &= ~MODE_RSHIFT;     /* clear bit */
             break;