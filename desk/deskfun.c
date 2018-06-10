emutos/emutos

commit 5b8713a2ec3e9086e7d7ef9a4cd7878ea8019655

Open new window with Alt+doubleclick on folder
This provides the same desktop function as Atari TOS 2/3/4.

desk/desksupp.c
@@ -828,7 +828,7 @@ WORD do_dopen(WORD curr)
 void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD redraw)
 {
     GRECT t;
-    WORD new_win;
+    WORD junk, keystate, new_win = FALSE;
     BYTE app_path[MAXPATHLEN];
 
     wind_get_grect(pw->w_id, WF_WXYWH, &t);
@@ -862,10 +862,22 @@ void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD redraw)
             return;
         }
         strcpy(p,"*.*");
+        new_win = TRUE;
+    }
+    else
+#endif
+    {
+        graf_mkstate(&junk, &junk, &junk, &keystate);
+        if (keystate & MODE_ALT)
+            new_win = TRUE;
+    }
 
-        /*
-         * handle opening directory on the desktop
-         */
+    /*
+     * if we are opening a folder on the desktop, or holding down the Alt
+     * key when opening a folder in a window, we need to create a new window
+     */
+    if (new_win)
+    {
         pw = win_alloc(curr);
         if (!pw)
         {
@@ -873,14 +885,10 @@ void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD redraw)
             return;
         }
         rc_copy((GRECT *)&G.g_screen[pw->w_root].ob_x,&t);
-
-        new_win = TRUE;
     }
     else
-#endif
     {
         pn_close(pw->w_path);
-        new_win = FALSE;
     }
 
     if (!do_diropen(pw, new_win, curr, app_path, &t, redraw))

commit 56bb73d3a2fa7a649676ddcd96559fa34d515a0a

Fix bug when closing folder via mouse emulation
If you try to close a (non-root) folder by "left-clicking" on the
closer gadget using the keyboard mouse emulation (Alt-Insert), and
you do not release the Alt key immediately, EmuDesk will instead
leave the existing window unchanged, and open another window
showing the desired folder.  Bug reported by David Galvez.
This bug was likely introduced by commit 5b8713a.

desk/deskfun.c
@@ -453,7 +453,8 @@ static void w_setpath(WNODE *pw, BYTE *pathname)
     graf_shrinkbox(icx, icy, G.g_wicon, G.g_hicon,
                     rc.g_x, rc.g_y, rc.g_w, rc.g_h);
 
-    do_fopen(pw, 0, pathname, TRUE);
+    /* we're closing a folder, so we never want a new window */
+    do_fopen(pw, 0, pathname, FALSE);
 }
 
 

desk/desksupp.c
@@ -884,8 +884,12 @@ WORD do_dopen(WORD curr)
 
 /*
  *  Open a folder
+ *
+ *  the folder is opened in a new window in two cases:
+ *  1) desktop shortcuts are configured & the current window is the desktop, or
+ *  2) 'allow_new_win' is TRUE and the Alt key is pressed
  */
-void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD redraw)
+void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD allow_new_win)
 {
     GRECT t;
     WORD junk, keystate, new_win = FALSE;
@@ -926,6 +930,7 @@ void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD redraw)
     }
     else
 #endif
+    if (allow_new_win)
     {
         graf_mkstate(&junk, &junk, &junk, &keystate);
         if (keystate & MODE_ALT)
@@ -951,7 +956,7 @@ void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD redraw)
         pn_close(&pw->w_pnode);
     }
 
-    if (!do_diropen(pw, new_win, curr, app_path, &t, redraw))
+    if (!do_diropen(pw, new_win, curr, app_path, &t, TRUE))
     {
         if (new_win)
             win_free(pw);
@@ -1422,7 +1427,8 @@ void refresh_window(WNODE *pw)
     if (!pw->w_id)      /* desktop */
         return;
 
-    do_fopen(pw, 0, pw->w_pnode.p_spec, TRUE);
+    /* make sure we don't open a new window */
+    do_fopen(pw, 0, pw->w_pnode.p_spec, FALSE);
 }
 
 

desk/desksupp.h
@@ -25,7 +25,7 @@ WORD do_diropen(WNODE *pw, WORD new_win, WORD curr_icon,
                 BYTE *pathname, GRECT *pt, WORD redraw);
 WORD do_aopen(ANODE *pa, WORD isapp, WORD curr, BYTE *pathname, BYTE *pname, BYTE *tail);
 WORD do_dopen(WORD curr);
-void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD redraw);
+void do_fopen(WNODE *pw, WORD curr, BYTE *pathname, WORD allow_new_win);
 WORD do_open(WORD curr);
 WORD do_info(WORD curr);
 void do_format(void);