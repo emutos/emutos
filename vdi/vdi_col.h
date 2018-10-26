3cfa923f2777e33c1ae32f273677b17a2cb92f26
Roger Burrows <anodynesoftware@users.sourceforge.net>
2018-10-25 20:54:21 -0400

    Remove unused header

--- vdi/vdi_col.h
+++
@@ -1,9 +0,0 @@
-/*
- * vdi_col.h - VDI color palette functions and tables.
- *
- * Copyright 2005-2016 by The EmuTOS development team.
- *
- * This file is distributed under the GPL, version 2 or at your
- * option any later version.  See doc/license.txt for details.
- */
-
