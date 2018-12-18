emutos/emutos

commit 39d71b1c7123cd8f8037dc16be24fc875f648f72

Do not allow EmuCON to start up in low resolution
If EmuCON is started in ST low, it will now switch to ST medium
before displaying its startup message.  Other resolutions are
unaffected.

cli/cmdmain.c
@@ -62,12 +62,6 @@ int cmdmain(void)
 WORD argc, rc;
 ULONG n;
 
-    clear_screen();
-    enable_cursor();
-    message(_("Welcome to EmuCON2 version ")); messagenl(version);
-    messagenl(_("Type HELP for builtin commands"));
-    messagenl("");
-
     /*
      *  initialise some global variables
      */
@@ -85,11 +79,23 @@ ULONG n;
     original_res = (vdo_value < _VDO_TT) ? Getrez() : -1;
 #endif
 #endif  /* STANDALONE_CONSOLE */
+    current_res = original_res;
+
+    /*
+     * start up in ST medium if we are currently in ST low
+     */
+    if (current_res == ST_LOW)
+        change_res(ST_MEDIUM);
+
+    clear_screen();
+    enable_cursor();
+    message(_("Welcome to EmuCON2 version ")); messagenl(version);
+    messagenl(_("Type HELP for builtin commands"));
+    messagenl("");
 
     linewrap = 0;
     dta = (DTA *)Fgetdta();
     redir_handle = -1L;
-    current_res = original_res;
 
     while(1) {
         n = getwh();        /* get max cell number for x and y */

commit e6460ab836522fbe229f777d522884e8c8248af5

EmuCON: Change resolution on Atari hardware only
Setscreen() may not be fully implemented on non-Atari hardware.
Currently, it hangs on M548x targets.
This fixes a regression in commit 39d71b1c.

cli/cmdmain.c
@@ -208,7 +208,7 @@ int i;
  */
 PRIVATE void change_res(WORD res)
 {
-#ifndef STANDALONE_CONSOLE
+#if !defined(STANDALONE_CONSOLE) && CONF_ATARI_HARDWARE
     if (res == current_res)
         return;
 