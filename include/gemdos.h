emutos/emutos

commit 018682b5cb1fefe3e4ad72cc7bbab9afd1d81c70

Use dos_xxx() functions instead of trap1() in VDI
This makes the code more readable and saves some ROM space.

include/gemdos.h
@@ -12,6 +12,7 @@
 
 WORD pgmld(WORD handle, BYTE *pname, LONG **ldaddr);
 
+LONG dos_rawio(WORD ch);
 LONG dos_rawcin(void);
 void dos_conws(BYTE *string);
 WORD dos_conis(void);

util/gemdos.c
@@ -28,7 +28,7 @@ extern LONG gemdos(short, ...);
 
 #define X_TABOUT 0x02
 #define X_PRTOUT 0x05
-#define X_RAWCON 0x06
+#define X_RAWIO  0x06
 #define X_RAWCIN 0x07
 #define X_CONWS  0x09
 #define X_CONIS  0x0B
@@ -95,13 +95,6 @@ void chrout(WORD chr)
 */
 
 /*
-BYTE rawcon(WORD parm)
-{
-    return( (BYTE)gemdos(X_RAWCON, parm) );
-}
-*/
-
-/*
 void prt_chr(WORD chr)
 {
     return( gemdos(X_PRTOUT,chr) );
@@ -115,6 +108,12 @@ void dos_func(UWORD function, LONG parm)
     return( gemdos(function,parm) );
 }
 */
+
+
+LONG dos_rawio(WORD ch)
+{
+    return gemdos(X_RAWIO,ch);
+}
 
 
 LONG dos_rawcin(void)

vdi/vdi_control.c
@@ -11,6 +11,7 @@
 
 #include "config.h"
 #include "portab.h"
+#include "gemdos.h"
 #include "../bios/lineavars.h"
 #include "vdi_defs.h"
 /* #include "kprint.h" */
@@ -342,7 +343,7 @@ void vdi_v_opnvwk(Vwk * vwk)
     /*
      * Allocate the memory for a virtual workstation
      */
-    vwk = (Vwk *)trap1(X_MALLOC, (LONG)(sizeof(Vwk)));
+    vwk = (Vwk *)dos_alloc_stram(sizeof(Vwk));
     if (vwk == NULL) {
         CONTRL[6] = 0;  /* No memory available, exit */
         return;
@@ -380,7 +381,7 @@ void vdi_v_clsvwk(Vwk * vwk)
      */
     CUR_WORK = &phys_work;
 
-    trap1(X_MFREE, vwk);
+    dos_free(vwk);
 }
 
 
@@ -451,7 +452,7 @@ void vdi_v_clswk(Vwk * vwk)
     /* close all open virtual workstations */
     for (handle = VDI_PHYS_HANDLE+1, p = vwk_ptr+handle; handle <= LAST_VDI_HANDLE; handle++, p++) {
         if (*p) {
-            trap1(X_MFREE, *p);
+            dos_free(*p);
             *p = NULL;
         }
     }

vdi/vdi_esc.c
@@ -11,6 +11,7 @@
 
 #include "config.h"
 #include "portab.h"
+#include "gemdos.h"
 #include "vdi_defs.h"
 #include "../bios/lineavars.h"
 #include "asm.h"
@@ -51,7 +52,7 @@ static void escfn1(Vwk * vwk)
  */
 static void escfn2(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033f\033E");  /* hide alpha cursor */
+    dos_conws("\033f\033E");    /* hide alpha cursor */
     vdi_v_clrwk(vwk);
 }
 
@@ -62,7 +63,7 @@ static void escfn2(Vwk * vwk)
 static void escfn3(Vwk * vwk)
 {
     vdi_v_clrwk(vwk);
-    trap1(X_CCONWS, "\033E\033e");  /* show alpha cursor */
+    dos_conws("\033E\033e");    /* show alpha cursor */
 }
 
 
@@ -71,7 +72,7 @@ static void escfn3(Vwk * vwk)
  */
 static void escfn4(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033A");
+    dos_conws("\033A");
 }
 
 
@@ -80,7 +81,7 @@ static void escfn4(Vwk * vwk)
  */
 static void escfn5(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033B");
+    dos_conws("\033B");
 }
 
 
@@ -89,7 +90,7 @@ static void escfn5(Vwk * vwk)
  */
 static void escfn6(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033C");
+    dos_conws("\033C");
 }
 
 
@@ -98,7 +99,7 @@ static void escfn6(Vwk * vwk)
  */
 static void escfn7(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033D");
+    dos_conws("\033D");
 }
 
 
@@ -107,7 +108,7 @@ static void escfn7(Vwk * vwk)
  */
 static void escfn8(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033H");
+    dos_conws("\033H");
 }
 
 
@@ -116,7 +117,7 @@ static void escfn8(Vwk * vwk)
  */
 static void escfn9(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033J");
+    dos_conws("\033J");
 }
 
 
@@ -125,7 +126,7 @@ static void escfn9(Vwk * vwk)
  */
 static void escfn10(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033K");
+    dos_conws("\033K");
 }
 
 
@@ -149,7 +150,7 @@ static void escfn11(Vwk * vwk)
     out[2] = 0x20 + INTIN[0] - 1;   /* zero-based */
     out[3] = 0x20 + INTIN[1] - 1;
     out[4] = '\0';
-    trap1(X_CCONWS, out);
+    dos_conws(out);
 }
 
 
@@ -172,7 +173,7 @@ static void escfn12(Vwk * vwk)
     chr = INTIN;                /* address of the character array */
 
     while (cnt--) {
-        trap1(X_CRAWIO, *chr++);/* raw i/o to standard input/output */
+        dos_rawio(*chr++);      /* raw i/o to standard input/output */
     }
 }
 
@@ -182,7 +183,7 @@ static void escfn12(Vwk * vwk)
  */
 static void escfn13(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033p");   /* enter reverse video */
+    dos_conws("\033p");     /* enter reverse video */
 }
 
 
@@ -191,7 +192,7 @@ static void escfn13(Vwk * vwk)
  */
 static void escfn14(Vwk * vwk)
 {
-    trap1(X_CCONWS, "\033q");   /* enter normal video */
+    dos_conws("\033q");     /* enter normal video */
 }
 
 

vdi/vdi_defs.h
@@ -16,12 +16,6 @@
 
 #define HAVE_BEZIER 0           /* switch on bezier capability */
 
-/* GEMDOS function numbers and parameters */
-#define X_CRAWIO    0x06        /* raw i/o to standard input/output */
-#define X_CCONWS    0x09        /* write null terminated string to std output */
-#define X_MALLOC    0x48
-#define X_MFREE     0x49
-
 
 /*
  * some minima and maxima

commit b0fcd338e90db8ce58976974bf8e7220122f6d20

Fix broken cartridge target
Commit 018682b broke the 'cart' target because util/gemdos.c is not
compiled into the cartridge, and therefore the dos_xxx() functions
are not available to the VDI.  This fix reverts to using trap1(), but
via an intermediate function for space efficiency.

include/gemdos.h
@@ -12,7 +12,6 @@
 
 WORD pgmld(WORD handle, BYTE *pname, LONG **ldaddr);
 
-LONG dos_rawio(WORD ch);
 LONG dos_rawcin(void);
 void dos_conws(BYTE *string);
 WORD dos_conis(void);

util/gemdos.c
@@ -110,12 +110,6 @@ void dos_func(UWORD function, LONG parm)
 */
 
 
-LONG dos_rawio(WORD ch)
-{
-    return gemdos(X_RAWIO,ch);
-}
-
-
 LONG dos_rawcin(void)
 {
     return gemdos(X_RAWCIN);

vdi/vdi_control.c
@@ -11,7 +11,6 @@
 
 #include "config.h"
 #include "portab.h"
-#include "gemdos.h"
 #include "../bios/lineavars.h"
 #include "vdi_defs.h"
 /* #include "kprint.h" */
@@ -20,6 +19,9 @@
 #include "asm.h"
 #include "string.h"
 #include "intmath.h"
+
+#define X_MALLOC 0x48
+#define X_MFREE 0x49
 
 #define FIRST_VDI_HANDLE    1
 #define LAST_VDI_HANDLE     (FIRST_VDI_HANDLE+NUM_VDI_HANDLES-1)
@@ -343,7 +345,7 @@ void vdi_v_opnvwk(Vwk * vwk)
     /*
      * Allocate the memory for a virtual workstation
      */
-    vwk = (Vwk *)dos_alloc_stram(sizeof(Vwk));
+    vwk = (Vwk *)trap1(X_MALLOC, sizeof(Vwk));
     if (vwk == NULL) {
         CONTRL[6] = 0;  /* No memory available, exit */
         return;
@@ -381,7 +383,7 @@ void vdi_v_clsvwk(Vwk * vwk)
      */
     CUR_WORK = &phys_work;
 
-    dos_free(vwk);
+    trap1(X_MFREE, vwk);
 }
 
 
@@ -452,7 +454,7 @@ void vdi_v_clswk(Vwk * vwk)
     /* close all open virtual workstations */
     for (handle = VDI_PHYS_HANDLE+1, p = vwk_ptr+handle; handle <= LAST_VDI_HANDLE; handle++, p++) {
         if (*p) {
-            dos_free(*p);
+            trap1(X_MFREE, *p);
             *p = NULL;
         }
     }

vdi/vdi_esc.c
@@ -11,7 +11,6 @@
 
 #include "config.h"
 #include "portab.h"
-#include "gemdos.h"
 #include "vdi_defs.h"
 #include "../bios/lineavars.h"
 #include "asm.h"
@@ -21,6 +20,27 @@
 /* Local Constants */
 
 #define ldri_escape             19      /* last DRI escape = 19. */
+
+#define X_RAWIO  0x06
+#define X_CONWS  0x09
+
+
+/*
+ * do console raw i/o
+ */
+static LONG crawio(WORD ch)
+{
+    return trap1(X_RAWIO,ch);
+}
+
+
+/*
+ * write string to console
+ */
+static void cconws(BYTE *string)
+{
+    trap1(X_CONWS, string);
+}
 
 
 /*
@@ -52,7 +72,7 @@ static void escfn1(Vwk * vwk)
  */
 static void escfn2(Vwk * vwk)
 {
-    dos_conws("\033f\033E");    /* hide alpha cursor */
+    cconws("\033f\033E");       /* hide alpha cursor */
     vdi_v_clrwk(vwk);
 }
 
@@ -63,7 +83,7 @@ static void escfn2(Vwk * vwk)
 static void escfn3(Vwk * vwk)
 {
     vdi_v_clrwk(vwk);
-    dos_conws("\033E\033e");    /* show alpha cursor */
+    cconws("\033E\033e");       /* show alpha cursor */
 }
 
 
@@ -72,7 +92,7 @@ static void escfn3(Vwk * vwk)
  */
 static void escfn4(Vwk * vwk)
 {
-    dos_conws("\033A");
+    cconws("\033A");
 }
 
 
@@ -81,7 +101,7 @@ static void escfn4(Vwk * vwk)
  */
 static void escfn5(Vwk * vwk)
 {
-    dos_conws("\033B");
+    cconws("\033B");
 }
 
 
@@ -90,7 +110,7 @@ static void escfn5(Vwk * vwk)
  */
 static void escfn6(Vwk * vwk)
 {
-    dos_conws("\033C");
+    cconws("\033C");
 }
 
 
@@ -99,7 +119,7 @@ static void escfn6(Vwk * vwk)
  */
 static void escfn7(Vwk * vwk)
 {
-    dos_conws("\033D");
+    cconws("\033D");
 }
 
 
@@ -108,7 +128,7 @@ static void escfn7(Vwk * vwk)
  */
 static void escfn8(Vwk * vwk)
 {
-    dos_conws("\033H");
+    cconws("\033H");
 }
 
 
@@ -117,7 +137,7 @@ static void escfn8(Vwk * vwk)
  */
 static void escfn9(Vwk * vwk)
 {
-    dos_conws("\033J");
+    cconws("\033J");
 }
 
 
@@ -126,7 +146,7 @@ static void escfn9(Vwk * vwk)
  */
 static void escfn10(Vwk * vwk)
 {
-    dos_conws("\033K");
+    cconws("\033K");
 }
 
 
@@ -150,7 +170,7 @@ static void escfn11(Vwk * vwk)
     out[2] = 0x20 + INTIN[0] - 1;   /* zero-based */
     out[3] = 0x20 + INTIN[1] - 1;
     out[4] = '\0';
-    dos_conws(out);
+    cconws(out);
 }
 
 
@@ -173,7 +193,7 @@ static void escfn12(Vwk * vwk)
     chr = INTIN;                /* address of the character array */
 
     while (cnt--) {
-        dos_rawio(*chr++);      /* raw i/o to standard input/output */
+        crawio(*chr++);         /* raw i/o to standard input/output */
     }
 }
 
@@ -183,7 +203,7 @@ static void escfn12(Vwk * vwk)
  */
 static void escfn13(Vwk * vwk)
 {
-    dos_conws("\033p");     /* enter reverse video */
+    cconws("\033p");        /* enter reverse video */
 }
 
 
@@ -192,7 +212,7 @@ static void escfn13(Vwk * vwk)
  */
 static void escfn14(Vwk * vwk)
 {
-    dos_conws("\033q");     /* enter normal video */
+    cconws("\033q");        /* enter normal video */
 }
 
 