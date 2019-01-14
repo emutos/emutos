emutos/emutos

commit f405c6bac3ab9fba6bcaca850a0861e323935330

Fix bug in wind_set() for WF_NAME/WF_INFO modes
Unlike Atari TOS, EmuTOS did not redraw the name and info lines of
background windows at the time they were updated.  This showed up as
a bug in EmuDesk: when a file/folder was dropped on to a background
window, the new count/size values were not displayed immediately.

aes/gemwmlib.c
@@ -1166,6 +1166,7 @@ static void wm_mktop(WORD w_handle)
 
 void wm_set(WORD w_handle, WORD w_field, WORD *pinwds)
 {
+    BOOL    do_cpwalk = FALSE;
     WORD    gadget = -1;
     WINDOW  *pwin;
 
@@ -1196,10 +1197,12 @@ void wm_set(WORD w_handle, WORD w_field, WORD *pinwds)
     case WF_NAME:
         gl_aname.te_ptext = pwin->w_pname = *(BYTE **)pinwds;
         gadget = W_NAME;
+        do_cpwalk = TRUE;       /* update name applies to all windows */
         break;
     case WF_INFO:
         gl_ainfo.te_ptext = pwin->w_pinfo = *(BYTE **)pinwds;
         gadget = W_INFO;
+        do_cpwalk = TRUE;       /* update info line applies to all windows */
         break;
     case WF_CXYWH:
         draw_change(w_handle, (GRECT *)pinwds);
@@ -1233,7 +1236,11 @@ void wm_set(WORD w_handle, WORD w_field, WORD *pinwds)
         break;
     }
 
-    if ((gadget != -1) && (w_handle == gl_wtop))
+    if (w_handle == gl_wtop)    /* only update slides in topped window */
+        if ((gadget == W_HSLIDE) || (gadget == W_VSLIDE))
+            do_cpwalk = TRUE;
+
+    if (do_cpwalk)
         w_cpwalk(w_handle, gadget, MAX_DEPTH, TRUE);
 
     wm_update(END_UPDATE);      /* give up the sync */

commit da4e33f1983a05667801d0b0be669967dd62a6c0

Do not draw name/info lines unless window is open
If a window was created but not open, then changing its name or info
line would cause the line to be displayed.  This bug was introduced
by commit f405c6b.

aes/gemwmlib.c
@@ -1196,13 +1196,19 @@ void wm_set(WORD w_handle, WORD w_field, WORD *pinwds)
     {
     case WF_NAME:
         gl_aname.te_ptext = pwin->w_pname = *(BYTE **)pinwds;
-        gadget = W_NAME;
-        do_cpwalk = TRUE;       /* update name applies to all windows */
+        if (pwin->w_flags & VF_INTREE)
+        {
+            gadget = W_NAME;
+            do_cpwalk = TRUE;   /* update name applies to all open windows */
+        }
         break;
     case WF_INFO:
         gl_ainfo.te_ptext = pwin->w_pinfo = *(BYTE **)pinwds;
-        gadget = W_INFO;
-        do_cpwalk = TRUE;       /* update info line applies to all windows */
+        if (pwin->w_flags & VF_INTREE)
+        {
+            gadget = W_INFO;
+            do_cpwalk = TRUE;   /* update info line applies to all open windows */
+        }
         break;
     case WF_CXYWH:
         draw_change(w_handle, (GRECT *)pinwds);