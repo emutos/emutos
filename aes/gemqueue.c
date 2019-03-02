543dae35fc308d7f7db55741de9eae0b327e70ed
Roger Burrows <anodynesoftware@users.sourceforge.net>
2019-03-01 19:05:05 -0500

    Fix bug in code that merges redraw messages
    The existing code had a test for a value of 0xFFFF in the "additional
    message length" word of system messages.  The test was incorrectly
    formed, but in any case unneeded since system messages always have a
    value of 0 in that word.  This bug was in the original DR imported
    sources.  Thanks to Eero Tamminen for reporting this.

--- aes/gemqueue.c
+++ aes/gemqueue.c
@@ -67,7 +67,7 @@ static void doq(WORD donq, AESPD *p, QPB *m)
                         n = 0;
                     }
                     else
-                        index += (om[2] != 0xFFFF) ? (om[2] + 16) : 16;
+                        index += om[2] + 16;
                 }
             }
         }
