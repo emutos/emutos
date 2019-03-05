emutos/emutos

commit 58fce7bc9acb095017ce1c2242740ef8046ebdd5

Eliminate unnecessary function

desk/deskact.c
@@ -496,8 +496,8 @@ void act_allchg(WORD wh, OBJECT *tree, WORD root, WORD ex_obj, GRECT *pt, GRECT 
                  */
                 assert(tree == G.g_screen);
                 fn = G.g_screeninfo[obj].fnptr;
-                if (fn && dochg)
-                    fn->f_selected = TRUE;
+                if (fn)
+                    fn->f_selected = dochg ? TRUE : FALSE;
                 /* make change */
                 newstate = tree[obj].ob_state;
                 if (dochg)
@@ -540,7 +540,6 @@ void act_allchg(WORD wh, OBJECT *tree, WORD root, WORD ex_obj, GRECT *pt, GRECT 
 void act_bsclick(WORD wh, OBJECT *tree, WORD root, WORD mx, WORD my, WORD keystate,
                  GRECT *pc, WORD dclick)
 {
-    WNODE *pw;
     WORD obj;
     WORD shifted;
     WORD state;
@@ -551,11 +550,9 @@ void act_bsclick(WORD wh, OBJECT *tree, WORD root, WORD mx, WORD my, WORD keysta
     if ((obj == root) && shifted)   /* shift-click within window does nothing */
         return;
 
-    pw = win_find(wh);
-
     if ((obj == root) || (obj == NIL))
     {
-        pn_clear(pw);               /* deselect all objects */
+        /* deselect all objects */
         act_allchg(wh, tree, root, obj, &gl_rfull, pc, FALSE);
     }
     else
@@ -565,7 +562,7 @@ void act_bsclick(WORD wh, OBJECT *tree, WORD root, WORD mx, WORD my, WORD keysta
         {
             if (dclick || !(state & SELECTED))
             {
-                pn_clear(pw);       /* deselect all objects */
+                /* deselect all objects & select one */
                 act_allchg(wh, tree, root, obj, &gl_rfull, pc, FALSE);
                 state |= SELECTED;
             }

desk/deskfpd.c
@@ -287,15 +287,3 @@ WORD pn_active(PNODE *pn, BOOL include_folders)
 
     return ((ret==ENMFIL) || (ret==EFILNF)) ? 0 : ret;
 }
-
-
-/*
- *  Clear the selection flag in all FNODES chained from the PNODE in the specified WNODE
- */
-void pn_clear(WNODE *pwin)
-{
-    FNODE *pf;
-
-    for (pf = pwin->w_pnode.p_flist; pf; pf = pf->f_next)
-        pf->f_selected = FALSE;
-}

desk/deskfpd.h
@@ -82,7 +82,6 @@ typedef struct _windnode WNODE; /* see deskwin.h */
 
 
 /* Prototypes: */
-void pn_clear(WNODE *pwin);
 void pn_close(PNODE *thepath);
 PNODE *pn_open(BYTE *pathname, WNODE *pw);
 FNODE *pn_sort(PNODE *pn);

desk/desksupp.c
@@ -131,8 +131,6 @@ void desk_clear(WORD wh)
     wind_get_grect(wh, WF_WXYWH, &c);
 
     /* clear all selections */
-    if (wh != DESKWH)
-        pn_clear(pw);
     act_allchg(wh, G.g_screen, root, 0, &gl_rfull, &c, FALSE);
 
     if (wh != DESKWH)           /* not the desktop */

commit eb6962f9b6234d60dbec17769dd67e4b4742d5ac

Fix handling of selected files (again)
Like Atari TOS, files and folders should stay selected (even if not
visible) when a desktop window is scrolled, but should be deselected
by certain button click actions.  Commit 58fce7b broke this: files
that were selected, but not visible, were not deselected by button
actions.  This feature should now work properly again.

desk/deskact.c
@@ -457,6 +457,9 @@ WORD act_chg(WORD wh, OBJECT *tree, WORD root, WORD obj, GRECT *pc,
  *  Change the SELECTED bit of all objects partially intersecting the
  *  given rectangle, allowing one object to be excluded
  *
+ *  If clearing the SELECTED bit, we also clear the selected indicator in
+ *  all FNODEs associated with this window
+ *
  *  If setting the SELECTED bit, we also set the selected indicator in
  *  the corresponding FNODE
  */
@@ -467,6 +470,10 @@ void act_allchg(WORD wh, OBJECT *tree, WORD root, WORD ex_obj, GRECT *pt, GRECT 
     WORD obj, newstate;
     WORD offx, offy;
     GRECT o, a, w;
+
+    /* clearing selections: do all FNODEs, not just the visible ones */
+    if (!dochg)
+        pn_clear(win_find(wh));
 
     offx = tree[root].ob_x;
     offy = tree[root].ob_y;

desk/deskfpd.c
@@ -292,3 +292,15 @@ WORD pn_active(PNODE *pn, BOOL include_folders)
 
     return ((ret==ENMFIL) || (ret==EFILNF)) ? 0 : ret;
 }
+
+
+/*
+ *  Clear the selection flag in all FNODES chained from the PNODE in the specified WNODE
+ */
+void pn_clear(WNODE *pw)
+{
+    FNODE *pf;
+
+    for (pf = pw->w_pnode.p_flist; pf; pf = pf->f_next)
+        pf->f_selected = FALSE;
+}

desk/deskfpd.h
@@ -82,6 +82,7 @@ typedef struct _windnode WNODE; /* see deskwin.h */
 
 
 /* Prototypes: */
+void pn_clear(WNODE *pw);
 void pn_close(PNODE *thepath);
 PNODE *pn_open(BYTE *pathname, WNODE *pw);
 FNODE *pn_sort(PNODE *pn);