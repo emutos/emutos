9e8a4c9b6b7f95e003e3d87de159b3765599d1dd
Roger Burrows <anodynesoftware@users.sourceforge.net>
2019-03-15 15:46:08 -0400

    Update source comments

--- bios/mouse.h
+++ bios/mouse.h
@@ -19,17 +19,20 @@
 
 
 /* Defines for mouse configuration in IKBD */
+                    /* the following values apply to 'topmode' in 'struct param' */
 #define IN_YTOP 0       /* Y=0 means top of screen */
 #define IN_YBOT 1       /* Y=0 means bottom of screen */
+                    /* the following values apply to 'buttons' in 'struct param'; */
+                    /* they are for documentation only, and are not used in EmuTOS */
 #define IN_PACKETS 3    /* absolute mouse position reported on button press */
 #define IN_KEYS 4       /* mouse buttons generate keycodes */
 
 
 /*
- * struct param - Used for parameter passing to mouse_init() function:
+ * struct param - Used for parameter passing to Initmous() function:
  *
- * topmode - indicates, if Y=0 is top or bottom of screen.
- * buttons - bit array, which affect the way mouse clicks are handled.
+ * topmode - indicates if Y=0 is at the top or bottom of screen.
+ * buttons - bit array, which affects the way mouse clicks are handled.
  *
  * xparam - number of mouse X increments between position report packets.
  * yparam - number of mouse Y increments between position report packets.
@@ -37,8 +40,8 @@
  * xmax - maximum X position the mouse should be allowed to move to.
  * ymax - maximum Y position the mouse should be allowed to move to.
  *
- * xinital - The mouse's initial x location.
- * yinital - The mouse's initial y location.
+ * xinitial - The mouse's initial x location.
+ * yinitial - The mouse's initial y location.
  */
 
 struct param
