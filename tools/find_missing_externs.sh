7e0e3a2f076068907777e23ebf29ccd757b13b5d
Vincent Rivière <vincent.riviere@freesbee.fr>
2019-05-07 00:15:17 +0200

    New tool: find_missing_externs.sh
    Usage:
    tools/find_missing_externs.sh
    
    Find missing .extern declaration inside .S files.
    They are ignored by gas, but useful for documentation.

---
+++ tools/find_missing_externs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Find missing .extern declarations in assembler files.
+# Although those declarations are ignored by gas,
+# they are useful for documentation.
+
+# Check if there are some object files
+if [ -z "$(ls obj/*.o 2>/dev/null)" ]
+then
+  echo "error: Please do \"make ...\" first." >&2
+  exit 1
+fi
+
+# For each assembler source file
+find . -name '*.S' | while read srcfile
+do
+  ofile=obj/$(basename -s .S  $srcfile).o
+
+  if [ ! -f $ofile ]
+  then
+    continue
+  fi
+
+  # Display differences between actual extern symbols and .extern declarations
+  echo $srcfile:
+  comm -3 -2 \
+    <(m68k-atari-mint-objdump -t $ofile | grep '\*UND\*' | tr -s ' ' | cut -d ' ' -f 6 | sort) \
+    <(grep '\.extern' $srcfile | tr -s ' ' | cut -d ' ' -f 3 | sort)
+done
