113ab518cb2071f488600361d250272ae2eba29c
Vincent Rivière <vincent.riviere@freesbee.fr>
2019-06-23 22:45:36 +0200

    Fix copyright holder name

--- bios/xbios.h
+++ bios/xbios.h
@@ -1,7 +1,7 @@
 /*
  * xbios.c - misc XBIOS function prototypes
  *
- * Copyright (C) 2002-2013 by the EmuTOS development team
+ * Copyright (C) 2002-2013 The EmuTOS development team
  *
  * This file is distributed under the GPL, version 2 or at your
  * option any later version.  See doc/license.txt for details.
