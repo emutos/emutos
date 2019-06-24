d81b40a8d87504eeef797f25966dd751b768638c
Vincent Rivière <vincent.riviere@freesbee.fr>
2019-06-25 00:01:24 +0200

    Remove unused positive error codes

--- include/dos.h
+++ include/dos.h
@@ -15,24 +15,6 @@
 *       -------------------------------------------------------------
 */
 
-#define E_BADFUNC 1
-#define E_FILENOTFND 2
-#define E_PATHNOTFND 3
-#define E_NOHANDLES 4
-#define E_NOACCESS 5
-#define E_BADHANDLE 6
-#define E_MEMBLKERR 7
-#define E_NOMEMORY 8
-#define E_BADMEMBLK 9
-#define E_BADENVIR 10
-#define E_BADFORMAT 11
-#define E_BADACCESS 12
-#define E_BADDATA 13
-#define E_BADDRIVE 15
-#define E_NODELDIR 16
-#define E_NOTDEVICE 17
-#define E_NOFILES 18
-
 #define F_RDONLY 0x01
 #define F_HIDDEN 0x02
 #define F_SYSTEM 0x04
