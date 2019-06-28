be36a29dfd9b7c63286684540b1f762ebfdbd485
Vincent Rivière <vincent.riviere@freesbee.fr>
2019-06-28 21:09:05 +0200

    Move public BIOS definitions from tosvars.h to biosdefs.h

--- include/biosdefs.h
+++ include/biosdefs.h
@@ -16,6 +16,41 @@
 
 #include "portab.h"
 
+/* Forward declarations */
+struct _pd;
+
+/* GEM memory usage parameters block.
+ * This is actually a structure used by the BIOS
+ * in order to call the main UI (i.e. AES) */
+
+#define GEM_MUPB_MAGIC 0x87654321
+
+typedef struct
+{
+    ULONG     gm_magic; /* Magical value, has to be GEM_MUPB_MAGIC */
+    UBYTE     *gm_end;  /* End of the static ST-RAM used by the OS */
+    PRG_ENTRY *gm_init; /* Start address of the main UI */
+} GEM_MUPB;
+
+typedef struct _osheader
+{
+    UWORD      os_entry;      /* BRA.S to reset handler */
+    UWORD      os_version;    /* TOS version number */
+    PFVOID     reseth;        /* Pointer to reset handler */
+    /* The following 3 longs are actually the default GEM_MUPB structure */
+    struct _osheader *os_beg; /* Base address of the operating system */
+    UBYTE      *os_end;       /* End of ST-RAM statically used by the OS */
+    PRG_ENTRY  *os_rsvl;      /* Entry point of default UI (unused) */
+    GEM_MUPB   *os_magic;     /* GEM memory usage parameters block */
+    ULONG      os_date;       /* TOS date in BCD format MMDDYYYY */
+    UWORD      os_conf;       /* Flag for PAL version + country */
+    UWORD      os_dosdate;    /* TOS date in BDOS format */
+    WORD       **os_root;     /* Pointer to the BDOS quick pool */
+    UBYTE      *os_kbshift;   /* Pointer to the BIOS shifty variable */
+    struct _pd **os_run;      /* Pointer to the pointer to current BASEPAGE */
+    ULONG      os_dummy;      /* 'ETOS' signature */
+} OSHEADER;
+
 /*
  *  BPB - Bios Parameter Block
  */
@@ -52,5 +87,21 @@ typedef struct _bpb BPB;
                         /* key is released, even if the Alt key is released first.   */
 #define MODE_HOME   0x20        /* Clr/Home is down        */
 #define MODE_INSERT 0x40        /* Insert is down          */
+
+/*
+ * Struct returned by Kbdvbase()
+ */
+struct kbdvecs
+{
+    PFVOID midivec;     /* MIDI Input */
+    PFVOID vkbderr;     /* IKBD Error */
+    PFVOID vmiderr;     /* MIDI Error */
+    PFVOID statvec;     /* IKBD Status */
+    PFVOID mousevec;    /* IKBD Mouse */
+    PFVOID clockvec;    /* IKBD Clock */
+    PFVOID joyvec;      /* IKBD Joystick */
+    PFVOID midisys;     /* Main MIDI Vector */
+    PFVOID ikbdsys;     /* Main IKBD Vector */
+};
 
 #endif /* BIOSDEFS_H */
