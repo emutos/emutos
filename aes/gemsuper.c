ca4e610063f802aa35bc08021d090e3e01c94e04
Roger Burrows <anodynesoftware@users.sourceforge.net>
2019-07-13 14:57:35 -0400

    Use define for location of AES return code
    The binaries are unchanged.

--- aes/gemsuper.c
+++ aes/gemsuper.c
@@ -448,7 +448,7 @@ static void xif(AESPB *pcrys_blk)
     if (AIN_LEN)
         memcpy(addr_in, pcrys_blk->addrin, min(AIN_LEN,AI_SIZE)*sizeof(LONG));
 
-    int_out[0] = crysbind(OP_CODE, (AESGLOBAL *)pcrys_blk->global, control, int_in, int_out,
+    RET_CODE = crysbind(OP_CODE, (AESGLOBAL *)pcrys_blk->global, control, int_in, int_out,
                                 addr_in);
 
     if (OUT_LEN)
