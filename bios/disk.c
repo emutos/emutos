emutos/emutos

commit d3a24b7ac3d26632110f7d4b5cf6040749cd7e11

Adjust size of units[] array at compile time
The number of entries in the array is adjusted according to the
configured hardware detection (ACSI, SCSI, IDE, SDMMC).  This
saves ROM and RAM space in the 192K ROMs.

bios/disk.c
@@ -154,10 +154,20 @@ void disk_init_all(void)
 {
     /* scan disk majors in the following order */
     static const int majors[] =
-        {16, 18, 17, 19, 20, 22, 21, 23,    /* IDE primary/secondary */
-         8, 9, 10, 11, 12, 13, 14, 15,      /* SCSI */
-         0, 1, 2, 3, 4, 5, 6, 7,            /* ACSI */
-         24, 25, 26, 27, 28, 29, 30, 31};   /* SD/MMC */
+    {
+#if CONF_WITH_IDE
+        16, 18, 17, 19, 20, 22, 21, 23,     /* IDE primary/secondary */
+#endif
+#if CONF_WITH_SCSI
+        8, 9, 10, 11, 12, 13, 14, 15,       /* SCSI */
+#endif
+#if CONF_WITH_ACSI
+        0, 1, 2, 3, 4, 5, 6, 7,             /* ACSI */
+#endif
+#if CONF_WITH_SDMMC
+        24, 25, 26, 27, 28, 29, 30, 31      /* SD/MMC */
+#endif
+    };
     int i;
     LONG devices_available = 0L;
     LONG bitmask;

bios/disk.h
@@ -23,7 +23,16 @@
 #define IDE_BUS             2
 #define SDMMC_BUS           3
 
-#define MAX_BUS             SDMMC_BUS
+#if CONF_WITH_SDMMC
+# define MAX_BUS            SDMMC_BUS
+#elif CONF_WITH_IDE
+# define MAX_BUS            IDE_BUS
+#elif CONF_WITH_SCSI
+# define MAX_BUS            SCSI_BUS
+#else
+# define MAX_BUS            ACSI_BUS
+#endif
+
 #define DEVICES_PER_BUS     8
 
 #define UNITSNUM            (NUMFLOPPIES+(DEVICES_PER_BUS*(MAX_BUS+1)))

commit 9462acf7df03088168fa5660c442f0404fa50b38

Fix detection of standalone partitions in ARAnyM
Standalone partitions were not detected, because they are accessed
using SCSI unit numbers.  These were no longer checked since ARAnyM
does not provide SCSI emulation. This bug was introduced by commit
d3a24b7ac3d26632110f7d4b5cf6040749cd7e11.

Many thanks to Vincent Riviere for finding this.

bios/disk.c
@@ -158,7 +158,7 @@ void disk_init_all(void)
 #if CONF_WITH_IDE
         16, 18, 17, 19, 20, 22, 21, 23,     /* IDE primary/secondary */
 #endif
-#if CONF_WITH_SCSI
+#if CONF_WITH_SCSI || CONF_WITH_ARANYM
         8, 9, 10, 11, 12, 13, 14, 15,       /* SCSI */
 #endif
 #if CONF_WITH_ACSI