emutos/emutos

commit e9457bba4ad3c2850892b4513024b8f9c18fed86

Fix VDI line clipping calculation
For line clipping, the VDI uses clip_line(), which uses mul_div().
In the original DRI code, the equivalent assembler function
SMUL_DIV() performed rounding before returning the result.  When
this was implemented in C after the original import of sources,
the rounding was dropped.  The current assembler re-implementation
also omits the rounding.  This caused bad output for some sloped
lines when clipping was in effect.

Rounding has now been restored in a new assembler function called
mul_div_round().  Initially, this is only called in clip_line().

include/optimopt.h
@@ -13,5 +13,6 @@
 
 char *scasb(char *p, char b);
 WORD expand_string(WORD *dest, char *src);
+WORD mul_div_round(WORD mult1, WORD mult2, WORD divisor);
 
 #endif

util/optimopt.S
@@ -15,6 +15,7 @@
 
         .globl  _scasb
         .globl  _expand_string
+        .globl  _mul_div_round
 
 /*
  * char *scasb(char *str, char c)
@@ -58,4 +59,44 @@ copy_loop:
         sub.l   d0,a0
         move.l  a0,d0
         subq.l  #1,d0           // d0.l = length
+        rts
+
+/*
+ *  WORD mul_div_round(WORD mult1,WORD mult2,WORD divisor)
+ *
+ *  multiplies two WORDs together and divides by a WORD, returning a WORD.
+ *  the result is rounded away from zero if the remainder is greater than
+ *  or equal to half the divisor.
+ *
+ *  if you do not need rounding, use mul_div() from intmath.h instead!
+ */
+_mul_div_round:
+        move.l  d2,-(sp)        // ColdFire friendly
+        move.l  d3,-(sp)
+        moveq   #1,d1           // preload increment
+        move.w  8+6(sp),d0
+        muls    8+4(sp),d0      // d0 = m1 * m2
+        bpl.s   mulplus
+        neg.l   d1              // negate increment
+mulplus:
+        move.w  8+8(sp),d2
+        divs    d2,d0           // d0 = m2 * m1 / d1
+        ext.l   d2              // extend to longword for ColdFire
+        bpl.s   divplus         // branch if divisor positive
+        neg.l   d1              // else negate increment
+        neg.l   d2              //  & make divisor positive
+divplus:
+        move.l  d0,d3
+        swap    d3              // d3.w = remainder
+        ext.l   d3              // extend to longword for ColdFire
+        bpl.s   remplus         // branch if positive
+        neg.l   d3              // else make remainder positive
+remplus:
+        asl.l   #1,d3           // d3.l = 2 * remainder
+        cmp.l   d2,d3           // bigger than divisor?
+        blt.s   noround         // no, branch
+        add.l   d1,d0           // else add increment
+noround:
+        move.l  (sp)+,d3
+        move.l  (sp)+,d2
         rts

vdi/vdi_line.c
@@ -19,6 +19,7 @@
 #include "biosext.h"    /* for cache control routines */
 #include "lineavars.h"
 #include "has.h"        /* for blitter-related items */
+#include "optimopt.h"   /* for mul_div_round() */
 
 
 #if CONF_WITH_BLITTER
@@ -876,16 +877,16 @@ BOOL clip_line(Vwk * vwk, Line * line)
         deltax = line->x2 - line->x1;
         deltay = line->y2 - line->y1;
         if (line_clip_flag & 1) {               /* left ? */
-            *y = line->y1 + mul_div(deltay, (vwk->xmn_clip - line->x1), deltax);
+            *y = line->y1 + mul_div_round(deltay, (vwk->xmn_clip-line->x1), deltax);
             *x = vwk->xmn_clip;
         } else if (line_clip_flag & 2) {        /* right ? */
-            *y = line->y1 + mul_div(deltay, (vwk->xmx_clip - line->x1), deltax);
+            *y = line->y1 + mul_div_round(deltay, (vwk->xmx_clip-line->x1), deltax);
             *x = vwk->xmx_clip;
         } else if (line_clip_flag & 4) {        /* top ? */
-            *x = line->x1 + mul_div(deltax, (vwk->ymn_clip - line->y1), deltay);
+            *x = line->x1 + mul_div_round(deltax, (vwk->ymn_clip-line->y1), deltay);
             *y = vwk->ymn_clip;
         } else if (line_clip_flag & 8) {        /* bottom ? */
-            *x = line->x1 + mul_div(deltax, (vwk->ymx_clip - line->y1), deltay);
+            *x = line->x1 + mul_div_round(deltax, (vwk->ymx_clip-line->y1), deltay);
             *y = vwk->ymx_clip;
         }
     }

commit 80ef7649e4a6c5468e007ecbf3f423468b653e41

Fix 'make cart'
It was broken by commit e9457bba4ad3c2850892b4513024b8f9c18fed86.

include/asm.h
@@ -1,7 +1,7 @@
 /*
  * asm.h - Assembler help routines
  *
- * Copyright (C) 2001-2017 The EmuTOS development team
+ * Copyright (C) 2001-2019 The EmuTOS development team
  *
  * Authors:
  *  LVL   Laurent Vogel
@@ -11,8 +11,9 @@
  */
 
 /*
- * This file contains utility routines (macros) to
- * perform functions not directly available from C.
+ * This file contains two types of item:
+ * . function prototypes for functions in miscasm.S
+ * . macros/inline functions to perform functions not directly available from C
  *
  * available macros:
  *
@@ -37,6 +38,9 @@ extern void just_rts(void);
 
 /* Wrapper around the STOP instruction. This preserves SR. */
 extern void stop_until_interrupt(void);
+
+/* perform WORD multiply/divide with rounding */
+WORD mul_div_round(WORD mult1, WORD mult2, WORD divisor);
 
 /*
  * Push/Pop registers from stack, with ColdFire support.

include/optimopt.h
@@ -13,6 +13,5 @@
 
 char *scasb(char *p, char b);
 WORD expand_string(WORD *dest, char *src);
-WORD mul_div_round(WORD mult1, WORD mult2, WORD divisor);
 
 #endif

util/miscasm.S
@@ -146,3 +146,44 @@ stop7:
         .globl  _just_rts
 _just_rts:
         rts
+
+/*
+ *  WORD mul_div_round(WORD mult1,WORD mult2,WORD divisor)
+ *
+ *  multiplies two WORDs together and divides by a WORD, returning a WORD.
+ *  the result is rounded away from zero if the remainder is greater than
+ *  or equal to half the divisor.
+ *
+ *  if you do not need rounding, use mul_div() from intmath.h instead!
+ */
+        .globl  _mul_div_round
+_mul_div_round:
+        move.l  d2,-(sp)        // ColdFire friendly
+        move.l  d3,-(sp)
+        moveq   #1,d1           // preload increment
+        move.w  8+6(sp),d0
+        muls    8+4(sp),d0      // d0 = m1 * m2
+        bpl.s   mulplus
+        neg.l   d1              // negate increment
+mulplus:
+        move.w  8+8(sp),d2
+        divs    d2,d0           // d0 = m2 * m1 / d1
+        ext.l   d2              // extend to longword for ColdFire
+        bpl.s   divplus         // branch if divisor positive
+        neg.l   d1              // else negate increment
+        neg.l   d2              //  & make divisor positive
+divplus:
+        move.l  d0,d3
+        swap    d3              // d3.w = remainder
+        ext.l   d3              // extend to longword for ColdFire
+        bpl.s   remplus         // branch if positive
+        neg.l   d3              // else make remainder positive
+remplus:
+        asl.l   #1,d3           // d3.l = 2 * remainder
+        cmp.l   d2,d3           // bigger than divisor?
+        blt.s   noround         // no, branch
+        add.l   d1,d0           // else add increment
+noround:
+        move.l  (sp)+,d3
+        move.l  (sp)+,d2
+        rts

util/optimopt.S
@@ -15,7 +15,6 @@
 
         .globl  _scasb
         .globl  _expand_string
-        .globl  _mul_div_round
 
 /*
  * char *scasb(char *str, char c)
@@ -59,44 +58,4 @@ copy_loop:
         sub.l   d0,a0
         move.l  a0,d0
         subq.l  #1,d0           // d0.l = length
-        rts
-
-/*
- *  WORD mul_div_round(WORD mult1,WORD mult2,WORD divisor)
- *
- *  multiplies two WORDs together and divides by a WORD, returning a WORD.
- *  the result is rounded away from zero if the remainder is greater than
- *  or equal to half the divisor.
- *
- *  if you do not need rounding, use mul_div() from intmath.h instead!
- */
-_mul_div_round:
-        move.l  d2,-(sp)        // ColdFire friendly
-        move.l  d3,-(sp)
-        moveq   #1,d1           // preload increment
-        move.w  8+6(sp),d0
-        muls    8+4(sp),d0      // d0 = m1 * m2
-        bpl.s   mulplus
-        neg.l   d1              // negate increment
-mulplus:
-        move.w  8+8(sp),d2
-        divs    d2,d0           // d0 = m2 * m1 / d1
-        ext.l   d2              // extend to longword for ColdFire
-        bpl.s   divplus         // branch if divisor positive
-        neg.l   d1              // else negate increment
-        neg.l   d2              //  & make divisor positive
-divplus:
-        move.l  d0,d3
-        swap    d3              // d3.w = remainder
-        ext.l   d3              // extend to longword for ColdFire
-        bpl.s   remplus         // branch if positive
-        neg.l   d3              // else make remainder positive
-remplus:
-        asl.l   #1,d3           // d3.l = 2 * remainder
-        cmp.l   d2,d3           // bigger than divisor?
-        blt.s   noround         // no, branch
-        add.l   d1,d0           // else add increment
-noround:
-        move.l  (sp)+,d3
-        move.l  (sp)+,d2
         rts