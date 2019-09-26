emutos/emutos

commit cc888cfc6ad03af815aa84db5860018779ee02d5

Implement 30-second timeout for Bconout(0)
This is the same as Atari TOS, except that the timeout can be
terminated via a keyboard ctrl-C.

bios/parport.c
@@ -19,20 +19,28 @@
 #include "sound.h"
 #include "mfp.h"
 #include "psg.h"
+#include "ikbd.h"
+#include "tosvars.h"
 #endif
 
 /*
  * known differences with respect to the original TOS:
  * - printer configuration and hardcopy not done
  * - no input
- * - no 30 seconds delay for printer output.
  */
 
 #if CONF_WITH_PRINTER_PORT
 /* timing stuff */
 #define DELAY_1US       delay_loop(delay1us)
 static ULONG delay1us;
+
 static WORD printer_config;
+
+/* timeout stuff */
+#define CTL_C           ('C'-0x40)
+#define SHORT_TIMEOUT   (5 * CLOCKS_PER_SEC)
+#define LONG_TIMEOUT    (30 * CLOCKS_PER_SEC)
+static LONG last_timeout;
 #endif
 
 #if CONF_WITH_PRINTER_PORT
@@ -48,6 +56,54 @@ WORD setprt(WORD config)
 
     return old_config;
 }
+
+/*
+ * do parallel port output
+ */
+static LONG prnout(WORD c)
+{
+    WORD old_sr;
+    WORD a;
+
+    /* disable interrupts */
+    old_sr = set_sr(0x2700);
+
+    /* read PSG multi-function register */
+    a = giaccess(0, PSG_MULTI | GIACCESS_READ);
+
+    /* set port B to output mode */
+    a |= PSG_PORTB_OUTPUT;
+
+    /* write new value in register */
+    giaccess(a, PSG_MULTI | GIACCESS_WRITE);
+
+    /* write char in port B */
+    giaccess(c, PSG_PORT_B | GIACCESS_WRITE);
+
+    /*
+     * according to the Centronics spec, we must leave valid data on
+     * the lines for at least 500ns before pulsing the strobe line,
+     * so we leave it for 1 microsecond
+     */
+    DELAY_1US;
+
+    /*
+     * according to the spec, the strobe line must be pulsed low
+     * for a minimum of 500ns, so we do it for 1 microsecond
+     */
+    offgibit(~0x20);
+    DELAY_1US;
+    ongibit(0x20);
+
+    /*
+     * at this point, we should wait for ACK to go low, but
+     * most drivers don't bother
+     */
+
+    /* restore sr */
+    set_sr(old_sr);
+    return 1L;
+}
 #endif
 
 void parport_init(void)
@@ -61,6 +117,7 @@ void parport_init(void)
 
     /* initialize other printer variables */
     printer_config = 0;     /* Setprt() default: output via parallel port */ 
+    last_timeout = 0UL;     /* parallel port: ticks value at last timeout */
 #endif
 }
 
@@ -87,50 +144,29 @@ LONG bcostat0(void)
 
 LONG bconout0(WORD dev, WORD c)
 {
-    if(bcostat0()) {
 #if CONF_WITH_PRINTER_PORT
-        WORD old_sr;
-        WORD a;
+    ULONG now = hz_200;
 
-        /* disable interrupts */
-        old_sr = set_sr(0x2700);
-        /* read PSG multi-function register */
-        a = giaccess(0, PSG_MULTI | GIACCESS_READ);
-        /* set port B to output mode */
-        a |= PSG_PORTB_OUTPUT;
-        /* write new value in register */
-        giaccess(a, PSG_MULTI | GIACCESS_WRITE);
-        /* write char in port B */
-        giaccess(c, PSG_PORT_B | GIACCESS_WRITE);
-
-        /*
-         * according to the Centronics spec, we must leave valid data on
-         * the lines for at least 500ns before pulsing the strobe line,
-         * so we leave it for 1 microsecond
-         */
-        DELAY_1US;
-
-        /*
-         * according to the spec, the strobe line must be pulsed low
-         * for a minimum of 500ns, so we do it for 1 microsecond
-         */
-        offgibit(~0x20);
-        DELAY_1US;
-        ongibit(0x20);
-
-        /*
-         * at this point, we should wait for ACK to go low, but
-         * most drivers don't bother
-         */
-
-        /* restore sr */
-        set_sr(old_sr);
-        return 1L;
-#endif
-    } else {
-        /* Atari TOS waits until the printer is available...
-         * We simply cancel here.
-         */
+    /*
+     * if we didn't time out 'recently', we check if the port is available,
+     * allowing a long timeout; otherwise we just time out immediately.
+     * this is how Atari TOS does things.  however, waiting for the long
+     * timeout because we accidentally tried to use the printer port is
+     * extremely painful, so EmuTOS allows a quick out via ctrl-C.
+     */
+    if (now >= (last_timeout+SHORT_TIMEOUT))
+    {
+        while(hz_200 < (now+LONG_TIMEOUT))
+        {
+            if (bcostat0())
+                return prnout(c);
+            if (bconstat2())
+                if ((bconin2() & 0xff) == CTL_C)
+                    break;
+        }
     }
+
+    last_timeout = hz_200;
+#endif
     return 0L;
 }

commit 9aeafde752407ec48c65c12385813d915500556b

Fix print bug with autoboot/AUTO-folder programs
If the initial Bconout(0) was "too soon" after EmuTOS startup, it
failed.  This bug was introduced by commit cc888cfc.  Many thanks
to Eero Tamminen for finding this.

bios/parport.c
@@ -39,7 +39,7 @@ static WORD printer_config;
 #define CTL_C           ('C'-0x40)
 #define SHORT_TIMEOUT   (5 * CLOCKS_PER_SEC)
 #define LONG_TIMEOUT    (30 * CLOCKS_PER_SEC)
-static LONG last_timeout;
+static ULONG last_timeout;
 #endif
 
 #if CONF_WITH_PRINTER_PORT
@@ -153,7 +153,8 @@ LONG bconout0(WORD dev, WORD c)
      * timeout because we accidentally tried to use the printer port is
      * extremely painful, so EmuTOS allows a quick out via ctrl-C.
      */
-    if (now >= (last_timeout+SHORT_TIMEOUT))
+    if ((last_timeout == 0UL)                   /* first time through? */
+     || (now >= (last_timeout+SHORT_TIMEOUT)))
     {
         while(hz_200 < (now+LONG_TIMEOUT))
         {