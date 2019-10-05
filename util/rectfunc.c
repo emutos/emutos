f4c330bbd3b943eb70787028d99c042c735b86f7
Vincent Rivière <vincent.riviere@freesbee.fr>
2019-10-05 17:30:35 +0200

    Fix copyright format
    It is unclear who is the original author of this file.

--- util/rectfunc.c
+++ util/rectfunc.c
@@ -1,8 +1,10 @@
 /*
- *       Copyright 2002-2019 The EmuTOS development team
+ * rectfunc.c - rectangle helpers
  *
- *       This software is licenced under the GNU Public License.
- *       Please see LICENSE.TXT for further information.
+ * Copyright (C) 2002-2019 The EmuTOS development team
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
  */
 
 #include "emutos.h"
