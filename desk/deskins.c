emutos/emutos

commit 0531ab7db4e5352541f853fd864310b81e70c032

Support creation of EmuDesk printer icon
Subsequent commits will support save/restore of the icon to
EMUDESK.INF, and actual printing via drag/drop.

desk/deskins.c
@@ -638,11 +638,13 @@ static WORD install_desktop_icon(ANODE *pa)
     icon_exists = pa ? TRUE : FALSE;
 
     /*
-     * deselect all objects & hide printer button
+     * deselect all objects & hide printer button if not supported
      */
     tree = G.a_trees[ADINSDSK];
     deselect_all(tree);
+#if !CONF_WITH_PRINTER_ICON
     tree[ID_PRINT].ob_flags |= HIDETREE;
+#endif
 
     /*
      * fill in dialog
@@ -678,25 +680,34 @@ static WORD install_desktop_icon(ANODE *pa)
         tree[ID_ID].ob_state |= DISABLED;
         tree[ID_DRIVE].ob_state |= DISABLED;
         tree[ID_TRASH].ob_state |= DISABLED;
+        tree[ID_PRINT].ob_state |= DISABLED;
         start_fld = ID_LABEL;
 #endif
         break;
     case AT_ISDISK:
         id[0] = pa->a_letter;
         FALLTHROUGH;
+#if CONF_WITH_PRINTER_ICON
+    case AT_ISPRNT:
+#endif
     case AT_ISTRSH:
         inf_sset(tree, ID_ID, id);
         if (pa->a_type == AT_ISDISK)
             tree[ID_DRIVE].ob_state |= SELECTED;
+#if CONF_WITH_PRINTER_ICON
+        else if (pa->a_type == AT_ISPRNT)
+            tree[ID_PRINT].ob_state |= SELECTED;
+#endif
         else
             tree[ID_TRASH].ob_state |= SELECTED;
 #if CONF_WITH_DESKTOP_SHORTCUTS
         /*
-         * for desktop disk/trash icons, anything can be changed
+         * for desktop disk/trash/printer icons, anything can be changed
          */
         tree[ID_ID].ob_state &= ~DISABLED;
         tree[ID_DRIVE].ob_state &= ~DISABLED;
         tree[ID_TRASH].ob_state &= ~DISABLED;
+        tree[ID_PRINT].ob_state &= ~DISABLED;
 #endif
     }
     strcpy(curr_label, pa->a_pappl);
@@ -730,7 +741,7 @@ static WORD install_desktop_icon(ANODE *pa)
             pa->a_letter = '\0';            /* default is no letter */
             switch(inf_gindex(tree, ID_DRIVE,3))
             {
-            case 0:
+            default:                        /* i.e. case 0 */
                 pa->a_type = AT_ISDISK;
                 inf_sget(tree, ID_ID, id);  /* disks must have a letter */
                 pa->a_letter = id[0];
@@ -740,6 +751,11 @@ static WORD install_desktop_icon(ANODE *pa)
             case 1:
                 pa->a_type = AT_ISTRSH;
                 break;
+#if CONF_WITH_PRINTER_ICON
+            case 2:
+                pa->a_type = AT_ISPRNT;
+                break;
+#endif
             }
             inf_sget(tree, ID_LABEL, new_label);
             if (strcmp(curr_label, new_label))      /* if label changed, */

desk/deskapp.h
@@ -43,6 +43,9 @@
 #define AT_ISFOLD 1
 #define AT_ISDISK 2
 #define AT_ISTRSH 3
+#if CONF_WITH_PRINTER_ICON
+#define AT_ISPRNT 4
+#endif
 
 /*
  * standard gem icons, always available

desk/deskfun.c
@@ -730,6 +730,9 @@ static void fun_desk2win(WORD wh, WORD dobj, WORD keystate)
                 if (!valid_drive(an_src->a_letter))
                     continue;
                 break;
+#if CONF_WITH_PRINTER_ICON
+            case AT_ISPRNT:
+#endif
             case AT_ISTRSH:
                 continue;
             }
@@ -772,6 +775,9 @@ static void fun_desk2desk(WORD dobj, WORD keystate)
                     continue;
                 }
                 break;
+#if CONF_WITH_PRINTER_ICON
+            case AT_ISPRNT:
+#endif
             case AT_ISTRSH:
                 continue;
         }

desk/deskmain.c
@@ -129,7 +129,7 @@ static WORD     ig_close;
  *                       file with an associated application, is selected
  *      ILL_DOCU[]      disabled if a normal non-executable file is selected
  *      ILL_FOLD[]      disabled if a folder is selected
- *      ILL_TRASH[]     disabled if the trash can is selected
+ *      ILL_TRASH[]     disabled if the trash can or printer is selected
  */
 static const UBYTE ILL_FILE[] =  { IDSKITEM, RICNITEM, 0 };
 static const UBYTE ILL_DOCU[] =  { IDSKITEM, IAPPITEM, RICNITEM, 0 };
@@ -306,6 +306,9 @@ static void men_update(void)
         case AT_ISDISK:
             pvalue = (appl->a_aicon == IG_FLOPPY) ? ILL_FDSK : ILL_HDSK;
             break;
+#if CONF_WITH_PRINTER_ICON
+        case AT_ISPRNT:                 /* Printer */
+#endif
         case AT_ISTRSH:                 /* Trash */
             pvalue = ILL_TRASH;
             break;

desk/desksupp.c
@@ -1149,6 +1149,19 @@ static void trash_alert(ANODE *pa)
 }
 
 
+#if CONF_WITH_PRINTER_ICON
+/*
+ *  Issue alert about the printer
+ *
+ *  The current name of the printer icon is obtained from the ANODE
+ */
+static void printer_alert(ANODE *pa)
+{
+    fun_alert_string(1, STPRINFO, pa->a_pappl);
+}
+#endif
+
+
 /*
  *  Open an icon
  */
@@ -1215,6 +1228,11 @@ WORD do_open(WORD curr)
     case AT_ISTRSH:
         trash_alert(pa);
         break;
+#if CONF_WITH_PRINTER_ICON
+    case AT_ISPRNT:
+        printer_alert(pa);
+        break;
+#endif
     }
 
     return FALSE;
@@ -1286,6 +1304,11 @@ WORD do_info(WORD curr)
     case AT_ISTRSH:
         trash_alert(pa);
         break;
+#if CONF_WITH_PRINTER_ICON
+    case AT_ISPRNT:
+        printer_alert(pa);
+        break;
+#endif
     }
 
     return ret;

include/config.h
@@ -284,6 +284,9 @@
 # ifndef CONF_WITH_SHOW_FILE
 #  define CONF_WITH_SHOW_FILE 0
 # endif
+# ifndef CONF_WITH_PRINTER_ICON
+#  define CONF_WITH_PRINTER_ICON 0
+# endif
 # ifndef CONF_WITH_68030_PMMU
 #  define CONF_WITH_68030_PMMU 0
 # endif
@@ -1170,6 +1173,13 @@
  */
 #ifndef CONF_WITH_SHOW_FILE
  # define CONF_WITH_SHOW_FILE 1
+#endif
+
+/*
+ * Set CONF_WITH_PRINTER_ICON to 1 to support a printer icon in EmuDesk
+ */
+#ifndef CONF_WITH_PRINTER_ICON
+ # define CONF_WITH_PRINTER_ICON 1
 #endif
 
 /*

commit f329431b065482b5c3b6d912788c244bc060274f

Fix bug with 'Install icon' & program/data files
If you installed or modified a program or data icon via EmuDesk's
'Install icon' menu item, the icon type was changed to a disk drive.
This bug was introduced by commit 0531ab7.  Many thanks to Christian
Quante for identifying and debugging this problem, and suggesting
a solution.

desk/deskins.c
@@ -736,7 +736,7 @@ static WORD install_desktop_icon(ANODE *pa)
             pa->a_letter = '\0';            /* default is no letter */
             switch(inf_gindex(tree, ID_DRIVE,3))
             {
-            default:                        /* i.e. case 0 */
+            case 0:
                 pa->a_type = AT_ISDISK;
                 inf_sget(tree, ID_ID, id);  /* disks must have a letter */
                 pa->a_letter = id[0];