emutos/emutos

commit 43adfabf8c9ea2796387e457eb14cc5cc7f2afcd

Improve VDI text output speed for 'plain' text
This increases VDI text output speed by a factor of 4 for a limited
subset of output.  The most significant restriction is that the
output must be byte-aligned on the screen.  Other restrictions
include: no effects, no rotation, text must be left-aligned.  Also,
the font must be monospaced with a width of 8 pixels, and the output
string must not be clipped.  The equivalent feature exists in all
versions of Atari TOS.

vdi/vdi_text.c
@@ -216,6 +216,52 @@ static WORD calc_width(Vwk *vwk, WORD cnt, WORD *str)
     return width;
 }
 
+#if CONF_WITH_VDI_TEXT_SPEEDUP
+/*
+ * returns TRUE if we can use a direct screen blit
+ *
+ * the following must all be true:
+ *  there are no effects
+ *  there is no rotation
+ *  the output is left-aligned
+ *  the output is not justified
+ *  the characters are byte-aligned
+ *  the font is monospace with a cell width of 8
+ *  the font contains glyphs for all 256 characters
+ *  the entire text string will not be clipped
+ */
+static BOOL ok_for_direct_blit(Vwk *vwk, WORD width, JUSTINFO *justified)
+{
+    const Fonthead *fnt_ptr = vwk->cur_font;
+
+    if (vwk->style | vwk->chup | vwk->h_align)
+        return FALSE;
+
+    if (justified)
+        return FALSE;
+
+    if (DESTX & 0x0007)
+        return FALSE;
+
+    if (!MONO || (fnt_ptr->max_cell_width != 8))
+        return FALSE;
+
+    if ((fnt_ptr->first_ade != 0) || (fnt_ptr->last_ade != 255))
+        return FALSE;
+
+    if (!vwk->clip)
+        return TRUE;
+
+    /* check that string falls entirely within clip area */
+    if ((DESTX < vwk->xmn_clip) || (DESTX+width > vwk->xmx_clip))
+        return FALSE;
+    if ((DESTY < vwk->ymn_clip) || (DESTY+DELY > vwk->ymx_clip))
+        return FALSE;
+
+    return TRUE;
+}
+#endif
+
 /*
  * output specified text string
  *
@@ -382,6 +428,18 @@ static void output_text(Vwk *vwk, WORD count, WORD *str, WORD width, JUSTINFO *j
 
     TEXTFG = vwk->text_color;
     DELY = fnt_ptr->form_height;
+
+#if CONF_WITH_VDI_TEXT_SPEEDUP
+    /*
+     * call special direct screen blit routine if applicable
+     */
+    if (ok_for_direct_blit(vwk, width, justified))
+    {
+        direct_screen_blit(count, str);
+        return;
+    }
+#endif
+
     XDDA = 32767;       /* init the horizontal dda */
 
     for (j = 0; j < count; j++) {

include/config.h
@@ -284,6 +284,9 @@
 # ifndef CONF_WITH_EXTENDED_MOUSE
 #  define CONF_WITH_EXTENDED_MOUSE 0
 # endif
+# ifndef CONF_WITH_VDI_TEXT_SPEEDUP
+#  define CONF_WITH_VDI_TEXT_SPEEDUP 0
+# endif
 # ifndef CONF_WITH_VDI_VERTLINE
 #  define CONF_WITH_VDI_VERTLINE 0
 # endif
@@ -377,6 +380,9 @@
 # endif
 # ifndef CONF_WITH_EXTENDED_MOUSE
 #  define CONF_WITH_EXTENDED_MOUSE 0
+# endif
+# ifndef CONF_WITH_VDI_TEXT_SPEEDUP
+#  define CONF_WITH_VDI_TEXT_SPEEDUP 0
 # endif
 # ifndef CONF_WITH_VDI_VERTLINE
 #  define CONF_WITH_VDI_VERTLINE 0
@@ -1212,6 +1218,13 @@
  */
 #ifndef CONF_WITH_VDI_VERTLINE
 # define CONF_WITH_VDI_VERTLINE 1
+#endif
+
+/*
+ * Set CONF_WITH_VDI_TEXT_SPEEDUP to 1 to improve some VDI text output performance
+ */
+#ifndef CONF_WITH_VDI_TEXT_SPEEDUP
+# define CONF_WITH_VDI_TEXT_SPEEDUP 1
 #endif
 
 /*

vdi/vdi_defs.h
@@ -381,6 +381,10 @@ void vdi_vqt_fontinfo(Vwk *);       /* 131 */
 void vdi_vex_wheelv(Vwk *);         /* 134 */
 #endif
 
+#if CONF_WITH_VDI_TEXT_SPEEDUP
+void direct_screen_blit(WORD count, WORD *str);
+#endif
+
 /* not in original TOS */
 void v_bez_qual(Vwk *);
 void v_bez_control(Vwk *);

vdi/vdi_textblit.c
@@ -742,6 +742,119 @@ void scale(LOCALVARS *vars)
 }
 
 
+#if CONF_WITH_VDI_TEXT_SPEEDUP
+/*
+ * output the font directly to the screen
+ *
+ * note: like Atari TOS, we assume that the font contains the full
+ * character set, i.e. first_ade==0, last_ade==255
+ */
+void direct_screen_blit(WORD count, WORD *str)
+{
+    WORD forecol, height, mode, n, planes;
+    WORD src_width, dst_width;
+    UBYTE *src, *dst, *save_dst;
+
+    height = DELY;
+    mode = WRT_MODE;
+    src_width = FWIDTH;
+    dst_width = v_lin_wr;
+
+    dst = (UBYTE *)get_start_addr(DESTX, DESTY);
+    if (DESTX & 0x0008)
+        dst++;
+
+    for ( ; count > 0; count--)
+    {
+        src = (UBYTE *)FBASE + *str++;
+        save_dst = dst;
+        forecol = TEXTFG;
+        for (planes = v_planes; planes > 0; planes--)
+        {
+            UBYTE *p, *q;
+
+            switch(mode) {
+            default:    /* WM_REPLACE */
+                if (forecol & 1)
+                {
+                    for (n = height, p = src, q = dst; n > 0; n--)
+                    {
+                        *q = *p;
+                        p += src_width;
+                        q += dst_width;
+                    }
+                }
+                else
+                {
+                    for (n = height, q = dst; n > 0; n--)
+                    {
+                        *q = 0;
+                        q += dst_width;
+                    }
+                }
+                break;
+            case WM_TRANS:
+                if (forecol & 1)
+                {
+                    for (n = height, p = src, q = dst; n > 0; n--)
+                    {
+                        *q |= *p;
+                        p += src_width;
+                        q += dst_width;
+                    }
+                }
+                else
+                {
+                    for (n = height, p = src, q = dst; n > 0; n--)
+                    {
+                        *q &= ~*p;
+                        p += src_width;
+                        q += dst_width;
+                    }
+                }
+                break;
+            case WM_XOR:
+                for (n = height, p = src, q = dst; n > 0; n--)
+                {
+                    *q ^= *p;
+                    p += src_width;
+                    q += dst_width;
+                }
+                break;
+            case WM_ERASE:
+                if (forecol & 1)
+                {
+                    for (n = height, p = src, q = dst; n > 0; n--)
+                    {
+                        *q |= ~*p;
+                        p += src_width;
+                        q += dst_width;
+                    }
+                }
+                else
+                {
+                    for (n = height, p = src, q = dst; n > 0; n--)
+                    {
+                        *q &= *p;
+                        p += src_width;
+                        q += dst_width;
+                    }
+                }
+                break;
+            }
+            dst += sizeof(WORD);    /* next plane */
+            forecol >>= 1;
+        }
+        dst = save_dst + 1;
+        if (!IS_ODD_POINTER(dst))   /* must go to next screen word */
+        {
+            dst += (v_planes-1)*sizeof(WORD);
+        }
+    }
+}
+#endif
+
+
 /*
  * output a block to the screen
  */

commit 13b201a3cd06b71622273d1c6e686715c42675b8

Fix clipping bug in 'fast text output'
The 'fast text output' code introduced by commit 43adfab did not
clip the text to the screen.  This caused screen artifacts when
displaying the name & info lines of desktop windows.

vdi/vdi_text.c
@@ -230,9 +230,10 @@ static WORD calc_width(Vwk *vwk, WORD cnt, WORD *str)
  *  the font contains glyphs for all 256 characters
  *  the entire text string will not be clipped
  */
-static BOOL ok_for_direct_blit(Vwk *vwk, WORD width, JUSTINFO *justified)
+static BOOL ok_for_direct_blit(Vwk *vwk, WORD width, WORD count, WORD *str, JUSTINFO *justified)
 {
-    const Fonthead *fnt_ptr = vwk->cur_font;
+    const Fonthead *fnt_ptr;
+    WORD xmin, xmax, ymin, ymax;
 
     if (vwk->style | vwk->chup | vwk->h_align)
         return FALSE;
@@ -243,19 +244,36 @@ static BOOL ok_for_direct_blit(Vwk *vwk, WORD width, JUSTINFO *justified)
     if (DESTX & 0x0007)
         return FALSE;
 
+    fnt_ptr = vwk->cur_font;
+
     if (!MONO || (fnt_ptr->max_cell_width != 8))
         return FALSE;
 
     if ((fnt_ptr->first_ade != 0) || (fnt_ptr->last_ade != 255))
         return FALSE;
 
-    if (!vwk->clip)
-        return TRUE;
+    if (width < 0)
+        width = calc_width(vwk, count, str);
+
+    if (vwk->clip)
+    {
+        xmin = vwk->xmn_clip;
+        ymin = vwk->ymn_clip;
+        xmax = vwk->xmx_clip;
+        ymax = vwk->ymx_clip;
+    }
+    else
+    {
+        xmin = 0;       /* must not exceed screen limits */
+        ymin = 0;
+        xmax = xres;
+        ymax = yres;
+    }
 
     /* check that string falls entirely within clip area */
-    if ((DESTX < vwk->xmn_clip) || (DESTX+width > vwk->xmx_clip))
+    if ((DESTX < xmin) || (DESTX+width > xmax))
         return FALSE;
-    if ((DESTY < vwk->ymn_clip) || (DESTY+DELY > vwk->ymx_clip))
+    if ((DESTY < ymin) || (DESTY+DELY > ymax))
         return FALSE;
 
     return TRUE;
@@ -433,7 +451,7 @@ static void output_text(Vwk *vwk, WORD count, WORD *str, WORD width, JUSTINFO *j
     /*
      * call special direct screen blit routine if applicable
      */
-    if (ok_for_direct_blit(vwk, width, justified))
+    if (ok_for_direct_blit(vwk, width, count, str, justified))
     {
         direct_screen_blit(count, str);
         return;