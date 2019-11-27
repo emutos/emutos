emutos/emutos

commit ec66aeaf80eaa6e4c2051536db0554ec7ce2a651

dont use hdv_init vector - STonX misuses it

bios/blkdev.c
@@ -41,7 +41,7 @@ void blkdev_init(void)
 {
     /* setup booting related vectors */
     hdv_boot    = blkdev_hdv_boot;
-    hdv_init    = blkdev_hdv_init;
+    hdv_init    = 0; // blkdev_hdv_init;
 
     /* setup general block device vectors */
     hdv_bpb     = blkdev_getbpb;
@@ -53,18 +53,22 @@ void blkdev_init(void)
 
     /* harddisk initialisation */
     // disk_init();
+
+    /* setting drvbits */
+    blkdev_hdv_init();
 }
 
 
 
 /*
- * blkdev_hdv_init - BIOS boot vector
+ * blkdev_hdv_init
  *
- * For now just floppy...
  */
 
 void blkdev_hdv_init(void)
 {
+	drvbits = 0;
+
     /* call the real */
     flop_hdv_init();
 
@@ -80,16 +84,12 @@ void blkdev_hdv_init(void)
 }
 
 
-
 /*
  * blkdev_hdv_boot - BIOS boot vector
  */
 
 LONG blkdev_hdv_boot(void)
 {
-    /* call hdv_init using the pointer - maybe vector is overloaded */
-    hdv_init();                     /* is flop_hdv_init in real... */
-
     return(flop_hdv_boot());
 
 
@@ -104,6 +104,7 @@ LONG blkdev_hdv_boot(void)
     return 0;
 #endif
 }
+
 
 /*
  * blkdev_rwabs - BIOS block device read/write vector

commit 5c04850f1381310e7bc8d169e499683995743016

Initialize hdv_init system variable
This fixes Ultimate Virus Killer 9. Thank you to ST Computer
magazine for reporting this crash.

This resolves a regression introduced by commit ec66aea.

bios/blkdev.c
@@ -99,7 +99,9 @@ void blkdev_init(void)
 
     /* setup booting related vectors */
     hdv_boot    = blkdev_hdv_boot;
-    hdv_init    = 0;    /* blkdev_hdv_init; */
+    /* On TOS hdv_init points to the floppy detection and setup routine. */
+    /* EmuTOS does not use hdv_init, but initializes it for compatibility. */
+    hdv_init    = flop_hdv_init;
 
     /* setup general block device vectors */
     hdv_bpb     = blkdev_getbpb;