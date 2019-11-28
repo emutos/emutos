emutos/emutos

commit 8530d98790b1d5a2c5c8ba5441200e3fa362c648

VDI cleaned up

vdi/vdi_main.c
@@ -17,35 +17,18 @@
 
 
 
-/* External declarations */
-//extern struct attribute *trap();
-//extern long trap13(int, ...);
-
-extern void escfn2();
-extern void escfn3();
-extern void vdimouse_init();
-extern void vdimouse_exit();
-extern WORD ** local_pb;
-
-//#define tickcal() trap13(0x06)          /* ms between timer C calls */
-//#define setexec(a,b) trap13(0x05, a,b)  /* change exception vector */
-
 #define ptsin_size 256          // max. # of elements allowed for PTSIN array
 #define ptsin_max  ptsin_size/2 // max. # of coordinate pairs for PTSIN array
 
 WORD lcl_ptsin[ptsin_size];
-WORD ** usr_pb;
 WORD flip_y;                    /* True if magnitudes being returned */
-
-Vwk virt_work;     /* attribute areas for workstations */
 WORD q_circle[MX_LN_WIDTH];     /* Holds the circle DDA */
-
 
 /* GDP variables */
 WORD angle, beg_ang, del_ang, deltay, end_ang;
 WORD start, xc, xrad, y, yc, yrad;
 
-struct font_head *cur_font;     /* Pointer to current font */
+//struct font_head *cur_font;     /* Pointer to current font */
 
 
 
@@ -143,7 +126,6 @@ void(*jmptb2[])(Vwk *) = {
 void screen()
 {
     WORD opcode, handle;
-    BOOL found;
     Vwk *vwk = NULL;
 
     /* get workstation handle */
@@ -154,53 +136,20 @@ void screen()
     CONTRL[4] = 0;
 
     flip_y = 0;
-
     opcode = CONTRL[0];
+
+    /* is it open work or vwork? */
     if (opcode != 1 && opcode != 100) {
-
-        /* Find the attribute area which matches the handle */
-        vwk = &virt_work;
-
-        found = 0;
-        do {
-            found = (handle == vwk->handle);
-        } while (!found && (vwk = vwk->next_work));
-
-        /* handle is invalid if we fall through, so exit */
-        if (!found)
+        /* Find the vwk which matches the handle, if there */
+        vwk = get_vwk_by_handle(handle);
+        if (!vwk)
             return;
 
         /* This copying is done for assembler routines */
-        INQ_TAB[19] = CLIP = vwk->clip;
-        XMN_CLIP = vwk->xmn_clip;
-        YMN_CLIP = vwk->ymn_clip;
-        XMX_CLIP = vwk->xmx_clip;
-        YMX_CLIP = vwk->ymx_clip;
-
-        WRT_MODE = vwk->wrt_mode;
-
         if (vwk->fill_style != 4)       /* multifill just for user */
             vwk->multifill = 0;
-
-        font_ring[2] = vwk->loaded_fonts;
-
-        DEV_TAB[10] = vwk->num_fonts;
-
-        DDA_INC = vwk->dda_inc;
-        T_SCLSTS = vwk->t_sclsts;
-        DOUBLE = vwk->scaled;
-
-        cur_font = vwk->cur_font;
-
-        MONO_STATUS = F_MONOSPACE & cur_font->flags;
-        scrpt2 = vwk->scrpt2;
-        scrtchp = vwk->scrtchp;
-        STYLE = vwk->style;
-        h_align = vwk->h_align;
-        v_align = vwk->v_align;
-        CHUP = vwk->chup;
     }
-    /* end if open work or vwork */
+
     if (opcode >= 1 && opcode <= 39) {
         (*jmptb1[opcode - 1]) (vwk);
     }

vdi/vdi_control.c
@@ -24,7 +24,7 @@
 #define X_MALLOC 0x48
 #define X_MFREE 0x49
 
-
+Vwk virt_work;     /* attribute areas for workstations */
 
 /*
  * SIZ_TAB - Returns text, line and marker sizes in device coordinates
@@ -129,6 +129,20 @@ WORD DEV_TAB_rom[45] = {
 
 
 
+Vwk * get_vwk_by_handle(WORD handle)
+{
+    Vwk * vwk = &virt_work;
+
+    /* Find the attribute area which matches the handle */
+    do {
+        if (handle == vwk->handle)
+            return vwk;
+    } while ((vwk = vwk->next_work));
+
+    return NULL;
+}
+
+
 /*
  * vs_color - set color index table
  */
@@ -154,7 +168,8 @@ void s_clip(Vwk * vwk)
 {
     WORD *xy, rtemp;
 
-    if ((vwk->clip = *INTIN) != 0) {
+    vwk->clip = *INTIN;
+    if (vwk->clip) {
         xy = PTSIN;
         arb_corner(xy);
 
@@ -170,12 +185,11 @@ void s_clip(Vwk * vwk)
         rtemp = *xy;
         vwk->ymx_clip = (rtemp > DEV_TAB[1]) ? DEV_TAB[1] : rtemp;
     } else {
-        vwk->clip = 0;
         vwk->xmn_clip = 0;
         vwk->ymn_clip = 0;
         vwk->xmx_clip = xres;
         vwk->ymx_clip = yres;
-    }                           /* End else:  clipping turned off. */
+    }
 }
 
 
@@ -220,7 +234,6 @@ void init_wk(Vwk * vwk)
     vwk->mark_color = MAP_COL[l];
 
     /* You always get the default font */
-
     pointer++;                  /* INTIN[5] */
 
     l = *pointer++;             /* INTIN[6] */
@@ -263,19 +276,7 @@ void init_wk(Vwk * vwk)
     vwk->ymx_clip = DEV_TAB[1];
     vwk->clip = FALSE;
 
-    vwk->cur_font = def_font;
-    vwk->loaded_fonts = NULLPTR;
-    vwk->num_fonts = font_count;
-
-    vwk->scrpt2 = scrtsiz;
-    vwk->scrtchp = deftxbuf;
-
-    vwk->style = 0;        /* reset special effects */
-    vwk->scaled = FALSE;
-    vwk->h_align = 0;
-    vwk->v_align = 0;
-    vwk->chup = 0;
-    vwk->pts_mode = FALSE;
+    text_init2(vwk);
 
     /* move default user defined pattern to RAM */
     src_ptr = ROM_UD_PATRN;
@@ -397,19 +398,9 @@ void v_opnwk(Vwk * vwk)
 
     line_cw = -1;               /* invalidate current line width */
 
-    text_init(vwk);                /* initialize the SIZ_TAB info */
+    text_init(vwk);             /* initialize the SIZ_TAB info */
+
     init_wk(vwk);
-
-    /* Input must be initialized here and not in init_wk */
-    loc_mode = 0;               /* default is request mode  */
-    val_mode = 0;               /* default is request mode  */
-    chc_mode = 0;               /* default is request mode  */
-    str_mode = 0;               /* default is request mode  */
-
-    /* mouse settings */
-    HIDE_CNT = 1;               /* mouse is initially hidden */
-    GCURX = DEV_TAB[0] / 2;     /* initialize the mouse to center */
-    GCURY = DEV_TAB[1] / 2;
 
     timer_init(vwk);
     vdimouse_init(vwk);		/* initialize mouse */
@@ -468,41 +459,39 @@ void v_clrwk(Vwk * vwk)
 
 void vq_extnd(Vwk * vwk)
 {
-    REG WORD i;
-    REG WORD *dp, *sp;
+    WORD i;
+    WORD *dst, *src;
 
-    dp = CONTRL;
-    *(dp + 2) = 6;
-    *(dp + 4) = 45;
+    CONTRL[2] = 6;
+    CONTRL[4] = 45;
 
     flip_y = 1;
-
-    dp = PTSOUT;
-
+    dst = PTSOUT;
     if (*(INTIN) == 0) {
-        sp = SIZ_TAB;
+        src = SIZ_TAB;
         for (i = 0; i < 12; i++)
-            *dp++ = *sp++;
+            *dst++ = *src++;
 
-        sp = DEV_TAB;
+        src = DEV_TAB;
     }
-
     else {
-        *dp++ = vwk->xmn_clip;       /* PTSOUT[0] */
-        *dp++ = vwk->ymn_clip;       /* PTSOUT[1] */
-        *dp++ = vwk->xmx_clip;       /* PTSOUT[2] */
-        *dp++ = vwk->ymx_clip;       /* PTSOUT[3] */
+        /* copy the clipping ranges to PTSOUT */
+        *dst++ = vwk->xmn_clip;       /* PTSOUT[0] */
+        *dst++ = vwk->ymn_clip;       /* PTSOUT[1] */
+        *dst++ = vwk->xmx_clip;       /* PTSOUT[2] */
+        *dst++ = vwk->ymx_clip;       /* PTSOUT[3] */
 
         for (i = 4; i < 12; i++)
-            *dp++ = 0;
+            *dst++ = 0;
 
-        sp = INQ_TAB;
+        src = INQ_TAB;
+        INQ_TAB[19] = vwk->clip;      /* now update INQTAB */
     }
 
-    dp = INTOUT;
+    /* copy DEV_TAB or INQ_TAB to INTOUT */
+    dst = INTOUT;
     for (i = 0; i < 45; i++)
-        *dp++ = *sp++;
-
+        *dst++ = *src++;
 }
 
 

vdi/vdi_defs.h
@@ -18,7 +18,6 @@
 #include "portab.h"
 
 
-
 /* different maximum settings */
 
 #define MAX_COLOR       16
@@ -37,10 +36,6 @@
 #define SQUARED 0
 #define ARROWED 1
 #define CIRCLED 2
-
-#define LLUR 0
-#define ULLR 1
-
 
 /* aliases for different table positions */
 #define xres            DEV_TAB[0]
@@ -71,28 +66,10 @@
 #define MD_XOR      3
 #define MD_ERASE    4
 
-//#define ABS(v) (v & 0x7FFF)
-#define ABS(x) ((x) >= 0 ? (x) : -(x))
 
 
-
-/* font-header definitions */
-
-/* fh_flags   */
-#define F_DEFAULT 1             /* this is the default font (face and size) */
-#define F_HORZ_OFF  2           /* there are left and right offset tables */
-#define F_STDFORM  4            /* is the font in standard format */
-#define F_MONOSPACE 8           /* is the font monospaced */
-
-/* style bits */
-#define F_THICKEN 1
-#define F_LIGHT 2
-#define F_SKEW  4
-#define F_UNDER 8
-#define F_OUTLINE 16
-#define F_SHADOW        32
-
-struct font_head {              /* descibes a font */
+typedef struct Fonthead_ Fonthead;
+struct Fonthead_ {              /* descibes a font */
     WORD font_id;
     WORD point;
     BYTE name[32];
@@ -119,7 +96,7 @@ struct font_head {              /* descibes a font */
     UWORD form_width;
     UWORD form_height;
 
-    struct font_head *next_font;        /* pointer to next font */
+    Fonthead *next_font;        /* pointer to next font */
     UWORD font_seg;
 };
 
@@ -127,39 +104,38 @@ struct font_head {              /* descibes a font */
 /* Structure to hold data for a virtual workstation */
 typedef struct Vwk_ Vwk;
 struct Vwk_ {
-    WORD chup;                  /* Character Up vector          */
-    WORD clip;                  /* Clipping Flag            */
-    struct font_head *cur_font; /* Pointer to current font      */
-    WORD dda_inc;               /* Fraction to be added to the DDA  */
-    WORD multifill;             /* Multi-plane fill flag        */
-    UWORD patmsk;               /* Current pattern mask         */
-    UWORD *patptr;              /* Current pattern pointer      */
-    WORD pts_mode;              /* TRUE if height set in points mode    */
-    WORD *scrtchp;              /* Pointer to text scratch buffer   */
-    WORD scrpt2;                /* Offset to large text buffer      */
-    WORD style;                 /* Current text style           */
-    WORD t_sclsts;              /* TRUE if scaling up           */
-    WORD fill_color;            /* Current fill color (PEL value)   */
-    WORD fill_index;            /* Current fill index           */
-    WORD fill_per;              /* TRUE if fill area outlined       */
-    WORD fill_style;            /* Current fill style           */
-    WORD h_align;               /* Current text horizontal alignment    */
+    WORD chup;                  /* Character Up vector */
+    WORD clip;                  /* Clipping Flag */
+    Fonthead *cur_font; /* Pointer to current font */
+    WORD dda_inc;               /* Fraction to be added to the DDA */
+    WORD multifill;             /* Multi-plane fill flag */
+    UWORD patmsk;               /* Current pattern mask */
+    UWORD *patptr;              /* Current pattern pointer */
+    WORD pts_mode;              /* TRUE if height set in points mode */
+    WORD *scrtchp;              /* Pointer to text scratch buffer */
+    WORD scrpt2;                /* Offset to large text buffer */
+    WORD style;                 /* Current text style */
+    WORD t_sclsts;              /* TRUE if scaling up */
+    WORD fill_color;            /* Current fill color (PEL value) */
+    WORD fill_index;            /* Current fill index */
+    WORD fill_per;              /* TRUE if fill area outlined */
+    WORD fill_style;            /* Current fill style */
+    WORD h_align;               /* Current text horizontal alignment */
     WORD handle;                /* The handle this attribute area is for */
-    WORD line_beg;              /* Beginning line endstyle      */
-    WORD line_color;            /* Current line color (PEL value)   */
-    WORD line_end;              /* Ending line endstyle         */
-    WORD line_index;            /* Current line style           */
-    WORD line_width;            /* Current line width           */
-    struct font_head *loaded_fonts;     /* Pointer to first loaded font     */
+    WORD line_beg;              /* Beginning line endstyle */
+    WORD line_color;            /* Current line color (PEL value) */
+    WORD line_end;              /* Ending line endstyle */
+    WORD line_index;            /* Current line style */
+    WORD line_width;            /* Current line width */
+    Fonthead *loaded_fonts;     /* Pointer to first loaded font     */
     WORD mark_color;            /* Current marker color (PEL value)     */
     WORD mark_height;           /* Current marker height        */
     WORD mark_index;            /* Current marker style         */
     WORD mark_scale;            /* Current scale factor for marker data */
-    Vwk *next_work;        /* Pointer to next virtual
-                                           workstation  */
+    Vwk *next_work;        	/* Pointer to next virtual workstation  */
     WORD num_fonts;             /* Total number of faces available  */
     WORD scaled;                /* TRUE if font scaled in any way   */
-    struct font_head scratch_head;      /* Holder for the doubled font data */
+    Fonthead scratch_head;      /* Holder for the doubled font data */
     WORD text_color;            /* Current text color (PEL value)   */
     WORD ud_ls;                 /* User defined linestyle       */
     WORD ud_patrn[4 * 16];      /* User defined pattern         */
@@ -181,42 +157,11 @@ typedef struct
 
 
 /* External definitions for internal use */
-extern Vwk virt_work;      	/* Virtual workstation attributes */
-//extern Vwk *cur_work;      	/* Pointer to current works attr. */
-
-extern WORD DDA_INC;            /* the fraction to be added to the DDA */
-extern WORD T_SCLSTS;           /* 0 if scale down, 1 if enlarge */
-
-extern WORD MONO_STATUS;        /* True if current font monospaced */
-
-extern WORD deftxbuf[];         /* Default text scratch buffer */
-extern WORD scrtsiz;            /* Default offset to large text buffer */
-
-extern WORD scrpt2;             /* Offset to large text buffer */
-extern WORD *scrtchp;           /* Pointer to text scratch buffer */
-
 extern WORD flip_y;             /* True if magnitudes being returned */
-extern WORD h_align;            /* Text horizontal alignment */
-extern WORD v_align;            /* Text vertical alignment */
-
-extern WORD font_count;         /* Number of fonts in driver */
-
-extern struct font_head *cur_font;      /* Current font */
-extern struct font_head *def_font;      /* Default font of open workstation */
-
-extern struct font_head *font_ring[];   /* Ring of available fonts */
-
-extern WORD STYLE;              /* Requested text special effects */
-extern WORD DOUBLE;             /* True if current font scaled */
-extern WORD CHUP;               /* Text baseline vector */
-
 extern WORD line_cw;            /* Linewidth for current circle */
 extern WORD num_qc_lines, q_circle[];
-
 extern WORD val_mode, chc_mode, loc_mode, str_mode;
-
 extern BYTE shft_off;           /* once computed Offset into a Scan Line */
-
 
 /* filled area variables */
 //extern UWORD *patptr, patmsk;
@@ -231,7 +176,6 @@ extern WORD start, angle, n_steps;
 extern WORD s_begsty, s_endsty, s_fil_col;
 
 /* These are still needed for text blitting */
-extern WORD CLIP, XMN_CLIP, XMX_CLIP, YMN_CLIP, YMX_CLIP;
 extern UWORD LINE_STYLE[];
 extern UWORD DITHER[];
 extern UWORD HATCH0[], HATCH1[], OEMPAT[];
@@ -251,14 +195,10 @@ extern WORD fg_bp[4];           /* points to ... */
 extern WORD FG_BP_1, FG_BP_2, FG_BP_3, FG_BP_4;
 
 extern WORD LN_MASK, LSTLIN;
-extern WORD HIDE_CNT;
-extern WORD MOUSE_BT;
-extern WORD WRT_MODE;
 extern WORD REQ_COL[3][MAX_COLOR];
 extern WORD MAP_COL[], REV_MAP_COL[];
 extern WORD X1, Y1, X2, Y2;
-extern WORD GCURX, GCURY, TERM_CH;
-extern WORD mousex, mousey;
+extern WORD TERM_CH;
 
 /* Bit-Blt variables */
 extern WORD COPYTRAN;
@@ -279,7 +219,10 @@ WORD vec_len(WORD x, WORD y);
 void arb_corner(WORD * corners);
 void arb_corner_llur(WORD * corners);
 
+
 /* C Support routines */
+Vwk * get_vwk_by_handle(WORD);
+
 void cur_display();
 void cur_replace();
 void v_show_c(Vwk *);
@@ -308,6 +251,7 @@ WORD Isqrt(ULONG x);
 
 /* initialization of subsystems */
 void text_init(Vwk *);
+void text_init2(Vwk *);
 void timer_init(Vwk *);
 void vdimouse_init(Vwk *);
 void esc_init(Vwk *);

vdi/vdi_fill.c
@@ -23,6 +23,10 @@
 
 
 
+#define ABS(v) (v & 0x7FFF)
+
+
+
 /* prototypes */
 void crunch_queue();
 
@@ -41,9 +45,6 @@ WORD qptr;               /* points to the active point   */
 WORD qtmp;
 WORD qhole;              /* an empty space in the queue */
 
-
-/* the six predefined line styles */
-UWORD LINE_STYLE[6] = { 0xFFFF, 0xFFF0, 0xC0C0, 0xFF18, 0xFF00, 0xF191 };
 
 /* the storage for the used defined fill pattern */
 UWORD UDPATMSK = 0xF;
@@ -298,10 +299,10 @@ void vqf_attr(Vwk * vwk)
     *pointer++ = vwk->fill_style;
     *pointer++ = REV_MAP_COL[vwk->fill_color];
     *pointer++ = vwk->fill_index + 1;
-    *pointer++ = WRT_MODE + 1;
+    *pointer++ = vwk->wrt_mode + 1;
     *pointer = vwk->fill_per;
 
-    *(CONTRL + 4) = 5;
+    CONTRL[4] = 5;
 }
 
 
@@ -604,15 +605,15 @@ void v_fillarea(Vwk * vwk)
  * clipbox - Just clips and copies the inputs for use by "rectfill"
  *
  * input:
- *     X1       = x coord of upper left corner.
- *     Y1       = y coord of upper left corner.
- *     X2       = x coord of lower right corner.
- *     Y2       = y coord of lower right corner.
- *     CLIP     = clipping flag. (0 => no clipping.)
- *     XMN_CLIP = x clipping minimum.
- *     XMX_CLIP = x clipping maximum.
- *     YMN_CLIP = y clipping minimum.
- *     YMX_CLIP = y clipping maximum.
+ *     X1        = x coord of upper left corner.
+ *     Y1        = y coord of upper left corner.
+ *     X2        = x coord of lower right corner.
+ *     Y2        = y coord of lower right corner.
+ *     vwk->clip = clipping flag. (0 => no clipping.)
+ *     vwk->xmn_clip = x clipping minimum.
+ *     vwk->xmx_clip = x clipping maximum.
+ *     vwk->ymn_clip = y clipping minimum.
+ *     vwk->ymx_clip = y clipping maximum.
  *
  * output:
  *     X1 = x coord of upper left corner.
@@ -671,11 +672,11 @@ BOOL clipbox(Vwk * vwk)
  *     Y1       = y coord of upper left corner.
  *     X2       = x coord of lower right corner.
  *     Y2       = y coord of lower right corner.
- *     CLIP     = clipping flag. (0 => no clipping.
- *     XMN_CLIP = x clipping minimum.
- *     XMX_CLIP = x clipping maximum.
- *     YMN_CLIP = y clipping minimum.
- *     YMX_CLIP = y clipping maximum.
+ *     vwk->clip = clipping flag. (0 => no clipping.)
+ *     vwk->xmn_clip = x clipping minimum.
+ *     vwk->xmx_clip = x clipping maximum.
+ *     vwk->ymn_clip = y clipping minimum.
+ *     vwk->ymx_clip = y clipping maximum.
  *
  * output:
  *     X1 = x coord of upper left corner.
@@ -737,7 +738,7 @@ void rectfill (Vwk * vwk)
     yinc = v_lin_wr;                    /* y coordinate increase */
     mypatmsk = vwk->patmsk;
 
-    switch (WRT_MODE) {
+    switch (vwk->wrt_mode) {
     case 3:  /* nor */
         for (y = y1; y <= y2; y++ ) {
             hzline_nor(vwk, addr, dx, leftpart, rightmask, leftmask, y&mypatmsk);
@@ -800,7 +801,7 @@ get_color (UWORD mask, UWORD * addr)
  */
 
 static UWORD
-pixelread(WORD x, WORD y)
+pixelread(const WORD x, const WORD y)
 {
     UWORD *addr;
     UWORD mask;
@@ -1082,9 +1083,7 @@ v_get_pixel(Vwk * vwk)
     int_out = INTOUT;
     *int_out++ = pel;
 
-    /* Correct the pel value for the number of planes so it is a standard
-       value */
-
+    /* Correct pel value for number of planes so it is a standard value */
     if ((INQ_TAB[4] == 1 && pel) || (INQ_TAB[4] == 2 && pel == 3))
         pel = 15;
 

vdi/vdi_line.c
@@ -19,6 +19,13 @@
 
 
 
+#define ABS(x) ((x) >= 0 ? (x) : -(x))
+
+
+/* the six predefined line styles */
+UWORD LINE_STYLE[6] = { 0xFFFF, 0xFFF0, 0xC0C0, 0xFF18, 0xFF00, 0xF191 };
+
+
 /* Wide line attribute save areas */
 WORD s_begsty, s_endsty, s_fil_col, s_fill_per;
 WORD *s_patptr;
@@ -127,20 +134,15 @@ void vsl_color(Vwk * vwk)
 
 void vql_attr(Vwk * vwk)
 {
-    WORD *pointer;
+    INTOUT[0] = vwk->line_index + 1;
+    INTOUT[1] = REV_MAP_COL[vwk->line_color];
+    INTOUT[2] = vwk->wrt_mode + 1;
 
-    pointer = INTOUT;
-    *pointer++ = vwk->line_index + 1;
-    *pointer++ = REV_MAP_COL[vwk->line_color];
-    *pointer = WRT_MODE + 1;
+    PTSOUT[0] = vwk->line_width;
+    PTSOUT[1] = 0;
 
-    pointer = PTSOUT;
-    *pointer++ = vwk->line_width;
-    *pointer = 0;
-
-    pointer = CONTRL;
-    *(pointer + 2) = 1;
-    *(pointer + 4) = 3;
+    CONTRL[2] = 1;
+    CONTRL[4] = 3;
 }
 
 
@@ -647,7 +649,7 @@ void horzline(Vwk * vwk, WORD x1, WORD x2, WORD y) {
     leftmask = ~(0xffff>>leftpart);     /* origin for not left fringe lookup */
     rightmask = 0x7fff>>rightpart;      /* origin for right fringe lookup */
 
-    switch (WRT_MODE) {
+    switch (vwk->wrt_mode) {
     case 3:  /* nor */
         hzline_nor(vwk, addr, dx, leftpart, rightmask, leftmask, patind);
         break;
@@ -836,12 +838,12 @@ void perp_off(WORD * px, WORD * py)
 
     quad_xform(quad, *vx, *vy, &x, &y);
 
-    /* Traverse the circle in a dda-like manner and find the coordinate pair */
-    /* (u, v) such that the magnitude of (u*y - v*x) is minimized.  In case
-       of */
-    /* a tie, choose the value which causes (u - v) to be minimized.  If not */
-    /* possible, do something. */
-
+    /*
+     * Traverse the circle in a dda-like manner and find the coordinate
+     * pair (u, v) such that the magnitude of (u*y - v*x) is minimized.
+     * In case of a tie, choose the value which causes (u - v) to be
+     * minimized.  If not possible, do something.
+     */
     min_val = 32767;
     u = *pcircle;
     v = 0;
@@ -991,10 +993,6 @@ void do_circ(Vwk * vwk, WORD cx, WORD cy)
 
 
 /*
- * wline - draw a line with width >1
- */
-
-/*
  * s_fa_attr - Save the fill area attribute
  */
 
@@ -1031,6 +1029,10 @@ void r_fa_attr(Vwk * vwk)
 
 
 
+/*
+ * wideline - draw a line with width >1
+ */
+
 void wideline(Vwk * vwk)
 {
     WORD i, k, box[10];         /* box two high to close polygon */
@@ -1052,7 +1054,6 @@ void wideline(Vwk * vwk)
     s_fa_attr(vwk);
 
     /* Initialize the starting point for the loop. */
-
     old_ptsin = pointer = PTSIN;
     wx1 = *pointer++;
     wy1 = *pointer++;
@@ -1062,38 +1063,34 @@ void wideline(Vwk * vwk)
     if (s_begsty != SQUARED) {
         do_circ(vwk, wx1, wy1);
     }
+
     /* Loop over the number of points passed in. */
-
     for (i = 1; i < numpts; i++) {
-        /* Get the ending point for the line segment and the vector from the */
-        /* start to the end of the segment.                                  */
-
+        /* Get ending point for line segment */
         pointer = src_ptr;
         wx2 = *pointer++;
         wy2 = *pointer++;
         src_ptr = pointer;
 
+        /* Get vector from start to end of the segment. */
         vx = wx2 - wx1;
         vy = wy2 - wy1;
 
         /* Ignore lines of zero length. */
-
         if ((vx == 0) && (vy == 0))
             continue;
 
-        /* Calculate offsets to fatten the line.  If the line segment is */
-        /* horizontal or vertical, do it the simple way.                 */
-
+        /* Calculate offsets to fatten the line. */
         if (vx == 0) {
+            /* line is horizontal - do it the simple way */
             vx = q_circle[0];
             vy = 0;
         }
-        /* End if:  vertical. */
         else if (vy == 0) {
+            /* line is vertical - do it the simple way */
             vx = 0;
             vy = num_qc_lines - 1;
         }
-        /* End else if:  horizontal. */
         else {
             /* Find the offsets in x and y for a point perpendicular */
             /* to the line segment at the appropriate distance. */
@@ -1105,7 +1102,6 @@ void wideline(Vwk * vwk)
                                    vertical. */
 
         /* Prepare the control and points parameters for the polygon call. */
-
         *(CONTRL + 1) = 4;
 
         PTSIN = pointer = box;
@@ -1140,11 +1136,11 @@ void wideline(Vwk * vwk)
         /* end point becomes the starting point for the next line segment. */
         wx1 = wx2;
         wy1 = wy2;
-    }                           /* End for:  over number of points. */
+    } /* End for:  over number of points. */
 
     /* Restore the attribute environment. */
     r_fa_attr(vwk);
-}                               /* End "wline". */
+} /* End "wline". */
 
 
 
@@ -1361,7 +1357,7 @@ void abline (Vwk * vwk)
 
 #if 0
     if (Y1 == Y2) {
-        kprintf("Y = %d, MODE = %d.\n", Y1, WRT_MODE);
+        kprintf("Y = %d, MODE = %d.\n", Y1, vwk->wrt_mode);
         //horzline(X1, X2, Y1);
         return;
     }
@@ -1419,7 +1415,7 @@ void abline (Vwk * vwk)
             eps = -dx;
             e2 = 2*dx;
 
-            switch (WRT_MODE) {
+            switch (vwk->wrt_mode) {
             case 3:              /* reverse transparent  */
                 if (*color) {
                     for (loopcnt=dx;loopcnt >= 0;loopcnt--) {
@@ -1535,7 +1531,7 @@ void abline (Vwk * vwk)
             eps = -dy;
             e2 = 2*dy;
 
-            switch (WRT_MODE) {
+            switch (vwk->wrt_mode) {
             case 3:              /* reverse transparent */
                 if (*color) {
                     for (loopcnt=dy;loopcnt >= 0;loopcnt--) {

vdi/vdi_marker.c
@@ -179,20 +179,15 @@ void v_pmarker(Vwk * vwk)
 
 void vqm_attr(Vwk * vwk)
 {
-    WORD *pointer;
+    INTOUT[0] = vwk->mark_index;
+    INTOUT[1] = REV_MAP_COL[vwk->mark_color];
+    INTOUT[2] = vwk->wrt_mode + 1;
 
-    pointer = INTOUT;
-    *pointer++ = vwk->mark_index;
-    *pointer++ = REV_MAP_COL[vwk->mark_color];
-    *pointer = WRT_MODE + 1;
+    PTSOUT[0] = 0;
+    PTSOUT[1] = vwk->mark_height;
 
-    pointer = PTSOUT;
-    *pointer++ = 0;
-    *pointer = vwk->mark_height;
-
-    pointer = CONTRL;
-    *(pointer + 4) = 3;
-    *(pointer + 2) = 1;
+    CONTRL[4] = 3;
+    CONTRL[2] = 1;
     flip_y = 1;
 }
 

vdi/vdi_mouse.c
@@ -44,6 +44,10 @@ extern WORD m_cdb_bg;       // (cdb+6) Mouse background color as pel value
 extern WORD m_cdb_fg;       // (cdb+8) Mouse foreground color as pel value
 extern UWORD mask_form;     // (cdb+10) Storage for mouse mask and cursor
 
+extern WORD HIDE_CNT;
+extern WORD MOUSE_BT;
+extern WORD GCURX, GCURY;
+extern WORD mousex, mousey;
 
 
 /* Default Mouse Cursor Definition */
@@ -455,6 +459,17 @@ void xfm_crfm (Vwk * vwk)
 
 void vdimouse_init(Vwk * vwk)
 {
+    /* Input must be initialized here and not in init_wk */
+    loc_mode = 0;               /* default is request mode  */
+    val_mode = 0;               /* default is request mode  */
+    chc_mode = 0;               /* default is request mode  */
+    str_mode = 0;               /* default is request mode  */
+
+    /* mouse settings */
+    HIDE_CNT = 1;               /* mouse is initially hidden */
+    GCURX = DEV_TAB[0] / 2;     /* initialize the mouse to center */
+    GCURY = DEV_TAB[1] / 2;
+
     user_but = do_nothing;
     user_mot = do_nothing;
     user_cur = mov_cur;         /* initialize user_cur vector */

commit b17f2c4943b2f617247438abdaf41f8cd33610b0

Ensure CUR_FONT lineA variable is set
This fixes a crash when starting Microsoft Write.  Thanks to
ST Computer magazine for reporting this.  This resolves a
regression introduced by commit 8530d98.

bios/lineavars.S
@@ -21,7 +21,7 @@
 // ===========================================================================
 
                 .globl  _mdata
-                .globl  _cur_font
+                .globl  _CUR_FONT
                 .globl  _cur_ms_stat
                 .globl  _disab_cnt
                 .globl  _draw_flag
@@ -97,7 +97,7 @@
 
 /* Font related VDI variables */
                 .ds.l   1       // -910 start angle
-_cur_font:      .ds.l   1       // -906 current VDI font
+_CUR_FONT:      .ds.l   1       // -906 current VDI font
                 .ds.w   23      // -902
 
 /* The mouse form storage area: */

include/lineavars.h
@@ -91,6 +91,7 @@ extern WORD *SCRTCHP;           /* Pointer to text scratch buffer */
 extern WORD SCRPT2;             /* Offset to large text buffer */
 
 /* font-specific variables */
+extern const Fonthead *CUR_FONT;/* most recently used font */
 extern const UWORD *v_fnt_ad;   /* address of current monospace font */
 extern const UWORD *v_off_ad;   /* address of font offset table */
 extern UWORD v_fnt_nd;          /* ascii code of last cell in font */

vdi/vdi_main.c
@@ -10,6 +10,7 @@
 
 #include "emutos.h"
 #include "vdi_defs.h"
+#include "lineavars.h"
 #include "asm.h"
 
 /* forward prototypes */
@@ -144,5 +145,13 @@ void screen(void)
 
     else if (opcode >= 100 && opcode < 100+JMPTB2_ENTRIES) {
         (*jmptb2[opcode - 100]) (vwk);
+    }
+
+    /*
+     * set some lineA variables from the vwk info (as long as
+     * the workstation is valid)
+     */
+    if (opcode != 2 && opcode != 101) {     /* if neither v_clswk() nor v_clsvwk() */
+        CUR_FONT = vwk->cur_font;
     }
 }