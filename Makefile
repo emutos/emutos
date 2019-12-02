emutos/emutos

commit e1455deea3ed2fd05e12f6bc5b67cedf6d5ad1eb

Move GEM_MUPB from AES to BIOS
This makes sense as that structure is defined and used by the BIOS
in order to run the main UI.

bios/bios.c
@@ -1114,3 +1114,11 @@ const PFLONG bios_vecs[] = {
 };
 
 const UWORD bios_ent = ARRAY_SIZE(bios_vecs);
+
+/* GEM memory usage parameters block */
+const GEM_MUPB ui_mupb =
+{
+    GEM_MUPB_MAGIC, /* Magic value identifying this structure */
+    _endgembss,     /* End of GEM BSS */
+    ui_start        /* AES entry point */
+};

aes/geminit.c
@@ -109,13 +109,6 @@ static char     infbuf[INF_SIZE+1];     /* used to read part of EMUDESK.INF */
 
 /* Some global variables: */
 
-GLOBAL const GEM_MUPB ui_mupb =
-{
-    GEM_MUPB_MAGIC, /* Magic value identifying this structure */
-    _endgembss,     /* End of GEM BSS */
-    ui_start        /* AES entry point */
-};
-
 GLOBAL WORD     totpds;
 GLOBAL WORD     num_accs;
 

aes/struct.h
@@ -159,15 +159,4 @@ struct fpd                  /* fork process descriptor */
         LONG    f_data;
 } ;
 
-/* GEM memory usage parameter block */
-
-#define GEM_MUPB_MAGIC 0x87654321
-
-typedef struct
-{
-    ULONG  gm_magic;        /* Magical value, has to be GEM_MUPB_MAGIC */
-    UBYTE  *gm_end;         /* End of the memory required by GEM */
-    PFVOID gm_init;         /* Start address of GEM */
-} GEM_MUPB;
-
 #endif /* GEMSTRUCT_H */

bios/tosvars.h
@@ -20,6 +20,21 @@
 
 #include "portab.h"
 
+/* GEM memory usage parameters block.
+ * This is actually a structure used by the BIOS
+ * in order to call the main UI (i.e. AES) */
+
+#define GEM_MUPB_MAGIC 0x87654321
+
+typedef struct
+{
+    ULONG  gm_magic; /* Magical value, has to be GEM_MUPB_MAGIC */
+    UBYTE  *gm_end;  /* End of the static ST-RAM used by the OS */
+    PFVOID gm_init;  /* Start address of the main UI */
+} GEM_MUPB;
+
+extern const GEM_MUPB *os_magic; /* Information about the main UI */
+
 extern LONG proc_lives;
 extern LONG proc_dregs[];
 extern LONG proc_aregs[];
@@ -98,8 +113,6 @@ extern UBYTE *memtop;
 extern UBYTE *ramtop;     /* top of TT-RAM, or NULL if no TT-RAM is present */
 #define RAMVALID_MAGIC 0x1357BD13
 extern LONG ramvalid;     /* if equal to RAMVALID_MAGIC, then ramtop is valid */
-
-extern LONG os_magic;     /* if == 0x87654321, means that GEM is present */
 
 extern LONG savptr;
 

commit 0e19aaf7422ca9c885bf0adb9494101890893a0f

Fix compatibility with Geneva 1.8
This bug has been introduced in commit e1455dee.
Thanks to Mia Jaap for having spotted this bug,
and to Christian Zietz for the investigation.

Without this, Geneva fails with:
"Something is already hooked into exec_os"

Solution is to move ui_mupb at the end of the ROM.

Note: Older Geneva Release 003 still crashes for other reasons.

Makefile
@@ -271,6 +271,12 @@ desk_src = deskstart.S deskmain.c gembind.c deskact.c deskapp.c deskdir.c \
 cli_src = cmdasm.S cmdmain.c cmdedit.c cmdexec.c cmdint.c cmdparse.c cmdutil.c
 
 #
+# source code to put at the end of the ROM
+#
+
+end_src = bios/endrom.c
+
+#
 # Makefile functions
 #
 
@@ -333,11 +339,12 @@ include country.mk
 # everything should work fine below.
 #
 
-SRC = $(foreach d,$(dirs),$(addprefix $(d)/,$($(d)_src)))
+SRC = $(foreach d,$(dirs),$(addprefix $(d)/,$($(d)_src))) $(end_src)
 
 CORE_OBJ = $(foreach d,$(core_dirs),$(patsubst %.c,obj/%.o,$(patsubst %.S,obj/%.o,$($(d)_src)))) $(FONTOBJ) obj/version.o
 OPTIONAL_OBJ = $(foreach d,$(optional_dirs),$(patsubst %.c,obj/%.o,$(patsubst %.S,obj/%.o,$($(d)_src))))
-OBJECTS = $(CORE_OBJ) $(OPTIONAL_OBJ)
+END_OBJ = $(patsubst %,obj/%.o,$(basename $(notdir $(end_src))))
+OBJECTS = $(CORE_OBJ) $(OPTIONAL_OBJ) $(END_OBJ)
 
 #
 # production targets
@@ -409,7 +416,7 @@ obj/emutospp.ld: emutos.ld include/config.h tosvars.ld
 TOCLEAN += *.img *.map
 
 emutos.img: $(OBJECTS) obj/emutospp.ld Makefile
-	$(LD) $(CORE_OBJ) $(LIBS) $(OPTIONAL_OBJ) $(LIBS) $(LDFLAGS) -Wl,-Map=emutos.map -o emutos.img
+	$(LD) $(CORE_OBJ) $(LIBS) $(OPTIONAL_OBJ) $(LIBS) $(END_OBJ) $(LDFLAGS) -Wl,-Map=emutos.map -o emutos.img
 	@if [ $$(($$(awk '/^\.data /{print $$3}' emutos.map))) -gt 0 ]; then \
 	  echo "### Warning: The DATA segment is not empty."; \
 	  echo "### Please examine emutos.map and use \"const\" where appropriate."; \

bios/bios.c
@@ -64,9 +64,6 @@
 #ifdef MACHINE_FIREBEE
 #include "coldfire.h"
 #endif
-#if WITH_AES
-#include "../aes/aesstub.h"
-#endif
 #if WITH_CLI
 #include "../cli/clistub.h"
 #endif
@@ -1123,20 +1120,6 @@ const PFLONG bios_vecs[] = {
 };
 
 const UWORD bios_ent = ARRAY_SIZE(bios_vecs);
-
-/* GEM memory usage parameters block */
-const GEM_MUPB ui_mupb =
-{
-    GEM_MUPB_MAGIC, /* Magic value identifying this structure */
-    _end_os_stram,  /* End of the static ST-RAM used by the OS */
-#if WITH_AES
-    ui_start        /* AES entry point */
-#elif WITH_CLI
-    coma_start      /* EmuCON entry point */
-#else
-#  error You must provide a main UI
-#endif
-};
 
 #if CONF_WITH_EXTENDED_MOUSE
 

bios/endrom.c
@@ -1,0 +1,37 @@
+/*
+ * endrom.c - data to put at the end of the ROM
+ *
+ * Copyright (C) 2019 The EmuTOS development team
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
+ */
+
+/* #define ENABLE_KDEBUG */
+
+#include "emutos.h"
+#include "biosdefs.h"
+#include "bios.h"
+#if WITH_AES
+#include "../aes/aesstub.h"
+#endif
+#if WITH_CLI
+#include "../cli/clistub.h"
+#endif
+
+/* GEM memory usage parameters block.
+ * This *must* live after the exec_os contents for Geneva compatibility.
+ * In other words, the address of ui_mupb must be greater than the value
+ * of its third field. */
+const GEM_MUPB ui_mupb =
+{
+    GEM_MUPB_MAGIC, /* Magic value identifying this structure */
+    _end_os_stram,  /* End of the static ST-RAM used by the OS */
+#if WITH_AES
+    ui_start        /* AES entry point */
+#elif WITH_CLI
+    coma_start      /* EmuCON entry point */
+#else
+#  error You must provide a main UI
+#endif
+};