emutos/emutos

commit 83b0bd08c8c295a6689824ba74819fe88f20283a

Dynamically allocate sector buffers (saves RAM).

bdos/bdosmain.c
@@ -261,9 +261,11 @@ void    osinit(void)
      */
     old_trap2 = (void(*)(void)) Setexc(0x22, (long)bdos_trap2);
 
-    bufl_init();                /* initialize BDOS buffer list */
     osmem_init();
     umem_init();
+
+    bufl_init();    /* initialize BDOS buffer list, now Malloc is available */
+
     time_init();
 
     run = MGET(PD);

bdos/fsbuf.c
@@ -18,44 +18,53 @@
 #include "fs.h"
 #include "gemerror.h"
 #include "biosbind.h"
+#include "ahdi.h"
+#include "mem.h"
+#include "string.h"
 #include "kprint.h"
 
+extern BCB *bufl[];     /* buffer lists - two lists:  FAT and dir/data */
 
+#define NUMBUFS 2       /* buffers per list */
 
- /* sector buffers: 16kB is TOS 4.0x limit */
-static BYTE secbuf[4][16384];
+/* creates a chain of BCBs and corresponding buffers */
+static void *create_chain(char *p,LONG n)
+{
+    BCB *bcbptr;
+    WORD i;
 
-static BCB bcbx[4];    /* buffer control block array for each buffer */
-extern BCB *bufl[];    /* buffer lists - two lists:  fat,dir / data */
+    for (i = 0; i < NUMBUFS; i++, p += n) {
+        bcbptr = (BCB *)p;
+        memset(bcbptr,0x00,sizeof(BCB));
+        if (i < NUMBUFS-1)                  /* chain to next */
+            bcbptr->b_link = (BCB *)(p + n);
+        bcbptr->b_bufdrv = -1;              /* mark as invalid */
+        bcbptr->b_bufr = p + sizeof(BCB);
+    }
+
+    return p;
+}
 
 /*
  * bufl_init - BDOS buffer list initialization
  */
 void bufl_init(void)
 {
-    /* set up sector buffers */
+    char *p;
+    LONG n;
 
-    bcbx[0].b_link = &bcbx[1];
-    bcbx[2].b_link = &bcbx[3];
+    n = sizeof(BCB) + pun_ptr->max_sect_siz;
+    p = (char *)xmalloc(2L*NUMBUFS*n);
+    if (!p)
+        panic("bufl_init(%ld): no memory\n",2L*NUMBUFS*n);
 
-    /* make BCBs invalid */
+    /* set up FAT chain */
+    bufl[BI_FAT] = (BCB *)p;
+    p = create_chain(p,n);
 
-    bcbx[0].b_bufdrv = -1;
-    bcbx[1].b_bufdrv = -1;
-    bcbx[2].b_bufdrv = -1;
-    bcbx[3].b_bufdrv = -1;
-
-    /* initialize buffer pointers in BCBs */
-
-    bcbx[0].b_bufr = &secbuf[0][0];
-    bcbx[1].b_bufr = &secbuf[1][0];
-    bcbx[2].b_bufr = &secbuf[2][0];
-    bcbx[3].b_bufr = &secbuf[3][0];
-
-    /* initialize the buffer list pointers */
-
-    bufl[BI_FAT] = &bcbx[0];                    /* fat buffers */
-    bufl[BI_DATA] = &bcbx[2];                   /* dir/data buffers */
+    /* set up dir/data chain */
+    bufl[BI_DATA] = (BCB *)p;
+    create_chain(p,n);
 }
 
 

bios/blkdev.c
@@ -19,6 +19,7 @@
 #include "gemerror.h"
 #include "kprint.h"
 #include "tosvars.h"
+#include "ahdi.h"
 #include "mfp.h"
 #include "floppy.h"
 #include "disk.h"
@@ -115,7 +116,7 @@ static void pun_info_setup(void)
     pun_info.cookie_ptr = &pun_info.cookie;
     pun_info.version_num = 0x300;      /* AHDI v3.00 */
 
-    pun_ptr = (LONG)&pun_info;
+    pun_ptr = &pun_info;
 
     KDEBUG(("PUN INFO: max sector size = %d\n",pun_info.max_sect_siz));
 }

bios/blkdev.h
@@ -74,29 +74,6 @@ struct fat16_bs {
 };
 
 
-struct _pun_info
-{
-        unsigned short  puns;                   /* Number of HD's */
-        unsigned char   pun [16];               /* AND with masks below: */
-
-# define PUN_DEV        0x1f                    /* device number of HD */
-# define PUN_UNIT       0x7                     /* Unit number */
-# define PUN_SCSI       0x8                     /* 1=SCSI 0=ACSI */
-# define PUN_IDE        0x10                    /* Falcon IDE */
-# define PUN_REMOVABLE  0x40                    /* Removable media */
-# define PUN_VALID      0x80                    /* zero if valid */
-
-        long            partition_start [16];
-        long            cookie;                 /* 'AHDI' if following valid */
-        long            *cookie_ptr;            /* Points to 'cookie' */
-        unsigned short  version_num;            /* AHDI version */
-        unsigned short  max_sect_siz;           /* Max logical sec size */
-        long            reserved[16];           /* Reserved */
-};
-
-typedef struct _pun_info PUN_INFO;
-
-
 struct _geometry        /* disk parameter block */
 {
     WORD spt;          /* number of sectors per track */

bios/tosvars.h
@@ -89,8 +89,6 @@ extern LONG ramvalid;     /* indicates if fastram is present */
 
 extern LONG os_magic;     /* if == 0x87654321, means that GEM is present */
 
-extern LONG pun_ptr;
-
 extern LONG savptr;
 
 extern void (*prt_stat)(void);

include/ahdi.h
@@ -1,0 +1,44 @@
+/*
+ * ahdi.h - AHDI-related structures and variables
+ *
+ * This file exists to allow AHDI stuff to be referenced by both
+ * the BDOS and the BIOS.
+ *
+ * Copyright (c) 2014 The EmuTOS development team
+ *
+ * Authors:
+ *  RFB    Roger Burrows
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
+ */
+
+#ifndef _AHDI_H
+#define _AHDI_H
+
+#include "portab.h"
+
+
+typedef struct
+{
+    UWORD puns;                 /* number of block devices (partitions) */
+    UBYTE pun[16];              /* bus/device for this partition */
+    ULONG partition_start[16];
+    ULONG cookie;               /* 'AHDI' if following valid */
+    ULONG *cookie_ptr;          /* points to 'cookie' */
+    UWORD version_num;          /* AHDI version */
+    UWORD max_sect_siz;         /* maximum logical sector size */
+    LONG reserved[16];
+} PUN_INFO;
+
+/* masks for pun[] array above: */
+#define PUN_DEV         0x1f    /* device number of HD */
+#define PUN_UNIT        0x7     /* Unit number */
+#define PUN_SCSI        0x8     /* 1=SCSI 0=ACSI */
+#define PUN_IDE         0x10    /* Falcon IDE */
+//#define PUN_REMOVABLE   0x40    /* Removable media */
+#define PUN_VALID       0x80    /* zero if valid */
+
+extern PUN_INFO *pun_ptr;
+
+#endif /* _AHDI_H */

commit 836fc148a68e99a849a2be56efd8e108939a0864

Fix crash in Dungeon Master
Dungeon Master is directly booted from (floppy) disk and assumes the
memory starting at membot is free.  Under Atari TOS, no Malloc has
yet been done, so this is safe.  This was not true under EmuTOS,
which Malloced the BDOS cache buffers.  EmuTOS now uses balloc_stram
instead.  This regression was introduced by commit 83b0bd0.

bdos/bdosmain.c
@@ -316,6 +316,8 @@ void osinit_before_xmaddalt(void)
      */
     old_trap2 = (PFVOID) Setexc(0x22, (long)bdos_trap2);
 
+    bufl_init();    /* initialize BDOS buffer list */
+
     osmem_init();
     umem_init();
 }
@@ -327,8 +329,6 @@ void osinit_after_xmaddalt(void)
     /* Set up initial process. Required by Malloc() */
     run = &initial_basepage;
     run->p_flags = PF_STANDARD;
-
-    bufl_init();    /* initialize BDOS buffer list, now Malloc is available */
 
     time_init();
 

bdos/fsbuf.c
@@ -45,6 +45,13 @@ static void *create_chain(UBYTE *p,LONG n)
 
 /*
  * bufl_init - BDOS buffer list initialization
+ *
+ * this must be called before memory is initialised, because we use
+ * balloc_stram().  we use balloc_stram() because we must not use
+ * Malloc() until after we have finished booting.  this is because TOS
+ * doesn't, and some programs that are direct-booted from a disk may
+ * therefore assume that all memory from membot upwards is available
+ * (I'm looking at you, Dungeon Master).
  */
 void bufl_init(void)
 {
@@ -52,7 +59,7 @@ void bufl_init(void)
     LONG n;
 
     n = sizeof(BCB) + pun_ptr->max_sect_siz;
-    p = xmalloc(2L*NUMBUFS*n);
+    p = balloc_stram(2L*NUMBUFS*n, FALSE);
     if (!p)
         panic("bufl_init(%ld): no memory\n",2L*NUMBUFS*n);
 

bios/biosmem.h
@@ -21,7 +21,6 @@ extern UBYTE dskbuf[DSKBUF_SIZE]; /* In ST-RAM */
 
 /* Prototypes */
 void bmem_init(void);
-UBYTE *balloc_stram(ULONG size, BOOL top);
 
 /* BIOS function */
 

include/biosext.h
@@ -46,6 +46,9 @@ void flush_data_cache(void *start, long size);
 void invalidate_data_cache(void *start, long size);
 void invalidate_instruction_cache(void *start, long size);
 
+/* bios allocation of ST-RAM */
+UBYTE *balloc_stram(ULONG size, BOOL top);
+
 /* print a panic message both via kprintf and cprintf, then halt */
 void panic(const char *fmt, ...) PRINTF_STYLE NORETURN;
 