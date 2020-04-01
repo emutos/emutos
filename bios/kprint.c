emutos/emutos

commit 162c0dac406248a7fe0efcdd5e189f57fada2308

Remove chardev duplicate declarations

bios/kprint.c
@@ -30,6 +30,7 @@
 #include "vectors.h"
 #include "super.h"      /* for Super() and SuperToUser() */
 #include "../bdos/bdosstub.h"
+#include "ikbd.h"
 #ifdef MACHINE_AMIGA
 #include "amiga.h"
 #endif

bios/chardev.c
@@ -21,6 +21,10 @@
 #include "mfp.h"
 #include "bios.h"
 #include "asm.h"
+#include "serport.h"
+#include "ikbd.h"
+#include "midi.h"
+#include "parport.h"
 
 #define NUM_CHAR_VECS   8
 

bios/chardev.h
@@ -15,27 +15,9 @@
 #define _CHARDEV_H
 
 /* Prototypes */
-LONG bconstat1(void);
-LONG bconstat2(void);
-LONG bconstat3(void);
-
-LONG bconin0(void);
-LONG bconin1(void);
-LONG bconin2(void);
-LONG bconin3(void);
-
-LONG bconout0(WORD, WORD);
-LONG bconout1(WORD, WORD);
 LONG bconout2(WORD, WORD);
-LONG bconout3(WORD, WORD);
-LONG bconout4(WORD, WORD);
 LONG bconout5(WORD, WORD);
-
-LONG bcostat0(void);
-LONG bcostat1(void);
 LONG bcostat2(void);
-LONG bcostat3(void);
-LONG bcostat4(void);
 
 LONG char_dummy(void);
 LONG charout_dummy(WORD dev, WORD x);

commit 70252d5f60ac05e24ce4b36d5f45791ed927dbcc

Make MIDI_DEBUG_PRINT work again
EmuTOS would fail compiling when MIDI_DEBUG_PRINT was set to 1.
This fixes a regression introduced by commit 162c0da.

bios/kprint.c
@@ -31,6 +31,7 @@
 #include "super.h"      /* for Super() and SuperToUser() */
 #include "../bdos/bdosstub.h"
 #include "ikbd.h"
+#include "midi.h"
 #ifdef MACHINE_AMIGA
 #include "amiga.h"
 #endif