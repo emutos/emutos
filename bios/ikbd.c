emutos/emutos

commit 988c6208b41df17d7123d7722ca9c598baec5a15

Fix key repeat handling
If key repeat was in progress, and a second key was pressed and held
down then the second key would start repeating instead of the first
one.  This is the same as Atari TOS.  However, if the first key was
then released, key repeat would stop, which is wrong: only the
release of the currently-repeating key should stop the repeat.  This
problem also affected mouse movement via the alt-arrow keys.
Thanks to Chris (exxos) for reporting the problem.

bios/ikbd.c
@@ -285,15 +285,6 @@ static BOOL is_mouse_key(WORD key)
 }
 
 /*
- * initialise mouse packet
- */
-static void init_mouse_packet(UBYTE *packet)
-{
-    packet[0] = MOUSE_REL_POS_REPORT;
-    packet[1] = packet[2] = 0;
-}
-
-/*
  * check if we should switch into or out of mouse emulation mode
  * if so, send the relevant mouse packet
  *
@@ -302,15 +293,13 @@ static void init_mouse_packet(UBYTE *packet)
 static BOOL handle_mouse_mode(WORD newkey)
 {
     SBYTE distance;
+    BOOL button = FALSE;
 
     /*
-     * if we shouldn't be in emulation mode, but we are, send an
-     * appropriate mouse packet and exit
+     * if we shouldn't be in emulation mode, but we are, cleanup and exit
      */
     if (!(shifty&MODE_ALT) || !is_mouse_key(newkey & ~KEY_RELEASED)) {
         if (mouse_packet[0]) {  /* emulating, need to clean up */
-            init_mouse_packet(mouse_packet);
-            call_mousevec(mouse_packet);
             mouse_packet[0] = '\0';
             KDEBUG(("Exiting mouse emulation mode\n"));
         }
@@ -324,13 +313,6 @@ static BOOL handle_mouse_mode(WORD newkey)
         KDEBUG(("Entering mouse emulation mode\n"));
         mouse_packet[0] = MOUSE_REL_POS_REPORT;
     }
-
-    /*
-     * always reset the x,y distance variables for the next
-     * mouse packet.  this is important when the packet is
-     * an emulated mouse button click.
-     */
-    mouse_packet[1] = mouse_packet[2] = 0;
 
     /*
      * set movement distance according to the Shift and Control keys.
@@ -347,12 +329,14 @@ static BOOL handle_mouse_mode(WORD newkey)
     switch(newkey) {
     case KEY_EMULATE_LEFT_BUTTON:
         mouse_packet[0] |= LEFT_BUTTON_DOWN;
+        button = TRUE;
         break;
     case KEY_EMULATE_LEFT_BUTTON | KEY_RELEASED:
         mouse_packet[0] &= ~LEFT_BUTTON_DOWN;
         break;
     case KEY_EMULATE_RIGHT_BUTTON:
         mouse_packet[0] |= RIGHT_BUTTON_DOWN;
+        button = TRUE;
         break;
     case KEY_EMULATE_RIGHT_BUTTON | KEY_RELEASED:
         mouse_packet[0] &= ~RIGHT_BUTTON_DOWN;
@@ -372,6 +356,13 @@ static BOOL handle_mouse_mode(WORD newkey)
         mouse_packet[2] = 0;        /* Atari TOS only allows one direction at a time */
         break;
     }
+
+    /*
+     * if the packet is for an emulated mouse button press,
+     * reset the x,y distance variables
+     */
+    if (button)
+        mouse_packet[1] = mouse_packet[2] = 0;
 
     KDEBUG(("Sending mouse packet %02x%02x%02x\n",mouse_packet[0],mouse_packet[1],mouse_packet[2]));
     call_mousevec(mouse_packet);
@@ -417,10 +408,11 @@ static union {
     ULONG key;                  /* combined value */
     struct {
         UBYTE shifty;           /* state of 'shifty' */
-        UBYTE scancode;         /* actual scancode */
+        UBYTE scancode;         /* possibly-converted scancode */
         UWORD ascii;            /* derived ascii value */
     } k;
 } kb_last;
+static UBYTE kb_last_actual;    /* actual last scancode */
 static PFVOID kb_last_ikbdsys;  /* ikbdsys when kb_last was set */
 
 WORD kbrate(WORD initial, WORD repeat)
@@ -487,11 +479,9 @@ static void do_key_repeat(void)
      * they change, we must do the scancode conversion again
      */
     if (shifty != kb_last.k.shifty) {
-        UBYTE scancode;
-
-        /* use a copy of scancode because convert_scancode() can change it */
-        scancode = kb_last.k.scancode;
-        kb_last.k.ascii = convert_scancode(&scancode);
+        /* get actual last scancode because convert_scancode() can change it */
+        kb_last.k.scancode = kb_last_actual;
+        kb_last.k.ascii = convert_scancode(&kb_last.k.scancode);
         kb_last.k.shifty = shifty;
         kb_last_ikbdsys = kbdvecs.ikbdsys;
     }
@@ -752,6 +742,8 @@ void kbd_int(UBYTE scancode)
             break;
         case KEY_ALT:
             shifty &= ~MODE_ALT;        /* clear bit */
+            if (mouse_packet[0])
+                kb_ticks = 0;           /* stop key repeat */
             if (kb_altnum >= 0) {
                 ascii = LOBYTE(kb_altnum);
                 kb_altnum = -1;
@@ -776,7 +768,8 @@ void kbd_int(UBYTE scancode)
             kb_ticks = 0;               /* stop key repeat */
             break;
         default:                    /* non-modifier keys: */
-            kb_ticks = 0;               /*  stop key repeat */
+            if (scancode_only == kb_last_actual)    /* key-up matches last key-down: */
+                kb_ticks = 0;                       /*  stop key repeat */
         }
         handle_mouse_mode(scancode);    /* exit mouse mode if appropriate */
         return;
@@ -785,6 +778,8 @@ void kbd_int(UBYTE scancode)
     /*
      * a key has been pressed
      */
+    kb_last_actual = scancode;      /* save because 'scancode' may get changed */
+
     modifier = TRUE;
     switch (scancode) {
     case KEY_RSHIFT:

commit 56a0a4ef45e775c5b31f884924cf08f52653da9d

Fix bug in keyboard mouse emulation
If keyboard mouse emulation was active, with Shift/Alt/arrowkey down,
and the arrowkey was released, mouse emulation mode was not exited
properly.  As a result, if you then toggled the Shift key, keeping
the Alt key down, the mouse cursor jumped 8 pixels at a time.  This
bug was introduced by commit 988c620.

bios/ikbd.c
@@ -110,6 +110,12 @@ static WORD convert_scancode(UBYTE *scancodeptr);
  */
 SBYTE mouse_packet[3];                  /* passed to mousevec() */
 
+/*
+ * the following is a count of the number of arrow keys currently down;
+ * it is used in mouse emulation mode.
+ */
+static WORD kb_arrowkeys;
+
 /*=== Keymaps handling (xbios) =======================================*/
 
 static struct keytbl current_keytbl;
@@ -260,31 +266,6 @@ void push_ascii_ikbdiorec(UBYTE ascii)
  */
 
 /*
- * is the key related to mouse emulation?
- */
-static BOOL is_mouse_key(WORD key)
-{
-    switch(key) {
-    case KEY_EMULATE_LEFT_BUTTON:
-    case KEY_EMULATE_RIGHT_BUTTON:
-    case KEY_UPARROW:
-    case KEY_DNARROW:
-    case KEY_LTARROW:
-    case KEY_RTARROW:
-    /*
-     * in this context, shift & control keys are also related to mouse
-     * emulation, in that they don't switch into or out of emulation mode
-     */
-    case KEY_LSHIFT:
-    case KEY_RSHIFT:
-    case KEY_CTRL:
-        return TRUE;
-    }
-
-    return FALSE;
-}
-
-/*
  * check if we should switch into or out of mouse emulation mode
  * if so, send the relevant mouse packet
  *
@@ -298,7 +279,7 @@ static BOOL handle_mouse_mode(WORD newkey)
     /*
      * check if we should be in emulation mode or not
      */
-    if ((shifty&MODE_ALT) && is_mouse_key(newkey & ~KEY_RELEASED))
+    if ((shifty&MODE_ALT) && (kb_arrowkeys > 0))
     {
         /* we should be, so ensure that mouse_packet is valid */
         if (!mouse_packet[0])
@@ -718,7 +699,7 @@ void kbd_int(UBYTE scancode)
 {
     WORD ascii = 0;
     UBYTE scancode_only = scancode & ~KEY_RELEASED;  /* get rid of release bits */
-    BOOL modifier;
+    BOOL modifier, arrowkey = FALSE;
 
     KDEBUG(("================\n"));
     KDEBUG(("Key-scancode: 0x%02x, key-shift bits: 0x%02x\n", scancode, shifty));
@@ -751,6 +732,18 @@ void kbd_int(UBYTE scancode)
         return;
     }
 #endif
+
+    /*
+     * check for arrow key, in case we're doing mouse emulation
+     */
+    switch(scancode_only) {
+    case KEY_UPARROW:
+    case KEY_DNARROW:
+    case KEY_LTARROW:
+    case KEY_RTARROW:
+        arrowkey = TRUE;
+        break;
+    }
 
     if (scancode & KEY_RELEASED) {
         switch (scancode_only) {
@@ -794,6 +787,8 @@ void kbd_int(UBYTE scancode)
             if (scancode_only == kb_last_actual)    /* key-up matches last key-down: */
                 kb_ticks = 0;                       /*  stop key repeat */
         }
+        if (arrowkey && (kb_arrowkeys > 0))
+            kb_arrowkeys--;
         handle_mouse_mode(scancode);    /* exit mouse mode if appropriate */
         return;
     }
@@ -801,6 +796,8 @@ void kbd_int(UBYTE scancode)
     /*
      * a key has been pressed
      */
+    if (arrowkey)
+        kb_arrowkeys++;
     modifier = TRUE;
     switch (scancode) {
     case KEY_RSHIFT:
@@ -1070,6 +1067,7 @@ void kbd_init(void)
     kb_initial = KB_INITIAL;
     kb_repeat = KB_REPEAT;
 
+    kb_arrowkeys = 0;  /* no arrow keys pressed initially */
     kb_dead = -1;      /* not in a dead key sequence */
     kb_altnum = -1;    /* not in an alt-numeric sequence */
     kb_switched = 0;   /* not switched initially */