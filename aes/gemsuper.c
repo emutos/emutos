e8d455df80e2d501290273ce8880da75e3fbf356
Roger Burrows <anodynesoftware@users.sourceforge.net>
2020-05-21 21:46:47 -0400

    Fix return code from appl_read()/appl_write()
    This was always set to nonzero, indicating success; it can now
    (theoretically at least) be zero.

--- aes/gemsuper.c
+++ aes/gemsuper.c
@@ -103,7 +103,7 @@ static UWORD crysbind(WORD opcode, AESGLOBAL *pglobal, WORD control[], WORD int_
         break;
     case APPL_READ:
     case APPL_WRITE:
-        ap_rdwr(opcode == APPL_READ ? MU_MESAG : MU_SDMSG,
+        ret = ap_rdwr(opcode == APPL_READ ? MU_MESAG : MU_SDMSG,
                 fpdnm(NULL, AP_RWID), AP_LENGTH, (WORD *)AP_PBUFF);
         break;
     case APPL_FIND:
