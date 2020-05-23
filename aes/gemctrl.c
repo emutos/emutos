d6b316baaa3775bcfd2286377085e63e98042183
Roger Burrows <anodynesoftware@users.sourceforge.net>
2020-05-23 16:29:02 -0400

    Prevent flickering mouse cursor over info line
    If the mouse button was held down over a window's info line, the
    cursor flickered, unlike Atari TOS.

--- aes/gemctrl.c
+++ aes/gemctrl.c
@@ -78,10 +78,8 @@ static const WORD gl_wa[] =
  */
 static void ct_msgup(WORD message, AESPD *owner, WORD wh, WORD m1, WORD m2, WORD m3, WORD m4)
 {
-    if (message == 0)
-        return;
-
-    ap_sendmsg(appl_msg, message, owner, wh, m1, m2, m3, m4);
+    if (message)
+        ap_sendmsg(appl_msg, message, owner, wh, m1, m2, m3, m4);
 
 #if CONF_WITH_PCGEM
     /*
