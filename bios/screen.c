emutos/emutos

commit f9481bcec1cf87f0e3e40218765e7acfb89257ca

Change calculation of VRAM size
Video RAM size is now calculated directly from lineA variables, in
anticipation of direct support for Hatari's 'extended VDI mode'.
The resulting sizes are unchanged from the previous calculations.

bios/screen.c
@@ -465,7 +465,7 @@ static BOOL get_default_palmode(void)
  */
 
 /* Initialize the video mode (address will be done later) */
-static void screen_init_mode(void)
+void screen_init_mode(void)
 {
 #if CONF_WITH_ATARI_VIDEO
 #if CONF_WITH_VIDEL
@@ -593,10 +593,12 @@ static void screen_init_mode(void)
 
 #endif /* CONF_WITH_ATARI_VIDEO */
     MAYBE_UNUSED(get_default_palmode);
+
+    rez_was_hacked = FALSE; /* initial assumption */
 }
 
 /* Initialize the video address (mode is already set) */
-static void screen_init_address(void)
+void screen_init_address(void)
 {
     ULONG vram_size;
     UBYTE *screen_start;
@@ -618,14 +620,6 @@ static void screen_init_address(void)
 #endif
     /* correct physical address */
     setphys(screen_start);
-}
-
-/* Initialize the video mode and screen address */
-void screen_init(void)
-{
-    screen_init_mode();
-    screen_init_address();
-    rez_was_hacked = FALSE; /* initial assumption */
 }
 
 /*
@@ -700,16 +694,6 @@ static const struct video_mode video_mode[] = {
 #endif
 };
 
-#ifndef MACHINE_AMIGA
-/* calculate the VRAM size required by a video mode */
-static ULONG shifter_vram_size(UWORD vmode)
-{
-    ULONG bytes_per_plane_line = video_mode[vmode].hz_rez / 8;
-    ULONG bytes_per_plane = bytes_per_plane_line * video_mode[vmode].vt_rez;
-    return bytes_per_plane * video_mode[vmode].planes;
-}
-#endif
-
 /*
  * calculate initial VRAM size based on video hardware
  *
@@ -722,19 +706,19 @@ ULONG initial_vram_size(void)
 #ifdef MACHINE_AMIGA
     return amiga_initial_vram_size();
 #else
+    ULONG vram_size;
+
     if (HAS_VIDEL)
         return FALCON_VRAM_SIZE + 256UL;
-    else if (HAS_TT_SHIFTER)
-        /* TT TOS allocates 256 bytes more than actually needed. */
-        return shifter_vram_size(TT_HIGH) + 0x100ul;
-    else
-    {
-        /*
-         * ST TOS allocates 768 bytes more than actually needed; this
-         * is equivalent to rounding up to the nearest kilobyte.
-         */
-        return shifter_vram_size(ST_LOW) + 768UL;
-    }
+
+    vram_size = (ULONG)BYTES_LIN * V_REZ_VT;
+
+    /* TT TOS allocates 256 bytes more than actually needed. */
+    if (HAS_TT_SHIFTER)
+        return vram_size + 256UL;
+
+    /* ST TOS allocates 768 bytes more than actually needed. */
+    return vram_size + 768UL;
 #endif
 }
 

bios/bios.c
@@ -268,10 +268,6 @@ static void bios_init(void)
     }
 #endif /* CONF_WITH_68040_PMMU */
 
-    /* Initialize the screen */
-    KDEBUG(("screen_init()\n"));
-    screen_init();      /* detect monitor type, ... */
-
     KDEBUG(("cookie_init()\n"));
     cookie_init();      /* sets a cookie jar */
     KDEBUG(("fill_cookie_jar()\n"));
@@ -291,9 +287,24 @@ static void bios_init(void)
         has_blitter = 0;
 #endif
 
+    /*
+     * Initialize the screen mode
+     * Must be done before calling linea_init().
+     */
+    KDEBUG(("screen_init_mode()\n"));
+    screen_init_mode(); /* detect monitor type, ... */
+
     /* Set up the BIOS console output */
     KDEBUG(("linea_init()\n"));
     linea_init();       /* initialize screen related line-a variables */
+
+    /*
+     * Initialize the screen address
+     * Must be done after calling linea_init().
+     */
+    KDEBUG(("screen_init_address()\n"));
+    screen_init_address();
+
     vt52_init();        /* initialize the vt52 console */
 
     /* Now kcprintf() will also send debug info to the screen */

bios/screen.h
@@ -89,8 +89,9 @@ WORD esetsmear(WORD mode);
 /* misc routines */
 void initialise_palette_registers(WORD rez,WORD mode);
 
-/* determine monitor type, ... */
-void screen_init(void);
+/* determine screen address, monitor type, ... */
+void screen_init_address(void);
+void screen_init_mode(void);
 void set_rez_hacked(void);
 void screen_get_current_mode_info(UWORD *planes, UWORD *hz_rez, UWORD *vt_rez);
 

commit b9ac62ca777d52263db8c7646d03acc43cffb8ef

Fix Amiga display
A regression introduced by commit f9481bc moved the initialization
of the Amiga video mode behind the setup of the line A variables.
Thus, on the Amiga linea_init() did not retrieve the correct video
mode information.

bios/screen.c
@@ -594,6 +594,10 @@ void screen_init_mode(void)
 #endif /* CONF_WITH_ATARI_VIDEO */
     MAYBE_UNUSED(get_default_palmode);
 
+#ifdef MACHINE_AMIGA
+    amiga_screen_init();
+#endif
+
     rez_was_hacked = FALSE; /* initial assumption */
 }
 
@@ -615,9 +619,6 @@ void screen_init_address(void)
     /* set new v_bas_ad */
     v_bas_ad = screen_start;
     KDEBUG(("v_bas_ad = %p, vram_size = %ld\n", v_bas_ad, vram_size));
-#ifdef MACHINE_AMIGA
-    amiga_screen_init();
-#endif
     /* correct physical address */
     setphys(screen_start);
 }