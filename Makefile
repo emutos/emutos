emutos/emutos

commit 51725b0cfb4348aa6501d16bcf5acfc9137d5398

Centralise localisation info
In preparation for splitting the 512K ROMs to provide room for
future enhancements, we introduce a single file (localise.ctl)
that combines localisation-related info from country.mk and
po/LINGUAS.  This is processed by a new program (localise) to
generate required files for the build process.  The actual
split of the ROMs will be implemented in a future commit.

Makefile
@@ -844,6 +844,22 @@ lisaboot.img: obj/lisaboot.o obj/bootram.o
 obj/lisaboot.o: obj/ramtos.h
 
 #
+# localisation support: create bios/ctables.h include/i18nconf.h po/LINGUAS
+#
+# we group targets, since localise generates all three targets at the same time
+#
+
+GEN_SRC += bios/ctables.h include/i18nconf.h po/LINGUAS
+TOCLEAN += localise include/i18nconf.h bios/ctables.h po/LINGUAS
+
+NODEP += localise
+localise: tools/localise.c
+	$(NATIVECC) $< -o $@
+
+bios/ctables.h include/i18nconf.h po/LINGUAS &: obj/country localise.ctl localise
+	./localise -u$(UNIQUE) localise.ctl bios/ctables.h include/i18nconf.h po/LINGUAS
+
+#
 # NLS support
 #
 
@@ -856,8 +872,11 @@ NODEP += bug
 bug: tools/bug.c
 	$(NATIVECC) $< -o $@
 
+# never run 'bug make' if UNIQUE is specified
+ifeq (,$(UNIQUE))
 util/langs.c: $(POFILES) po/LINGUAS bug po/messages.pot
 	./bug make
+endif
 
 po/messages.pot: bug po/POTFILES.in $(shell grep -v '^#' po/POTFILES.in)
 	./bug xgettext
@@ -1040,44 +1059,6 @@ obj/country: always-execute-recipe
 	  echo "rm -f $$i $$j"; \
 	  rm -f $$i $$j; \
 	done
-
-#
-# i18nconf.h - this file is automatically created by the Makefile. This
-# is done this way instead of simply passing the flags as -D on the
-# command line because:
-# - the command line is shorter
-# - it allows #defining CONF_KEYB as KEYB_US with KEYB_US #defined elsewhere
-# - explicit dependencies can force rebuilding files that include it
-#
-
-GEN_SRC += include/i18nconf.h
-
-ifneq (,$(UNIQUE))
-include/i18nconf.h: obj/country
-	@echo '# Generating $@ with CONF_KEYB=KEYB_$(ETOSKEYB) CONF_CHARSET=CHARSET_$(ETOSCSET)'
-	@rm -f $@; touch $@
-	@echo '#define CONF_MULTILANG 0' >> $@
-	@echo '#define CONF_WITH_NLS 0' >> $@
-	@echo '#define CONF_KEYB KEYB_$(ETOSKEYB)' >> $@
-	@echo '#define CONF_CHARSET CHARSET_$(ETOSCSET)' >> $@
-	@echo "#define CONF_IDT ($(ETOSIDT))" >> $@
-else
-include/i18nconf.h: obj/country
-	@echo '# Generating $@ with CONF_MULTILANG=1'
-	@rm -f $@; touch $@
-	@echo '#define CONF_MULTILANG 1' >> $@
-	@echo '#define CONF_WITH_NLS 1' >> $@
-endif
-
-#
-# ctables.h - the country tables, generated from country.mk, and only
-# included in bios/country.c
-#
-
-GEN_SRC += bios/ctables.h
-
-bios/ctables.h: country.mk tools/genctables.awk
-	awk -f tools/genctables.awk < country.mk > $@
 
 #
 # OS header

country.mk
@@ -1,99 +1,11 @@
 #
-# country.mk - the country tables
+# country.mk - country-related items
 #
 # Copyright (C) 2002-2020 The EmuTOS development team
 #
 # This file is distributed under the GPL, version 2 or at your
 # option any later version.  See doc/license.txt for details.
 #
-
-# this file is included in the Makefile to build unique-country ROMs.
-#
-# information in lines below serve also as source to fill the tables
-# needed by bios/country.c (tables generated in bios/ctables.h). For
-# each country there should be a four line paragraph giving the language,
-# keyboard code, charset and IDT.
-#
-
-i18n_us_lang = us
-i18n_us_keyb = US
-i18n_us_cset = ST
-i18n_us_idt  = IDT_12H | IDT_MMDDYY | '/'
-
-i18n_de_lang = de
-i18n_de_keyb = DE
-i18n_de_cset = ST
-i18n_de_idt  = IDT_24H | IDT_DDMMYY | '.'
-
-i18n_fr_lang = fr
-i18n_fr_keyb = FR
-i18n_fr_cset = ST
-i18n_fr_idt  = IDT_24H | IDT_DDMMYY | '/'
-
-i18n_cz_lang = cs
-i18n_cz_keyb = CZ
-i18n_cz_cset = L2
-i18n_cz_idt  = IDT_24H | IDT_DDMMYY | '.'
-
-i18n_gr_lang = gr
-i18n_gr_keyb = GR
-i18n_gr_cset = GR
-i18n_gr_idt  = IDT_24H | IDT_DDMMYY | '/'
-
-i18n_es_lang = es
-i18n_es_keyb = ES
-i18n_es_cset = ST
-i18n_es_idt  = IDT_24H | IDT_DDMMYY | '/'
-
-i18n_fi_lang = fi
-i18n_fi_keyb = SE
-i18n_fi_cset = ST
-i18n_fi_idt  = IDT_24H | IDT_DDMMYY | '.'
-
-i18n_sg_lang = de
-i18n_sg_keyb = SG
-i18n_sg_cset = ST
-i18n_sg_idt  = IDT_24H | IDT_DDMMYY | '.'
-
-i18n_ru_lang = ru
-i18n_ru_keyb = RU
-i18n_ru_cset = RU
-i18n_ru_idt  = IDT_24H | IDT_DDMMYY | '/'
-
-i18n_it_lang = it
-i18n_it_keyb = IT
-i18n_it_cset = ST
-i18n_it_idt  = IDT_24H | IDT_DDMMYY | '/'
-
-i18n_uk_lang = us
-i18n_uk_keyb = UK
-i18n_uk_cset = ST
-i18n_uk_idt  = IDT_12H | IDT_DDMMYY | '/'
-
-i18n_no_lang = us
-i18n_no_keyb = NO
-i18n_no_cset = ST
-i18n_no_idt  = IDT_24H | IDT_DDMMYY | '/'
-
-i18n_se_lang = us
-i18n_se_keyb = SE
-i18n_se_cset = ST
-i18n_se_idt  = IDT_24H | IDT_DDMMYY | '/'
-
-i18n_nl_lang = nl
-i18n_nl_keyb = NL
-i18n_nl_cset = ST
-i18n_nl_idt  = IDT_24H | IDT_DDMMYY | '-'
-
-i18n_pl_lang = pl
-i18n_pl_keyb = PL
-i18n_pl_cset = L2
-i18n_pl_idt  = IDT_24H | IDT_DDMMYY | '/'
-
-i18n_hu_lang = hu
-i18n_hu_keyb = HU
-i18n_hu_cset = L2
-i18n_hu_idt  = IDT_24H | IDT_YYMMDD | '/'
 
 COUNTRIES = us de fr cz gr es fi sg ru it uk no se nl pl hu
 ifeq (,$(findstring $(COUNTRY),$(COUNTRIES)))
@@ -102,25 +14,7 @@ endif
 
 #
 
-ETOSLANG = $(i18n_$(COUNTRY)_lang)
-ifeq (,$(ETOSLANG))
-$(error Variable i18n_$(COUNTRY)_lang incorrectly configured)
-endif
-
-ETOSKEYB = $(i18n_$(COUNTRY)_keyb)
-ifeq (,$(ETOSKEYB))
-$(error Variable i18n_$(COUNTRY)_keyb incorrectly configured)
-endif
-
-ETOSCSET = $(i18n_$(COUNTRY)_cset)
-ifeq (,$(ETOSCSET))
-$(error Variable i18n_$(COUNTRY)_cset incorrectly configured)
-endif
-
-ETOSIDT = $(i18n_$(COUNTRY)_idt)
-ifeq (,$(ETOSIDT))
-$(error Variable i18n_$(COUNTRY)_idt incorrectly configured)
-endif
+ETOSLANG = $(shell awk '$$1 == country { print $$2 }' country=$(COUNTRY) <localise.ctl)
 
 #
 

localise.ctl
@@ -1,0 +1,36 @@
+#
+# localise.ctl - control file for 'localise'
+#
+# Copyright (C) 2021 The EmuTOS development team
+#
+# This file is distributed under the GPL, version 2 or at your
+# option any later version.  See doc/license.txt for details.
+#
+
+#
+# each line corresponds to one country code; the sequence of
+# data is as follows:
+#   <country> <language> <keyboard> <font> <IDT defines> <group>
+#
+# notes:
+# . fields are separated by white space
+# . <group> is used to split multi-language ROMs (* => all groups)
+#
+us us us st IDT_12H|IDT_MMDDYY|'/'  *
+
+de de de st IDT_24H|IDT_DDMMYY|'.'  A
+fr fr fr st IDT_24H|IDT_DDMMYY|'/'  A
+uk us uk st IDT_12H|IDT_DDMMYY|'/'  A
+es es es st IDT_24H|IDT_DDMMYY|'/'  A
+it it it st IDT_24H|IDT_DDMMYY|'/'  A
+se us se st IDT_24H|IDT_DDMMYY|'/'  A
+sg de sg st IDT_24H|IDT_DDMMYY|'.'  A
+fi fi se st IDT_24H|IDT_DDMMYY|'.'  A
+no us no st IDT_24H|IDT_DDMMYY|'/'  A
+nl nl nl st IDT_24H|IDT_DDMMYY|'-'  A
+
+cz cs cz l2 IDT_24H|IDT_DDMMYY|'.'  B
+hu hu hu l2 IDT_24H|IDT_DDMMYY|'/'  B
+pl pl pl l2 IDT_24H|IDT_DDMMYY|'/'  B
+ru ru ru ru IDT_24H|IDT_DDMMYY|'/'  B
+gr gr gr gr IDT_24H|IDT_DDMMYY|'/'  B

po/.gitignore
@@ -1,1 +1,2 @@
 /messages.pot
+/LINGUAS

po/LINGUAS
@@ -1,25 +1,0 @@
-#
-# this file contains the name of languages that will
-# be generated in file langs.c by bug make.
-# each line should be of the form
-#
-#   xy charset
-#
-# where xy is a lowercase two-letter language name,
-# and charset is the name of the destination charset
-#
-
-fr atarist
-de atarist
-fi atarist
-it atarist
-nl atarist
-es atarist
-
-cs latin2
-pl latin2
-hu latin2
-
-gr cp737
-
-ru russian-atarist

tools/genctables.awk
@@ -1,120 +1,0 @@
-#
-# genctables.awk - generate country tables
-#
-# Copyright (C) 2002-2016 The EmuTOS development team
-#
-# Authors:
-#  LVL     Laurent Vogel
-#
-# This file is distributed under the GPL, version 2 or at your
-# option any later version.  See doc/license.txt for details.
-#
-
-function now(   d)
-{
-    "date" | getline d
-    close("date")
-    return d
-}
-
-BEGIN {
-    print "/*"
-    print " * ctables.h - country tables for bios/country.h"
-    print " *"
-    print " * This file was automatically generated by genctables.awk on"
-    print " * " now() " - do not alter!"
-    print " */\n"
-}
-
-/^i18n_[^_]*_lang/ {
-    country = toupper(substr($1,6))
-    sub(/_.*/, "", country)
-    sub(/^[^=]*= */, "")
-    langs[country] = $0
-}
-
-/^i18n_[^_]*_keyb/ {
-    sub(/^[^=]*= */, "")
-    keybs[country] = $0
-}
-
-/^i18n_[^_]*_cset/ {
-    sub(/^[^=]*= */, "")
-    csets[country] = $0
-}
-
-/^i18n_[^_]*_idt/ {
-    sub(/^[^=]*= */, "")
-    idts[country] = $0
-}
-
-/^COUNTRIES/ {
-    sub(/^[^=]*= */, "")
-    $0 = toupper($0)
-    ncountries = split($0, countries)
-}
-
-END {
-    for(i = 1 ; i <= ncountries ; i++) {
-        country = countries[i]
-        needkeybs[keybs[country]] = 1
-        needcsets[csets[country]] = 1
-    }
-
-    print "/* Indexes of keyboard layouts inside keytables[] */"
-    i = 0
-    for(keyb in needkeybs) {
-        print "#define KEYB_" keyb " " i
-        i++
-    }
-    print ""
-
-    print "/* Indexes of font sets inside font_sets[] */"
-    i = 0
-    for(cset in needcsets) {
-        print "#define CHARSET_" cset " " i
-        i++
-    }
-    print ""
-
-    print "#if CONF_MULTILANG"
-    print "static const struct country_record countries[] = {"
-    for(i = 1 ; i <= ncountries ; i++) {
-        country = countries[i]
-        print "    { COUNTRY_" country ", \"" langs[country] "\", " \
-            "KEYB_" keybs[country] ", CHARSET_" csets[country] ", " \
-            idts[country] "},"
-    }
-    print "};"
-    print "#endif\n\n"
-
-    for(keyb in needkeybs) {
-        print "#if CONF_MULTILANG || CONF_KEYB == KEYB_" keyb
-        print "#include \"keyb_" tolower(keyb) ".h\""
-        print "#endif"
-    }
-    print "\nstatic const struct keytbl *const keytables[] = {"
-    for(keyb in needkeybs) {
-        print "#if CONF_MULTILANG || CONF_KEYB == KEYB_" keyb
-        print "    &keytbl_" tolower(keyb) ","
-        print "#endif"
-    }
-    print "};\n\n"
-
-    for(cset in needcsets) {
-        lcset = tolower(cset)
-        print "extern const Fonthead fnt_" lcset "_6x6;"
-        print "extern const Fonthead fnt_" lcset "_8x8;"
-        print "extern const Fonthead fnt_" lcset "_8x16;"
-    }
-    print "\nstatic const struct charset_fonts font_sets[] = {"
-    for(cset in needcsets) {
-        lcset = tolower(cset)
-        print "#if CONF_MULTILANG || CONF_CHARSET == CHARSET_" cset
-        print "    { &fnt_" lcset "_6x6, &fnt_" lcset \
-            "_8x8, &fnt_" lcset "_8x16 },"
-        print "#endif"
-    }
-    print "};"
-
-}

commit 3cfe6e57c0103fb3c65dd3d77c73e7559ed97737

Fix build error for UNIQUE builds
If you start from a fresh download and build a country-specific
version of EmuTOS, it will fail because obj/header.h does not exist.
This is ultimately because makefile.dep depends on $(GEN_SRC), which
includes util/langs.c which make does not know how to build.  This
causes make to ignore other dependencies in $(GEN_SRC), including
obj/header.h.  This problem was introduced by commit 51725b0.
Many thanks to Christian Zietz for debugging this.

Makefile
@@ -872,11 +872,9 @@ NODEP += bug
 bug: tools/bug.c
 	$(NATIVECC) $< -o $@
 
-# never run 'bug make' if UNIQUE is specified
-ifeq (,$(UNIQUE))
+# even if UNIQUE is specified, we ensure langs.c exists to avoid dependency problems
 util/langs.c: $(POFILES) po/LINGUAS bug po/messages.pot
 	./bug make
-endif
 
 po/messages.pot: bug po/POTFILES.in $(shell grep -v '^#' po/POTFILES.in)
 	./bug xgettext