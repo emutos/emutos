emutos/emutos

commit 4b95fd005528db89ed33ecb8a4db5f243fe7cfe7

Create temporary library for fonts
This simplifies the inclusion of the correct fonts for different
flavours of EmuTOS, in preparation for future changes.

country.mk
@@ -130,9 +130,9 @@ FONTOBJ_GR = fnt_gr_6x6.o fnt_gr_8x8.o fnt_gr_8x16.o
 FONTOBJ_RU = fnt_ru_6x6.o fnt_ru_8x8.o fnt_ru_8x16.o
 FONTOBJ_ALL = $(FONTOBJ_ST) $(FONTOBJ_L2) $(FONTOBJ_GR) $(FONTOBJ_RU)
 FONTOBJ_COMMON = obj/fnt_off_6x6.o obj/fnt_off_8x8.o
+FONTOBJ = $(FONTOBJ_ALL:%=obj/%)
 
-ifneq (,$(UNIQUE))
-FONTOBJ = $(FONTOBJ_$(ETOSCSET):%=obj/%) $(FONTOBJ_COMMON)
-else
-FONTOBJ = $(FONTOBJ_ALL:%=obj/%) $(FONTOBJ_COMMON)
-endif
+TOCLEAN = obj/*.a
+
+obj/libfont.a: $(FONTOBJ)
+	$(AR) $(ARFLAGS) obj/libfont.a $(FONTOBJ)

Makefile
@@ -129,9 +129,13 @@ endif
 # indent flags
 INDENT = indent -kr
 
+# Archiver
+AR = $(TOOLCHAIN_PREFIX)ar
+ARFLAGS = rc
+
 # Linker with relocation information and binary output (image)
 LD = $(CC) $(MULTILIBFLAGS) -nostartfiles -nostdlib
-LIBS = -lgcc
+LIBS = -L obj -lgcc -lfont
 LDFLAGS = -Wl,-T,obj/emutospp.ld
 PCREL_LDFLAGS = -Wl,--oformat=binary,-Ttext=0,--entry=0
 
@@ -345,7 +349,7 @@ include country.mk
 
 SRC = $(foreach d,$(dirs),$(addprefix $(d)/,$($(d)_src))) $(end_src)
 
-CORE_OBJ = $(foreach d,$(core_dirs),$(patsubst %.c,obj/%.o,$(patsubst %.S,obj/%.o,$($(d)_src)))) $(FONTOBJ) obj/version.o
+CORE_OBJ = $(foreach d,$(core_dirs),$(patsubst %.c,obj/%.o,$(patsubst %.S,obj/%.o,$($(d)_src)))) $(FONTOBJ_COMMON) obj/version.o
 OPTIONAL_OBJ = $(foreach d,$(optional_dirs),$(patsubst %.c,obj/%.o,$(patsubst %.S,obj/%.o,$($(d)_src))))
 END_OBJ = $(patsubst %,obj/%.o,$(basename $(notdir $(end_src))))
 OBJECTS = $(CORE_OBJ) $(OPTIONAL_OBJ) $(END_OBJ)
@@ -421,7 +425,7 @@ obj/emutospp.ld: emutos.ld include/config.h tosvars.ld
 
 TOCLEAN += *.img *.map
 
-emutos.img: $(OBJECTS) obj/emutospp.ld Makefile
+emutos.img: $(OBJECTS) obj/emutospp.ld obj/libfont.a Makefile
 	$(LD) $(CORE_OBJ) $(LIBS) $(OPTIONAL_OBJ) $(LIBS) $(END_OBJ) $(LDFLAGS) -Wl,-Map=emutos.map -o emutos.img
 	@if [ $$(($$(awk '/^\.data /{print $$3}' emutos.map))) -gt 0 ]; then \
 	  echo "### Warning: The DATA segment is not empty."; \

commit 5b3e3cc17e877d22bea66c0ec5fde80417f6ce2a

Fix "make clean" regression
Some generated files were not cleaned as expected.
This regression was introduced in commit 4b95fd00.

country.mk
@@ -26,7 +26,7 @@ FONTOBJ_ALL = $(FONTOBJ_ST) $(FONTOBJ_L2) $(FONTOBJ_GR) $(FONTOBJ_RU)
 FONTOBJ_COMMON = obj/fnt_off_6x6.o obj/fnt_off_8x8.o
 FONTOBJ = $(FONTOBJ_ALL:%=obj/%)
 
-TOCLEAN = obj/*.a
+TOCLEAN += obj/*.a
 
 obj/libfont.a: $(FONTOBJ)
 	$(AR) $(ARFLAGS) obj/libfont.a $(FONTOBJ)