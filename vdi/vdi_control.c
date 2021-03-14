0fe68198f3f1e4ff965caaab5679cee8108a0b06
Roger Burrows <anodynesoftware@users.sourceforge.net>
2021-03-14 17:12:02 -0400

    Chain virtual workstations together
    This is what Atari TOS does, and some programs (e.g. Warp9) may
    rely on this.

--- vdi/vdi_control.c
+++ vdi/vdi_control.c
@@ -358,6 +358,29 @@ static void init_wk(Vwk * vwk)
 
 
 
+/*
+ * build a chain of Vwks
+ *
+ * this links all of the currently allocated Vwks together, as in
+ * Atari TOS.  some programs may depend on this.
+ */
+static void build_vwk_chain(void)
+{
+    Vwk *prev, **vwk;
+    WORD handle;
+
+    prev = &phys_work;
+    for (handle = VDI_PHYS_HANDLE+1, vwk = vwk_ptr+handle; handle <= LAST_VDI_HANDLE; handle++, vwk++) {
+        if (*vwk) {
+            prev->next_work = *vwk;
+            prev = *vwk;
+        }
+    }
+    prev->next_work = NULL;
+}
+
+
+
 void vdi_v_opnvwk(Vwk * vwk)
 {
     WORD handle;
@@ -392,6 +415,7 @@ void vdi_v_opnvwk(Vwk * vwk)
     vwk_ptr[handle] = vwk;
     vwk->handle = CONTRL[6] = handle;
     init_wk(vwk);
+    build_vwk_chain();
     CUR_WORK = vwk;
 }
 
@@ -407,6 +431,8 @@ void vdi_v_clsvwk(Vwk * vwk)
         return;
 
     vwk_ptr[handle] = NULL;         /* close it */
+
+    build_vwk_chain();              /* rebuild chain */
 
     /*
      * When we close a virtual workstation, Atari TOS and previous versions
