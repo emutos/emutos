emutos/emutos

commit 6552b0e8ecbe68188a25ff5829b794b2c55be111

Implement MFP interrupt-driven transmit

bios/serport.c
@@ -155,15 +155,26 @@ LONG bconin1(void)
     return value;
 }
 
+static WORD incr_tail(IOREC *iorec)
+{
+    WORD tail;
+
+    tail = iorec->tail + 1;
+    if (tail >= iorec->size)
+        tail = 0;
+
+    return tail;
+}
+
 LONG bcostat1(void)
 {
 #if CONF_WITH_COLDFIRE_RS232
     return coldfire_rs232_can_write() ? -1 : 0;
 #elif CONF_WITH_MFP_RS232
-    if (MFP_BASE->tsr & 0x80)
-        return -1;
-    else
-        return 0;
+    IOREC *out = &iorec1.out;
+
+    /* set the status according to buffer availability */
+    return (out->head == incr_tail(out)) ? 0L : -1L;
 #else
     return -1;
 #endif
@@ -179,8 +190,28 @@ LONG bconout1(WORD dev, WORD b)
     coldfire_rs232_write_byte(b);
     return 1;
 #elif CONF_WITH_MFP_RS232
-    /* Output to RS232 interface */
-    MFP_BASE->udr = (char)b;
+    WORD old_sr, tail;
+    IOREC *out = &iorec1.out;
+
+    /* disable interrupts */
+    old_sr = set_sr(0x2700);
+
+    /*
+     * if the buffer is empty & the port is empty, output directly.
+     * otherwise queue the data.
+     */
+    if ((out->head == out->tail) && (MFP_BASE->tsr & 0x80)) {
+        MFP_BASE->udr = (UBYTE)b;
+    } else {
+        *(out->buf + out->tail) = (UBYTE)b;
+        tail = incr_tail(out);
+        if (tail != out->head) {        /* buffer not full,  */
+            out->tail = tail;           /*  so ok to advance */
+        }
+    }
+
+    /* restore interrupts */
+    set_sr(old_sr);
     return 1L;
 #else
     /* The above loop will never return */
@@ -233,8 +264,8 @@ static const struct mfp_rs232_table mfp_rs232_init[] = {
 };
 
 /*
- * the following routine is called by the assembler interrupt handler.
- * it runs at interrupt level 6 & is therefore never interrupted.
+ * the following routines are called by assembler interrupt handlers.
+ * they run at interrupt level 6 and are therefore never interrupted.
  */
 void mfp_rs232_rx_interrupt_handler(void)
 {
@@ -251,6 +282,23 @@ void mfp_rs232_rx_interrupt_handler(void)
 
     /* clear the interrupt service bit */
     MFP_BASE->isra = 0xef;
+}
+
+void mfp_rs232_tx_interrupt_handler(void)
+{
+    IOREC *out = &iorec1.out;
+
+    /*
+     * if there's any queued output data, send it
+     */
+    if (out->head != out->tail) {
+        MFP_BASE->udr = *(out->buf + out->head);
+        if (++out->head >= out->size)
+            out->head = 0;
+    }
+
+    /* clear the interrupt service bit (bit 2) */
+    MFP_BASE->isra = 0xfb;
 }
 
 #endif  /* CONF_WITH_MFP_RS232 */
@@ -780,8 +828,9 @@ void init_serport(void)
 #endif
 
 #if CONF_WITH_MFP_RS232
-    /* Set up a handler for MFP receive buffer full interrupt */
+    /* Set up handlers for MFP USART buffer interrupts */
     mfpint(MFP_RBF, (LONG) mfp_rs232_rx_interrupt);
+    mfpint(MFP_TBE,(LONG)mfp_rs232_tx_interrupt);
 #endif
 
 #ifdef __mcoldfire__

bios/serport.h
@@ -1,7 +1,7 @@
 /*
  * serport.h - header for serport.c
  *
- * Copyright (C) 2013 The EmuTOS development team
+ * Copyright (C) 2013-2021 The EmuTOS development team
  *
  * Authors:
  *  RFB    Roger Burrows
@@ -88,6 +88,7 @@ LONG bconoutB(WORD,WORD);
 
 #if CONF_WITH_MFP_RS232
 void mfp_rs232_rx_interrupt_handler(void);
+void mfp_rs232_tx_interrupt_handler(void);
 #endif
 
 #if BCONMAP_AVAILABLE

bios/vectors.S
@@ -1,7 +1,7 @@
 /*
  * vectors.S - exception vectors
  *
- * Copyright (C) 2001-2020 The EmuTOS development team
+ * Copyright (C) 2001-2021 The EmuTOS development team
  *
  * This file is distributed under the GPL, version 2 or at your
  * option any later version.  See doc/license.txt for details.
@@ -568,7 +568,12 @@ timerc_end:
         rte
 
 #if CONF_WITH_MFP_RS232
+
+// ==== MFP USART interrupt handlers ============================================
+
         .globl _mfp_rs232_rx_interrupt
+        .globl _mfp_rs232_tx_interrupt
+
 _mfp_rs232_rx_interrupt:
 #ifdef __mcoldfire__
         lea     -16(sp),sp
@@ -585,7 +590,27 @@ _mfp_rs232_rx_interrupt:
         movem.l (sp)+,d0-d1/a0-a1
 #endif
         rte
+
+_mfp_rs232_tx_interrupt:
+#ifdef __mcoldfire__
+        lea     -16(sp),sp
+        movem.l d0-d1/a0-a1,(sp)
+#else
+        movem.l d0-d1/a0-a1,-(sp)
 #endif
+
+        jbsr    _mfp_rs232_tx_interrupt_handler
+
+#ifdef __mcoldfire__
+        movem.l (sp),d0-d1/a0-a1
+        lea     16(sp),sp
+#else
+        movem.l (sp)+,d0-d1/a0-a1
+#endif
+        rte
+
+#endif
+
 
 // ==== Critical error handler functions =====================================
 

bios/vectors.h
@@ -1,7 +1,7 @@
 /*
  * vectors.h - exception vectors, interrupt routines and system hooks
  *
- * Copyright (C) 2001-2019 The EmuTOS development team
+ * Copyright (C) 2001-2021 The EmuTOS development team
  *
  * Authors:
  *  LVL     Laurent Vogel
@@ -98,8 +98,10 @@ extern WORD trap_save_area[];
 extern void (*vector_5ms)(void);              /* 200 Hz system timer */
 #endif
 
+/* interrupt handlers in vectors.S */
 #if CONF_WITH_MFP_RS232
 void mfp_rs232_rx_interrupt(void);
+void mfp_rs232_tx_interrupt(void);
 #endif
 
 /* protect d2/a2 when calling external user-supplied code */

commit 04282999b1c6354f0d6beca030b5d7ab8d74c7a7

Fix panic() display bug with RS232_DEBUG_PRINT
When panic() is called, it disables all interrupts.  This prevents
interrupt-driven serial output from working properly.  This bug was
introduced by commit 6552b0e.  Thanks to Rob Gowin for finding this.

bios/serport.c
@@ -191,7 +191,9 @@ static ULONG rsconf_mfp(MFP *mfp, EXT_IOREC *iorec, WORD baud, WORD ctrl, WORD u
 
     return old;
 }
+#endif
 
+#if (CONF_WITH_MFP_RS232 && !RS232_DEBUG_PRINT) || CONF_WITH_TT_MFP
 static void put_iorecbuf(MFP *mfp, IOREC *out, WORD b)
 {
     WORD old_sr, tail;
@@ -263,15 +265,25 @@ LONG bconin1(void)
     return get_iorecbuf(&iorec1.in);
 }
 
+/*
+ * For serial output via the MFP, bcostat1()/bconout1() normally use
+ * interrupts.  However, when debug output is via the serial port this
+ * can cause complications (e.g. when panic() disables interrupts).
+ * Therefore we avoid using interrupts in that situation.
+ */
 LONG bcostat1(void)
 {
 #if CONF_WITH_COLDFIRE_RS232
     return coldfire_rs232_can_write() ? -1 : 0;
 #elif CONF_WITH_MFP_RS232
+# if RS232_DEBUG_PRINT
+    return (MFP_BASE->tsr & 0x80) ? -1 : 0;
+# else
     IOREC *out = &iorec1.out;
 
     /* set the status according to buffer availability */
     return (out->head == incr_tail(out)) ? 0L : -1L;
+# endif
 #else
     return -1;
 #endif
@@ -287,8 +299,13 @@ LONG bconout1(WORD dev, WORD b)
     coldfire_rs232_write_byte(b);
     return 1;
 #elif CONF_WITH_MFP_RS232
+# if RS232_DEBUG_PRINT
+    MFP_BASE->udr = (char)b;
+    return 1L;
+# else
     put_iorecbuf(MFP_BASE, &iorec1.out, b);
     return 1L;
+# endif
 #else
     /* The above loop will never return */
     return 0L;
@@ -884,7 +901,9 @@ void init_serport(void)
 #if CONF_WITH_MFP_RS232
     /* Set up handlers for MFP USART buffer interrupts */
     mfpint(MFP_RBF, (LONG) mfp_rs232_rx_interrupt);
+# if !RS232_DEBUG_PRINT
     mfpint(MFP_TBE,(LONG)mfp_rs232_tx_interrupt);
+# endif
 #endif
 
 #ifdef __mcoldfire__