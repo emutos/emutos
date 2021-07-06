emutos/emutos

commit 8e40b049d8eb0f77012630f8898488ba675a45d4

Simplify MFP initialisation
This saves over 100 bytes in the 192K ROMs.

bios/mfp.c
@@ -13,6 +13,7 @@
  */
 
 #include "emutos.h"
+#include "string.h"
 #include "mfp.h"
 #include "tosvars.h"
 #include "vectors.h"
@@ -23,31 +24,7 @@
 
 static void reset_mfp_regs(MFP *mfp)
 {
-    mfp->gpip = 0x00;
-    mfp->aer = 0x00;
-    mfp->ddr = 0x00;
-
-    mfp->iera = 0x00;
-    mfp->ierb = 0x00;
-    mfp->ipra = 0x00;
-    mfp->iprb = 0x00;
-    mfp->isra = 0x00;
-    mfp->isrb = 0x00;
-    mfp->imra = 0x00;
-    mfp->imrb = 0x00;
-    mfp->vr = 0x00;
-
-    mfp->tacr = 0x00;
-    mfp->tbcr = 0x00;
-    mfp->tcdcr = 0x00;
-
-    mfp->tadr = 0x00;
-    mfp->tbdr = 0x00;
-    mfp->tcdr = 0x00;
-    mfp->tddr = 0x00;
-
-    mfp->rsr = 0x00;
-    mfp->tsr = 0x00;
+    bzero(mfp, sizeof(MFP));
 }
 
 static void disable_mfp_interrupt(MFP *mfp, WORD num)

commit 705a34d333371201ff71fb215729fc516412903b

Fix regression in MFP initialisation
The MFP was initialised by writing zeros to all addresses, including
the UDR.  This caused a zero byte to be written to the serial port at
boot time.  This bug was introduced by commit 8e40b04.  Thanks to
Eero Tamminen for the report & to Christian Zietz for debugging.

bios/mfp.c
@@ -24,7 +24,9 @@
 
 static void reset_mfp_regs(MFP *mfp)
 {
-    bzero(mfp, sizeof(MFP));
+    /* see mfp.h for why we cannot just bzero() the entire MFP */
+    bzero(mfp, MFP_ZERO_LEN);
+    mfp->tsr = 0;
 }
 
 static void disable_mfp_interrupt(MFP *mfp, WORD num)

bios/mfp.h
@@ -70,6 +70,19 @@ typedef struct
         volatile UBYTE  udr;    /* USART data register               */
 } MFP;
 
+/*
+ * The cheapest way to initialise the MFP is to write zeros to it
+ * with bzero().  However, there are two complications:
+ * 1. we must avoid writing anything to the UDR, since it would be
+ *    sent as soon as the baud rate clock (Timer D) is enabled.
+ * 2. the length to zero must be a multiple of 4, because our bzero()
+ *    implementation writes any 'extra' bytes a byte at a time, and
+ *    byte accesses to the even bytes in the MFP cause a bus error.
+ * Since the MFP's length is a multiple of 4, with the UDR at the
+ * very end, we set the length to be zeroed as 4 bytes less than
+ * the MFP length.  This obliges us to zero the TSR separately.
+ */
+#define MFP_ZERO_LEN    (sizeof(MFP)-4)
 #endif
 
 