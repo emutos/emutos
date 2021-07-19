emutos/emutos

commit f891fd7bf3c02fe0584e78f61fbc0d1af9a345db

corrected some AES declarations of asm functions

aes/gemfslib.c
@@ -1,20 +1,14 @@
-/*      GEMFSLIB.C      5/14/84 - 07/16/85      Lee Lorenzen            */
-/*      merge High C vers. w. 2.2               8/21/87         mdf     */ 
-
 /*
-*       Copyright 1999, Caldera Thin Clients, Inc.
-*                 2002 The EmuTOS development team
-*
-*       This software is licenced under the GNU Public License.
-*       Please see LICENSE.TXT for further information.
-*
-*                  Historical Copyright
-*       -------------------------------------------------------------
-*       GEM Application Environment Services              Version 2.3
-*       Serial No.  XXXX-0000-654321              All Rights Reserved
-*       Copyright (C) 1987                      Digital Research Inc.
-*       -------------------------------------------------------------
-*/
+ * gemfslib.c - 
+ *
+ * Copyright 1999, Caldera Thin Clients, Inc.
+ *           2002 by EmuTOS development team.
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
+ */
+
+
 
 #include "portab.h"
 #include "machine.h"
@@ -40,7 +34,7 @@
 #include "rectfunc.h"
 
 #include "string.h"
-
+#include "intmath.h"
 
 #define NM_NAMES (F9NAME-F1NAME+1)
 #define NAME_OFFSET F1NAME

aes/gemgrlib.c
@@ -35,6 +35,7 @@
 #include "rectfunc.h"
 #include "optimize.h"
 
+#include "intmath.h"
 
 /*
 *       Routine to watch the mouse while the button is down and

aes/gemwmlib.c
@@ -44,8 +44,10 @@
 #include "optimopt.h"
 #include "optimize.h"
 #include "gemwmlib.h"
+#include "kprint.h"
 
 #include "string.h"
+#include "intmath.h"
 
 #define DESKWH  0x0
 
@@ -806,62 +808,58 @@ WORD w_move(WORD w_handle, WORD *pstop, GRECT *prc)
 
 
 /*
-*       Draw windows from top to bottom.  If top is 0, then start at
-*       the topmost window.  If bottom is 0, then start at the 
-*       bottomost windwo.  For the first window drawn, just do the
-*       insides, since DRAW_CHANGE has already drawn the outside
-*       borders.
-*/
+ * Draw windows from top to bottom.  If top is 0, then start at the topmost
+ * window.  If bottom is 0, then start at the bottomost window.  For the
+ * first window drawn, just do the insides, since DRAW_CHANGE has already
+ * drawn the outside borders.
+ */
 
 void w_update(WORD bottom, GRECT *pt, WORD top, WORD moved, WORD usetrue)
 {
-        register WORD   i, ni;
-        register WORD   done;
-                                                /* limit to screen      */
-        rc_intersect(&gl_rfull, pt);
-        gsx_moff();
-                                                /* update windows from  */
-                                                /*   top to bottom      */
-        if (bottom == DESKWH)
-          bottom = W_TREE[ROOT].ob_head;
-                                                /* if there are windows */
-        if (bottom != NIL)
-        {
-                                                /* start at the top     */ 
-          if (top == DESKWH) 
+    WORD   i, ni;
+    WORD   done;
+
+    /* limit to screen */
+    rc_intersect(&gl_rfull, pt);
+    gsx_moff();
+    /* update windows from top to bottom */
+    if (bottom == DESKWH)
+        bottom = W_TREE[ROOT].ob_head;
+    /* if there are windows */
+    if (bottom != NIL) {
+        /* start at the top     */
+        if (top == DESKWH)
             top = W_TREE[ROOT].ob_tail;
-                                                /* draw windows from    */
-                                                /*   top to bottom      */
-          do
-          {
-                                                
-            if ( !((moved) && (top == gl_wtop)) )
-            {
-                                                /* set clip and draw    */
-                                                /*   a window's border  */
-              gsx_sclip(pt);
-                                                /* CHANGED 1/10/86 LKW  */
-/*            w_clipdraw(top, 0, MAX_DEPTH, 2); !* from FALSE to 2      *!
-*/
-              w_cpwalk(top, 0, MAX_DEPTH, usetrue);
-                                                /* let appl. draw inside*/
-              w_redraw(top, pt);
+        /* draw windows from    */
+        /*   top to bottom      */
+        do {
+            if ( !((moved) && (top == gl_wtop)) ) {
+                /* set clip and draw a window's border  */
+                gsx_sclip(pt);
+                /* CHANGED 1/10/86 LKW  */
+                /* w_clipdraw(top, 0, MAX_DEPTH, 2); !* from FALSE to 2      *! */
+                //w_clipdraw(top, 0, MAX_DEPTH, usetrue);
+                kprintf("=== WIND_OPEN1 \n");
+                w_cpwalk(top, 0, MAX_DEPTH, usetrue);	/* let appl. draw inside*/
+                w_redraw(top, pt);
+                kprintf("=== WIND_OPEN2 \n");
             }
-                                                /* scan to find prev    */
+            /* scan to find prev    */
             i = bottom;
             done = (i == top);
+            //kprintf("=== WIND_OPEN i = %x\n", i);
             while (i != top)
             {
-              ni = W_TREE[i].ob_next;
-              if (ni == top)
-                top = i;
-              else
-                i = ni;
+                ni = W_TREE[i].ob_next;
+                if (ni == top)
+                    top = i;
+                else
+                    i = ni;
             }
-          }
-          while( !done );
         }
-        gsx_mon();
+        while( !done );
+    }
+    gsx_mon();
 }
 
 

Makefile
@@ -128,7 +128,7 @@ bdos_ssrc = rwa.S
 # source code in util/
 #
 
-util_csrc = doprintf.c nls.c langs.c string.c
+util_csrc = doprintf.c nls.c langs.c string.c intmath.c
 util_ssrc = memset.S memmove.S nlsasm.S setjmp.S miscasm.S stringasm.S
 
 #

aes/gemasm.S
@@ -1,9 +1,14 @@
-/* GEMASM.S */
 /*
-;       Copyright 1999, Caldera Thin Clients, Inc.                      
-;       This software is licenced under the GNU Public License.         
-;       Please see LICENSE.TXT for further information.                 
-*/                                                                       
+ * gemasm.S - assembler part of GEM
+ *
+ * Copyright 2002, The EmuTOS development team
+ *           1999, Caldera Thin Clients, Inc.
+ *           1987, Digital Research Inc.
+ *
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
+ */
+
 
 
 .globl _psetup

aes/gemdosif.S
@@ -1,17 +1,12 @@
-/* gemdosif.S
+/*
+ * gemdosif.S - Assembler part of the GEMDOS interface
  *
- * Copyright 1999, Caldera Thin Clients, Inc.
- *           2002 The EmuTOS development team
+ * Copyright 2002, The EmuTOS development team
+ *           1999, Caldera Thin Clients, Inc.
+ *           1987, Digital Research Inc.
  *
- * This software is licenced under the GNU Public License.
- * Please see LICENSE.TXT for further information.
- *
- * Historical Copyright
- * -------------------------------------------------------------
- * GEM Application Environment Services              Version 2.3
- * Serial No.  XXXX-0000-654321              All Rights Reserved
- * Copyright (C) 1987                      Digital Research Inc.
- * -------------------------------------------------------------
+ * This file is distributed under the GPL, version 2 or at your
+ * option any later version.  See doc/license.txt for details.
  */
 
 
@@ -44,22 +39,22 @@
 
 /* disable interrupts */
 _cli:
-            move    sr,savesr
-            ori     #0x0700,sr
-            rts
+	move    sr,savesr
+        ori     #0x0700,sr
+        rts
 
 /* restore interrupt mask as it was before cli() */
 _sti:
-            move    savesr,sr
-            rts
+        move    savesr,sr
+        rts
 
 _hcli:
-            ori     #0x0700,sr
-            rts
+        ori     #0x0700,sr
+        rts
 
 _hsti:
-            andi    #0xF8FF,sr
-            rts
+        andi    #0xF8FF,sr
+        rts
 
 
 /*
@@ -67,57 +62,61 @@ _hsti:
 */
 _retake:
 L1F0C:
-            move.l  #_aestrap,0x88
-            move.l  #_err_tra,d0
+        move.l  #_aestrap,0x88
+        move.l  #_err_tra,d0
 L1F1C:
-            move.l  d0,-(sp)
-            move.w  #0x0101,-(sp)
-            move.w  #0x05,-(sp)
-            trap    #13
-            addq.w  #8,sp
-            rts
+        move.l  d0,-(sp)
+        move.w  #0x0101,-(sp)
+        move.w  #0x05,-(sp)
+        trap    #13
+        addq.w  #8,sp
+        rts
+        
 _giveerr:
 L1F2C:
-            move.l  _crit_er,d0
-            bra     L1F1C
+        move.l  _crit_er,d0
+        bra     L1F1C
+        
 _takeerr:
 L1F34:
-            moveq   #-1,d0
-            bsr     L1F1C
-            move.l  d0,_crit_er
-            move.l  #_err_tra,d0
-            bra     L1F1C
+        moveq   #-1,d0
+        bsr     L1F1C
+        move.l  d0,_crit_er
+        move.l  #_err_tra,d0
+        bra     L1F1C
+        
 _err_tra:
 L1F46:
-            move.w  0x04(sp),d0
-            move.w  0x06(sp),d1
-            movem.l d3-d7/a3-a6,-(sp)
-            movea.l #LE3EE,a1
-            move.w  d0,-(sp)
-            bne     L1F62
-            movea.w #0x00,a0
-            bra     L1F70
+        move.w  0x04(sp),d0
+        move.w  0x06(sp),d1
+        movem.l d3-d7/a3-a6,-(sp)
+        movea.l #LE3EE,a1
+        move.w  d0,-(sp)
+        bne     L1F62
+        movea.w #0x00,a0
+        bra     L1F70
+        
 L1F62:
-            not.w   d0
-            movea.w d0,a0
-            cmp.w   #0x11,d0
-            ble     L1F70
-            movea.w #0x00,a0
+        not.w   d0
+        movea.w d0,a0
+        cmp.w   #0x11,d0
+        ble     L1F70
+        movea.w #0x00,a0
 L1F70:
-            move.b  0x00(a0,a1.l),d0
-            move.w  d1,-(sp)
-            move.w  d0,-(sp)
-            jsr     _eralert
-            addq.l  #4,sp
-            move.w  (sp)+,d1
-            ext.l   d1
-            cmp.w   #0x00,d0
-            beq     L1F90
-            move.l  #0x010000,d1
+        move.b  0x00(a0,a1.l),d0
+        move.w  d1,-(sp)
+        move.w  d0,-(sp)
+        jsr     _eralert
+        addq.l  #4,sp
+        move.w  (sp)+,d1
+        ext.l   d1
+        cmp.w   #0x00,d0
+        beq     L1F90
+        move.l  #0x010000,d1
 L1F90:
-            move.l  d1,d0
-            movem.l (sp)+,d3-d7/a3-a6
-            rts
+        move.l  d1,d0
+        movem.l (sp)+,d3-d7/a3-a6
+        rts
 
 
 

aes/gemdosif.h
@@ -5,8 +5,8 @@
 
 extern LONG     drwaddr;
 
-extern LONG     tikaddr;
-extern LONG     tiksav;
+extern void *   tikaddr;
+extern void *   tiksav;
 
 extern LONG     NUM_TICK;                       /* number of ticks      */
                                                 /*   since last sample  */

commit a04ebb0262c29db58447701d5e4da85ba37304e2

Fix 'slider creep' in AES windows
If you single click on the vertical or horizontal slider in a window,
it will often move upwards/leftwards even if you haven't moved the
mouse.  This is because the translation from slider position to pixel
position and back again does not yield the original result, because
the mul_div() function does not round.  This bug was introduced by
commit f891fd7 (Nov/2002).  We now call mul_div_round() instead.

aes/gemfslib.c
@@ -31,6 +31,7 @@
 #include "gemobed.h"
 #include "string.h"
 #include "intmath.h"
+#include "asm.h"
 
 #define NM_NAMES (F9NAME-F1NAME+1)
 #define NAME_OFFSET F1NAME
@@ -295,9 +296,9 @@ static void fs_format(OBJECT *tree, WORD currtop, WORD count)
     th = h = obj->ob_height;
     if (count > NM_NAMES)
     {
-        h = mul_div(NM_NAMES, h, count);
+        h = mul_div_round(NM_NAMES, h, count);
         h = max(gl_hbox, h);            /* min size elevator */
-        y = mul_div(currtop, th-h, count-NM_NAMES);
+        y = mul_div_round(currtop, th-h, count-NM_NAMES);
     }
     obj = treeptr + FSVELEV;
     obj->ob_y = y;
@@ -691,7 +692,7 @@ WORD fs_input(char *pipath, char *pisel, WORD *pbutton, char *pilabel)
             fm_own(TRUE);
             value = gr_slidebox(tree, FSVSLID, FSVELEV, TRUE);
             fm_own(FALSE);
-            value = curr - mul_div(value, count-NM_NAMES, 1000);
+            value = curr - mul_div_round(value, count-NM_NAMES, 1000);
             if (value >= 0)
                 touchob = FUPAROW;
             else

aes/gemgrlib.c
@@ -36,6 +36,7 @@
 #include "rectfunc.h"
 
 #include "intmath.h"
+#include "asm.h"
 
 /*
  *  Routine to watch the mouse while the button is down and it stays
@@ -371,7 +372,7 @@ WORD gr_slidebox(OBJECT *tree, WORD parent, WORD obj, WORD isvert)
     }
 
     if (divis)
-        return mul_div(divnd, 1000, divis);
+        return mul_div_round(divnd, 1000, divis);
     else
         return 0;
 }

aes/gemwmlib.c
@@ -41,6 +41,7 @@
 
 #include "string.h"
 #include "intmath.h"
+#include "asm.h"
 
 /*
  *  defines
@@ -404,8 +405,8 @@ static void w_bldvbar(UWORD kind, BOOL istop, WINDOW *pw, WORD x, WORD y, WORD w
 
             if (pw->w_vslsiz == -1)
                 size = gl_hbox;
-            else size = max(gl_hbox, mul_div(h, pw->w_vslsiz, 1000));
-            posn = mul_div(h-size, pw->w_vslide, 1000);
+            else size = max(gl_hbox, mul_div_round(h, pw->w_vslsiz, 1000));
+            posn = mul_div_round(h-size, pw->w_vslide, 1000);
             w_adjust(W_VSLIDE, W_VELEV, 0, posn, gl_wbox, size);
         }
     }
@@ -445,8 +446,8 @@ static void w_bldhbar(UWORD kind, BOOL istop, WINDOW *pw, WORD x, WORD y, WORD w
 
             if (pw->w_hslsiz == -1)
                 size = gl_wbox;
-            else size = max(gl_wbox, mul_div(w, pw->w_hslsiz, 1000));
-            posn = mul_div(w-size, pw->w_hslide, 1000);
+            else size = max(gl_wbox, mul_div_round(w, pw->w_hslsiz, 1000));
+            posn = mul_div_round(w-size, pw->w_hslide, 1000);
             w_adjust(W_HSLIDE, W_HELEV, posn, 0, size, gl_hbox);
         }
     }