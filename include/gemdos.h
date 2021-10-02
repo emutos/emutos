0d6f6ff53737a44d46903ca1c20212ace5092844
Christian Zietz <czietz@gmx.net>
2021-10-02 12:32:45 +0100

    Inline functions that are merely wrappers around GEMDOS calls
    The correspoding GEMDOS console, file, and directory functions are called
    so sparingly that a direct GEMDOS call results in less overhead than the
    call through a wrapper function. This change frees about 130 bytes in the
    192k ROMs.

--- include/gemdos.h
+++ include/gemdos.h
@@ -10,34 +10,13 @@
 #ifndef GEMDOS_H
 #define GEMDOS_H
 
-WORD pgmld(WORD handle, char *pname, LONG **ldaddr);
+#include "bdosbind.h"
 
-LONG dos_rawcin(void);
-void dos_conws(char *string);
-WORD dos_conis(void);
-WORD dos_gdrv(void);
-void dos_sdta(void *ldta);
-void *dos_gdta(void);
-WORD dos_sfirst(char *pspec, WORD attr);
-WORD dos_snext(void);
-LONG dos_open(char *pname, WORD access);
-WORD dos_close(WORD handle);
-LONG dos_read(WORD handle, LONG cnt, void *pbuffer);
-LONG dos_write(WORD handle, LONG cnt, void *pbuffer);
-LONG dos_lseek(WORD handle, WORD smode, LONG sofst);
+WORD pgmld(WORD handle, char *pname, LONG **ldaddr);
 LONG dos_exec(WORD mode, const char *pcspec, const char *pcmdln, const char *segenv); /* see: gemstart.S */
-LONG dos_chdir(char *pdrvpath);
-WORD dos_gdir(WORD drive, char *pdrvpath);
-LONG dos_sdrv(WORD newdrv);
-LONG dos_create(char *name, WORD attr);
-WORD dos_mkdir(char *path);
-WORD dos_chmod(char *name, WORD wrt, WORD mod);
 WORD dos_setdt(UWORD h, UWORD time, UWORD date);
 WORD dos_label(char drive, char *plabel);
-LONG dos_delete(char *name);
 void dos_space(WORD drv, LONG *ptotal, LONG *pavail);
-WORD dos_rename(char *p1, char *p2);
-WORD dos_rmdir(char *path);
 LONG dos_load_file(char *filename, LONG count, char *buf);
 
 void *dos_alloc_stram(LONG nbytes);
@@ -47,5 +26,115 @@ LONG dos_avail_altram(void);
 LONG dos_avail_anyram(void);
 WORD dos_free(void *maddr);
 WORD dos_shrink(void *maddr, LONG length);
+
+static __inline__ LONG dos_rawcin(void)
+{
+    return Crawcin();
+}
+
+static __inline__ void dos_conws(char *string)
+{
+    Cconws(string);
+}
+
+static __inline__ WORD dos_conis(void)
+{
+    return Cconis();
+}
+
+static __inline__ WORD dos_gdrv(void)
+{
+    return Dgetdrv();
+}
+
+static __inline__ void dos_sdta(void *ldta)
+{
+    Fsetdta(ldta);
+}
+
+static __inline__ void *dos_gdta(void)
+{
+    return (void *)Fgetdta();
+}
+
+static __inline__ WORD dos_sfirst(char *pspec, WORD attr)
+{
+    return Fsfirst(pspec,attr);
+}
+
+static __inline__ WORD dos_snext(void)
+{
+    return Fsnext();
+}
+
+static __inline__ LONG dos_open(char *pname, WORD access)
+{
+    return Fopen(pname,access);
+}
+
+static __inline__ WORD dos_close(WORD handle)
+{
+    return Fclose(handle);
+}
+
+static __inline__ LONG dos_read(WORD handle, LONG cnt, void *pbuffer)
+{
+    return Fread(handle,cnt,pbuffer);
+}
+
+static __inline__ LONG dos_write(WORD handle, LONG cnt, void *pbuffer)
+{
+    return Fwrite(handle,cnt,pbuffer);
+}
+
+static __inline__ LONG dos_lseek(WORD handle, WORD smode, LONG sofst)
+{
+    return Fseek(sofst, handle, smode);
+}
+
+static __inline__ LONG dos_chdir(char *pdrvpath)
+{
+    return Dsetpath(pdrvpath);
+}
+
+static __inline__ WORD dos_gdir(WORD drive, char *pdrvpath)
+{
+    return Dgetpath(pdrvpath,drive);
+}
+
+static __inline__ LONG dos_sdrv(WORD newdrv)
+{
+    return Dsetdrv(newdrv);
+}
+
+static __inline__ LONG dos_create(char *name, WORD attr)
+{
+    return Fcreate(name,attr);
+}
+
+static __inline__ WORD dos_mkdir(char *path)
+{
+    return Dcreate(path);
+}
+
+static __inline__ WORD dos_chmod(char *name, WORD wrt, WORD mod)
+{
+    return Fattrib(name,wrt,mod);
+}
+
+static __inline__ LONG dos_delete(char *name)
+{
+    return Fdelete(name);
+}
+
+static __inline__ WORD dos_rename(char *p1, char *p2)
+{
+    return Frename(p1,p2);
+}
+
+static __inline__ WORD dos_rmdir(char *path)
+{
+    return Ddelete(path);
+}
 
 #endif
