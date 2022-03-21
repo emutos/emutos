emutos/emutos

commit f93e029fa46d62d259501d67a758d66877eb66e9

Allow 'Show item' to handle multiple items
This provides compatibility with the TOS 2/3/4 desktop.

desk/deskfun.c
@@ -126,11 +126,65 @@ void fun_msg(WORD type, WORD w3, WORD w4, WORD w5, WORD w6, WORD w7)
 
 
 /*
+ *  Mark window nodes for rebuild
+ */
+void fun_mark_for_rebld(BYTE *path)
+{
+    WNODE *pwin;
+
+    for (pwin = G.g_wfirst; pwin; pwin = pwin->w_next)
+    {
+        /* if opened and same path then mark */
+        if ( (pwin->w_id) && (strcmp(pwin->w_pnode.p_spec, path)==0) )
+            pwin->w_flags |= WN_REBUILD;
+    }
+}
+
+
+/*
+ *  Rebuild a window
+ */
+static void rebuild_window(WNODE *pwin)
+{
+    GRECT gr;
+
+    pn_active(&pwin->w_pnode, TRUE);
+    desk_verify(pwin->w_id, TRUE);
+    win_sinfo(pwin);
+    wind_set(pwin->w_id, WF_INFO, pwin->w_info, 0, 0);
+    wind_get_grect(pwin->w_id, WF_WXYWH, &gr);
+    fun_msg(WM_REDRAW, pwin->w_id, gr.g_x, gr.g_y, gr.g_w, gr.g_h);
+}
+
+
+/*
+ *  Rebuild marked windows
+ */
+void fun_rebld_marked(void)
+{
+    WNODE *pwin;
+
+    graf_mouse(HGLASS, NULL);
+
+    /* check all wnodes     */
+    for (pwin = G.g_wfirst; pwin; pwin = pwin->w_next)
+    {
+        if (pwin->w_flags & WN_REBUILD)
+        {
+            rebuild_window(pwin);
+            pwin->w_flags &= ~WN_REBUILD;
+        }
+    }
+
+    graf_mouse(ARROW, NULL);
+}
+
+
+/*
  *  Rebuild any windows with matching path
  */
 void fun_rebld(BYTE *ptst)
 {
-    GRECT gr;
     WNODE *pwin;
 
     graf_mouse(HGLASS, NULL);
@@ -141,12 +195,7 @@ void fun_rebld(BYTE *ptst)
         /* if opened and same path then rebuild */
         if ( (pwin->w_id) && (strcmp(pwin->w_pnode.p_spec, ptst)==0) )
         {
-            pn_active(&pwin->w_pnode, TRUE);
-            desk_verify(pwin->w_id, TRUE);
-            win_sinfo(pwin);
-            wind_set(pwin->w_id, WF_INFO, pwin->w_info, 0, 0);
-            wind_get_grect(pwin->w_id, WF_WXYWH, &gr);
-            fun_msg(WM_REDRAW, pwin->w_id, gr.g_x, gr.g_y, gr.g_w, gr.g_h);
+            rebuild_window(pwin);
         } /* if */
     } /* for */
 

desk/deskwin.h
@@ -47,7 +47,8 @@
 /*
  * flags in w_flags below
  */
-#define WN_DESKTOP      0x01                /* the desktop pseudo-window */
+#define WN_DESKTOP      0x0001              /* the desktop pseudo-window */
+#define WN_REBUILD      0x8000              /* this window needs rebuilding */
 
 
 /*
@@ -57,7 +58,7 @@ typedef struct _windnode WNODE;
 struct _windnode
 {
         WNODE           *w_next;            /* -> next 'highest' window */
-        WORD            w_flags;
+        UWORD           w_flags;                /* see above */
         WORD            w_id;                   /* window handle id #   */
         WORD            w_obid;                 /* desktop object id    */
         WORD            w_root;                 /* pseudo root ob. in   */

desk/deskapp.c
@@ -871,7 +871,6 @@ void app_start(void)
     G.g_xytext[7] = gl_hchar;
 }
 
-
 /*
  *  Reverse the list of ANODEs
  */
@@ -1168,6 +1167,13 @@ void app_blddesk(void)
     /* set background pattern/colours */
     set_background();
 
+    /*
+     * reverse the order of the ANODEs, so that the disk icons will be
+     * allocated in the same sequence as they appear on the desktop.
+     * this is important for 'Show info'.
+     */
+    app_revit();
+
     for(pa = G.g_ahead; pa; pa = pa->a_next)
     {
         if (pa->a_flags & AF_ISDESK)
@@ -1201,6 +1207,9 @@ void app_blddesk(void)
             pic->ib_char |= pa->a_letter;
         }
     }
+
+    app_revit();    /* back to normal ... */
+
     do_wredraw(0, G.g_xdesk, G.g_ydesk, G.g_wdesk, G.g_hdesk);
 }
 

desk/deskfun.h
@@ -35,6 +35,8 @@ void fun_mask(WNODE *pw);
 void fun_close(WNODE *pw, WORD closetype);
 BOOL fun_drag(WORD wh, WORD dest_wh, WORD sobj, WORD dobj, WORD mx, WORD my, WORD keystate);
 void fun_msg(WORD type, WORD w3, WORD w4, WORD w5, WORD w6, WORD w7);
+void fun_mark_for_rebld(BYTE *path);
+void fun_rebld_marked(void);
 void fun_rebld(BYTE *path);
 WORD fun_mkdir(WNODE *pw_node);
 WORD fun_op(WORD op, WORD icontype_src, PNODE *pspath, BYTE *pdest);

desk/deskinf.c
@@ -391,19 +391,6 @@ WORD inf_show(OBJECT *tree, WORD start)
 
 
 /*
- * Routine for finishing off a simple ok-only dialog box
- */
-static void inf_finish(OBJECT *tree, WORD dl_ok)
-{
-    OBJECT *obj;
-
-    inf_show(tree, ROOT);
-    obj = tree + dl_ok;
-    obj->ob_state = NORMAL;
-}
-
-
-/*
  *  Insert an unsigned long value into the te_ptext field of the TEDINFO
  *  structure for the specified object, truncating if necessary
  */
@@ -471,10 +458,17 @@ static WORD count_ffs(BYTE *path)
 /************************************************************************/
 /* i n f _ f i l e _ f o l d e r                                        */
 /************************************************************************/
+/*
+ * returns:
+ *      0   cancel
+ *      1   continue
+ *      -1  continue, and mark window for redraw
+ */
 WORD inf_file_folder(BYTE *ppath, FNODE *pf)
 {
     OBJECT *tree;
-    WORD more, nmidx, title, ret;
+    WORD nmidx, title, ret;
+    BOOL more, changed;
     BYTE attr;
     BYTE srcpth[MAXPATHLEN];
     BYTE dstpth[MAXPATHLEN];
@@ -482,6 +476,8 @@ WORD inf_file_folder(BYTE *ppath, FNODE *pf)
     OBJECT *obj;
 
     tree = G.a_trees[ADFFINFO];
+    deselect_all(tree);
+
     title = (pf->f_attr & F_SUBDIR) ? STFOINFO : STFIINFO;
     obj = tree + FFTITLE;
     obj->ob_spec = (LONG) ini_str(title);
@@ -504,7 +500,7 @@ WORD inf_file_folder(BYTE *ppath, FNODE *pf)
         graf_mouse(ARROW, NULL);
 
         if (!more)
-            return FALSE;
+            return 1;
 
         inf_fifosz(tree, FFNUMFIL, FFNUMFOL, FFSIZE);
     }
@@ -543,15 +539,15 @@ WORD inf_file_folder(BYTE *ppath, FNODE *pf)
         obj->ob_state = NORMAL;
 
     inf_show(tree, ROOT);
-    if (inf_what(tree, FFOK, FFCNCL) != 1)
-        return FALSE;
+    ret = inf_what(tree, FFSKIP, FFCNCL);
+    if (ret >= 0)       /* skip or cancel */
+        return ret;
 
     /*
      * user selected OK - we rename and/or change attributes
      */
     graf_mouse(HGLASS, NULL);
 
-    more = TRUE;
     inf_sget(tree, FFNAME, pnname);
 
     /* unformat the strings */
@@ -561,11 +557,16 @@ WORD inf_file_folder(BYTE *ppath, FNODE *pf)
     /*
      * if user has changed the name, do the DOS rename
      */
+    more = TRUE;
+    changed = FALSE;
     if (strcmp(srcpth+nmidx, dstpth+nmidx))
     {
         ret = dos_rename(srcpth, dstpth);
         if ((more=d_errmsg(ret)) != 0)
+        {
             strcpy(pf->f_name, dstpth+nmidx);
+            changed = TRUE;
+        }
     }
 
     /*
@@ -583,20 +584,28 @@ WORD inf_file_folder(BYTE *ppath, FNODE *pf)
         if (attr != pf->f_attr)
         {
             ret = dos_chmod(dstpth, F_SETMOD, attr);
-            if ((more=d_errmsg(ret)) != 0)
+            if (d_errmsg(ret) != 0)
+            {
                 pf->f_attr = attr;
+                changed = TRUE;
+            }
         }
     }
 
     graf_mouse(ARROW, NULL);
 
-    return more;
+    return changed ? -1 : 1;
 }
 
 
 /************************************************************************/
 /* i n f _ d i s k                                                      */
 /************************************************************************/
+/*
+ * returns:
+ *      0   cancel
+ *      1   continue
+ */
 WORD inf_disk(BYTE dr_id)
 {
     OBJECT *tree;
@@ -619,7 +628,7 @@ WORD inf_disk(BYTE dr_id)
     if (!more)
     {
         graf_mouse(ARROW, NULL);
-        return FALSE;
+        return 1;
     }
 
     dos_space(dr_id - 'A' + 1, &total, &avail);
@@ -635,9 +644,8 @@ WORD inf_disk(BYTE dr_id)
     inf_fifosz(tree, DINFILES, DINFOLDS, DIUSED);
     inf_numset(tree, DIAVAIL, avail);
 
-    inf_finish(tree, DIOK);
-
-    return TRUE;
+    inf_show(tree, ROOT);
+    return inf_what(tree, DIOK, DICNCL);
 }
 
 

desk/deskmain.c
@@ -139,7 +139,7 @@ static const BYTE ILL_FOLD[] =  { IDSKITEM, IAPPITEM, RICNITEM, 0 };
 static const BYTE ILL_FDSK[] =  { IAPPITEM, 0 };
 static const BYTE ILL_HDSK[] =  { IAPPITEM, 0 };
 static const BYTE ILL_NOSEL[] = { OPENITEM, SHOWITEM, DELTITEM, IAPPITEM, RICNITEM, 0 };
-static const BYTE ILL_MULTSEL[] = { OPENITEM, SHOWITEM, 0 };
+static const BYTE ILL_MULTSEL[] = { OPENITEM, 0 };
 static const BYTE ILL_TRASH[] = { OPENITEM, DELTITEM, IDSKITEM, IAPPITEM, 0 };
 static const BYTE ILL_NOWIN[] = {
     NFOLITEM, CLOSITEM, CLSWITEM,
@@ -416,8 +416,20 @@ static WORD do_filemenu(WORD item)
         break;
     case SHOWITEM:
         if (curr)
-            do_info(curr);
-        else if (pw)
+        {
+            for ( ; curr; curr = win_isel(G.g_screen, G.g_croot, curr))
+            {
+                if (!do_info(curr))
+                    break;
+            }
+            fun_rebld_marked(); /* rebuild any changed windows */
+            break;
+        }
+        /*
+         * here if there are no highlighted icons: display info for
+         * the disk associated with the top window (just like TOS)
+         */
+        if (pw)
             inf_disk(pw->w_pnode.p_spec[0]);
         break;
     case NFOLITEM:

desk/desksupp.c
@@ -1069,10 +1069,14 @@ WORD do_open(WORD curr)
 
 /*
  *  Get information on an icon
+ *
+ *  returns:
+ *      0   cancel
+ *          otherwise continue
  */
 WORD do_info(WORD curr)
 {
-    WORD ret, drive;
+    WORD ret = 1, drive;
     ANODE *pa;
     WNODE *pw;
     FNODE fn, *pf;
@@ -1084,7 +1088,7 @@ WORD do_info(WORD curr)
 
     pa = i_find(G.g_cwin, curr, &pf, NULL);
     if (!pa)
-        return FALSE;
+        return ret;
 
     switch(pa->a_type)
     {
@@ -1118,20 +1122,20 @@ WORD do_info(WORD curr)
                 pathptr = pw->w_pnode.p_spec;
             }
             ret = inf_file_folder(pathptr, pf);
-            if (ret)
-                fun_rebld(pathptr);
+            if (ret < 0)
+                fun_mark_for_rebld(pathptr);
         }
         break;
     case AT_ISDISK:
         drive = LOBYTE(get_iconblk_ptr(G.g_screen, curr)->ib_char);
-        inf_disk(drive);
+        ret = inf_disk(drive);
         break;
     case AT_ISTRSH:
         fun_alert(1, STTRINFO);
         break;
     }
 
-    return FALSE;
+    return ret;
 }
 
 

commit dd79fdf8460bf589cc78cdbc8169e90ee1c22ba8

Fix obscure EmuDesk bug in 'Info/Rename'
This bug is quasi-random but is easiest to reproduce just after
EmuDesk loads.  Open a disk, select a file/folder, do an Info/Rename:
the info of the file/folder is shown.  Press Cancel: the file/folder
(correctly) remains selected.  Do an Info/Rename again: now the info
for the entire disk is shown.
Cause: the function fun_rebld_marked() follows the chain of WNODEs
and rebuilds windows that have the WN_REBUILD flag set.  However, it
did not check that the WNODE was valid (w_id==0).  Since the flags
field is not initialised, residual junk could cause rebuild_window()
to be called with w_id=0, causing desk_verify() to reset some global
variables incorrectly.  This bug was introduced by commit f93e029f.

desk/deskfun.c
@@ -156,7 +156,7 @@ void fun_rebld_marked(void)
     /* check all wnodes     */
     for (pwin = G.g_wfirst; pwin; pwin = pwin->w_next)
     {
-        if (pwin->w_flags & WN_REBUILD)
+        if ( (pwin->w_id) && (pwin->w_flags & WN_REBUILD) )
         {
             rebuild_window(pwin);
             pwin->w_flags &= ~WN_REBUILD;

desk/deskwin.h
@@ -59,7 +59,7 @@ struct _windnode
 {
         WNODE           *w_next;            /* -> next 'highest' window */
         UWORD           w_flags;                /* see above */
-        WORD            w_id;                   /* window handle id #   */
+        WORD            w_id;                   /* AES window handle (0 => not in use) */
         WORD            w_obid;                 /* desktop object id    */
         WORD            w_root;                 /* pseudo root object in G.g_screen */
                                                 /*  for this window's objects       */