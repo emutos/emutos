emutos/emutos

commit 0fe68198f3f1e4ff965caaab5679cee8108a0b06

Chain virtual workstations together
This is what Atari TOS does, and some programs (e.g. Warp9) may
rely on this.

vdi/vdi_control.c
@@ -358,6 +358,29 @@ static void init_wk(Vwk * vwk)
 
 
 
+/*
+ * build a chain of Vwks
+ *
+ * this links all of the currently allocated Vwks together, as in
+ * Atari TOS.  some programs may depend on this.
+ */
+static void build_vwk_chain(void)
+{
+    Vwk *prev, **vwk;
+    WORD handle;
+
+    prev = &phys_work;
+    for (handle = VDI_PHYS_HANDLE+1, vwk = vwk_ptr+handle; handle <= LAST_VDI_HANDLE; handle++, vwk++) {
+        if (*vwk) {
+            prev->next_work = *vwk;
+            prev = *vwk;
+        }
+    }
+    prev->next_work = NULL;
+}
+
+
+
 void vdi_v_opnvwk(Vwk * vwk)
 {
     WORD handle;
@@ -392,6 +415,7 @@ void vdi_v_opnvwk(Vwk * vwk)
     vwk_ptr[handle] = vwk;
     vwk->handle = CONTRL[6] = handle;
     init_wk(vwk);
+    build_vwk_chain();
     CUR_WORK = vwk;
 }
 
@@ -407,6 +431,8 @@ void vdi_v_clsvwk(Vwk * vwk)
         return;
 
     vwk_ptr[handle] = NULL;         /* close it */
+
+    build_vwk_chain();              /* rebuild chain */
 
     /*
      * When we close a virtual workstation, Atari TOS and previous versions

commit f9f751d699502dba62b195c2237713d297ec1e05

Fix EmuTOS VDI under FreeMiNT with memory protect
This fixes a regression caused by commit 0fe6819, which re-introduced
the chaining together of VDI workstations.  For this to work with
FreeMiNT memory protection enabled, the workstation must be allocated
in memory that is always readable by the OS.  Thanks to Miro Kropacek
for pointing out the problem.

vdi/vdi_control.c
@@ -26,6 +26,14 @@
 #define VDI_PHYS_HANDLE     FIRST_VDI_HANDLE
 
 /*
+ * Mxalloc() mode used when allocating the virtual workstation.  This
+ * is only significant when running under FreeMiNT, since EmuTOS ignores
+ * these bits of the mode field.
+ */
+#define MX_PRIVREAD         (4<<4)
+
+
+/*
  * ptr to current mouse cursor save area, based on v_planes
  */
 MCS *mcs_ptr;
@@ -405,8 +413,13 @@ void vdi_v_opnvwk(Vwk * vwk)
 
     /*
      * Allocate the memory for a virtual workstation
+     *
+     * The virtual workstations for all programs are chained together by
+     * build_vwk_chain(), because some programs (notably Warp9) expect this.
+     * To avoid problems when running FreeMiNT with memory protection, we
+     * must allocate the virtual workstations in private/readable memory.
      */
-    vwk = (Vwk *)Malloc(sizeof(Vwk));
+    vwk = (Vwk *)Mxalloc(sizeof(Vwk), MX_PRIVREAD);
     if (vwk == NULL) {
         CONTRL[6] = 0;  /* No memory available, exit */
         return;