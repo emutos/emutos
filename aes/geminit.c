emutos/emutos

commit 3e24c66b0f5d174015a1284531d0d452d527d251

Use new Setscreen() feature in EmuDesk
EmuDesk now causes screen memory to be reallocated when switching
resolution in a videl-capable system (e.g. Falcon), just like TOS4.

aes/geminit.c
@@ -762,7 +762,7 @@ void run_accs_and_desktop(void)
  */
 static void new_resolution(WORD rez, WORD videlmode)
 {
-    Setscreen(-1L, -1L, rez, videlmode);        /* change resolution */
+    Setscreen(0L, 0L, rez, videlmode);          /* change resolution */
     initialise_palette_registers(rez, videlmode);
 }
 #endif

vdi/vdi_control.c
@@ -471,7 +471,7 @@ void vdi_v_opnwk(Vwk * vwk)
 #endif
        ) {
         if (newrez != Getrez()) {
-            Setscreen(-1L, -1L, newrez, 0);
+            Setscreen(0L, 0L, newrez, 0);
         }
     }
 #if CONF_WITH_VIDEL
@@ -480,7 +480,7 @@ void vdi_v_opnwk(Vwk * vwk)
         WORD newvidel = INTOUT[45];
         WORD curvidel = VsetMode(-1);
         if (curvidel != newvidel) {
-            Setscreen(-1L, -1L, newrez, newvidel);
+            Setscreen(0L, 0L, newrez, newvidel);
         }
     }
 #endif

desk/deskrez.c
@@ -30,7 +30,9 @@
 #include "deskinf.h"
 #include "deskfun.h"
 #include "deskrez.h"
+#include "desksupp.h"
 
+#include "bdosbind.h"
 #include "xbiosbind.h"
 #include "has.h"                /* for has_videl etc */
 #include "biosdefs.h"
@@ -149,6 +151,7 @@ static int change_falcon_rez(WORD *newres,WORD *newmode)
 OBJECT *tree, *obj;
 int i, selected;
 WORD oldmode, oldbase, oldoptions;
+WORD mode;
 
     oldmode = VsetMode(-1);
     oldbase = oldmode & (VIDEL_VERTICAL|VIDEL_COMPAT|VIDEL_80COL|VIDEL_BPPMASK);
@@ -194,10 +197,17 @@ WORD oldmode, oldbase, oldoptions;
     if (i == selected)          /* no change */
         return 0;
 
-    *newres = FALCON_REZ;
-    *newmode = falconmode_from_button[i] | oldoptions;
+    mode = falconmode_from_button[i] | oldoptions;
     if (!(oldoptions&VIDEL_VGA))    /* if RGB mode, */
-        *newmode ^= VIDEL_VERTICAL; /* invert the bit returned */
+        mode ^= VIDEL_VERTICAL;     /* invert the bit returned */
+
+    if (Srealloc(-1L) < VgetSize(mode)) {
+        malloc_fail_alert();
+        return 0;
+    }
+
+    *newres = FALCON_REZ;
+    *newmode = mode;
 
     return 1;
 }

include/xbiosbind.h
@@ -60,6 +60,7 @@
 #define EsetColor(a,b) xbios_w_ww(83,a,b)
 #define VsetMode(a) xbios_w_w(88,a)
 #define VgetMonitor() xbios_w_v(89)
+#define VgetSize(a) xbios_l_w(91,a)
 #define VsetRGB(a,b,c) xbios_v_wwl(93,a,b,c)
 #define VgetRGB(a,b,c) xbios_v_wwl(94,a,b,c)
 

commit 187c7eca3151b85593741a9cc8d700ec12c0245b

Fix crash on resolution change with NVDI active
When running the 256K or 512K ROMs in an add-on card on a Mega ST,
with NVDI 4.11 active, EmuDesk crashes when changing resolution.
This regression was caused by commit 3e24c66.  Thanks to Claude
Labelle for reporting this, and Christian Zietz for debugging.

aes/geminit.c
@@ -752,10 +752,24 @@ void run_accs_and_desktop(void)
 #if CONF_WITH_ATARI_VIDEO || defined(MACHINE_AMIGA)
 /*
  * change resolution & reinitialise the palette registers
+ *
+ * note: passing a screen address of 0L to Setscreen() means reallocate
+ * screen memory.  on non-Falcon systems (where the size of screen memory
+ * is fixed), this is effectively the same as passing an address of -1L,
+ * and our implementation of Setscreen() handles this without problems.
+ *
+ * however, if someone has hooked Setscreen() and does not understand
+ * this, passing a screen address of 0L will set the screen address to
+ * NULL, and the system will crash.  this happens, for example, when
+ * running NVDI 4.11 on a Mega ST.
+ *
+ * therefore, for safety, we only use 0L on explicitly Falcon resolutions.
  */
 static void new_resolution(WORD rez, WORD videlmode)
 {
-    Setscreen(0L, 0L, rez, videlmode);          /* change resolution */
+    LONG addr = (rez==FALCON_REZ) ? 0L : -1L;
+
+    Setscreen(addr, addr, rez, videlmode);      /* change resolution */
     initialise_palette_registers(rez, videlmode);
 }
 #endif

vdi/vdi_control.c
@@ -484,7 +484,7 @@ void vdi_v_opnwk(Vwk * vwk)
 #endif
        ) {
         if (newrez != Getrez()) {
-            Setscreen(0L, 0L, newrez, 0);
+            Setscreen(-1L, -1L, newrez, 0);
         }
     }
 #if CONF_WITH_VIDEL