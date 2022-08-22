emutos/emutos

commit f17778afa8cb4a3994290ec4fc4c0121ed1db226

Eliminate unnecessary local variable

aes/gemaplib.c
@@ -107,11 +107,8 @@ void ap_rdwr(WORD code, AESPD *p, WORD length, LONG pbuff)
 WORD ap_find(LONG pname)
 {
     register AESPD  *p;
-    BYTE    temp[9];
 
-    strcpy(temp, (char *)pname);
-
-    p = fpdnm(&temp[0], 0x0);
+    p = fpdnm((BYTE *)pname, 0);
     return p ? p->p_pid : -1;
 }
 

commit 612a7f583e08a67be92716ce861d74b39e2845d5

Fix obscure bug in appl_find()
If a NULL pointer was passed as the argument to appl_find(), it
returned a process id of 0, rather than -1 for an error.  This bug
was introduced by commit f17778a.

aes/gemaplib.c
@@ -96,6 +96,13 @@ WORD ap_find(char *pname)
 {
     AESPD  *p;
 
+    /*
+     * explicitly disallow a NULL filename pointer, since this has
+     * a special meaning for fpdnm()
+     */
+    if (!pname)
+        return -1;
+
     p = fpdnm(pname, 0);
     return p ? p->p_pid : -1;
 }