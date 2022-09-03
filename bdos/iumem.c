emutos/emutos

commit 83d40768a0b1b37c20b1d0e870495de1b4eba4e4

Rewrite low-level code used by Mshrink()
This is the second part of a rewrite of iumem.c to fix a bug in
handling memory descriptors.

bdos/iumem.c
@@ -188,3 +188,46 @@ void freeit(MD *m, MPB *mp)
             xmfremd(m);
         }
 }
+
+
+/*
+ *  shrinkit - Shrink a memory descriptor
+ */
+WORD shrinkit(MD *m, MPB *mp, LONG newlen)
+{
+    MD *f, *p, *q;
+
+    /*
+     * Create a memory descriptor for the freed portion of memory.
+     */
+    f = xmgetmd();
+    if (!f)
+    {
+        KDEBUG(("BDOS shrinkit: not enough OS memory for new MD\n"));
+        return -1;
+    }
+
+    f->m_start = m->m_start + newlen;
+    f->m_length = m->m_length - newlen;
+
+    /*
+     * Add it to the free list.
+     */
+    for (p = mp->mp_mfl, q = NULL; p; q = p, p = p->m_link)
+        if (f->m_start <= p->m_start)
+            break;
+
+    f->m_link = p;
+
+    if (q)
+        q->m_link = f;
+    else
+        mp->mp_mfl = f;
+
+    /*
+     * Update existing memory descriptor.
+     */
+    m->m_length = newlen;
+
+    return 0;
+}

bdos/mem.h
@@ -95,6 +95,8 @@ void umem_init(void);
 MD *ffit(long amount, MPB *mp);
 /* Free up a memory descriptor */
 void freeit(MD *m, MPB *mp);
+/* shrink a memory descriptor */
+WORD shrinkit(MD *m, MPB *mp, LONG newlen);
 
 
 #endif /* MEM_H */

bdos/umem.c
@@ -154,7 +154,7 @@ long xmfree(void *addr)
  */
 long xsetblk(int n, void *blk, long len)
 {
-    MD *m,*p;
+    MD *p;
     MPB *mpb;
 
     KDEBUG(("BDOS: Mshrink(0x%08lx,%ld)\n",(long)blk,len));
@@ -206,20 +206,11 @@ long xsetblk(int n, void *blk, long len)
     }
 
     /*
-     * Create a memory descriptor for the freed portion of memory.
+     * Shrink the existing MD & create a new one for the freed portion of memory.
      */
-    m = xmgetmd();
+    if (shrinkit(p,mpb,len) < 0)
+        return ERR;
 
-#ifdef ENABLE_KDEBUG
-    /* what if 0? */
-    if (m == 0)
-        panic("umem.c/xsetblk: Null Return From MGET\n");
-#endif
-
-    m->m_start = p->m_start + len;
-    m->m_length = p->m_length - len;
-    p->m_length = len;
-    freeit(m,mpb);
     dump_mem_map();
 
     return E_OK;

commit e65ae1495743bdde7b3cd88127e5433f2465eafb

Fix bug in Mshrink()
For the proper functioning of memory management, a single contiguous
area of free memory must be described by one MD.  However, using
Mshrink() more than once on the same block of allocated memory could
cause there to be two or more adjacent MDs, describing adjacent areas
of free memory, on the free memory chain.  This bug was introduced by
commit 83d4076.  Many thanks to Vincent Barilliot for reporting this.

bdos/iumem.c
@@ -227,7 +227,7 @@ void freeit(MD *m, MPB *mp)
  */
 WORD shrinkit(MD *m, MPB *mp, LONG newlen)
 {
-    MD *f, *p, *q;
+    MD *f;
 
     /*
      * Create a memory descriptor for the freed portion of memory.
@@ -243,23 +243,21 @@ WORD shrinkit(MD *m, MPB *mp, LONG newlen)
     f->m_length = m->m_length - newlen;
 
     /*
-     * Add it to the free list.
+     * Add it to the allocated list.
      */
-    for (p = mp->mp_mfl, q = NULL; p; q = p, p = p->m_link)
-        if (f->m_start <= p->m_start)
-            break;
-
-    f->m_link = p;
-
-    if (q)
-        q->m_link = f;
-    else
-        mp->mp_mfl = f;
+    f->m_link = mp->mp_mal;
+    mp->mp_mal = f;
 
     /*
      * Update existing memory descriptor.
      */
     m->m_length = newlen;
+
+    /*
+     * Free new memory descriptor via freeit() which takes care of
+     * coalescing free blocks (important!).
+     */
+    freeit(f, mp);
 
     return 0;
 }