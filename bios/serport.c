8283dcca50c2ef2dca6a65217d153412e9db2c97
Christian Zietz <czietz@gmx.net>
2022-09-06 18:41:29 -0400

    Fix compile error with RS232_DEBUG_PRINT
    'make 256' fails if RS232_DEBUG_PRINT is set to 1, because
    put_iorecbuf() is not defined.  This bug was introduced by
    commit 4cecd45 (addition of interrupt-driven SCC I/O).
    Thanks to Vincent Barilliot for reporting this.

--- bios/serport.c
+++ bios/serport.c
@@ -154,7 +154,7 @@ static WORD incr_tail(IOREC *iorec)
     return tail;
 }
 
-#if (!CONF_WITH_COLDFIRE_RS232 && CONF_WITH_MFP_RS232 && !RS232_DEBUG_PRINT) || CONF_WITH_TT_MFP
+#if (!CONF_WITH_COLDFIRE_RS232 && CONF_WITH_MFP_RS232 && !RS232_DEBUG_PRINT) || CONF_WITH_TT_MFP || CONF_WITH_SCC
 static void put_iorecbuf(IOREC *out, WORD b)
 {
     WORD old_sr, tail;
