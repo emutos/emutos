emutos/emutos

commit 8bc21cbc4cc1cfbfe826341c29d97a521165d7e5

Simplify get_par() function
This saves about 60 bytes in the 192K ROMs.

aes/gemoblib.c
@@ -284,7 +284,6 @@ static void add_3d_effect(GRECT *pt, WORD obstate, WORD th, WORD color)
  */
 void ob_offset(OBJECT *tree, WORD obj, WORD *pxoff, WORD *pyoff)
 {
-    WORD   junk;
     OBJECT *treeptr = tree;
 
     *pxoff = *pyoff = 0;
@@ -292,7 +291,7 @@ void ob_offset(OBJECT *tree, WORD obj, WORD *pxoff, WORD *pyoff)
     {
         *pxoff += (treeptr+obj)->ob_x;  /* add in current object's offsets */
         *pyoff += (treeptr+obj)->ob_y;
-        obj = get_par(tree, obj, &junk);/* then get parent */
+        obj = get_par(tree, obj);       /* then get parent */
     } while (obj != NIL);
 }
 
@@ -961,7 +960,8 @@ void ob_draw(OBJECT *tree, WORD obj, WORD depth)
     WORD last, pobj;
     WORD sx, sy;
 
-    pobj = get_par(tree, obj, &last);
+    last = tree[obj].ob_next;
+    pobj = get_par(tree, obj);
 
     if (pobj != NIL)
         ob_offset(tree, pobj, &sx, &sy);
@@ -1014,7 +1014,7 @@ static WORD get_prev(OBJECT *tree, WORD parent, WORD obj)
 WORD ob_find(OBJECT *tree, WORD currobj, WORD depth, WORD mx, WORD my)
 {
     WORD lastfound;
-    WORD dosibs, done, junk;
+    WORD dosibs, done;
     GRECT t, o;
     WORD parent, childobj, flags;
     OBJECT *objptr;
@@ -1025,7 +1025,7 @@ WORD ob_find(OBJECT *tree, WORD currobj, WORD depth, WORD mx, WORD my)
         r_set(&o, 0, 0, 0, 0);
     else
     {
-        parent = get_par(tree, currobj, &junk);
+        parent = get_par(tree, currobj);
         ob_actxywh(tree, parent, &o);
     }
 
@@ -1134,7 +1134,8 @@ WORD ob_delete(OBJECT *tree, WORD obj)
     if (obj == ROOT)
         return 0;           /* can't delete the root object! */
 
-    parent = get_par(tree, obj, &nextsib);
+    nextsib = tree[obj].ob_next;
+    parent = get_par(tree, obj);
 
     parentptr = treeptr + parent;
 
@@ -1180,14 +1181,14 @@ WORD ob_delete(OBJECT *tree, WORD obj)
 BOOL ob_order(OBJECT *tree, WORD mov_obj, WORD new_pos)
 {
     WORD parent;
-    WORD chg_obj, ii, junk;
+    WORD chg_obj, ii;
     OBJECT *treeptr = tree;
     OBJECT *parentptr, *movptr, *chgptr;
 
     if (mov_obj == ROOT)
         return FALSE;
 
-    parent = get_par(tree, mov_obj, &junk);
+    parent = get_par(tree, mov_obj);
     parentptr = treeptr + parent;
     movptr = treeptr + mov_obj;
 

aes/gemctrl.c
@@ -312,7 +312,7 @@ static void hctl_rect(void)
     AESPD   *owner;
 #if CONF_WITH_MENU_EXTENSION
     OBJECT *tree;
-    WORD    treehi, treelo, parent, dummy;
+    WORD    treehi, treelo, parent;
 #endif
 
     if ( gl_mntree )
@@ -345,7 +345,7 @@ static void hctl_rect(void)
 #if CONF_WITH_MENU_EXTENSION
                 treehi = HIWORD(tree);
                 treelo = LOWORD(tree);
-                parent = get_par(tree, item, &dummy);
+                parent = get_par(tree, item);
 #endif
             }
             /*

aes/gemfmlib.c
@@ -203,7 +203,7 @@ WORD fm_button(OBJECT *tree, WORD new_obj, WORD clks, WORD *pnew_obj)
     WORD    tobj;
     WORD    orword;
     WORD    parent, state, flags;
-    WORD    cont, junk, tstate, tflags;
+    WORD    cont, tstate, tflags;
     WORD    rets[6];
     OBJECT  *objptr;
 
@@ -227,7 +227,7 @@ WORD fm_button(OBJECT *tree, WORD new_obj, WORD clks, WORD *pnew_obj)
         if (flags & RBUTTON)
         {
             /* check siblings to find and turn off the old RBUTTON */
-            parent = get_par(tree, new_obj, &junk);
+            parent = get_par(tree, new_obj);
             objptr = tree + parent;
             tobj = objptr->ob_head;
             while (tobj != parent)

aes/gemobjop.c
@@ -141,31 +141,22 @@ sibling:
  * Routine that will find the parent of a given object.  The
  * idea is to walk to the end of our siblings and return
  * our parent.  If object is the root then return NIL as parent.
- * Also have this routine return the immediate next object of
- * this object.
  */
-WORD get_par(OBJECT *tree, WORD obj, WORD *pnobj)
+WORD get_par(OBJECT *tree, WORD obj)
 {
-    WORD    pobj = NIL, nobj = NIL;
-    OBJECT  *objptr, *pobjptr;
+    WORD next;
 
-    if (obj != ROOT)
+    if (obj == ROOT)
+        return NIL;
+
+    while(1)
     {
-        while(1)
-        {
-            objptr = tree + obj;
-            pobj = objptr->ob_next;
-            if (nobj == NIL)        /* first time */
-                nobj = pobj;
-            if (pobj < ROOT)
-                break;
-            pobjptr = tree + pobj;
-            if ( pobjptr->ob_tail == obj )
-                break;
-            obj = pobj;
-        }
+        next = tree[obj].ob_next;
+        /* if the next object's tail points to us, it must be our parent */
+        if (tree[next].ob_tail == obj)
+            break;
+        obj = next;
     }
 
-    *pnobj = nobj;
-    return pobj;
+    return next;
 }

aes/gemobjop.h
@@ -16,7 +16,6 @@ char ob_sst(OBJECT *tree, WORD obj, LONG *pspec, WORD *pstate, WORD *ptype,
             WORD *pflags, GRECT *pt, WORD *pth);
 void everyobj(OBJECT *tree, WORD this, WORD last, EVERYOBJ_CALLBACK routine,
               WORD startx, WORD starty, WORD maxdep);
-WORD get_par(OBJECT *tree, WORD obj, WORD *pnobj);
-
+WORD get_par(OBJECT *tree, WORD obj);
 
 #endif

commit 39bb3bd6c977b95374938d8f1432bffe739c0029

Fix ob_draw() when called for the ROOT object
This solves a regression introduced by commit 8bc21cbc. Before that commit,
when ob_draw() was called for the root of the object tree, the 'last'
variable was always set to 'NIL', regardless of the value of 'ob_next'. We
now replicate the old behavior of the get_par() function.

This fixes CodeHead Software's Icon Juggler. Thank you to Eero Tamminen for
reporting this issue.

aes/gemoblib.c
@@ -957,10 +957,12 @@ static void just_draw(OBJECT *tree, WORD obj, WORD sx, WORD sy)
  */
 void ob_draw(OBJECT *tree, WORD obj, WORD depth)
 {
-    WORD last, pobj;
+    WORD pobj;
+    WORD last = NIL;
     WORD sx, sy;
 
-    last = tree[obj].ob_next;
+    if (obj != ROOT)
+        last = tree[obj].ob_next;
     pobj = get_par(tree, obj);
 
     if (pobj != NIL)