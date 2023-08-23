1786d09a83974bdbbf65e8665995c57702dd2686
Roger Burrows <anodynesoftware@users.sourceforge.net>
2023-08-23 15:30:55 -0400

    Update source code comments

--- desk/deskrez.c
+++ desk/deskrez.c
@@ -57,27 +57,28 @@ static const WORD ttrez_from_button[NUM_TT_BUTTONS] =
  * note: these correspond to the VGA videomode settings; the
  * VIDEL_VERTICAL bit is inverted for RGB mode (see the code)
  */
-#define NUM_FALCON_BUTTONS  19
-static const WORD falconmode_from_button[NUM_FALCON_BUTTONS] =
-    { VIDEL_80COL|VIDEL_1BPP,                       /* 640x480x2 */
-      VIDEL_80COL|VIDEL_2BPP,                       /* 640x480x4 */
-      VIDEL_80COL|VIDEL_4BPP,                       /* 640x480x16 */
-      VIDEL_80COL|VIDEL_8BPP,                       /* 640x480x256 */
-      VIDEL_VERTICAL|VIDEL_80COL|VIDEL_1BPP,        /* 640x240x2 */
-      VIDEL_VERTICAL|VIDEL_80COL|VIDEL_2BPP,        /* 640x240x4 */
-      VIDEL_VERTICAL|VIDEL_80COL|VIDEL_4BPP,        /* 640x240x16 */
-      VIDEL_VERTICAL|VIDEL_80COL|VIDEL_8BPP,        /* 640x240x256 */
-      VIDEL_2BPP,                                   /* 320x480x4 */
-      VIDEL_4BPP,                                   /* 320x480x16 */
-      VIDEL_8BPP,                                   /* 320x480x256 */
-      VIDEL_TRUECOLOR,                              /* 320x480x65536 */
-      VIDEL_VERTICAL|VIDEL_2BPP,                    /* 320x240x4 */
-      VIDEL_VERTICAL|VIDEL_4BPP,                    /* 320x240x16 */
-      VIDEL_VERTICAL|VIDEL_8BPP,                    /* 320x240x256 */
-      VIDEL_VERTICAL|VIDEL_TRUECOLOR,               /* 320x240x65536 */
+static const WORD falconmode_from_button[] =        /*     VGA           RGB     */
+    { VIDEL_80COL|VIDEL_1BPP,                       /* 640x480x2     640x400x2   */
+      VIDEL_80COL|VIDEL_2BPP,                       /* 640x480x4     640x400x4   */
+      VIDEL_80COL|VIDEL_4BPP,                       /* 640x480x16    640x400x16  */
+      VIDEL_80COL|VIDEL_8BPP,                       /* 640x480x256   640x400x256 */
+      VIDEL_VERTICAL|VIDEL_80COL|VIDEL_1BPP,        /* 640x240x2     640x200x2   */
+      VIDEL_VERTICAL|VIDEL_80COL|VIDEL_2BPP,        /* 640x240x4     640x200x4   */
+      VIDEL_VERTICAL|VIDEL_80COL|VIDEL_4BPP,        /* 640x240x16    640x200x16  */
+      VIDEL_VERTICAL|VIDEL_80COL|VIDEL_8BPP,        /* 640x240x256   640x200x256 */
+      VIDEL_2BPP,                                   /* 320x480x4     320x400x4   */
+      VIDEL_4BPP,                                   /* 320x480x16    320x400x16  */
+      VIDEL_8BPP,                                   /* 320x480x256   320x400x256 */
+      VIDEL_TRUECOLOR,                              /* 320x480x32K   320x400x32K */
+      VIDEL_VERTICAL|VIDEL_2BPP,                    /* 320x240x4     320x200x4   */
+      VIDEL_VERTICAL|VIDEL_4BPP,                    /* 320x240x16    320x200x16  */
+      VIDEL_VERTICAL|VIDEL_8BPP,                    /* 320x240x256   320x200x256 */
+      VIDEL_VERTICAL|VIDEL_TRUECOLOR,               /* 320x240x32K   320x200x32K */
       VIDEL_COMPAT|VIDEL_80COL|VIDEL_1BPP,                  /* ST High */
       VIDEL_COMPAT|VIDEL_VERTICAL|VIDEL_80COL|VIDEL_2BPP,   /* ST Medium */
       VIDEL_COMPAT|VIDEL_VERTICAL|VIDEL_4BPP };             /* ST Low */
+
+#define NUM_FALCON_BUTTONS ARRAY_SIZE(falconmode_from_button)
 
 #endif /* CONF_WITH_VIDEL */
 
