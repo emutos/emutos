297f340a6c3ba3723f905bdbda985966dab2e160
none <user@example.com>
2024-04-13 18:44:20 -0700

    Adjust 040/060 cache operations to support copyback mode
    Also avoid assuming P=V when performing cache operations.

--- include/asmdefs.h
+++ include/asmdefs.h
@@ -59,10 +59,11 @@
 #define FSAVE_MINUS_SP      .dc.w 0xf327            /* 6888X, 68040-68060 (except 68ec040/68ec060) */
 #define FRESTORE_SP_PLUS    .dc.w 0xf35f            /* 6888X, 68040-68060 (except 68ec040/68ec060) */
 
+#define CPUSHA_DC           .dc.w 0xf478            /* 68040-68060 */
+#define CPUSHA_BC           .dc.w 0xf4f8            /* 68040-68060 */
 #define CINVA_DC            .dc.w 0xf458            /* 68040-68060 */
 #define CINVA_IC            .dc.w 0xf498            /* 68040-68060 */
 #define CINVA_BC            .dc.w 0xf4d8            /* 68040-68060 */
-#define CINVL_IC_A0         .dc.w 0xf488            /* 68040-68060 */
 
 #define ADDIWL_0_A0         .dc.l 0x06d00000        /* 68080 */
 
