04bac280c67c0f440020bb45f2473fb8e292dce5
Christian Zietz <czietz@gmx.net>
2026-03-12 18:18:11 +0100

    Remove unused defines
    The defines for interpreting the pun[] array were outdated, e.g., not
    considering the SD card driver. They were also unused by EmuTOS.

--- include/ahdi.h
+++ include/ahdi.h
@@ -4,7 +4,7 @@
  * This file exists to allow AHDI stuff to be referenced by both
  * the BDOS and the BIOS.
  *
- * Copyright (C) 2014-2020 The EmuTOS development team
+ * Copyright (C) 2014-2026 The EmuTOS development team
  *
  * Authors:
  *  RFB    Roger Burrows
@@ -29,14 +29,6 @@ typedef struct
     UWORD max_sect_siz;         /* maximum logical sector size */
     LONG reserved[PUN_MAXUNITS];
 } PUN_INFO;
-
-/* masks for pun[] array above: */
-#define PUN_DEV         0x1f    /* device number of HD */
-#define PUN_UNIT        0x7     /* Unit number */
-#define PUN_SCSI        0x8     /* 1=SCSI 0=ACSI */
-#define PUN_IDE         0x10    /* Falcon IDE */
-#define PUN_REMOVABLE   0x40    /* Removable media */
-#define PUN_VALID       0x80    /* zero if valid */
 
 extern PUN_INFO *pun_ptr;
 
